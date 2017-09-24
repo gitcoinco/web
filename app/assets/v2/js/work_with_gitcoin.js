a3cdbd68fdf419f6e2d4e14ae551ca48a7ccc1ec
Owocki <ksowocki@gmail.com>
2017-09-24 15:51:17 -0600

    fixes popper error, hiring solicitation

---
+++ app/assets/v2/js/work_with_gitcoin.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    if(console && console.log){
+        console.log('############################################');
+        console.log('# Hi there technical person,');
+        console.log('# ');
+        console.log('# We\'d love your help to ');
+        console.log('# **** Pushing Open Source Forward **** ');
+        console.log('# Check us out: ');
+        console.log('# - http://github.com/gitcoinco');
+        console.log('# - founders@gitcoin.co');
+        console.log('# ');
+        console.log('# <3 ');
+        console.log('# Gitcoin Core');
+        console.log('############################################');
+    }
+
+});
