f02fd4ad8dee03ce2520a268a0bf83656d36f7c3
Owocki <ksowocki@gmail.com>
2017-09-25 06:30:47 -0600

    some marketing cleanup

--- app/dashboard/notifications.py
+++ app/dashboard/notifications.py
@@ -28,6 +28,10 @@ def maybe_market_to_twitter(bounty, event_name, txid):
         return False
     if event_name != 'new_bounty':
         return False
+    if bounty.get_natural_value() < 0.0001:
+        print(bounty)
+        print(bounty.pk)
+        return False
 
     api = twitter.Api(
         consumer_key=settings.TWITTER_CONSUMER_KEY,
@@ -54,6 +58,8 @@ def maybe_market_to_twitter(bounty, event_name, txid):
 def maybe_market_to_slack(bounty, event_name, txid):
     if not settings.TWITTER_CONSUMER_KEY:
         return False
+    if bounty.get_natural_value() < 0.0001:
+        return False
 
     title = bounty.title if bounty.title else bounty.github_url
     msg = "{} worth {} {}: {} \n\n{}".format(event_name, round(bounty.get_natural_value(), 4), bounty.token_name, title, bounty.get_absolute_url())
@@ -77,11 +83,13 @@ def maybe_market_to_slack(bounty, event_name, txid):
 def maybe_market_to_github(bounty, event_name, txid):
     if not settings.GITHUB_CLIENT_ID:
         return False
+    if bounty.get_natural_value() < 0.0001:
+        return False
 
     # prepare message
     msg = ''
     if event_name == 'new_bounty':
-        usdt_value = "(" + bounty.value_in_usdt + " USDT)" if bounty.value_in_usdt else ""
+        usdt_value = "(" + str(round(bounty.value_in_usdt, 2)) + " USDT)" if bounty.value_in_usdt else ""
         msg = "__This issue now has a bounty of {} {} {} attached to it.__  To view or claim this bounty, [click here]({}).".format(round(bounty.get_natural_value(), 4), bounty.token_name, usdt_value, bounty.get_absolute_url())
     elif event_name == 'approved_claim':
         msg = "__The bounty of {} {} attached to this issue has been approved & issue.d__  \n\nLearn more at: {}".format(round(bounty.get_natural_value(), 4), bounty.token_name, bounty.get_absolute_url())
