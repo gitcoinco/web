dcd4c82bb8dfb0e3ca3836f936fb9732c2947322
Owocki <ksowocki@gmail.com>
2017-10-26 04:48:19 -0600

    sites framework

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sitemaps',
+    'django.contrib.sites',
     'app',
     'retail',
     'rest_framework',
