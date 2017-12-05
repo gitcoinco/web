9abe50b2b666a01213de6842ead21f58b10c07bf
Rajat Kapoor <rajat100493@gmail.com>
2017-12-05 11:18:43 +0530

    Moved amounts logic out of shared.js

---
+++ app/assets/v2/js/amounts.js
@@ -0,0 +1,31 @@
+var getUSDEstimate = function (amount, denomination) {
+    if (document.conversion_rates && document.conversion_rates[denomination]) {
+        var usd_amount = amount / document.conversion_rates[denomination];
+    } else {
+        var request_url = '/sync/get_amount?amount=' + amount + '&denomination=' + denomination;
+        jQuery.get(request_url, function (result) {
+            var usd_amount = result['usdt'];
+            var conv_rate = amount / usd_amount;
+            //store conv rate for later in cache
+            if (typeof document.conversion_rates == 'undefined') {
+                document.conversion_rates = {}
+            }
+            document.conversion_rates[denomination] = conv_rate;
+        }).fail(function () {
+            return None
+        });
+    }
+    if (usd_amount) {
+        usd_amount = Math.round(usd_amount * 100) / 100;
+        if (usd_amount > 1000000) {
+            usd_amount = Math.round(usd_amount / 100000) / 10 + "m"
+        } else if (usd_amount > 1000) {
+            usd_amount = Math.round(usd_amount / 100) / 10 + "k"
+        }
+        return ('Approx: ' + usd_amount + ' USD');
+    } else {
+        return null;
+    }
+
+
+};
