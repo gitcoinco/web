307c0bb8f48f095bae792b11483834e20974b0a0
Owocki <ksowocki@gmail.com>
2017-12-05 06:12:59 -0700

    debug scripts

--- scripts/debug/bounty_details.js
+++ scripts/debug/bounty_details.js
@@ -1,10 +1,10 @@
-localStorage['txid'] = '0x274ba28a861463192cb894f7ebb589db4d5d9521dfbd5c9032757582a42f24faTxReceipt'
-var issueURL = 'https://github.com/pipermerriam/web3.py/issues/430'
 //TODO -- accept stdin
+var issueURL = 'https://github.com/gitcoinco/web/issues/54'
+localStorage[issueURL] = timestamp();
 
 var callback = function (error, result){
 console.log(error,result);
 };
-var bounty = web3.eth.contract(bounty_abi).at(bounty_address());
-bounty.bountydetails.call(issueURL, callback);
+var bounty_contract = web3.eth.contract(bounty_abi).at(bounty_address());
+bounty_contract.bountydetails.call(issueURL, callback);
 
---
+++ scripts/debug/process_bounty.js
@@ -0,0 +1,4 @@
+localStorage['txid'] = '0x274ba28a861463192cb894f7ebb589db4d5d9521dfbd5c9032757582a42f24fa'
+var issueURL = 'https://github.com/gitcoinco/web/issues/54'
+localStorage[issueURL] = timestamp();
+document.location.href = document.location.href 
