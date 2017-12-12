1137b1ea2891351cf43826736ad723f63f9be416
Owocki <ksowocki@gmail.com>
2017-12-12 08:44:22 -0700

    rate limit update

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -326,7 +326,7 @@ def save_search(request):
     return TemplateResponse(request, 'save_search.html', context)
 
 @csrf_exempt
-@ratelimit(key='ip', rate='5/m', method=ratelimit.UNSAFE, block=True)
+@ratelimit(key='ip', rate='2/s', method=ratelimit.UNSAFE, block=True)
 def sync_web3(request):
 
     #setup
