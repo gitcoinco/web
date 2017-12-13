c5784d711f88c7d478fa1a774a938519d9576a0a
Rajat Kapoor <rajat100493@gmail.com>
2017-12-13 22:51:35 +0530

    Added usd estimates to new bounty post

--- app/assets/v2/js/amounts.js
+++ app/assets/v2/js/amounts.js
@@ -1,31 +1,47 @@
+var estimate= function(amount, conv_rate) {
+    var estimateAmount = amount / conv_rate;
+    if (estimateAmount) {
+        estimateAmount = Math.round(estimateAmount * 100) / 100;
+        if (estimateAmount > 1000000) {
+            estimateAmount = Math.round(estimateAmount / 100000) / 10 + "m"
+        } else if (estimateAmount > 1000) {
+            estimateAmount = Math.round(estimateAmount / 100) / 10 + "k"
+        }
+        return ('Approx: ' + estimateAmount + ' USD');
+    } else {
+        return null;
+    }
+};
+
 var getUSDEstimate = function (amount, denomination) {
+    var conv_rate;
+    var usd_amount;
+    var eth_amount;
+    try {
+        amount = parseFloat(amount);
+    } catch (e) {
+        return null;
+    }
+
     if (document.conversion_rates && document.conversion_rates[denomination]) {
-        var usd_amount = amount / document.conversion_rates[denomination];
+        conv_rate = document.conversion_rates[denomination]
+        return estimate(amount, conv_rate)
     } else {
         var request_url = '/sync/get_amount?amount=' + amount + '&denomination=' + denomination;
         jQuery.get(request_url, function (result) {
-            var usd_amount = result['usdt'];
-            var eth_amount = result['eth'];
-            var conv_rate = amount / usd_amount / eth_amount
+            usd_amount = result['usdt'];
+            eth_amount = parseFloat(result['eth']);
+            conv_rate = (amount/eth_amount)*usd_amount;
             //store conv rate for later in cache
             if (typeof document.conversion_rates == 'undefined') {
                 document.conversion_rates = {}
             }
             document.conversion_rates[denomination] = conv_rate;
+            return estimate(amount, conv_rate)
+
         }).fail(function () {
-            return None
+            return null
         });
-    }
-    if (usd_amount) {
-        usd_amount = Math.round(usd_amount * 100) / 100;
-        if (usd_amount > 1000000) {
-            usd_amount = Math.round(usd_amount / 100000) / 10 + "m"
-        } else if (usd_amount > 1000) {
-            usd_amount = Math.round(usd_amount / 100) / 10 + "k"
-        }
-        return ('Approx: ' + usd_amount + ' USD');
-    } else {
-        return null;
     }
 
 
--- app/assets/v2/js/pages/new_bounty.js
+++ app/assets/v2/js/pages/new_bounty.js
@@ -1,4 +1,10 @@
 load_tokens();
+var setUsdAmount= function (event) {
+    var amount  = $('input[name=amount]').val();
+    var denomination  = $('#token option:selected').text();
+    var estimate = getUSDEstimate(amount, denomination);
+    $('#usd_amount').html(estimate);
+};
 
 $(document).ready(function(){
 
@@ -22,9 +28,9 @@ $(document).ready(function(){
         $('input[name=issueURL]').val(localStorage['issueURL']);
     }
     //fetch issue URL related info
-    $("input[name=amount]").keyup(retrieveAmount);
-    $("input[name=amount]").blur(retrieveAmount);
-    $("select[name=deonomination]").change(retrieveAmount);
+    $("input[name=amount]").keyup(setUsdAmount);
+    $("input[name=amount]").blur(setUsdAmount);
+    $("select[name=deonomination]").change(setUsdAmount);
     $("input[name=issueURL]").blur(retrieveTitle);
     $("input[name=issueURL]").blur(retrieveKeywords);
 
