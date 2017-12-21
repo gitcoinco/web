3964b86b4e9c79c2f3406cc120cfb9a1378edd2e
Owocki <ksowocki@gmail.com>
2017-12-21 11:51:44 -0700

    adds new video to landing page, adds case studies to help page

--- app/app/urls.py
+++ app/app/urls.py
@@ -88,6 +88,7 @@ urlpatterns = [
     url(r'^slack/?', retail.views.slack, name='slack'),
     url(r'^ios/?', retail.views.ios, name='ios'),
     url(r'^casestudy/?', retail.views.casestudy, name='casestudy'),
+    url(r'^casestudies/?', retail.views.casestudy, name='casestudies'),
     url(r'^schwag/?', retail.views.schwag, name='schwag'),
     url(r'^btctalk/?', retail.views.btctalk, name='btctalk'),
     url(r'^reddit/?', retail.views.reddit, name='reddit'),
