fbed68ae339326fb08154eafc5f0f16eb9ca4e9b
Greg Gray (Get Net EZ) <getnetez@gmail.com>
2018-01-01 16:24:03 -0700

    - hook into existing function for faucet w3 errors

--- app/assets/v2/js/shared.js
+++ app/assets/v2/js/shared.js
@@ -561,6 +561,31 @@ window.addEventListener('load', function() {
                 });
             };
         }
+        if($("#faucet_form").length){
+            if(typeof web3 == 'undefined'){
+                $("#no_metamask_error").css('display', 'block');
+                $("#faucet_form").remove();
+                mixpanel.track("No Metamask Error", params);
+                return;
+            } else {
+                if(!web3.eth.coinbase){
+                    $("#unlock_metamask_error").css('display', 'block');
+                    $("#faucet_form").remove();
+                    mixpanel.track("Unlock Metamask Error", params);
+                    return;
+                }
+                web3.eth.getBalance(web3.eth.coinbase, function(errors,result){
+                    var balance = result.toNumber();
+                    var faucet_amount = $("#currentFaucet").val();
+                    $('#ethAddress').val(web3.eth.accounts[0]);
+                    if(balance >= faucet_amount){
+                        $("#over_balance_error").css('display', 'block');
+                        $("#faucet_form").remove();
+                        mixpanel.track("Faucet Available Funds Metamask Error", params);
+                    }
+                });
+            };
+        }
     }, timeout_value);
 
 });
