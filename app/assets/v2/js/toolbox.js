d69481321ed83eb9e879e3c905d7366a3f608b73
Owocki <ksowocki@gmail.com>
2017-12-31 23:16:16 -0700

    preload usage

--- app/assets/v2/js/toolbox.js
+++ app/assets/v2/js/toolbox.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     $('.cards .img img').each(function(){
+        $.get($(this).data('hover'));
         $(this).mouseover(function(){
             $(this).data('og-src',$(this).attr('src'));
             $(this).attr('src',$(this).data('hover'));
