27d4a0917a40ad18d21b8eaae8db3f3208aceb89
Jason Haas <jasonrhaas@gmail.com>
2018-01-17 11:17:32 -0600

    Return an error to the user if Github URL not found in db

--- app/assets/v2/js/pages/fulfill_bounty.js
+++ app/assets/v2/js/pages/fulfill_bounty.js
@@ -111,9 +111,15 @@ window.onload = function(){
 
                         // Get bountyId from the database
                         var uri = '/api/v0.1/bounties/?github_url='+issueURL;
-                        $.get(uri, function(results){
+                        $.get(uri, function(results, status){
                             results = sanitizeAPIResults(results);
                             result = results[0];
+                            if (result == null){
+                                _alert({ message: "No active bounty found for this Github URL." });
+                                unloading_button($('#submitBounty'));
+                                return;
+                            }
+
                             var bountyId = result['standard_bounties_id'];
 
                             bounty.fulfillBounty(bountyId, localStorage['dataHash'], web3Callback);
