f2d3f8ad2df43d85aec5159ae1a08cdde8e07789
Mark Beacom <mark.beacom@cirrustack.com>
2018-01-25 20:43:34 -0500

    Minor imp fix

--- app/legacy/views.py
+++ app/legacy/views.py
@@ -19,10 +19,11 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import logging
 
-from django.shortcuts import render
+from django.http import JsonResponse
 from django.template.response import TemplateResponse
 from django.views.decorators.csrf import csrf_exempt
 
+from dashboard.helpers import normalizeURL
 from dashboard.models import BountySyncRequest
 from gas.utils import conf_time_spread, eth_usd_conv_rate, recommend_min_gas_price_to_confirm_in_time
 from ratelimit.decorators import ratelimit
@@ -32,12 +33,6 @@ from .helpers import process_bounty_changes, process_bounty_details
 logging.basicConfig(level=logging.DEBUG)
 
 confirm_time_minutes_target = 60
-
-
-def normalizeURL(url):
-    if url[-1] == '/':
-        url = url[0:-1]
-    return url
 
 
 @csrf_exempt
