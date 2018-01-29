c2e95d8f14d374d6d714f425820af830df20c1a1
Owocki <ksowocki@gmail.com>
2018-01-29 11:15:58 -0700

    fixes https://github.com/gitcoinco/web/issues/286

--- app/assets/v2/js/shared.js
+++ app/assets/v2/js/shared.js
@@ -457,7 +457,7 @@ window.addEventListener('load', function() {
         if (typeof web3 =='undefined'){
             $("#sidebar_head").html("Web3 disabled <br> <img src='/static/v2/images/icons/question.png'>");
             $("#sidebar_p").html("Please install <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://metamask.io/?utm_source=gitcoin.co&utm_medium=referral\">Metamask</a>.");
-        } else if (typeof web3.eth.accounts[0] =='undefined'){
+        } else if (typeof web3 != 'undefined' && typeof web3.eth.accounts[0] =='undefined'){
             $("#sidebar_head").html("Web3 locked <br> <img src='/static/v2/images/icons/lock.png'>");
             $("#sidebar_p").html("Please unlock <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://metamask.io/?utm_source=gitcoin.co&utm_medium=referral\">Metamask</a>.");
         } else {
