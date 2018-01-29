1c8e47b3006e115c70eafad031f396c3c15f0e54
Owocki <ksowocki@gmail.com>
2018-01-29 09:18:13 -0700

    backup from stdin

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -20,7 +20,5 @@ BACKUPSTR=`date +"%Y%m%d"`
 MONTH=`date +"%m"`
 DAY=`date +"%d"`
 YEAR=`date +"%Y"`
-sudo runuser -l postgres -c '/usr/bin/pg_dump gitcoin' > /tmp/$BACKUPSTR.sql
-s3cmd put /tmp/$BACKUPSTR.sql s3://gitcoinbackups/$YEAR/$MONTH/$DAY/$BACKUPSTR-$(hostname).sql
-rm /tmp/$BACKUPSTR.sql
+sudo runuser -l postgres -c '/usr/bin/pg_dump gitcoin' | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/$BACKUPSTR-$(hostname).sql
 
