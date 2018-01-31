e511a2fba1c0126f77ede8cbff5013ba96f66ca7
Owocki <ksowocki@gmail.com>
2018-01-31 14:13:06 -0700

    broken progress bars

--- app/assets/v2/js/pages/leaderboard.js
+++ app/assets/v2/js/pages/leaderboard.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-invalid-this */
 
 $( document ).ready( () => {
-  $( '.leaderboard_entry .progress-bar' ).each( () => {
+  $( '.leaderboard_entry .progress-bar' ).each( function (){
     const max = parseInt( $( this ).attr( 'aria-valuemax' ) );
     const now = parseInt( $( this ).attr( 'aria-valuenow' ) );
     const width = ( now * 100 ) / max;
-
     $( this ).css( 'width', `${width}%` )
   })
 
