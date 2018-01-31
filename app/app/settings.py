fb9c79ffc15688b5268325b303561bf08b4403bc
Mark Beacom <mark.beacom@cirrustack.com>
2018-01-31 17:20:03 -0500

    Add whitenoise collectstatic override

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'django.contrib.sitemaps',
