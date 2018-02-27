a14603645f5f432e65ac2800ce8b870af3983283
Vikash Bajaj <contactvikashbajaj@gmail.com>
2018-02-27 15:34:53 -0700

    ENS Subdomain feature: work started

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -807,6 +807,14 @@ def toolbox(request):
             "active": "true",
             'stat_graph': 'gas_page',
             },
+            {
+            "name": "ENS Subdomain Registrar",
+            "img": "/static/v2/images/ens.png",
+            "description": '''Register ENS Subdomain''',
+            "link": "/ens_subdomain_registrar",
+            "active": "true",
+            'stat_graph': 'ens_subdomain_registration_count',
+            },
           ]
        }, {
           "title": "Tools for Building Gitcoin",
---
+++ app/enssubdomain/utils.py
@@ -0,0 +1,9 @@
+from web3 import HTTPProvider, Web3
+from web3.exceptions import BadFunctionCallOutput
+
+def register_ens_subdomain():
+    pass
+
+def delete_ens_subdomain():
+    pass
+
