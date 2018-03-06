de6cfb62f83099e7d361475a3fffcfb08f875e64
Owocki <ksowocki@gmail.com>
2018-03-06 14:56:52 -0700

    debug scripsts

--- scripts/debug/add_interest.py
+++ scripts/debug/add_interest.py
@@ -1,9 +1,13 @@
-github_url = 'https://github.com/gitcoinco/web/issues/380'
-username = 'iliketobuildstuff'
+github_url = 'https://github.com/ethgasstation/ethgasstation-api/issues/1'
+username = 'eswarasai'
 
 from dashboard.models import Bounty, Interest, Profile
 
 bounty = Bounty.objects.get(current_bounty=True, github_url=github_url)
 profile = Profile.objects.filter(handle=username).first()
-interest = Interest.objects.get_or_create(profile=profile)
+try:
+    interest = Interest.objects.get_or_create(profile=profile)
+except Interest.MultipleObjectsReturned:
+    interest = Interest.objects.filter(profile=profile)
+
 bounty.interested.add(interest[0])
