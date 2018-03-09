45843e8ac30366a15080e2d66d11f2241a4466d8
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2018-03-09 10:12:02 -0500

    Remove unused import and some tests

--- app/app/context.py
+++ app/app/context.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Define additional context data to be passed to any request.
+
+Copyright (C) 2018 Gitcoin Core
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 import json
 
 from django.conf import settings
-from django.contrib.auth.models import AnonymousUser
 from django.utils import timezone
 
 from dashboard.models import Tip
