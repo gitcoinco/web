c89f52622b74ca3b681459ec0569bfa1c60878e7
Mark Beacom <Beacom@gmail.com>
2018-03-29 16:55:44 -0400

    Add flower handling

--- bin/celery/worker.bash
+++ bin/celery/worker.bash
@@ -8,9 +8,14 @@ set -o xtrace
 CELERY_BEAT_ENABLED=${CELERY_BEAT_ENABLED:-''}
 CELERY_APP=${CELERY_APP:-'app'}
 CELERY_LOG_LEVEL=${CELERY_LOG_LEVEL:-'INFO'}
+CELERY_FLOWER_INTERFACE=${CELERY_FLOWER_INTERFACE:-'0.0.0.0'}
+CELERY_FLOWER_PORT=${CELERY_FLOWER_PORT:-'5555'}
 
 cd app || echo "Misconfigured Docker container..."
 
 rm -f './celerybeat.pid'
-exec celery -A "$CELERY_APP" "$SERVICE_TYPE" -l "$CELERY_LOG_LEVEL"
-
+if [ "$SERVICE_TYPE" == "flower" ]; then
+    exec celery flower -A "$CELERY_APP" --address="$CELERY_FLOWER_INTERFACE" --port="$CELERY_FLOWER_PORT" --loglevel="$CELERY_LOG_LEVEL"
+else
+    exec celery -A "$CELERY_APP" "$SERVICE_TYPE" --loglevel="$CELERY_LOG_LEVEL"
+fi
