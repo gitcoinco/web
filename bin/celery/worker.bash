e80b6f13176c0b86797cd14562def11be2df3b17
Mark Beacom <Beacom@gmail.com>
2018-03-29 22:05:08 -0400

    Update entry and pin latest flower

--- bin/celery/worker.bash
+++ bin/celery/worker.bash
@@ -14,8 +14,8 @@ CELERY_FLOWER_PORT=${CELERY_FLOWER_PORT:-'5555'}
 cd app || echo "Misconfigured Docker container..."
 
 rm -f './celerybeat.pid'
-if [ "$SERVICE_TYPE" == "flower" ]; then
-    exec celery flower -A "$CELERY_APP" --address="$CELERY_FLOWER_INTERFACE" --port="$CELERY_FLOWER_PORT" --loglevel="$CELERY_LOG_LEVEL"
+if [ "$SERVICE_TYPE" = "flower" ]; then
+    exec flower -A "$CELERY_APP" --address="$CELERY_FLOWER_INTERFACE" --port="$CELERY_FLOWER_PORT" --loglevel="$CELERY_LOG_LEVEL"
 else
     exec celery -A "$CELERY_APP" "$SERVICE_TYPE" --loglevel="$CELERY_LOG_LEVEL"
 fi
