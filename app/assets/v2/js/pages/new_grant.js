65e9f4f9d76e1e5edfcd134fd68cc0f5c7997bab
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-03-29 21:52:00 -0400

    Enable saving of Grant

--- app/assets/v2/js/pages/new_grant.js
+++ app/assets/v2/js/pages/new_grant.js
@@ -2,15 +2,12 @@
 window.onload = function() {
   $('#js-newGrant').validate({
     submitHandler: function(form) {
+      var action = $(form).attr('action');
       var data = {};
-      var disabled = $(form)
-        .find(':input:disabled')
-        .removeAttr('disabled');
 
       $.each($(form).serializeArray(), function() {
         data[this.name] = this.value;
       });
-
     }
   });
 };
