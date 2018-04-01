0ed5e763d53d81ffbc7ecc524f5348a2cac82b79
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-04-01 18:56:42 -0400

    Add profile field to grants and calculate percentage done

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1447,6 +1447,7 @@ def new_bounty(request):
 
 def grant_show(request, grant_id):
     grant = Grant.objects.get(pk=grant_id)
+
     params = {
         'active': 'dashboard',
         'title': 'Grant Show',
@@ -1457,6 +1458,8 @@ def grant_show(request, grant_id):
 
 def new_grant(request):
     """Handle new grant."""
+    profile_id = request.session.get('profile_id')
+    profile = Profile.objects.get(pk=profile_id)
 
     if request.method == "POST":
         grant = Grant()
@@ -1466,6 +1469,7 @@ def new_grant(request):
         grant.description = request.POST.get('description')
         grant.reference_url = request.POST.get('reference_url');
         grant.goal_funding = request.POST.get('goal_funding')
+        grant.profile = profile
 
         grant.save()
     else:
