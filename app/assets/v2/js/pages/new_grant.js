389621fa8e4302899ae837a19b0a0ce402b52c6c
Kenneth Ashley <ken.u.diggitt@gmail.com>
2018-04-08 19:52:56 -0400

    Add spacing to grants index, and add dragover styling.

--- app/assets/v2/js/pages/new_grant.js
+++ app/assets/v2/js/pages/new_grant.js
@@ -1,5 +1,25 @@
 /* eslint-disable no-console */
 window.onload = function() {
+  $('#js-drop').on('dragover', function(event) {
+    event.preventDefault();  
+    event.stopPropagation();
+    $(this).addClass('is-dragging');
+  });
+
+  $('#js-drop').on('dragleave', function(event) {
+    event.preventDefault();  
+    event.stopPropagation();
+    $(this).removeClass('is-dragging');
+  });
+
+  $('#js-drop').on('drop', function(event) {
+    if (event.originalEvent.dataTransfer.files.length) {
+      event.preventDefault();  
+      event.stopPropagation();
+      $(this).removeClass('is-dragging');
+    }
+  });
+
   $('#js-newGrant').validate({
     submitHandler: function(form) {
       var action = $(form).attr('action');
@@ -8,8 +28,26 @@ window.onload = function() {
       $.each($(form).serializeArray(), function() {
         data[this.name] = this.value;
       });
+
+      $(form).submit();
     }
 
-    $(form).submit();
   });
+
+  waitforWeb3(function() {
+    tokens(document.web3network).forEach(function(ele) {
+      var option = document.createElement('option');
+
+      option.text = ele.name;
+      option.value = ele.addr;
+
+      $('#js-token').append($('<option>', {
+        value: ele.addr,
+        text: ele.name
+      }));
+    });
+
+    $('#js-token').select2();
+  });
+
 };
