ee9af725348731e72597cb727fcf387ef50b6c15
Mark Beacom <7315957+mbeacom@users.noreply.github.com>
2018-04-13 18:00:17 -0400

    Fix autodiscover tasks

--- app/app/celery.py
+++ app/app/celery.py
@@ -30,7 +30,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 app = Celery('app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.autodiscover_tasks()
 
 if os.environ.get('SERVICE_TYPE', '') in ['worker', 'beat'] and not settings.DEBUG:
     import rollbar
