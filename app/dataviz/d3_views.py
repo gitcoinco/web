6d08ffefb76dec7fc8c1a518970cd63d64415262
Owocki <ksowocki@gmail.com>
2018-04-16 22:26:42 -0600

    chord

--- app/dataviz/d3_views.py
+++ app/dataviz/d3_views.py
@@ -140,55 +140,6 @@ def viz_chord(request, key='bounties_paid'):
     type_options = ['bounties_paid']
 
     if request.GET.get('data'):
-        output = """creditor,debtor,amount,risk
-Britain,France,22.4,3
-Britain,Greece,0.55,0
-Britain,Italy,26,0
-Britain,Portugal,19.4,0
-Britain,United States,345,1
-France,Germany,53.8,1
-France,Greece,53.9,0
-France,Ireland,17.3,0
-France,Italy,366,0
-France,Japan,7.73,1
-France,Portugal,18.3,0
-France,Spain,118,2
-France,United States,322,1
-Germany,Britain,321,1
-Germany,Greece,19.3,0
-Germany,Ireland,48.9,0
-Germany,Portugal,32.5,0
-Germany,Spain,57.6,2
-Germany,United States,324,1
-Ireland,Britain,12,1
-Ireland,Greece,0.34,0
-Ireland,Spain,6.38,2
-Italy,Germany,111,1
-Italy,Greece,3.22,0
-Italy,Ireland,2.83,0
-Italy,Portugal,0.87,0
-Japan,Britain,28.2,1
-Japan,Germany,88.5,1
-Japan,Greece,1.37,0
-Japan,Ireland,18.9,0
-Japan,Italy,38.8,0
-Japan,Portugal,2.18,0
-Japan,Spain,25.9,2
-Japan,United States,796,1
-Portugal,Greece,10.1,0
-Portugal,Ireland,3.77,0
-Portugal,United States,0.52,1
-Spain,Britain,326,1
-Spain,Greece,0.78,0
-Spain,Italy,9.79,0
-Spain,Portugal,62,0
-Spain,United States,163,1
-United States,Greece,3.1,0
-United States,Italy,3.16,0
-Ireland,Spain,6.38,2
-"""
-        if False:
-            return HttpResponse(output)
         rows = [
             ['creditor', 'debtor', 'amount', 'risk']
         ]
@@ -197,9 +148,10 @@ Ireland,Spain,6.38,2
             weight = bounty.value_in_usdt_then
             if weight:
                 for fulfillment in bounty.fulfillments.filter(accepted=True):
+                    length = (fulfillment.created_on - bounty.web3_created).seconds
                     target = fulfillment.fulfiller_github_username.lower()
                     source = bounty.bounty_owner_github_username.lower()
-                    rows.append((source, target, str(weight), str(1)))
+                    rows.append((source, target, str(weight), str(length)))
 
         output_rows = []
         for row in rows:
