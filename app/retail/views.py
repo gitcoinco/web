a54b1627368b527549e629453ea0544da13a9ae5
Mason Bourgeois <mason.bourgeois@gmail.com>
2018-04-21 08:37:50 -0500

    Add remaining hair colors

--- app/retail/views.py
+++ app/retail/views.py
@@ -671,15 +671,15 @@ def avatar(request):
               'options': ('0', '1', '2', '3') },
             { 'name': 'Hair Style', 'title': 'Pick a hairstyle',
               'options': (
-                ['None', '0'],
-                ['None', '1'],
-                ['None', '2'],
-                ['None', '3'],
-                ['None', '4'],
-                ['5', 'None'],
-                ['6-back', '6-front'],
-                ['7-back', '7-front'],
-                ['8-back', '8-front']
+                ["None", "0"],
+                ["None", "1"],
+                ["None", "2"],
+                ["None", "3"],
+                ["None", "4"],
+                ["5", "None"],
+                ["6-back", "6-front"],
+                ["7-back", "7-front"],
+                ["8-back", "8-front"]
               )
             },
             { 'name': 'Hair Color', 'title': 'Pick a hair color',
