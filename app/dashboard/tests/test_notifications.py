c9ba80abc5e0670ec86af98adec01b531577347b
Owocki <ksowocki@gmail.com>
2018-04-26 16:43:47 -0600

    https://github.com/gitcoinco/web/issues/987

--- app/dashboard/tests/test_notifications.py
+++ app/dashboard/tests/test_notifications.py
@@ -57,7 +57,7 @@ class DashboardNotificationsTest(TestCase):
     def test_build_github_notification_new_bounty(self):
         """Test the dashboard helper build_github_notification method with new_bounty."""
         message = build_github_notification(self.bounty, 'new_bounty')
-        assert message.startswith(f'__This issue now has a funding of {self.natural_value} {self.bounty.token_name}')
+        assert f'__This issue now has a funding of {self.natural_value} {self.bounty.token_name}' in message
         assert self.usdt_value in message
         assert f'[here]({self.absolute_url})' in message
         assert f'${self.amount_open_work}' in message
@@ -65,14 +65,14 @@ class DashboardNotificationsTest(TestCase):
     def test_build_github_notification_killed_bounty(self):
         """Test the dashboard helper build_github_notification method with killed_bounty."""
         message = build_github_notification(self.bounty, 'killed_bounty')
-        assert message.startswith(f"__The funding of {self.natural_value} {self.bounty.token_name} {self.usdt_value}")
+        assert f"__The funding of {self.natural_value} {self.bounty.token_name} {self.usdt_value}" in message
         assert 'Questions?' in message
         assert f'${self.amount_open_work}' in message
 
     def test_build_github_notification_increased_bounty(self):
         """Test the dashboard helper build_github_notification method with new_bounty."""
         message = build_github_notification(self.bounty, 'increased_bounty')
-        assert message.startswith(f'__The funding of this issue was increased to {self.natural_value} {self.bounty.token_name}')
+        assert f'__The funding of this issue was increased to {self.natural_value} {self.bounty.token_name}' in message
         assert self.usdt_value in message
         assert f'[here]({self.absolute_url})' in message
         assert f'${self.amount_open_work}' in message
