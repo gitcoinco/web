b33e05f2ec6589afa947b43f46dde2233ca28d59
Mark Beacom <markvbeacom@gmail.com>
2018-05-05 01:03:53 -0400

    Resolve line length stickler errors in ethos views

--- app/ethos/views.py
+++ app/ethos/views.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-'''
-    Copyright (C) 2017 Gitcoin Core
+"""Define the EthOS views.
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published
-    by the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+Copyright (C) 2018 Gitcoin Core
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU Affero General Public License for more details.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-    You should have received a copy of the GNU Affero General Public License
-    along with this program. If not, see <http://www.gnu.org/licenses/>.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
 
-'''
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 
 import json
 
@@ -55,7 +56,10 @@ def redeem_coin(request, shortcode):
 
             ethos = ShortCode.objects.get(shortcode=shortcode)
 
-            user_hop = Hop.objects.filter(shortcode=ethos, twitter_username=username, web3_address=address).order_by('-id').first()
+            user_hop = Hop.objects.filter(
+                shortcode=ethos,
+                twitter_username=username,
+                web3_address=address).order_by('-id').first()
 
             # Restrict same user from redeeming the same coin within 30 minutes
             if user_hop and (timezone.now() - user_hop.created_on).total_seconds() < 1800:
@@ -75,7 +79,53 @@ def redeem_coin(request, shortcode):
 
             address = Web3.toChecksumAddress(address)
 
-            abi = json.loads('[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"amount","type":"uint256"}],"name":"Mint","type":"event"},{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]')
+            abi = json.loads(
+                '[{"constant":true,"inputs":[],"name":"mintingFinished","outputs":[{"name":"","type":"bool"}],'
+                '"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],'
+                '"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view",'
+                '"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},'
+                '{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],'
+                '"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],'
+                '"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,'
+                '"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from",'
+                '"type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],'
+                '"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,'
+                '"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals",'
+                '"outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},'
+                '{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_amount","type":"uint256"}],'
+                '"name":"mint","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable",'
+                '"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"",'
+                '"type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,'
+                '"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],'
+                '"name":"decreaseApproval","outputs":[{"name":"","type":"bool"}],"payable":false,'
+                '"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner",'
+                '"type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],'
+                '"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],'
+                '"name":"finishMinting","outputs":[{"name":"","type":"bool"}],"payable":false,'
+                '"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner",'
+                '"outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view",'
+                '"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"",'
+                '"type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,'
+                '"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer",'
+                '"outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable",'
+                '"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},'
+                '{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"",'
+                '"type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,'
+                '"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],'
+                '"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,'
+                '"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner",'
+                '"type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,'
+                '"stateMutability":"nonpayable","type":"function"},{"payable":false,"stateMutability":"nonpayable",'
+                '"type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"to","type":"address"},'
+                '{"indexed":false,"name":"amount","type":"uint256"}],"name":"Mint","type":"event"},'
+                '{"anonymous":false,"inputs":[],"name":"MintFinished","type":"event"},{"anonymous":false,'
+                '"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,'
+                '"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},'
+                '{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,'
+                '"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],'
+                '"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from",'
+                '"type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value",'
+                '"type":"uint256"}],"name":"Transfer","type":"event"}]')
 
             # Instantiate EthOS contract
             contract = w3.eth.contract(Web3.toChecksumAddress(settings.ETHOS_CONTRACT_ADDRESS), abi=abi)
