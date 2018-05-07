b474d836060f9f95f872c820acb4c21aca8fbb4d
Sanyam Khurana <sanyam.khurana01@gmail.com>
2018-05-07 23:21:32 +0530

    Fix imports using isort

--- app/marketing/utils.py
+++ app/marketing/utils.py
@@ -16,8 +16,8 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 '''
-from datetime import datetime, timedelta
 import logging
+from datetime import datetime, timedelta
 
 from django.conf import settings
 from django.utils.translation import gettext
