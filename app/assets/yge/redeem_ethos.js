e035a411f19fdb8d5fb6e5f68bcaf7904c4fd907
Eswara Sai <eswarasai.v@gmail.com>
2018-05-11 18:57:02 +0530

    Added etherscan url in EthOS Tweet

--- app/assets/yge/redeem_ethos.js
+++ app/assets/yge/redeem_ethos.js
@@ -9,34 +9,6 @@ window.onload = function() {
     $('loading').style.display = 'none';
     $('send_eth').style.display = 'block';
   }, 500);
-
-  /* var dataset = {
-    nodes: [
-      {
-        'name': 'owocki',
-        'img': window.location.origin + '/ethos/proxy/?image=' + encodeURIComponent('https://twitter.com/owocki/profile_image?size=original')
-      },
-      {
-        'name': 'mbeacom',
-        'img': window.location.origin + '/ethos/proxy/?image=' + encodeURIComponent('https://twitter.com/mbeacom/profile_image?size=original')
-      },
-      {
-        'name': 'eswara_sai',
-        'img': window.location.origin + '/ethos/proxy/?image=' + encodeURIComponent('https://twitter.com/eswara_sai/profile_image?size=original')
-      },
-      {
-        'name': 'thelostone-mc',
-        'img': window.location.origin + '/ethos/proxy/?image=' + encodeURIComponent('https://gitcoin.co/static/avatar/thelostone-mc/')
-      }
-    ],
-    edges: [
-      {source: 0, target: 1, distance: 200},
-      {source: 1, target: 2, distance: 50},
-      {source: 2, target: 3, distance: 100}
-    ]
-  };
-
-  generateGraph(dataset); */
 };
 
 function redeemCoin() {
@@ -76,7 +48,15 @@ function redeemCoin() {
       function(response) {
         response.json().then(function(data) {
           if (data.status === 'OK') {
-            generateGraph(data.dataset, twitter_username, data.message);
+            mixpanel.track('Redeem EthOS Coin Success', {});
+            startConfetti();
+
+            $('send_eth').innerHTML = '<h1>Got Ethos?</h1>' +
+              '<h3>Prove it -- Give this coin to someone *whom you don\'t know*.</h3>' +
+              '<h4>The faster you perform this task, the more Ethos you will receive.</h4>' +
+              '<p>This coin has been shared ' + data.num_scans + ' times. ' +
+              'The top coin has been shared ' + data.num_scans_top + ' times.</p>' +
+              '<a href="' + data.tweet_url + '" target="_blank" rel="noopener noreferrer">Checkout your tweet here</a>';
           } else {
             if (data.message.indexOf('Address has an invalid EIP checksum') !== -1) {
               _alert({message: 'Please enter a valid checksum address.'}, 'warning');
@@ -103,144 +83,3 @@ function redeemCoin() {
       console.log('Fetch Error :-S', err);
     });
 }
-
-var generateGraph = function(dataset, username, txid) {
-  var w = 600;
-  var h = 400;
-
-  var force = d3.layout.force()
-    .nodes(dataset.nodes)
-    .links(dataset.edges)
-    .size([ w, h ])
-    .linkDistance(function(link) {
-      return link.distance;
-    })
-    .charge([-150])
-    .start();
-
-  var colors = d3.scale.category10();
-
-  var svg = d3.select('#graph')
-    .append('svg')
-    .attr('width', w)
-    .attr('height', h);
-
-  var edges = svg.selectAll('line')
-    .data(dataset.edges)
-    .enter()
-    .append('line')
-    .attr('marker-end', 'url(#arrowhead)')
-    .style('stroke', '#CACACA')
-    .style('stroke-width', 2);
-
-  var nodes = svg.selectAll('circle')
-    .data(dataset.nodes)
-    .enter()
-    .append('circle')
-    .attr('r', 16)
-    .attr('stroke', '#1AB56D')
-    .attr('stroke-width', '0.2%')
-    .attr('fill', function(d) {
-      return 'url(#' + d.name + ')';
-    });
-
-  var label = svg.selectAll('.mytext')
-    .data(dataset.nodes)
-    .enter()
-    .append('text')
-    .text(function(d) {
-      return d.name;
-    })
-    .attr('dx', 14)
-    .attr('dy', '1.2em')
-    .style('fill', '#555');
-
-  svg.append('defs').append('marker')
-    .attr({
-      'id': 'arrowhead',
-      'viewBox': '-0 -5 20 10',
-      'refX': 23,
-      'refY': 0,
-      'orient': 'auto',
-      'markerWidth': 10,
-      'markerHeight': 10,
-      'xoverflow': 'visible'
-    })
-    .append('svg:path')
-    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
-    .attr('fill', '#CACACA')
-    .attr('stroke', '#CACACA');
-
-
-  svg.selectAll('pattern').data(dataset.nodes).enter().append('pattern')
-    .attr('id', function(d) {
-      return d.name;
-    })
-    .attr('x', '0%')
-    .attr('y', '0%')
-    .attr('height', '100%')
-    .attr('width', '100%')
-    .attr('viewBox', '0 0 512 512')
-    .append('image')
-    .attr('x', '0%')
-    .attr('y', '0%')
-    .attr('height', '512')
-    .attr('width', '512')
-    .attr('xlink:href', function(d) {
-      return d.img;
-    });
-
-  force.on('tick', function() {
-    edges.attr('x1', function(d) {
-      return d.source.x;
-    })
-      .attr('y1', function(d) {
-        return d.source.y;
-      })
-      .attr('x2', function(d) {
-        return d.target.x;
-      })
-      .attr('y2', function(d) {
-        return d.target.y;
-      });
-    nodes.attr('cx', function(d) {
-      return d.x;
-    })
-      .attr('cy', function(d) {
-        return d.y;
-      });
-    label.attr('x', function(d) {
-      return d.x;
-    })
-      .attr('y', function(d) {
-        return d.y - 10;
-      });
-  });
-
-  setTimeout(function() {
-    svgAsPngUri(svg.node(), {}, function(uri) {
-      fetch('/ethos/tweet/', {
-        method: 'POST',
-        body: JSON.stringify({
-          media: uri.replace(/^data:image\/(png|jpg);base64,/, ''),
-          username: username
-        })
-      })
-        .then(
-          function(response) {
-            response.json().then(function(data) {
-              mixpanel.track('Redeem EthOS Coin Success', {});
-              startConfetti();
-
-              // TODO: Display the stats page after posting the tweet
-              $('send_eth').innerHTML = '<h1>Success 🚀!</h1> <a href="https://' + etherscanDomain() + '/tx/' + txid + '" target="_blank" rel="noopener noreferrer">See your transaction on the blockchain here</a>.<br><br><span id="mighttake">It might take a few minutes to sync, depending upon: <br> - network congestion<br> - network fees that sender allocated to transaction<br></span><br><a href="/" class="button" style="background-color: #15003e;">⬅ Check out Gitcoin.co</a>';
-            });
-          }
-        )
-        .catch(function(err) {
-          console.log('Tweet Error', err);
-          mixpanel.track('Redeem EthOS Coin Error', {});
-        });
-    });
-  }, 5000);
-};
