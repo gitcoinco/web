6459dcaa7da65f19ffcb1b614f52c6be2f4b82f7
Mason Bourgeois <mason.bourgeois@gmail.com>
2018-05-29 16:35:17 -0500

    revisions1

--- app/assets/v2/js/landing_page.js
+++ app/assets/v2/js/landing_page.js
@@ -1,65 +1,31 @@
+scrollContainer = $('#landing_page_wrapper');
 
 // Header and Nav
 $(document).ready(function() {
-  const $background = $('#gitcoin-background');
   const $navbar = $('.navbar');
-  const $header = $('.header');
-  const scrollWrapper = $('#landing_page_wrapper');
   const $gcRobot = $('#gc-robot');
   const $gcTree = $('#gc-tree');
 
-  let mouseX = 0;
-  let mouseY = 0;
   const followStateHeight = 500;
-  let navFollowState = scrollWrapper.scrollTop() > followStateHeight;
+  let navFollowState = scrollContainer.scrollTop() > followStateHeight;
 
-  const movementStrength = 25;
-  const height = movementStrength / $(window).height();
-  const width = movementStrength / $(window).width();
-  let throttledHandler;
-  const moveBackground = e => {
-    mouseX = e.pageX || mouseX;
-    mouseY = e.pageY || mouseY;
-    if (throttledHandler) {
-      return;
-    }
-    throttledHandler = requestAnimationFrame(() => {
-      if (!navFollowState && scrollWrapper.scrollTop() > followStateHeight) {
-        $navbar.addClass('following');
-        navFollowState = true;
-      } else if (navFollowState && scrollWrapper.scrollTop() < followStateHeight) {
-        $navbar.removeClass('following');
-        navFollowState = false;
-      }
-      const pageX = mouseX - ($(window).width() / 2);
-      const pageY = mouseY - ($(window).height() / 2) - scrollWrapper.scrollTop() * 2;
-      const newvalueX = width * pageX - 10;
-      let newvalueY = height * pageY;
-
-      $gcRobot.css('transform', `translateY(${($gcRobot.parent()[0].getBoundingClientRect().top - 100) / 2}px)`);
-      $gcTree.css('transform', `translateY(${(-$gcTree.parent()[0].getBoundingClientRect().top) / 6 + 40}px)`);
-
-      $background.css('transform', `translate(${newvalueX}px, ${newvalueY}px)`);
-      throttledHandler = undefined;
-    });
-  };
-
-  $navbar.mousemove(moveBackground);
-  $header.mousemove(moveBackground);
-  let robotContainerPos = $('.case-studies-container').offset().top;
-  let treeContainerPos = $('.tree_container').offset().top;
-
-  // Robot and Tree Parallax
-  const moveWithScroll = (e) => {
+  scrollContainer.scroll(RAFThrottle((e) => {
     moveBackground(e);
-  };
-
-  scrollWrapper.scroll(moveWithScroll);
+    // Robot and Tree Parallax
+    if (!navFollowState && scrollContainer.scrollTop() > followStateHeight) {
+      $navbar.addClass('following');
+      navFollowState = true;
+    } else if (navFollowState && scrollContainer.scrollTop() < followStateHeight) {
+      $navbar.removeClass('following');
+      navFollowState = false;
+    }
+    $gcRobot.css('transform', `translateY(${$gcRobot.parent()[0].getBoundingClientRect().top - 100}px)`);
+    $gcTree.css('transform', `translateY(${$gcTree.parent()[0].getBoundingClientRect().top / 2 - 50}px)`);
+  }));
   moveBackground({});
 
-  const $toggleIndicator = $('#howworks-toggle-indicator');
-
   // How It Works section toggle
+  const $toggleIndicator = $('#howworks-toggle-indicator');
   const $funderToggle = $('#funder-toggle');
   const $contributorToggle = $('#contributor-toggle');
   const $sections = $('.howworks-sections');
@@ -75,24 +41,24 @@ $(document).ready(function() {
     $funderToggle.removeClass('active');
     $contributorToggle.addClass('active');
     $toggleIndicator.css('transform', `scaleX(${$contributorToggle.innerWidth() / $funderToggle.innerWidth()})`);
-    $toggleIndicator.css('left', `${$funderToggle.innerWidth()}px`);
+    $toggleIndicator.css('left', `${$funderToggle.innerWidth() + 20}px`);
     $sections.addClass('contributor-section');
   }
   $funderToggle.click(switchToFunder);
   $contributorToggle.click(switchToContributor);
-  switchToContributor();
+  switchToFunder();
 
   const prevScroll = localStorage.getItem('scrollTop');
   const lastAccessed = localStorage.getItem('lastAccessed');
 
   // Preserve scroll position if user was just here
   if (prevScroll && lastAccessed && new Date().getTime() - lastAccessed < 60 * 1000) {
-    scrollWrapper.scrollTop(prevScroll);
+    scrollContainer.scrollTop(prevScroll);
     moveWithScroll({});
   }
   // before the current page goes away, save the menu position
   $(window).on('beforeunload', function() {
-    localStorage.setItem('scrollTop', scrollWrapper.scrollTop());
+    localStorage.setItem('scrollTop', scrollContainer.scrollTop());
     localStorage.setItem('lastAccessed', new Date().getTime());
   });
 });
--- app/assets/v2/js/note.js
+++ app/assets/v2/js/note.js
@@ -1,16 +1,30 @@
+const movementStrength = 25;
+let startX = null;
+let startY = null;
+let mouseX = 0;
+let mouseY = 0;
+let scrollContainer;
+const moveBackground = RAFThrottle(e => {
+  mouseX = e.pageX || mouseX;
+  mouseY = e.pageY || mouseY;
+  const pageX = mouseX - ($(window).width() / 2);
+  const pageY = mouseY - ($(window).height() / 2) -
+    ((scrollContainer === window) ? window.scrollY : scrollContainer.scrollTop()) * 2;
+  const newvalueX = movementStrength / $(window).width() * pageX - 10;
+  let newvalueY = movementStrength / $(window).height() * pageY;
+
+  $('#gitcoin-background').css('transform', `translate(${newvalueX}px, ${newvalueY}px)`);
+});
+
 $(document).ready(function() {
-  let startX = null;
-  let startY = null;
-  const movementStrength = 25;
-  const height = movementStrength / $(window).height();
-  const width = movementStrength / $(window).width();
+  scrollContainer = scrollContainer || $(window); // Allows overriding page scroll container
 
   $('.white-light-bg, .offchain .body').each(function(index, element) {
     $(element).mousemove(e => {
       const pageX = e.pageX - ($(window).width() / 2);
       const pageY = e.pageY - ($(window).height() / 2);
-      const newvalueX = width * (pageX - startX) * -1 - 25;
-      const newvalueY = height * (pageY - startY) * -1 - 50;
+      const newvalueX = movementStrength / $(window).width() * (pageX - startX) * -1 - 25;
+      const newvalueY = movementStrength / $(window).height() * (pageY - startY) * -1 - 50;
 
       if (!startX) {
         startX = newvalueX;
@@ -23,4 +37,26 @@ $(document).ready(function() {
       $(element).css('background-position', `${newvalueX - startX}px ${newvalueY - startY}px`);
     });
   });
+
+  const $navbar = $('.navbar');
+  const $header = $('.header');
+
+  $navbar.mousemove(moveBackground);
+  $header.mousemove(moveBackground);
+  scrollContainer.scroll(moveBackground);
 });
+
+function RAFThrottle(f) {
+  let throttledHandler;
+
+  return function() {
+    if (throttledHandler) {
+      return;
+    }
+
+    throttledHandler = requestAnimationFrame(() => {
+      f(...arguments);
+      throttledHandler = undefined;
+    });
+  };
+}
