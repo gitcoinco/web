38f73f3b5ae341e88384264ccf57b1cc5d6899f3
Owocki <ksowocki@gmail.com>
2018-06-01 19:12:18 -0600

    card desc

--- app/retail/utils.py
+++ app/retail/utils.py
@@ -84,6 +84,7 @@ def build_stat_results():
     context = {
         'active': 'results',
         'title': _('Results'),
+        'card_desc': _('Gitcoin is designed to be completely transparent about our network results'),
     }
 
     base_bounties = Bounty.objects.current().filter(network='mainnet')
