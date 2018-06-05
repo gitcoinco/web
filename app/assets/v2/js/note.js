ce630866f073456a3dacb4d1ab9683c6f8221bb8
Aditya Anand <adianand@paypal.com>
2018-06-06 00:30:21 +0530

    landing : added weird hover animation

--- app/assets/v2/js/note.js
+++ app/assets/v2/js/note.js
@@ -5,22 +5,18 @@ let mouseX = 0;
 let mouseY = 0;
 let scrollContainer;
 const moveBackground = RAFThrottle(e => {
-  mouseX = e.pageX || mouseX;
   mouseY = e.pageY || mouseY;
-  const pageX = mouseX - ($(window).width() / 2);
   const pageY = mouseY - ($(window).height() / 2) -
     ((scrollContainer === window) ? window.scrollY : scrollContainer.scrollTop()) * 2;
-  const newvalueX = movementStrength / $(window).width() * pageX - 10;
   let newvalueY = movementStrength / $(window).height() * pageY;
 
-  // TODO : Evaluate if the scrolling effect is needed
-  // $('.header').css('transform', `translate(${newvalueX}px, ${newvalueY}px)`);
+  $('.index #gitcoin-background').css('transform', `translateY(${newvalueY}px)`);
 });
 
 $(document).ready(function() {
   scrollContainer = scrollContainer || $(window); // Allows overriding page scroll container
 
-  $('.white-light-bg, .offchain .body').each(function(index, element) {
+  $('.header, .white-light-bg, .offchain .body').each(function(index, element) {
     $(element).mousemove(e => {
       const pageX = e.pageX - ($(window).width() / 2);
       const pageY = e.pageY - ($(window).height() / 2);
