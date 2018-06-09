edd057b59873f5af65565f7a381fc624f989b812
Sanyam Khurana <sanyam.khurana01@gmail.com>
2018-06-09 16:48:04 +0530

    fix filter in job listing API

--- app/jobs/filters.py
+++ app/jobs/filters.py
@@ -9,17 +9,18 @@ class JobPostedFilter(filters.BaseFilterBackend):
     def filter_queryset(self, request, queryset, view):
         job_posted = request.query_params.get('job_posted', None)
         if job_posted:
-            job_posted = datetime.now() - timedelta(days=job_posted)
+            job_posted_days_ago = int(job_posted)
+            job_posted = datetime.now() - timedelta(days=job_posted_days_ago)
             return queryset.filter(posted_at__gte=job_posted)
         return queryset
 
 
 class EmploymentTypeFilter(filters.BaseFilterBackend):
     """
-    Filter on employment_type for the Job.
+    Filter on job_type for the Job.
     """
     def filter_queryset(self, request, queryset, view):
-        employment_type = request.query_params.get('employment_type', None)
-        if employment_type:
-            return queryset.filter(employment_types__iexact=employment_type)
+        job_type = request.query_params.get('employment_type', None)
+        if job_type:
+            return queryset.filter(job_type__iexact=job_type)
         return queryset
