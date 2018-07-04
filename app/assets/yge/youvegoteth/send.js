5e533c20702254d776f58413e724fbc859eaae00
Owocki <ksowocki@gmail.com>
2018-07-04 09:00:35 -0700

    scroll fix

--- app/assets/yge/youvegoteth/send.js
+++ app/assets/yge/youvegoteth/send.js
@@ -104,7 +104,6 @@ function prePopulateGitcoinerFields() {
 }
 
 window.onload = function() {
-  $('yge').style.overflow='scroll';
   jQuery('#amount').on('keyup blur change', updateEstimate);
   jQuery('#token').on('change', updateEstimate);
   jQuery('#token').on('change', promptForAuth);
