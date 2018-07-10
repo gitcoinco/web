dead551dd0679b42124b9a229d136d76a550a35e
pinkiebell <pinkiebell@localhost>
2018-07-10 20:10:55 +0200

    Display an error banner on network error on start-/stop work..
    ...instead of showing nothing.
    
    Related #1678

--- app/assets/v2/js/shared.js
+++ app/assets/v2/js/shared.js
@@ -266,7 +266,11 @@ var mutate_interest = function(bounty_pk, direction, data) {
     }
     return false;
   }).fail(function(result) {
-    alert(result.responseJSON.error);
+    _alert('Network or API error. Please reload the page and try again.', 'error');
+
+    if (result) {
+      console.log(result.responseJSON.error);
+    }
   });
 };
 
