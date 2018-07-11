fb08151476c5139f1c78839ef83eadfbb41f6f8c
Owocki <ksowocki@gmail.com>
2018-07-11 09:59:21 -0600

    prevent 404 for avatar

--- app/app/urls.py
+++ app/app/urls.py
@@ -69,6 +69,7 @@ urlpatterns = [
     url(r'^universe/(?P<issuenum>.*)/(?P<slug>.*)/?', external_bounties.views.external_bounties_show, name='universe'),
     url(r'^universe/?', external_bounties.views.external_bounties_index, name="universe_index"),
     re_path(r'^onboard/(?P<flow>\w+)/$', dashboard.views.onboard, name='onboard'),
+    re_path(r'^onboard/contributor/avatar/?$', dashboard.views.onboard_avatar, name='onboard_avatar'),
     url(r'^dashboard/?', dashboard.views.dashboard, name='dashboard'),
     url(r'^explorer/?', dashboard.views.dashboard, name='explorer'),
 
