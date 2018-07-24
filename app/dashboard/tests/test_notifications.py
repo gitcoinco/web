95289b24996c534f9a006b56770343b2990007f7
jesush <holycrapmoli@hotmail.com>
2018-07-24 00:21:11 -0400

    Preliminary testing-outline completed-just prints instead of assers for now, while finalizing changes

--- app/dashboard/tests/test_notifications.py
+++ app/dashboard/tests/test_notifications.py
@@ -19,7 +19,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 from datetime import datetime
 
-from dashboard.models import Bounty
+from dashboard.models import Bounty, Profile, BountyFulfillment, Interest, Activity
 from dashboard.notifications import amount_usdt_open_work, append_snooze_copy, build_github_notification
 from pytz import UTC
 from test_plus.test import TestCase
@@ -38,9 +38,11 @@ class DashboardNotificationsTest(TestCase):
             github_url='https://github.com/gitcoinco/web/issues/11',
             token_address='0x0',
             issue_description='hello world',
-            bounty_owner_github_username='flintstone',
-            is_open=False,
-            accepted=True,
+            bounty_owner_github_username='samplegitcoindeveloper1',
+            bounty_owner_profile= Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+            is_open=True,
+            accepted=False,
+            permission_type='permissionless',
             expires_date=datetime(2008, 11, 30, tzinfo=UTC),
             idx_project_length=5,
             project_length='Months',
@@ -54,6 +56,30 @@ class DashboardNotificationsTest(TestCase):
         self.amount_open_work = amount_usdt_open_work()
         self.bounty_owner = f"(@{self.bounty.bounty_owner_github_username})"
 
+    def test_build_github_notification_new_bounty_traditional_permissionless(self):
+        message = build_github_notification(self.bounty, 'new_bounty')
+        print(message)
+        return message
+        
+    def test_build_github_notification_new_bounty_traditional_approval(self):
+        self.bounty.bounty_type = 'traditional'
+        self.bounty.permission_type='approval'
+        message = build_github_notification(self.bounty, 'new_bounty')
+        print(message)
+
+    def test_build_github_notification_new_bounty_cooperative(self):
+        self.bounty.bounty_type = 'cooperative'
+        self.bounty.permission_type = 'permissionless'
+        message = build_github_notification(self.bounty, 'new_bounty')
+        print(message)
+
+    def test_build_github_notification_new_bounty_contest(self):
+        self.bounty.bounty_type = 'contest'
+        self.bounty.permission_type='permissionless'
+        message = build_github_notification(self.bounty, 'new_bounty')
+        print(message)
+
+
     def test_build_github_notification_new_bounty(self):
         """Test the dashboard helper build_github_notification method with new_bounty."""
         message = build_github_notification(self.bounty, 'new_bounty')
@@ -61,6 +87,142 @@ class DashboardNotificationsTest(TestCase):
         assert self.usdt_value in message
         assert f'This issue now has a funding of' in message
         assert f'${self.amount_open_work}' in message
+
+    def test_build_github_notification_express_interest_traditional_approval(self):
+        self.bounty.bounty_type = 'traditional'
+        self.bounty.permission_type='approval'
+        interest = Interest.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               issue_message='hello world',
+               pending=True
+        )
+        self.bounty.interested.add(interest)
+        message = build_github_notification(self.bounty, 'work_started')
+        print(message)
+
+    def test_build_github_notification_start_work_traditional_permissionless(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='permissionless'
+        interest = Interest.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               issue_message='hello world',
+               pending=False,
+               acceptance_date=datetime(2018, 11, 30, tzinfo=UTC)
+        )
+        self.bounty.interested.add(interest)
+        message = build_github_notification(self.bounty, 'work_started')
+        print(message)
+
+    def test_build_github_notification_start_work_traditional_approval(self):
+        self.bounty.bounty_type='traaditional'
+        self.bounty.permission_type='approval'
+        interest = Interest.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               issue_message='hello world',
+               pending=False,
+               acceptance_date=datetime(2018, 11, 30, tzinfo=UTC)
+        )
+        self.bounty.interested.add(interest)
+        message = build_github_notification(self.bounty, 'work_started')
+        print(message)
+
+    def test_build_github_notification_start_work_cooperative(self):
+        self.bounty.bounty_type='cooperative'
+        self.bounty.permission_type='permissionless'
+        interest = Interest.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               issue_message='hello world',
+               pending=False,
+               acceptance_date=datetime(2018, 11, 30, tzinfo=UTC)
+        )
+        self.bounty.interested.add(interest)
+        message = build_github_notification(self.bounty, 'work_started')
+        print(message)
+
+    def test_build_github_notification_start_work_contest(self):
+        self.bounty.bounty_type='contest'
+        self.bounty.permission_type='permissionless'
+        interest = Interest.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               issue_message='hello world',
+               pending=False,
+               acceptance_date=datetime(2018, 11, 30, tzinfo=UTC)
+        )
+        self.bounty.interested.add(interest)
+        message = build_github_notification(self.bounty, 'work_started')
+        print(message)
+
+    def test_build_github_notification_stop_work_contest(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='permissionless'
+        activity = Activity.objects.create(
+               profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+               activity_type='stop_work',
+               bounty=self.bounty)
+        self.bounty.activities.add(activity)
+        message = build_github_notification(self.bounty, 'stop_work')
+        print(message)
+
+    def test_build_github_notification_submit_work_traditional_permissionless(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='permissionless'
+        fulfillment = BountyFulfillment.objects.create(
+              fulfiller_address='0x0000000000000000000000000000000000000000',
+              fulfiller_email='gitcointestdeveloper1@gmail.com',
+              fulfiller_github_username='samplegitcoindeveloper1',
+              bounty=self.bounty,
+              profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+              accepted=False
+        )
+        self.bounty.fulfillments.add(fulfillment)
+        message = build_github_notification(self.bounty, 'work_submitted')
+        print(message)
+
+    def test_build_github_notification_submit_work_traditional_approval(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='approval'
+        message = build_github_notification(self.bounty, 'work_submitted')
+        print(message)
+
+    def test_build_github_notification_submit_work_cooperative(self):
+        self.bounty.bounty_type='cooperative'
+        self.bounty.permission_type='permissionless'
+        message = build_github_notification(self.bounty, 'work_submitted')
+        print(message)
+
+    def test_build_github_notification_accept_work_traditional_permissionless(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='permissionless'
+        fulfillment = BountyFulfillment.objects.create(
+              fulfiller_address='0x0000000000000000000000000000000000000000',
+              fulfiller_email='gitcointestdeveloper1@gmail.com',
+              fulfiller_github_username='samplegitcoindeveloper1',
+              bounty=self.bounty,
+              profile=Profile.objects.filter(handle='samplegitcoindeveloper1').first(),
+              accepted=True
+        )
+        self.bounty.fulfillments.add(fulfillment)
+        message = build_github_notification(self.bounty, 'work_done')
+        print(message)
+
+    def test_build_github_notification_accept_work_traditional_approval(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='permissionless'
+        message = build_github_notification(self.bounty, 'work_done')
+        print(message)
+
+    def test_build_github_notification_accept_work_cooperative(self):
+        self.bounty.bounty_type='traditional'
+        self.bounty.permission_type='approval'
+        message = build_github_notification(self.bounty, 'work_done')
+        print(message)
+
+    def test_build_github_notification_accept_work_contest(self):
+        self.bounty.bounty_type='cooperativve'
+        self.bounty.permission_type='permissionless'
+        message = build_github_notification(self.bounty, 'work_done')
+        print(message)
+
 
     def test_build_github_notification_killed_bounty(self):
         """Test the dashboard helper build_github_notification method with killed_bounty."""
