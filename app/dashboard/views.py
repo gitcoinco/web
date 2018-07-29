818576eb31d2ce0a306d0d648f5584dac3da7b0b
NedelescuVlad <nedelescu.vlad@gmail.com>
2018-07-29 14:53:51 +0100

    Use bounty.interested

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -1082,8 +1082,7 @@ def funder_dashboard(request):
         etherscan_link = '#'
 
         # TODO: Should we check if the user has received the bounty instead, similar to a Tip's received_on field?
-        # if bounty.interests.exists(): # TODO: interests is not defined, am I using it right?
-        if False:
+        if bounty.interested.exists():
             pending_or_claimed = 'Claimed'
         else:
             pending_or_claimed = 'Pending'
@@ -1154,8 +1153,7 @@ def funder_dashboard(request):
 
     all_bounties = []
     for bounty in funder_bounties:
-        if False:
-        # if bounty.interests.exists(): #  TODO: interests is not defined, am I using it right?
+        if bounty.interested.exists():
             pending_or_claimed = 'Claimed'
         else:
             # TODO: Is any bounty that's not claimed, in pending status?
