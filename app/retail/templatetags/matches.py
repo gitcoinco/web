70256d3488b0de44dde7d59113d64494e211bfc7
Aditya Anand <adianand@paypal.com>
2018-08-01 01:55:48 +0530

    isort fixes

--- app/retail/templatetags/matches.py
+++ app/retail/templatetags/matches.py
@@ -17,8 +17,9 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
-from django import template
 import re
+
+from django import template
 
 register = template.Library()
 
