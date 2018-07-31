f69df157d1041c2713872dfdd0c29012b355eb8b
octavioamu <octavioamuchastegui@gmail.com>
2018-07-31 18:55:49 -0300

    add mint kudos styles

---
+++ app/assets/v2/js/range-slider.js
@@ -0,0 +1,31 @@
+
+/**
+ * Simple plugin for range slider
+ *
+ * ex:
+ * <input type="range" min="1" max="100" value="50" data-rangeslider="demo">
+ * Value: <span id="demo"></span>
+ * 
+*/
+
+(function (){
+    this.createRanger = function(slider,id) {
+      var output = document.getElementById(id);
+      output.innerHTML = slider.value;
+      slider.oninput = function() {
+        output.innerHTML = this.value;
+      }
+      
+    }
+    
+    this.rangeslider = function () {
+      var elem = document.querySelectorAll('[data-rangeslider]')
+      for (var i = 0; i < elem.length; ++i) {
+        if (elem[i].dataset.rangeslider) {
+          new createRanger (elem[i],elem[i].dataset.rangeslider)  
+        }
+      }
+    }
+  }())
+  
+  new rangeslider()  
