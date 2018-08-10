3b70b20c6d6c9b3fe683cbd73a52ba8a66122ce3
Mark Beacom <markvbeacom@gmail.com>
2018-08-10 14:34:53 -0400

    Adjust existing auth pipeline module

--- app/app/settings.py
+++ app/app/settings.py
@@ -348,7 +348,7 @@ SOCIAL_AUTH_GITHUB_SCOPE = ['read:public_repo', 'read:user', 'user:email', ]
 SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.social_details', 'social_core.pipeline.social_auth.social_uid',
     'social_core.pipeline.social_auth.auth_allowed', 'social_core.pipeline.social_auth.social_user',
-    'social_core.pipeline.user.get_username', 'social_core.pipeline.user.create_user', 'app.pipeline.save_profile',
+    'social_core.pipeline.user.get_username', 'social_core.pipeline.user.create_user', 'app.auth_pipeline.save_profile',
     'social_core.pipeline.social_auth.associate_user', 'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
