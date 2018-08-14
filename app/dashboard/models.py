c863b893574928ad7a8bc31c3173f26bb004a0bb
Owocki <ksowocki@gmail.com>
2018-08-14 09:59:28 -0600

    correct conversions from token value to usdt value

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -577,15 +577,7 @@ class Bounty(SuperModel):
 
     @property
     def get_value_in_usdt_now(self):
-        decimals = 10**18
-        if self.token_name == 'USDT':
-            return float(self.value_in_token)
-        if self.token_name == 'DAI':
-            return float(self.value_in_token / 10**18)
-        try:
-            return round(float(convert_amount(self.value_in_token, self.token_name, 'USDT')) / decimals, 2)
-        except ConversionRateNotFoundError:
-            return None
+        return self.value_in_usdt_at_time(None)
 
     @property
     def get_value_in_usdt(self):
@@ -595,15 +587,22 @@ class Bounty(SuperModel):
 
     @property
     def value_in_usdt_then(self):
+        return self.value_in_usdt_at_time(self.web3_created)
+
+    def value_in_usdt_at_time(self, at_time):
         decimals = 10 ** 18
         if self.token_name == 'USDT':
             return float(self.value_in_token)
         if self.token_name == 'DAI':
             return float(self.value_in_token / 10 ** 18)
         try:
-            return round(float(convert_amount(self.value_in_token, self.token_name, 'USDT', self.web3_created)) / decimals, 2)
+            return round(float(convert_amount(self.value_true, self.token_name, 'USDT', at_time)), 2)
         except ConversionRateNotFoundError:
-            return None
+            try:
+                in_eth = round(float(convert_amount(self.value_true, self.token_name, 'ETH', at_time)), 2)
+                return round(float(convert_amount(in_eth, 'USDT', 'USDT', at_time)), 2)
+            except ConversionRateNotFoundError:
+                return None
 
     @property
     def token_value_in_usdt_now(self):
