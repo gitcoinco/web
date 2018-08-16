e4227bd040712ea53240f72ddfcae26650420ce1
Mark Beacom <markvbeacom@gmail.com>
2018-08-16 06:25:48 -0400

    Fix test_bounty since 8a5115eacabf8d53a128bbf885b4e021fdf13fe3
    Add token import!
    
    Make stickler happy :D
    
    Adjust value_true since it's a field on Bounty and not a prop like Tip...

--- app/dashboard/tests/test_dashboard_models.py
+++ app/dashboard/tests/test_dashboard_models.py
@@ -23,7 +23,7 @@ from django.conf import settings
 
 import pytz
 from dashboard.models import Bounty, BountyFulfillment, Interest, Profile, Tip, Tool, ToolVote
-from economy.models import ConversionRate
+from economy.models import ConversionRate, Token
 from test_plus.test import TestCase
 
 
@@ -43,6 +43,11 @@ class DashboardModelsTest(TestCase):
     @staticmethod
     def test_bounty():
         """Test the dashboard Bounty model."""
+        Token.objects.create(
+            priority=999,
+            symbol='ETH',
+            address='0x0000000000000000000000000000000000000000'
+        )
         fulfiller_profile = Profile.objects.create(
             data={},
             handle='fred',
@@ -65,6 +70,7 @@ class DashboardModelsTest(TestCase):
             bounty_type='Feature',
             experience_level='Intermediate',
             raw_data={},
+            value_true=3,  # shouldn't this be determined based on the token value and converted like Tip?
         )
         bounty_fulfillment = BountyFulfillment.objects.create(
             fulfiller_address='0x0000000000000000000000000000000000000000',
@@ -74,7 +80,7 @@ class DashboardModelsTest(TestCase):
             bounty=bounty,
             profile=fulfiller_profile,
         )
-        assert str(bounty) == 'foo 3 ETH 2008-10-31 00:00:00+00:00'
+        assert str(bounty) == '1: foo, 3 ETH @ 9 years, 9 months ago'
         assert bounty.url == f'{settings.BASE_URL}issue/gitcoinco/web/11/{bounty.standard_bounties_id}'
         assert bounty.title_or_desc == 'foo'
         assert bounty.issue_description_text == 'hello world'
@@ -84,7 +90,7 @@ class DashboardModelsTest(TestCase):
         assert bounty.is_funder('fred') is False
         assert bounty.is_funder('flintstone') is True
         assert bounty.status == 'done'
-        assert bounty.value_true == 0
+        assert bounty.value_true == 3
         assert bounty.value_in_eth == 3
         assert bounty.value_in_usdt_now == 0
         assert 'ago 5 Feature Intermediate' in bounty.desc
