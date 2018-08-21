db48e03edef68726cc9fea869bc08c7e6faef206
Owocki <ksowocki@gmail.com>
2018-08-20 22:07:31 -0600

    reenables contrib specific LPs

--- app/retail/views.py
+++ app/retail/views.py
@@ -215,7 +215,7 @@ def contributor_landing(request, tech_stack):
 
     available_bounties_count = open_bounties().count()
     available_bounties_worth = amount_usdt_open_work()
-    tech_stack = ''
+    # tech_stack = '' #uncomment this if you wish to disable contributor specific LPs
     context = {
         'activities': get_activities(tech_stack),
         'title': tech_stack.title() + str(_(" Open Source Opportunities")) if tech_stack else "Open Source Opportunities",
