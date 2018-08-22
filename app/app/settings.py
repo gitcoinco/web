6e8d2c5ec0a831f0664b8de383f9d995fb608fcf
Mark Beacom <markvbeacom@gmail.com>
2018-08-22 10:25:09 -0400

    enable psutil hc

--- app/app/settings.py
+++ app/app/settings.py
@@ -76,7 +76,7 @@ INSTALLED_APPS = [
     'health_check.db',
     'health_check.cache',
     'health_check.storage',
-    # 'health_check.contrib.psutil',
+    'health_check.contrib.psutil',
     'health_check.contrib.s3boto3_storage',
     'app',
     'avatar',
