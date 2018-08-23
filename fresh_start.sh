901a05fb72fe1bf3c17d0f00df17fe406075af7d
Jason Haas <jasonrhaas@gmail.com>
2018-08-23 12:17:52 -0500

    Update fresh_start.sh script to use standard directories

--- fresh_start.sh
+++ fresh_start.sh
@@ -1,12 +1,11 @@
-#!/bin/zsh
+#!/bin/bash
 
 docker-compose down
-docker volume rm gitcoin-web-priv_pgdata
+docker volume rm kudos_pgdata
 docker-compose up -d
 echo "sleeping for 10 seconds..."
 sleep 10
 cd ../gitcoin-erc721
 truffle migrate --reset
-cd ../gitcoin-web-priv
+cd ../kudos
 docker-compose exec web bash -c 'cd app && python manage.py mint_all_kudos localhost /code/app/kudos/kudos.yaml'
-# docker-compose exec web bash -c 'cd app && python manage.py createsuperuser --help'
