c4a62a66691f4d585b62cf16dbec1ced0fb26fd2
Owocki <ksowocki@gmail.com>
2018-08-23 09:52:46 -0600

    backup

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -23,8 +23,8 @@ MONTH=`date +"%m"`
 DAY=`date +"%d"`
 YEAR=`date +"%Y"`
 PG_DUMP="/usr/lib/postgresql/9.6/bin/pg_dump"
-export PGPASSWORD=$(cat app/app/.env | grep "DATABASE_URL" | awk -F "=" '{print $2}' | awk -F "@" '{print $2}' | awk -F ":" '{print $1}')
-export HOST=$(cat app/app/.env | grep "DATABASE_URL" | awk '{print $2}' | sed "s/'//g" | sed "s/,//g")
+export PGPASSWORD=$(cat app/app/.env | grep "DATABASE_URL" | awk -F "=" '{print $2}' | awk -F "@" '{print $1}' | awk -F ":" '{print $3}')
+export HOST=$(cat app/app/.env | grep "DATABASE_URL" | awk -F "=" '{print $2}' | awk -F "@" '{print $2}' | awk -F ":" '{print $1}')
 IS_PROD=$(cat app/app/.env | grep ENV | grep prod | wc -l)
 if [ "$IS_PROD" -eq "1" ]; then
     $PG_DUMP gitcoin -U gitcoin -h $HOST | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/$BACKUPSTR-$(hostname).sql
