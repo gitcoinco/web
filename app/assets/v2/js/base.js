a08649158f0415511f2cb3929c554664c3b97149
Saptak Sengupta <saptak013@gmail.com>
2018-08-23 23:05:29 +0530

    Add overflow in header if minihero not present

--- app/assets/v2/js/base.js
+++ app/assets/v2/js/base.js
@@ -16,6 +16,10 @@ $(document).ready(function() {
 
   force_no_www();
 
+  if (!$('.header > .minihero')) {
+    $('.header').css('overflow', 'visible');
+  }
+
   $('.nav-link.dropdown-toggle').click(function(e) {
     e.preventDefault();
     var parent = $(this).parents('.nav-item');
