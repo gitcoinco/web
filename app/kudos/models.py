1ba8c475af847f6889511d2863a8f4e697dde4a6
Jason Haas <jasonrhaas@gmail.com>
2018-08-26 11:24:38 -0500

    Fix models and migrations

--- app/kudos/models.py
+++ app/kudos/models.py
@@ -36,44 +36,6 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class Email(SuperModel):
-
-    web3_type = models.CharField(max_length=50, default='v3')
-    emails = JSONField()
-    url = models.CharField(max_length=255, default='', blank=True)
-    tokenName = models.CharField(max_length=255)
-    tokenAddress = models.CharField(max_length=255)
-    amount = models.DecimalField(default=1, decimal_places=4, max_digits=50)
-    comments_priv = models.TextField(default='', blank=True)
-    comments_public = models.TextField(default='', blank=True)
-    ip = models.CharField(max_length=50)
-    expires_date = models.DateTimeField()
-    github_url = models.URLField(null=True, blank=True)
-    from_name = models.CharField(max_length=255, default='', blank=True)
-    from_email = models.CharField(max_length=255, default='', blank=True)
-    from_username = models.CharField(max_length=255, default='', blank=True)
-    username = models.CharField(max_length=255, default='')  # to username
-    network = models.CharField(max_length=255, default='')
-    txid = models.CharField(max_length=255, default='')
-    receive_txid = models.CharField(max_length=255, default='', blank=True)
-    received_on = models.DateTimeField(null=True, blank=True)
-    from_address = models.CharField(max_length=255, default='', blank=True)
-    receive_address = models.CharField(max_length=255, default='', blank=True)
-    recipient_profile = models.ForeignKey(
-        'dashboard.Profile', related_name='received_kudos', on_delete=models.SET_NULL, null=True, blank=True
-    )
-    sender_profile = models.ForeignKey(
-        'dashboard.Profile', related_name='sent_kudos', on_delete=models.SET_NULL, null=True, blank=True
-    )
-    metadata = JSONField(default={}, blank=True)
-    is_for_bounty_fulfiller = models.BooleanField(
-        default=False,
-        help_text='If this option is chosen, this tip will be automatically paid to the bounty'
-                  ' fulfiller, not self.usernameusername.',
-    )
-
-
-
 class Token(SuperModel):
     # Kudos specific fields -- lines up with Kudos contractpass
     name = models.CharField(max_length=255)
@@ -120,6 +82,45 @@ class Token(SuperModel):
         return self
 
 
+class Email(SuperModel):
+
+    web3_type = models.CharField(max_length=50, default='v3')
+    emails = JSONField()
+    url = models.CharField(max_length=255, default='', blank=True)
+    # tokenName is a reference to the Kudos Token that is slated to be cloned.
+    tokenName = models.OneToOneField(Token, on_delete=models.SET_NULL, null=True)
+    tokenAddress = models.CharField(max_length=255)
+    amount = models.DecimalField(default=1, decimal_places=4, max_digits=50)
+    comments_priv = models.TextField(default='', blank=True)
+    comments_public = models.TextField(default='', blank=True)
+    ip = models.CharField(max_length=50)
+    expires_date = models.DateTimeField()
+    github_url = models.URLField(null=True, blank=True)
+    from_name = models.CharField(max_length=255, default='', blank=True)
+    from_email = models.CharField(max_length=255, default='', blank=True)
+    from_username = models.CharField(max_length=255, default='', blank=True)
+    username = models.CharField(max_length=255, default='')  # to username
+    network = models.CharField(max_length=255, default='')
+    txid = models.CharField(max_length=255, default='')
+    receive_txid = models.CharField(max_length=255, default='', blank=True)
+    received_on = models.DateTimeField(null=True, blank=True)
+    from_address = models.CharField(max_length=255, default='', blank=True)
+    receive_address = models.CharField(max_length=255, default='', blank=True)
+    recipient_profile = models.ForeignKey(
+        'dashboard.Profile', related_name='received_kudos', on_delete=models.SET_NULL, null=True, blank=True
+    )
+    sender_profile = models.ForeignKey(
+        'dashboard.Profile', related_name='sent_kudos', on_delete=models.SET_NULL, null=True, blank=True
+    )
+    metadata = JSONField(default={}, blank=True)
+    is_for_bounty_fulfiller = models.BooleanField(
+        default=False,
+        help_text='If this option is chosen, this tip will be automatically paid to the bounty'
+                  ' fulfiller, not self.usernameusername.',
+    )
+
+
+
 class Wallet(SuperModel):
     address = models.CharField(max_length=255, unique=True)
     profile = models.ForeignKey('dashboard.Profile', on_delete=models.SET_NULL, null=True)
@@ -128,82 +129,3 @@ class Wallet(SuperModel):
         self.address = to_checksum_address(self.address)
         super().save(*args, **kwargs)
 
-# class KudosToken(SuperModel):
-
-#     # Kudos specific fields -- lines up with Kudos contract
-#     name = models.CharField(max_length=255)
-#     description = models.CharField(max_length=255)
-#     rarity = models.IntegerField()
-#     price = models.IntegerField()
-#     num_clones_allowed = models.IntegerField(null=True, blank=True)
-#     num_clones_in_wild = models.IntegerField(null=True, blank=True)
-
-    # Gitcoin App only fields
-    # lister = models.CharField(max_length=255)
-
-    # web3_type = models.CharField(max_length=50, default='v2')
-    # from_name = models.CharField(max_length=255, default='', blank=True)
-    # from_email = models.CharField(max_length=255, default='', blank=True)
-    # # from_username = models.CharField(max_length=255, default='', blank=True)
-    # username = models.CharField(max_length=255, default='')  # to username
-    # network = models.CharField(max_length=255, default='')
-    # txid = models.CharField(max_length=255, default='')
-    # receive_txid = models.CharField(max_length=255, default='', blank=True)
-    # received_on = models.DateTimeField(null=True, blank=True)
-    # from_address = models.CharField(max_length=255, default='', blank=True)
-    # receive_address = models.CharField(max_length=255, default='', blank=True)
-    # # recipient_profile = models.ForeignKey(
-    # #     'dashboard.Profile', related_name='received_tips', on_delete=models.SET_NULL, null=True, blank=True
-    # # )
-    # # sender_profile = models.ForeignKey(
-    # #     'dashboard.Profile', related_name='sent_tips', on_delete=models.SET_NULL, null=True, blank=True
-    # # )
-    # metadata = JSONField(default={}, blank=True)
-
-    # def __str__(self):
-    #     """Return the string representation for a Kudos."""
-    #     if self.web3_type == 'yge':
-    #         return f"({self.network}) - {self.status}{' ORPHAN' if not self.emails else ''} " \
-    #            f"{self.amount} {self.tokenName} to {self.username} from {self.from_name or 'NA'}, " \
-    #            f"created: {naturalday(self.created_on)}, expires: {naturalday(self.expires_date)}"
-    #     status = 'sent' if self.txid else 'not sent'
-    #     status = status if not self.receive_txid else 'received'
-    #     return f"{status} {self.amount} {self.tokenName} to {self.username} from {self.from_name or 'NA'}"
-
-    # @property
-    # def receive_url(self):
-    #     if self.web3_type == 'yge':
-    #         return self.url
-
-    #     pk = self.metadata['priv_key']
-    #     txid = self.txid
-    #     network = self.network
-    #     return f"{settings.BASE_URL}tip/receive/v2/{pk}/{txid}/{network}"
-
-    # @property
-    # def status(self):
-    #     if self.receive_txid:
-    #         return "RECEIVED"
-    #     return "PENDING"
-
-    # @property
-    # def github_org_name(self):
-    #     try:
-    #         return org_name(self.github_url)
-    #     except Exception:
-    #         return None
-
-    # def is_notification_eligible(self, var_to_check=True):
-    #     """Determine whether or not a notification is eligible for transmission outside of production.
-
-    #     Returns:
-    #         bool: Whether or not the Tip is eligible for outbound notifications.
-
-    #     """
-    #     if not var_to_check or self.network != settings.ENABLE_NOTIFICATIONS_ON_NETWORK:
-    #         return False
-    #     if self.network == 'mainnet' and (settings.DEBUG or settings.ENV != 'prod'):
-    #         return False
-    #     if (settings.DEBUG or settings.ENV != 'prod') and settings.GITHUB_API_USER != self.github_org_name:
-    #         return False
-    #     return True
