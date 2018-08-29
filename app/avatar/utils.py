42346566171224e150a8e61a2f523e60e844b6fe
ajayns <ajay.ns@outlook.com>
2018-08-30 00:47:11 +0530

    new asset spacesuit integration

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -68,7 +68,7 @@ def get_avatar_context():
         }, {
             'name': 'Clothing',
             'title': 'Pick your clothing',
-            'options': ('cardigan', 'hoodie', 'knitsweater', 'plaid', 'shirt', 'shirtsweater')
+            'options': ('cardigan', 'hoodie', 'knitsweater', 'plaid', 'shirt', 'shirtsweater', 'spacesuit')
         }, {
             'name': 'Hair Style',
             'title': 'Pick a hairstyle',
