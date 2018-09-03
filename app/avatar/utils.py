6cd51626aa28e1185bdc920a921617b5dab5b0e0
Aditya Anand <aditya.anandmc@gmail.com>
2018-09-03 20:32:15 +0530

    avatar: fix

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -83,10 +83,10 @@ def get_avatar_context():
         }, {
             'name': 'Accessories',
             'title': 'Pick your accessories',
-            'options': (['Glasses-0'], ['Glasses-1'], ['Glasses-2'], ['Glasses-3'], ['Glasses-4'], [
-                'HatShort-backwardscap'
-            ], ['HatShort-ballcap'], ['HatShort-headphones'], ['HatShort-shortbeanie'], ['HatShort-tallbeanie'],
-                        ['Earring-0'], ['Earring-1'], ['EarringBack-2', 'Earring-2'], ['Earring-3'], ['Earring-4'], ['Helmet-space'])
+            'options': (['Glasses-0'], ['Glasses-1'], ['Glasses-2'], ['Glasses-3'], ['Glasses-4'],
+                        ['HatShort-backwardscap'], ['HatShort-ballcap'], ['HatShort-headphones'],
+                        ['HatShort-shortbeanie'], ['HatShort-tallbeanie'], ['HatShort-helmet'], ['Earring-0'],
+                        ['Earring-1'], ['EarringBack-2', 'Earring-2'], ['Earring-3'], ['Earring-4'])
         }, {
             'name': 'Background',
             'title': 'Pick a background color',
