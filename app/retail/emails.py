bb311737ab5d921287f837d21ece149d4b03edd8
Mark Beacom <markvbeacom@gmail.com>
2018-09-05 13:07:23 -0400

    Adjust leaderboardrank query for line length

--- app/retail/emails.py
+++ app/retail/emails.py
@@ -619,7 +619,8 @@ Back to BUIDLing,
         },
     }
     for key, __ in leaderboard.items():
-        leaderboard[key]['items'] = LeaderboardRank.objects.active().filter(leaderboard=key).order_by('rank')[0:num_leadboard_items]
+        leaderboard[key]['items'] = LeaderboardRank.objects.active() \
+            .filter(leaderboard=key).order_by('rank')[0:num_leadboard_items]
 
     bounties = []
     for nb in bounties_spec:
