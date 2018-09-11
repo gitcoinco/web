dacf0c5527ec1a7a3e4213295686af4dc87be0b4
Jason Haas <jasonrhaas@gmail.com>
2018-09-10 20:37:07 -0500

    Revert "Fix initial profile creation bug"
    This reverts commit 23fbcb5a729f3a0292fb9f76caa4264cc547e369.

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1569,10 +1569,7 @@ class Profile(SuperModel):
     @property
     def github_created_on(self):
         from datetime import datetime
-        try:
-            created_on = datetime.strptime(self.data['created_at'], '%Y-%m-%dT%H:%M:%SZ')
-        except KeyError:
-            created_on = datetime.datetime.now()
+        created_on = datetime.strptime(self.data['created_at'], '%Y-%m-%dT%H:%M:%SZ')
         return created_on.replace(tzinfo=pytz.UTC)
 
     @property
