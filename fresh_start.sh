55c27e526c97edebc9184f2158a090ffbf6504ff
Jason Haas <jasonrhaas@gmail.com>
2018-09-11 15:38:56 -0500

    Update fresh_start to take a network name as $1

--- fresh_start.sh
+++ fresh_start.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
+#!/bin/zsh
+
+# If no network is specified, use localhost
+if [ -z $1 ]
+then
+	NETWORK=localhost
+else
+	NETWORK=$1
+fi
 
 docker-compose down
 docker volume rm kudos_pgdata
@@ -8,4 +16,4 @@ sleep 10
 cd ../gitcoin-erc721
 truffle migrate --reset
 cd ../kudos
-docker-compose exec web bash -c 'cd app && python manage.py mint_all_kudos localhost /code/app/kudos/kudos.yaml'
+docker-compose exec web bash -c "cd app && python manage.py mint_all_kudos ${NETWORK} /code/app/kudos/kudos.yaml"
