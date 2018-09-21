83e75f1febd2a42038e5d359de9ab963795cf431
Mark Beacom <markvbeacom@gmail.com>
2018-09-21 02:39:20 -0400

    Add VSCode remote debugging handling and documentation

--- bin/docker-command.bash
+++ bin/docker-command.bash
@@ -2,14 +2,13 @@
 
 # Settings
 # Web
-WEB_WORKER=${WEB_WORKER_TYPE:-'runserver_plus'}
-WEB_INTERFACE=${WEB_INTERFACE:-'0.0.0.0'}
-WEB_PORT=${WEB_PORT:-'8000'}
+WEB_WORKER=${WEB_WORKER_TYPE:-runserver_plus}
+
 # General / Overrides
 FORCE_PROVISION=${FORCE_PROVISION:-'off'}
 FORCE_GET_PRICES=${FORCE_GET_PRICES:-'off'}
 
-cd app
+cd app || exit 1
 # Check whether or not the .env file exists. If not, create it.
 if [ ! -f app/.env ]; then
     cp app/local.env app/.env
@@ -20,6 +19,18 @@ if  [ ! -z "${INSTALL_REQS}" ]; then
     echo "Installing Python packages..."
     pip3 install -r ../requirements/test.txt
     echo "Python package installation completed!"
+fi
+
+WEB_OPTS="${WEB_WORKER} ${WEB_INTERFACE:-0.0.0.0}:${WEB_PORT:-8000}"
+
+if [ "$VSCODE_DEBUGGER_ENABLED" = "on" ]; then
+    pip install ptvsd
+    WEB_OPTS="${WEB_OPTS} --nothreading"
+    echo "VSCode remote debugger enabled! This has disabled threading!"
+fi
+
+if [ "$WEB_WORKER" = "runserver_plus" ]; then
+    WEB_OPTS="${WEB_OPTS} --extra-file /code/app/app/.env --nopin"
 fi
 
 # Provision the Django test environment.
@@ -37,4 +48,4 @@ if [ "$FORCE_GET_PRICES" = "on" ]; then
     python manage.py get_prices
 fi
 
-python manage.py "$WEB_WORKER" "$WEB_INTERFACE":"$WEB_PORT" --extra-file /code/app/app/.env --nopin
+exec python manage.py $WEB_OPTS
