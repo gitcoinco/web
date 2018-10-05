6dc3c592834ac7df382d15dee8cd78f27195fa46
CryptoMental <cryptomental@users.noreply.github.com>
2018-10-05 16:51:22 +0200

    WIP: Feature/increase dashboard tests coverage (#858)
    * dashboard: Tests cleanup.
    
    * fixed timezone warnings
    * added @staticmethod decorators where needed
    * introduced consistent test naming scheme, all classes deriving from TestCase end with Test
    * PEP8 line too long warnings fixed by splitting to multiline strings
    * added __init__.py to let pytest find all tests when running `docker-compose exec web pytest app/dashboard/tests/*`
    
    Refs: #607
    
    * test_dashboard_models: Add test_exclude_bounty_by_status.
    
    Verify that bounties can be excluded by status.
    
    Refs: #607
    
    * test_dashboard_models: Add test_relative_bounty_url_with_malformed_issue_number test.
    
    The test verifies relative bounty url when malformed issue number is provided.
    
    Refs: #607
    
    * test_dashboard_models: Add test_get_natural_value_if_bad_token_address_provided test.
    
    Natural value should be zero when bad token address is provided.
    
    Issue: #607
    
    * test_dashboard_models: Add bounty submit after expiration date tests.
    
    Add tests:
    
    * test_can_submit_legacy_bounty_after_expiration_date
    * test_cannot_submit_standard_bounty_after_expiration_date
    * test_can_submit_standard_bounty_after_expiration_date_if_deadline_extended
    
    Refs: #607
    
    * test_dashboard_models: Add test_title_or_desc test.
    
    Test test_title_or_desc bounty property. Verify it returns bounty title if defined.
    
    Refs: #607
    
    * test_dashboard_models: title_or_desc shall return GitHub URL if no title.
    
    Refs: #607
    
    * test_dashboard_models: test github_issue_number.
    
    Refs: #607
    
    * test_dashboard_models: test github_org_name.
    
    Refs: #607
    
    * test_dashboard_models: test github_repo_name.
    
    Refs: #607
    
    * test_dashboard_models: test bounty status.
    
    Refs: #607
    
    * test_dashboard_embed: Add embed tests.
    
    * test_wrap_text
    * test_summarize_bounties
    * test_stat
    * test_embed
    
    Issue: #607
    
    * test_dashboard_embed: Add bounty to embed test.
    
    Refs: #607
    
    * test_dashboard_embed: Add avatar test.
    
    Refs: #607
    
    * test_dashboard_embed: Travis vs rate limiting.
    
    GitHub API is restricted https://developer.github.com/v3/#rate-limiting
    Test will also pass in that scenario. Other than that later we can mock the response.
    
    Refs: #607
    
    * test_notifications: Test craiglist post.
    
    Refs: #607
    
    * test_dashboard_utils: Add get_bounty and bounty contract tests.
    
    * test_get_bounty
    * test_get_bounty_contract
    
    Refs: #607
    
    * test_dashboard_utils: add get_bounty_id tests.
    
    * test_get_bounty_id_from_db
    * test_get_bounty_id_from_web3
    
    Refs: #607
    
    * Update test_dashboard_embed.py
    
    * Line length
    
    * Remove stat
    
    * Update test_dashboard_models.py
    
    * Remove avatar view test and fix indentation throughout new tests
    
    * Resolve travis failures
    
    * Remove some tests we shouldn't be performing
    
    * Remove craigslist test and unused imports

--- app/dashboard/tests/test_dashboard_utils.py
+++ app/dashboard/tests/test_dashboard_utils.py
@@ -17,7 +17,9 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
-from dashboard.utils import clean_bounty_url, get_ordinal_repr, get_web3, humanize_event_name
+from dashboard.utils import (
+    clean_bounty_url, get_bounty, get_ordinal_repr, get_web3, getBountyContract, humanize_event_name,
+)
 from test_plus.test import TestCase
 from web3.main import Web3
 from web3.providers.rpc import HTTPProvider
@@ -36,6 +38,14 @@ class DashboardUtilsTest(TestCase):
             assert len(web3_provider.providers) == 1
             assert isinstance(web3_provider.providers[0], HTTPProvider)
             assert web3_provider.providers[0].endpoint_uri == f'https://{network}.infura.io'
+
+    @staticmethod
+    def test_get_bounty_contract():
+        assert getBountyContract('mainnet').address == "0x2af47a65da8CD66729b4209C22017d6A5C2d2400"
+
+    @staticmethod
+    def test_get_bounty():
+        assert get_bounty(100, 'rinkeby')['contract_deadline'] == 1515699751
 
     @staticmethod
     def test_get_ordinal_repr():
