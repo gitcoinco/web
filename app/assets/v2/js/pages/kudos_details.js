d6cbe6e8af56566e95af60616625f6b15ec7d68c
Owocki <ksowocki@gmail.com>
2018-10-10 14:35:50 -0600

    refactor

--- app/assets/v2/js/pages/kudos_details.js
+++ app/assets/v2/js/pages/kudos_details.js
@@ -20,6 +20,8 @@ var getKudosById = function(kudosId) {
 
 
 $(document).ready(function() {
+  kudos_levitate();
+  console.log('here');
   let kudosId = window.location.pathname.split('/')[2];
   // let kudosId = $('#kudosId').text()
   // let kudosName = $('#kudosName').text()
@@ -45,6 +47,44 @@ $(document).ready(function() {
   })
 
 })
+
+
+var kudos_levitate = function(){
+  var sapphire = document.getElementById('kudos-image');
+  var shadow = document.getElementById('shadow');
+  var sparkle1 = document.getElementById('sparkle1');
+  var sparkle2 = document.getElementById('sparkle2');
+  var sparkle3 = document.getElementById('sparkle3');
+  var sparkle4 = document.getElementById('sparkle4');
+  var twinkle1 = document.querySelectorAll('#sparkle1 > .st6, #sparkle1 > .st7');
+  var twinkle2 = document.querySelectorAll('#sparkle2 > .st6, #sparkle2 > .st7');
+  var twinkle3 = document.querySelectorAll('#sparkle3 > .st6, #sparkle3 > .st7');
+  var twinkle4 = document.querySelectorAll('#sparkle4 > .st6, #sparkle4 > .st7');
+  var sapphireTl = new TimelineLite();
+  var sparkleTl = new TimelineLite({
+    repeat: -1,
+    yoyo: true,
+    onComplete: function() {
+      this.restart();
+    }   
+  });
+
+  sapphireTl.from(sapphire, 1, { y: '10px', ease: Power1.easeInOut, repeat: -1, yoyo: true }, 'sapph')
+          .fromTo(shadow, 1, { scale: 1, transformOrigin: 'center center', ease: Power1.easeInOut, yoyo: true, repeat: -1 }, { scale: 0.5, transformOrigin: 'center center', ease: Power1.easeInOut, yoyo: true, repeat: -1 },  'sapph')
+     
+
+
+  sparkleTl.to(twinkle1, 1, { scale: 0, transformOrigin: 'center center', ease: Power1.easeOut }, 'sparkle')
+           .to(twinkle2, 1, { scale: 0, transformOrigin: 'center center', ease: Power1.easeOut }, 'sparkle+=0.3')
+           .to(twinkle3, 1, { scale: 0, transformOrigin: 'center center', ease: Power1.easeOut }, 'sparkle+=0.6')
+           .to(twinkle4, 1, { scale: 0, transformOrigin: 'center center', ease: Power1.easeOut }, 'sparkle+=0.9')
+           .to(twinkle1, 1, { scale: 1, transformOrigin: 'center center', ease: Elastic.easeOut }, 'sparkleshow')
+           .to(twinkle2, 1, { scale: 1, transformOrigin: 'center center', ease: Elastic.easeOut }, 'sparkleshow+=0.3')
+           .to(twinkle3, 1, { scale: 1, transformOrigin: 'center center', ease: Elastic.easeOut }, 'sparkleshow+=0.6')
+           .to(twinkle4, 1, { scale: 1, transformOrigin: 'center center', ease: Elastic.easeOut }, 'sparkleshow+=0.9')
+
+  sparkleTl.timeScale(2);
+}
 
 // $('#getKudos').click(function() {
 
