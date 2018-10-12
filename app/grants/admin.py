2808a8456e39792eca129e1c0d2c7feb4df94e5c
Mark Beacom <markvbeacom@gmail.com>
2018-10-12 19:06:02 -0400

    Remove unused profile import until we add inline profile for admin

--- app/grants/admin.py
+++ app/grants/admin.py
@@ -20,7 +20,6 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 from django.contrib import admin
 from django.utils.safestring import mark_safe
 
-from dashboard.models import Profile
 from grants.models import Contribution, Grant, Subscription
 
 
