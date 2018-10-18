0bb5e58e0400ee4411767a05a0f768754be39316
Kevin Seagraves <k.s.seagraves@gmail.com>
2018-10-18 10:44:00 -0600

    remove new.js network log

--- app/assets/v2/js/grants/new.js
+++ app/assets/v2/js/grants/new.js
@@ -69,8 +69,6 @@ $(document).ready(function() {
 
       web3.eth.getAccounts(function(err, accounts) {
         web3.eth.net.getId(function(err, network) {
-          console.log(network);
-          network = "testrpc"
           $('#network').val(network);
           SubscriptionContract.deploy({
             data: compiledSubscription.bytecode,
