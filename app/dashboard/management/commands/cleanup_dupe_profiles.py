db92a16d0b849e0b4bd5137ed2f4e463a441daca
Owocki <ksowocki@gmail.com>
2018-10-19 14:11:42 -0600

    code review

--- app/dashboard/management/commands/cleanup_dupe_profiles.py
+++ app/dashboard/management/commands/cleanup_dupe_profiles.py
@@ -25,6 +25,8 @@ from dashboard.models import Profile
 def combine_profiles(p1, p2):
     # p2 is the delete profile, p1 is the save profile
     # switch if p2 has the user
+    # TODO: refactor to use https://github.com/mighty-justice/django-super-deduper 
+    # instead
     if p2.user:
         p1, p2 = p2, p1
 
@@ -40,6 +42,34 @@ def combine_profiles(p1, p2):
     p1.trust_profile = any([p1.trust_profile, p2.trust_profile])
     p1.hide_profile = any([p1.hide_profile, p2.hide_profile])
     p1.suppress_leaderboard = any([p1.suppress_leaderboard, p2.suppress_leaderboard])
+    # tips, bounties, fulfillments, and interests , activities, actions
+    for obj in p2.received_tips.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.sent_tips.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.bounties_funded.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.interested.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.fulfilled.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.activities.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.actions.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.token_approvals.all():
+        obj.profile = p1
+        obj.save()
+    for obj in p2.votes.all():
+        obj.profile = p1
+        obj.save()
     p1.save()
     p2.delete()
 
@@ -53,7 +83,7 @@ class Command(BaseCommand):
         dupes = Profile.objects.values('handle').annotate(Count('handle')).filter(handle__count__gt=1)
 
         for dupe in dupes:
-            handle = dupe.handle
+            handle = dupe['handle']
             profiles = Profile.objects.filter(handle=handle)
-            print(f"combining {profiles[0].pk} and {profiles[1].pk})")
+            print(f"combining {profiles[0].pk} and {profiles[1].pk}")
             combine_profiles(profiles[0], profiles[1])
