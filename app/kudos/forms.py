b65bac087485f43a84050fa67ccd26a7a5377756
Jason Haas <jasonrhaas@gmail.com>
2018-10-19 19:29:12 -0500

    Add gettext to label

--- app/kudos/forms.py
+++ app/kudos/forms.py
@@ -1,5 +1,25 @@
+# -*- coding: utf-8 -*-
+"""Define Forms.
+
+Copyright (C) 2018 Gitcoin Core
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
 from django import forms
+from django.utils.translation import gettext as _
 
 
 class KudosSearchForm(forms.Form):
-    kudos_search = forms.CharField(label='Kudos Search', max_length=100)
+    kudos_search = forms.CharField(label=_('Kudos Search'), max_length=100)
