9895feaa0b8114b0e942aee2b54220055d80242b
Jason Haas <jasonrhaas@gmail.com>
2018-10-22 17:06:52 -0500

    Update sync_kudos log file name

--- scripts/sync_kudos_local.bash
+++ scripts/sync_kudos_local.bash
@@ -1,4 +1,4 @@
 while true; do
-    python manage.py sync_kudos localhost filter -s earliest >> /var/log/kudos_sync.log 2>&1
+    python manage.py sync_kudos localhost filter -s earliest >> /var/log/sync_kudos.log 2>&1
     sleep 60
 done
