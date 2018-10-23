d983e83f0e31daad95c6f604349c3f761167472c
Jason Haas <jasonrhaas@gmail.com>
2018-10-23 15:03:50 -0500

    Add helper to resolve a kudos token

--- app/kudos/helpers.py
+++ app/kudos/helpers.py
@@ -19,9 +19,50 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 import logging
 
-from .models import Wallet
+from .models import Wallet, Contract, Token
+from django.conf import settings
+from eth_utils import is_address, to_checksum_address
 
 logger = logging.getLogger(__name__)
+
+
+def get_token(token_id, network, address):
+    """Helper to find the kudos.Token primary key, given the contract address and token_id fields.
+    This function was created so that we can find the kudos token pk, only know the contract and
+    token_id from the blockchain.  This is useful when linking to gitcoin from outside sites such
+    as Open Sea.
+
+    Args:
+        token_id (int): The token_id field in the database (from blockchain)
+        network (str): The ethereum network
+        address (str): Contract address for this token (from blockchain)
+
+    Returns:
+        obj or None: Return Django object if found, else None.
+
+
+    """
+    if address == 'latest':
+        contract = Contract.objects.get(
+            is_latest=True,
+            network=network
+        )
+    else:
+        if not is_address(address):
+            return None
+        contract = Contract.objects.get(
+            address=address,
+            network=network
+        )
+
+    try:
+        token = Token.objects.get(
+            contract=contract,
+            token_id=token_id
+        )
+    except Token.DoesNotExist:
+        return None
+    return token
 
 
 def reconcile_kudos_preferred_wallet(profile):
