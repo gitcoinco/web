fe53eaf3b477f23257b8dca92cc9850ebf5a3c54
octavioamu <octavioamuchastegui@gmail.com>
2018-10-24 22:47:01 -0300

    rewrites marketplace search

--- app/assets/v2/js/pages/kudos-search.js
+++ app/assets/v2/js/pages/kudos-search.js
@@ -1,65 +1,34 @@
+/* eslint-disable no-div-regex */
+
 $(document).ready(function() {
-  var queryParam = getParameterByName('q');
+  var queryParam = getQueryParams(document.location.search);
+  var clearBtn = $('.clear-search');
 
-  $('#kudos-search').val(queryParam);
-  checkSearch();
-});
-var searchInput = $('#kudos-search');
-var clearBtn = $('.clear-search');
-
-searchInput.change(function() {
-  checkSearch();
-});
-function checkSearch() {
-  console.log('test');
-  if (searchInput.val() === '') {
-    clearBtn.addClass('hidden');
-  } else {
+  if (queryParam && queryParam.q) {
+    $('#kudos-search').val(queryParam.q);
     clearBtn.removeClass('hidden');
   }
-}
 
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-  var results = regex.exec(location.search);
-
-  return results == null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-}
-
-
-function getQueryParams(qs) {
-  qs = qs.split('+').join(' ');
-
-  var params = {};
-  var re = /[?&]?([^=]+)=([^&]*)/g;
-  var tokens = re.exec(qs);
-
-  while (tokens) {
-    params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
-  }
-
-  return params;
-}
-
-$(document).ready(function() {
-
-  var query = getQueryParams(document.location.search);
-
-  if (query.order_by) {
-    console.log(query.order_by);
-    $('#sort_order').val(query.order_by);
+  if (queryParam && queryParam.order_by) {
+    $('#sort_order').val(queryParam.order_by);
   }
 
   $('#sort_order').change(function() {
-    var query = getQueryParams(document.location.search);
-    // window.location.href = window.location.href + '?order_by=' + $(this).val();
-
-    if (query.q) {
-      window.location.search = '?q=' + query.q + '&order_by=' + $(this).val();
+    if (queryParam && queryParam.q) {
+      window.location.search = '?q=' + queryParam.q + '&order_by=' + $(this).val();
     } else {
       window.location.search = '?order_by=' + $(this).val();
     }
 
   });
 });
+
+function getQueryParams(query) {
+  if (!query.length) {
+    return;
+  }
+
+  var search = query.substring(1);
+
+  return JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}');
+}
