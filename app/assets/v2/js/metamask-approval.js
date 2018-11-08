01d7a153ef9caf149fe9636324fc58ec7b089b08
Muhammad Usman <me@usmanmuhd.com>
2018-11-08 14:23:45 +0530

    Alert for metamask not connected in tips, ens and kudos

--- app/assets/v2/js/metamask-approval.js
+++ app/assets/v2/js/metamask-approval.js
@@ -12,6 +12,7 @@ async function metamaskApproval() {
       _alert('Permission to connect to metamask rejected. Allow gitcoin to connect to metamask.', 'warning');
     }
   }
+  ask_metamask_connection();
 }
 
 window.addEventListener('load', metamaskApproval);
@@ -21,5 +22,21 @@ async function approve_metamask() {
     await ethereum.enable();
   } catch (error) {
     _alert('Permission to connect to metamask rejected. Allow gitcoin to connect to metamask.', 'warning');
+  }
+}
+
+function ask_metamask_connection() {
+  var page_url = $(location).attr('pathname');
+  shown_on = ['/tip/send/2', '/kudos/send', '/ens'];
+  var len = page_url.length - 1;
+  if (page_url.lastIndexOf('/') === len) {
+    page_url = page_url.substring(0, len);
+  }
+  if ($.inArray(page_url, shown_on) != -1 && !is_metamask_approved) {
+    _alert('Metamask not connected. <button id="metamask_connect" onclick="approve_metamask()">Click here to connect to metamask</button>', 'error');
+    $('#metamask_connect').css('background', 'none');
+    $('#metamask_connect').css('color', 'white');
+    $('#metamask_connect').css('border', '2px solid white');
+    $('#metamask_connect').css('border-radius', '10px');
   }
 }
