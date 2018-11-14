3ce8398f88a4404f6dce8183a939ff4f67cf7443
Owocki <ksowocki@gmail.com>
2018-11-14 16:30:43 -0700

    do not warm cache

--- app/retail/management/commands/warm_cache.py
+++ app/retail/management/commands/warm_cache.py
@@ -50,11 +50,6 @@ class Command(BaseCommand):
         paths = [reverse('results') + f"/{pl}" for pl in programming_languages]
         paths.append(reverse('results'))
         paths.append(reverse('activity'))
-        for name in ['gas_history_view', 'gas_guzzler_view']:
-            path = reverse(name)
-            paths.append(path)
-            paths.append(path + "?breakdown=daily")
-            paths.append(path + "?breakdown=weekly")
         paths.append(reverse('gas'))
         paths.append(reverse('gas_heatmap'))
 
