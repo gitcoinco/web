a4f9260502ea4ec3cbfb272197a6fda521834168
Owocki <ksowocki@gmail.com>
2018-11-26 15:01:19 -0700

    dont allow ppl with too new gitcoin profiles to receive kudos

--- app/kudos/views.py
+++ app/kudos/views.py
@@ -648,44 +648,49 @@ def receive_bulk(request, secret):
             'value': int(coupon.token.price_finney / 1000.0 * 10**18),
         })
 
-        signed = w3.eth.account.signTransaction(tx, settings.KUDOS_PRIVATE_KEY)
-        txid = w3.eth.sendRawTransaction(signed.rawTransaction).hex()
+        if not profile.trust_profile and profile.github_created_on > (timezone.now() - timezone.timedelta(days=7)):
+            messages.error(request, f'Your github profile is too new.  Cannot receive kudos.')
+        else:
 
-        with transaction.atomic():
-            kudos_transfer = KudosTransfer.objects.create(
-                emails=[request.user.email],
-                # For kudos, `token` is a kudos.models.Token instance.
-                kudos_token_cloned_from=coupon.token,
-                amount=0,
-                comments_public=coupon.comments_to_put_in_kudos_transfer,
-                ip=ip_address,
-                github_url='',
-                from_name=coupon.sender_profile.handle,
-                from_email='',
-                from_username=coupon.sender_profile.handle,
-                username=profile.handle,
-                network=coupon.token.contract.network,
-                from_address=settings.KUDOS_OWNER_ACCOUNT,
-                is_for_bounty_fulfiller=False,
-                metadata={'coupon_redemption': True},
-                recipient_profile=profile,
-                sender_profile=coupon.sender_profile,
-                txid=txid,
-                receive_txid=txid,
-                tx_status='pending',
-                receive_tx_status='pending',
-            )
+            signed = w3.eth.account.signTransaction(tx, settings.KUDOS_PRIVATE_KEY)
+            txid = w3.eth.sendRawTransaction(signed.rawTransaction).hex()
 
-            # save to DB
-            BulkTransferRedemption.objects.create(
-                coupon=coupon,
-                redeemed_by=profile,
-                ip_address=ip_address,
-                kudostransfer=kudos_transfer,
+            with transaction.atomic():
+                kudos_transfer = KudosTransfer.objects.create(
+                    emails=[request.user.email],
+                    # For kudos, `token` is a kudos.models.Token instance.
+                    kudos_token_cloned_from=coupon.token,
+                    amount=0,
+                    comments_public=coupon.comments_to_put_in_kudos_transfer,
+                    ip=ip_address,
+                    github_url='',
+                    from_name=coupon.sender_profile.handle,
+                    from_email='',
+                    from_username=coupon.sender_profile.handle,
+                    username=profile.handle,
+                    network=coupon.token.contract.network,
+                    from_address=settings.KUDOS_OWNER_ACCOUNT,
+                    is_for_bounty_fulfiller=False,
+                    metadata={'coupon_redemption': True},
+                    recipient_profile=profile,
+                    sender_profile=coupon.sender_profile,
+                    txid=txid,
+                    receive_txid=txid,
+                    tx_status='pending',
+                    receive_tx_status='pending',
                 )
 
-            coupon.num_uses_remaining -= 1
-            coupon.current_uses += 1
+                # save to DB
+                BulkTransferRedemption.objects.create(
+                    coupon=coupon,
+                    redeemed_by=profile,
+                    ip_address=ip_address,
+                    kudostransfer=kudos_transfer,
+                    )
+
+                coupon.num_uses_remaining -= 1
+                coupon.current_uses += 1
+                coupon.save()
 
     title = f"Redeem AirDropped *{coupon.token.humanized_name}* Kudos"
     desc = f"This Kudos has been AirDropped to you.  About this Kudos: {coupon.token.description}"
