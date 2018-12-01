27bc1943d1a4b7367cdc9d1180a1e35874f47125
Anish-Agnihotri <contact@anishagnihotri.com>
2018-12-01 16:48:07 -0500

    Autolint.

--- app/assets/v2/js/landing_page.js
+++ app/assets/v2/js/landing_page.js
@@ -92,8 +92,8 @@ function showDivs(n) {
   for (i = 0; i < x.length; i++) {
     x[i].style.display = 'none';
   }
-  x[slideIndex-1].style.display = 'block';
-}  
+  x[slideIndex - 1].style.display = 'block';
+}
 
 function screenClass() {
   if ($(window).innerWidth() < 1160) {
