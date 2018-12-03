75817e47c350f47bf3a227b5456a969aa7007c70
Kevin Seagraves <k.s.seagraves@gmail.com>
2018-12-03 15:57:00 -0700

    remove unnecessary fetch call

--- app/assets/v2/js/grants/fund.js
+++ app/assets/v2/js/grants/fund.js
@@ -101,45 +101,16 @@ $(document).ready(function() {
                 web3.eth.personal.sign('' + subscriptionHash, accounts[0], function(err, signature) {
 
                   $('#signature').val(signature);
-
-                  let postData = {
-                    subscriptionContract: data.contract_address,
-                    parts: parts,
-                    subscriptionHash: subscriptionHash,
-                    signature: signature
-                  };
-
-                  console.log('postData', postData);
-
-                  fetch('http://localhost:10003/saveSubscription', {
-                    method: 'POST',
-                    headers: {
-                      'Content-Type': 'application/json'
-                    },
-                    body: JSON.stringify({
-                      postData
-                    })
-                  }).then((response)=>{
-                    console.log('TX RESULT', response);
-
-                  }).catch((error)=>{
-                    console.log(error);
-                    alert('Your subscription data failed to save. Please try again.');
-
-                  });
                 });
               });
             });
           }).on('confirmation', function(confirmationNumber, receipt) {
-            console.log('receipt', receipt);
-
             $('#real_period_seconds').val(realPeriodSeconds);
 
             $.each($(form).serializeArray(), function() {
               data[this.name] = this.value;
             });
 
-            console.log('data', data);
             form.submit();
           });
 
