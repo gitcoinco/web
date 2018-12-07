fad9fb9ab5fdb51ddf029992063333af0bbeaf30
Muhammad Usman <me@usmanmuhd.com>
2018-12-07 09:52:57 +0530

    Changes to notifs backend

--- app/inbox/models.py
+++ app/inbox/models.py
@@ -1,18 +1,24 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from economy.models import SuperModel
-
-# Create your models here.
+from django.contrib.auth import get_user_model
 
 class Notification(SuperModel):
-    created_on = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    to_user_id = models.IntegerField()
-    from_user_id = models.IntegerField()
-    username = models.CharField(max_length=255)
+    created_on = models.DateTimeField(auto_now_add=True, editable=False)
     CTA_URL = models.URLField(max_length=255, null=True)
     CTA_Text = models.CharField(max_length=255)
     message_html = models.CharField(max_length=255, null=True, help_text=_("Html message"))
     is_read = models.BooleanField(default=False)
+    to_user_id = models.ForeignKey(
+            get_user_model(),
+            on_delete=models.CASCADE,
+            related_name='to_user'
+        )
+    from_user_id = models.ForeignKey(
+            get_user_model(),
+            on_delete=models.CASCADE,
+            related_name='from_user'
+        )
 
     def __str__(self):
         return str(self.id)
--- app/inbox/views.py
+++ app/inbox/views.py
@@ -2,33 +2,23 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from inbox.models import Notification
 from django.utils import timezone
+from django.contrib.auth import get_user_model
 
 from django.http import JsonResponse, HttpResponseForbidden
 
 def inbox(request):
     """Handle grants explorer."""
     profile = request.user.profile if request.user.is_authenticated and request.user.profile else None
-    limit = request.GET.get('limit', 25)
-    page = request.GET.get('page', 1)
-    sort = request.GET.get('sort_option', '-created_on')
-
     if profile is None:
         return HttpResponseForbidden('Not Allowed')
 
-    '''_notifications = Notification.objects.filter(to_user_id=profile.id).order_by(sort)
-
-    if request.method == 'POST':
-        sort = request.POST.get('sort_option', '-created_on')
-        _notifications = Notification.objects.order_by(sort)
-
-    paginator = Paginator(_notifications, limit)
-    notifications = paginator.get_page(page)
-    '''
-    all_notifs = Notification.objects.all()
+    all_notifs = Notification.objects.filter(to_user_id=get_user_model().objects.get(username=profile).id)
     params = []
     for i in all_notifs:
         new_notif = i.to_standard_dict()
         new_notif['created_on'] = i.created_on
+        new_notif['username'] = get_user_model().objects.get(id=new_notif['from_user_id']).username
+
         params.append(new_notif)
 
     return JsonResponse(params, status=200, safe=False)
