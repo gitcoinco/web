3f8be678b121d68ad11bee345ff455a7bf4e10a2
aashutoshrathi <aashutoshrathi@gmail.com>
2018-12-15 11:51:37 +0530

    Make image field optional

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1639,7 +1639,7 @@ class LabsResearch(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=1000)
     link = models.URLField(null=True)
-    image = models.ImageField(upload_to='labs')
+    image = models.ImageField(upload_to='labs', blank=True, null=True)
 
     def __str__(self):
         return self.title
