896b8fa8d1d451a5fa9641322d0b84fc2121890a
Muhammad Usman <me@usmanmuhd.com>
2018-12-20 22:06:44 +0530

    Docstring in utils.py

--- app/inbox/utils.py
+++ app/inbox/utils.py
@@ -3,6 +3,7 @@ from django.contrib.auth import get_user_model
 
 
 def send_notification_to_user(from_user, to_user, cta_url, cta_text, msg_html):
+    """Helper method to create a new notification."""
     Notification.objects.create(
         cta_url=cta_url,
         cta_text=cta_text,
