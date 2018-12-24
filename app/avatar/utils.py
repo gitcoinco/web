fb8f1bf1fd00100817cbbb463f4a90471184f769
Animesh Tewari <animesh@Animeshs-MacBook-Pro.local>
2018-12-24 17:18:38 +0530

    Clothing - gitcoin professional style 1

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -111,8 +111,8 @@ def get_avatar_context():
             'title': 'Pick your clothing',
             'options': (
                 'cardigan', 'hoodie', 'knitsweater', 'plaid', 'shirt', 'shirtsweater', 'spacecadet', 'suit', 'ethlogo',
-                'cloak', 'robe', 'pjs',
-                'cloak', 'robe', 'pjs', 'elf_inspired'
+                'cloak', 'robe', 'pjs', 'elf_inspired', 'gitcoinpro',
+
             ),
             'paid_options': {
                 'robe': 0.01,
