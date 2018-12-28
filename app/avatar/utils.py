7c36cf7fb1432ad708c1d9556939bdf3f655b58d
Mehul Gupta <1998mehul@gmail.com>
2018-12-29 01:42:08 +0530

    Refs:#3195 Shades added

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -155,7 +155,7 @@ def get_avatar_context():
                 ['Extras-reindeer'], ['Masks-gitcoinbot'],
                 ['Extras-tattoo'], ['Masks-batman_inspired'], ['Masks-eye-patch'],
                 ['Masks-flash_inspired'], ['Masks-deadpool_inspired'], ['Masks-darth_inspired'],
-                ['Masks-spiderman_inspired'],
+                ['Masks-spiderman_inspired'], ['Glasses-5'],
             ),
             'paid_options': {
                 'Extras-Parrot': 0.01,
