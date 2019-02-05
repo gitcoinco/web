c7ff4f53f33686c3c49e5980cd69f93c27041e8e
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-02-05 19:07:16 +0100

    Update tests

--- app/dashboard/tests/test_dashboard_helpers.py
+++ app/dashboard/tests/test_dashboard_helpers.py
@@ -18,12 +18,17 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 import json
+from datetime import date, datetime, timedelta
+from unittest.mock import patch
 
 from django.test.client import RequestFactory
 
+import pytz
 import requests_mock
 from dashboard.helpers import amount, issue_details, normalize_url
+from dashboard.models import Bounty
 from economy.models import ConversionRate
+from marketing.mails import featured_funded_bounty
 from test_plus.test import TestCase
 
 
@@ -50,3 +55,28 @@ class DashboardHelpersTest(TestCase):
     def test_normalize_url(self):
         """Test the dashboard helper normalize_url method."""
         assert normalize_url('https://gitcoin.co/') == 'https://gitcoin.co'
+    
+    @patch('marketing.mails.featured_funded_bounty')
+    def test_featured_funded_mail(self, mock_featured_funded_bounty):
+        """Test sending featured funded bounty mail."""
+        Bounty.objects.create(
+            title='foo',
+            value_in_token=3,
+            token_name='ETH',
+            web3_created=datetime(2008, 10, 31, tzinfo=pytz.UTC),
+            github_url='https://github.com/gitcoinco/web/issues/305#issuecomment-999999999',
+            token_address='0x0',
+            issue_description='hello world',
+            bounty_owner_github_username='iamonuwa',
+            bounty_owner_email='matrix4u2002@gmail.com',
+            is_open=False,
+            accepted=False,
+            expires_date=datetime(2008, 11, 30, tzinfo=pytz.UTC),
+            idx_project_length=5,
+            project_length='Months',
+            bounty_type='Feature',
+            experience_level='Intermediate',
+            raw_data={},
+            is_featured=True
+        )
+        assert mock_featured_funded_bounty().call_count == 0
