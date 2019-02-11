119bacbf71ca1e9fea905a5ecfd7bfae251b69cc
kvndlgs <es2thekay@gmail.com>
2019-02-11 18:24:52 -0500

    Adding batgirls mask Refs: https://github.com/gitcoinco/web/issues/3713

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -167,7 +167,7 @@ def get_avatar_context():
                 ], ['Extras-reindeer'], ['Masks-gitcoinbot'], ['Extras-tattoo'], ['Masks-batman_inspired'], [
                     'Masks-eye-patch'
                 ], ['Masks-flash_inspired'], ['Masks-deadpool_inspired'], ['Masks-darth_inspired'],
-                            ['Masks-spiderman_inspired'], ['Glasses-5'], ['Glasses-geordi-visor'],
+                            ['Masks-spiderman_inspired'], ['Glasses-5'], ['Glasses-geordi-visor'],['Masks-batgirl']
                             ),
                 'paid_options': {
                     'Extras-Parrot': 0.01,
