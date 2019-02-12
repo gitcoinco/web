ab0fd4db15924520d801c19e2bae89c756c01444
Owocki <ksowocki@gmail.com>
2019-02-12 15:19:34 -0700

    ethdenver

--- app/event_ethdenver2019/admin.py
+++ app/event_ethdenver2019/admin.py
@@ -5,6 +5,7 @@ from .models import Event_ETHDenver2019_Customizing_Kudos
 
 class ETHDenver2019_CustomizingAdmin(admin.ModelAdmin):
     ordering = ['-id']
+    raw_id_fields = ['kudos_required']
 
 
 admin.site.register(Event_ETHDenver2019_Customizing_Kudos, ETHDenver2019_CustomizingAdmin)
