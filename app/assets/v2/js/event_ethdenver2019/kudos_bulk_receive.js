d994d8a5b84e96867cf02fed0ce5a751f8fed9fd
Owocki <ksowocki@gmail.com>
2019-02-13 10:43:26 -0700

    linter

--- app/assets/v2/js/event_ethdenver2019/kudos_bulk_receive.js
+++ app/assets/v2/js/event_ethdenver2019/kudos_bulk_receive.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
 
-  setInterval(function(){
-    var current_forwarding_address = $("#forwarding_address").val();
-    if(current_forwarding_address){
+  setInterval(function() {
+    var current_forwarding_address = $('#forwarding_address').val();
+
+    if (current_forwarding_address) {
       console.log('got forwarding address');
       localStorage.setItem('forwarding_address', current_forwarding_address);
-    } else {
-      if(localStorage['forwarding_address'] && !$("#forwarding_address").is(":focus");){
-        console.log('setting forwarding address');
-        $("#forwarding_address").val(localStorage['forwarding_address']);
-      }
+    } else if (localStorage['forwarding_address'] && !$('#forwarding_address').is(':focus')) {
+      console.log('setting forwarding address');
+      $('#forwarding_address').val(localStorage['forwarding_address']);
     }
-  }, 100)
+  }, 100);
 
   waitforWeb3(function() {
     if (document.web3network == 'locked') {
