f7687389b284d9c478e70f26206b35c236bca2b6
Owocki <ksowocki@gmail.com>
2019-02-13 10:40:49 -0700

    set / restore forwarding address

--- app/assets/v2/js/event_ethdenver2019/kudos_bulk_receive.js
+++ app/assets/v2/js/event_ethdenver2019/kudos_bulk_receive.js
@@ -1,5 +1,18 @@
 $(document).ready(function() {
 
+  setInterval(function(){
+    var current_forwarding_address = $("#forwarding_address").val();
+    if(current_forwarding_address){
+      console.log('got forwarding address');
+      localStorage.setItem('forwarding_address', current_forwarding_address);
+    } else {
+      if(localStorage['forwarding_address'] && !$("#forwarding_address").is(":focus");){
+        console.log('setting forwarding address');
+        $("#forwarding_address").val(localStorage['forwarding_address']);
+      }
+    }
+  }, 100)
+
   waitforWeb3(function() {
     if (document.web3network == 'locked') {
       _alert('Metamask not connected. <button id="metamask_connect" onclick="approve_metamask()">Click here to connect to metamask</button>', 'error');
