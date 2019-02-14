c697ddfa15e03e74831d9a484befe94e27f93cea
Owocki <ksowocki@gmail.com>
2019-02-14 13:43:18 -0700

    profile pic debug script

--- scripts/debug/migrate_profile_picture.py
+++ scripts/debug/migrate_profile_picture.py
@@ -1,8 +1,8 @@
 from app.utils import sync_profile
-
-handle='whalechat'
 from avatar.utils import get_avatar
 from dashboard.models import Profile
+
+handle='whalechat'
 profile = Profile.objects.get(handle=handle)
 avatar = profile.avatar_baseavatar_related.first()
 avatar.delete()
