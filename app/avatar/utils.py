b0dbc0046ae30a7fa2b3ee71148fd152395e9ce6
j0ker1234 <jbatman835@gmail.com>
2019-02-19 16:24:13 +0530

    Refs:#3713 more designs added

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -170,7 +170,7 @@ def get_avatar_context():
                 ], ['Masks-flash_inspired'], ['Masks-deadpool_inspired'], ['Masks-darth_inspired'],
                             ['Masks-spiderman_inspired'], ['Glasses-5'], ['Glasses-geordi-visor'], 
                             ['Masks-funny_face'], ['Masks-viking'], ['Masks-construction_helmet'], 
-                            ['Glasses-6'],
+                            ['Glasses-6'], ['HatShort-green'], 
                             ),
                 'paid_options': {
                     'Extras-Parrot': 0.01,
