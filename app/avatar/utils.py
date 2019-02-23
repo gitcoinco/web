8987d89c57162d0ad161fa33671b5545ef98388f
Yash Mathne <yash6866@gmail.com>
2019-02-23 22:23:22 +0530

    Resolving Conflict

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -175,6 +175,9 @@ def get_avatar_context():
                             ['Masks-gas'], ['Masks-surgical'], ['Extras-monkey'], ['Masks-power'], ['Glasses-7'],
                             ['Glasses-8'], ['HatShort-angel'], ['HatShort-devil'], ['Extras-necklace1'],
                             ['Extras-necklace2'], ['Extras-necklace3'], ['Glasses-9'], ['Masks-clown'],
+                            ['Extras-bird'],['HatShort-sleepy'],
+                            ['Earring-tribal'],['Glasses-google'],['Masks-marshmellow'],['Masks-pirate'],
+                            ['Masks-power1'],['HatShort-elf'],['Extras-necklaceb'],
                             ),
                 'paid_options': {
                     'Extras-Parrot': 0.01,
