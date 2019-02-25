05caa6aee8e6b5b6e40720d2a2349765f55fbd71
j0ker1234 <jbatman835@gmail.com>
2019-02-25 21:54:05 +0530

    Refs:#3713 designs added

--- app/avatar/utils.py
+++ app/avatar/utils.py
@@ -117,7 +117,7 @@ def get_avatar_context():
                 'options': (
                     'cardigan', 'hoodie', 'knitsweater', 'plaid', 'shirt', 'shirtsweater', 'spacecadet', 'suit',
                     'ethlogo', 'cloak', 'robe', 'pjs', 'elf_inspired', 'business_suit', 'suspender', 'gitcoinpro',
-                    'star_uniform',
+                    'star_uniform', 'jersey',
                 ),
                 'paid_options': {
                     'robe': 0.01,
