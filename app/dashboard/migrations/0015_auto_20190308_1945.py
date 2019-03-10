a271bd047d2c04bbc1ca6bcc6f20e408ca404e26
Emilio Silva Schlenker <e18r@protonmail.com>
2019-03-10 13:19:23 -0300

    owocki's requested changes 2 (#3316)
    * Removed unrelated database modifications from migration file

--- app/dashboard/migrations/0015_auto_20190308_1945.py
+++ app/dashboard/migrations/0015_auto_20190308_1945.py
@@ -18,19 +18,4 @@ class Migration(migrations.Migration):
             name='subscription',
             field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, related_name='activities', to='grants.Subscription'),
         ),
-        migrations.AlterField(
-            model_name='activity',
-            name='activity_type',
-            field=models.CharField(blank=True, choices=[('new_bounty', 'New Bounty'), ('start_work', 'Work Started'), ('stop_work', 'Work Stopped'), ('work_submitted', 'Work Submitted'), ('work_done', 'Work Done'), ('worker_approved', 'Worker Approved'), ('worker_rejected', 'Worker Rejected'), ('worker_applied', 'Worker Applied'), ('increased_bounty', 'Increased Funding'), ('killed_bounty', 'Canceled Bounty'), ('new_tip', 'New Tip'), ('receive_tip', 'Tip Received'), ('bounty_abandonment_escalation_to_mods', 'Escalated for Abandonment of Bounty'), ('bounty_abandonment_warning', 'Warning for Abandonment of Bounty'), ('bounty_removed_slashed_by_staff', 'Dinged and Removed from Bounty by Staff'), ('bounty_removed_by_staff', 'Removed from Bounty by Staff'), ('bounty_removed_by_funder', 'Removed from Bounty by Funder'), ('new_crowdfund', 'New Crowdfund Contribution'), ('new_grant', 'New Grant'), ('update_grant', 'Updated Grant'), ('killed_grant', 'Cancelled Grant'), ('new_grant_contribution', 'Contributed to Grant'), ('new_grant_subscription', 'Subscribed to Grant'), ('killed_grant_contribution', 'Cancelled Grant Contribution'), ('new_milestone', 'New Milestone'), ('update_milestone', 'Updated Milestone'), ('new_kudos', 'New Kudos')], db_index=True, max_length=50),
-        ),
-        migrations.AlterField(
-            model_name='profile',
-            name='job_location',
-            field=django.contrib.postgres.fields.jsonb.JSONField(blank=True, default=dict),
-        ),
-        migrations.AlterField(
-            model_name='tool',
-            name='category',
-            field=models.CharField(choices=[('AD', 'advanced'), ('TO', 'gas'), ('AL', 'alpha'), ('BA', 'basic'), ('BU', 'tools to build'), ('CS', 'coming soon'), ('CO', 'community'), ('FF', 'just for fun'), ('CR', 'retired')], max_length=2),
-        ),
     ]
