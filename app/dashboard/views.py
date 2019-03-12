53d4598b4aea0d2c8d4c5da85932248c9dcde1ba
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-03-12 21:58:36 +0100

    Added hackathon landing page

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -639,6 +639,14 @@ def dashboard(request):
     }
     return TemplateResponse(request, 'dashboard/index.html', params)
 
+def ethhack(request):
+    """Handle displaying ethhack landing page."""
+
+    title = str(_(" Eth hackathon 2019"))
+    params = {
+        'title': title
+    }
+    return TemplateResponse(request, 'dashboard/hackathon/ethhack_2019.html', params)
 
 def accept_bounty(request):
     """Process the bounty.
