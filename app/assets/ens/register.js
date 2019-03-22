14501e2092ccbe738d6f5cdecc17eef13cb6f969
Arsen A. Gutsal <a.gutsal@softsky.com.ua>
2019-03-22 12:05:28 +0200

    linting fixed

--- app/assets/ens/register.js
+++ app/assets/ens/register.js
@@ -1,20 +1,21 @@
 window.onload = function() {
   var registerBtn = $('#register');
-  if(register.length > 0)
-    register[0].onclick = function(e) {
-    if ($('#tos')[0].checked === false) {
-      return _alert({ message: gettext('Please check the TOS checkbox.')}, 'error');
-    }
-    if (typeof web3 == 'undefined') {
-      return _alert({ message: gettext('Please install metamask in your browser and try again.')}, 'error');
-    }
-    // Get the github handle
-    githubHandle = $('#githubHandle').text();
-    // Get the current account and call sign function:
-    web3.eth.getAccounts(function(err, accounts) {
-      if (!accounts)
-        return;
-      signMsgAndCreateSubdomain('Github Username : ' + $('#githubHandle').text(), accounts[0]);
-    });
-  };
+  if(registerBtn.length > 0) {
+    registerBtn[0].onclick = function(e) {
+      if ($('#tos')[0].checked === false) {
+        return _alert({ message: gettext('Please check the TOS checkbox.')}, 'error');
+      }
+      if (typeof web3 == 'undefined') {
+        return _alert({ message: gettext('Please install metamask in your browser and try again.')}, 'error');
+      }
+      // Get the github handle
+      githubHandle = $('#githubHandle').text();
+      // Get the current account and call sign function:
+      web3.eth.getAccounts(function(err, accounts) {
+        if (!accounts)
+          return;
+        signMsgAndCreateSubdomain('Github Username : ' + $('#githubHandle').text(), accounts[0]);
+      });
+    };
+  }
 };
