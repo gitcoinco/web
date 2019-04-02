e3685b1d4809230f1410ce5da7c53bd015aab0f8
Saptak Sengupta <saptak013@gmail.com>
2019-04-02 13:30:53 +0530

    Make adjustment to allow average ratings for new feedback model

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1931,11 +1931,11 @@ class Profile(SuperModel):
         feedbacks = FeedbackEntry.objects.filter(receiver_profile=self).all()
         average_rating = {}
         average_rating['overall'] = sum([feedback.rating for feedback in feedbacks]) / feedbacks.count()
-        # average_rating['code_quality_rating'] = sum([feedback.code_quality_rating for feedback in feedbacks]) / feedbacks.count()
-        # average_rating['communication_rating'] = sum([feedback.communication_rating for feedback in feedbacks]) / feedbacks.count()
-        # average_rating['recommendation_rating'] = sum([feedback.recommendation_rating for feedback in feedbacks]) / feedbacks.count()
-        # average_rating['satisfaction_rating'] = sum([feedback.satisfaction_rating for feedback in feedbacks]) / feedbacks.count()
-        # average_rating['speed_rating'] = sum([feedback.speed_rating for feedback in feedbacks]) / feedbacks.count()
+        average_rating['code_quality_rating'] = sum([feedback.code_quality_rating for feedback in feedbacks]) / feedbacks.count()
+        average_rating['communication_rating'] = sum([feedback.communication_rating for feedback in feedbacks]) / feedbacks.count()
+        average_rating['recommendation_rating'] = sum([feedback.recommendation_rating for feedback in feedbacks]) / feedbacks.count()
+        average_rating['satisfaction_rating'] = sum([feedback.satisfaction_rating for feedback in feedbacks]) / feedbacks.count()
+        average_rating['speed_rating'] = sum([feedback.speed_rating for feedback in feedbacks]) / feedbacks.count()
         return average_rating
 
 
