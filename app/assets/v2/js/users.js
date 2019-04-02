8c4f877ceb99e058966d4af6cb5118765854a167
octavioamu <octavioamuchastegui@gmail.com>
2019-04-02 16:58:20 -0300

    change to rest

---
+++ app/assets/v2/js/users.js
@@ -0,0 +1,58 @@
+let users = [];
+let usersPage = 1;
+let usersNumPages = '';
+let usersHasNext = false;
+
+Vue.mixin({
+  methods: {
+    fetchUsers: function(newPage) {
+      var vm = this;
+
+      if (newPage) {
+        vm.usersPage = newPage;
+      }
+
+      var getNotifications = fetchData (`/api/v0.1/users_fetch/?page=${vm.usersPage}`, 'GET');
+
+      $.when(getNotifications).then(function(response) {
+        newNotifications = newData(response.data, vm.notifications);
+        newNotifications.forEach(function(item) {
+          vm.notifications.push(item);
+        });
+
+        vm.usersNumPages = response.num_pages;
+        vm.usersHasNext = response.has_next;
+        vm.numNotifications = response.count;
+
+        vm.checkUnread();
+        if (vm.usersHasNext) {
+          vm.usersPage = ++vm.usersPage;
+
+        } else {
+          vm.usersPage = 1;
+        }
+      });
+    }
+
+  }
+
+});
+
+if (document.getElementById('gc-users-directory')) {
+  var app = new Vue({
+    delimiters: [ '[[', ']]' ],
+    el: '#gc-users-directory',
+    data: {
+      users,
+      usersPage,
+      usersNumPages,
+      usersHasNext
+    },
+    mounted() {
+      this.fetchUsers();
+    },
+    created() {
+      // this.sendState();
+    }
+  });
+}
