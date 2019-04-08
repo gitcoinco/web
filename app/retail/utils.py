c132d565a946420993a55fad6df05118d10a751c
Owocki <ksowocki@gmail.com>
2019-04-08 15:45:43 -0600

    update codefund numbers

--- app/retail/utils.py
+++ app/retail/utils.py
@@ -167,7 +167,7 @@ def get_codefund_history_at_date(date, keyword):
     if date > timezone.datetime(2019, 3, 23):
         amount += 18726
     if date > timezone.datetime(2019, 4, 23):
-        amount += 0 # MTD
+        amount += 7643 # MTD
     return amount
 
 
