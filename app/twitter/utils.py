015860bb98d2e94a2b53daa6f03404be001df3f9
Kumo Lam <GDUT7890@outlook.com>
2019-04-10 10:48:38 +0800

    Hello! We are trying to fix this issue: Fixs: https://github.com/gitcoinco/web/issues/4111 We have finished and tested basic function of "login with Twitter" process. Further optimization and add-to-page step will be done in few days.
    And we edited this files:
    Add app/twitter/utils.py
    Add methods to format a request and use them to obtain and access OAuth token of Twitter API. "request_oauth_token_with_lib" function will generate a signature and request the API for token.
    
    Add app/twitter/app.py
    Register the "login with twitter" application using Django's AppConfig method.
    
    Modify app/retail/views.py
    Add handler for login and callback request: twitter_login() for handling login request and twitter_callback for handling callback request.
    
    Modify app/app/urls.py
    Add urls for login and callback: /twilogin/login and /twilogin/callback
    
    Modify app/app/settings.py
    Modify app/app/local.env
    Add Environment Variables: TWITTER_CONSUMER_KEY/TWITTER_CONSUMER_SECRET/TWITTER_ACCESS_TOKEN/TWITTER_ACCESS_SECRET
    
    Fixs: https://github.com/gitcoinco/web/issues/4111

--- app/twitter/utils.py
+++ app/twitter/utils.py
@@ -16,8 +16,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-Author: Kumo Lam
-Date: April/06/2019
 """
 
 import requests
