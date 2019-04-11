2dc8acaefdd7d12b12e8ffe0b5120806b2aa3f16
Octavio Amuchástegui <octavioamuchastegui@gmail.com>
2019-04-11 11:34:33 -0300

    clean

--- app/assets/v2/js/users.js
+++ app/assets/v2/js/users.js
@@ -12,29 +12,24 @@ Vue.mixin({
       if (newPage) {
         vm.usersPage = newPage;
       }
-      vm.params.page = vm.usersPage
+      vm.params.page = vm.usersPage;
 
-      console.log(vm.searchTerm)
-      if(vm.searchTerm) {
-        vm.params.search = vm.searchTerm
+      if (vm.searchTerm) {
+        vm.params.search = vm.searchTerm;
       } else {
-        delete vm.params['search']
+        delete vm.params['search'];
       }
       let searchParams = new URLSearchParams(vm.params);
       
-      let apiUrlUsers = `/api/v0.1/users_fetch/?${searchParams.toString()}`
+      let apiUrlUsers = `/api/v0.1/users_fetch/?${searchParams.toString()}`;
 
       var getUsers = fetchData (apiUrlUsers, 'GET');
 
       $.when(getUsers).then(function(response) {
-        console.log(response.data)
-        // vm.users = response.data;
-        // vm.users.push(response.data);
       
         response.data.forEach(function(item) {
           vm.users.push(item);
         });
-      
 
         vm.usersNumPages = response.num_pages;
         vm.usersHasNext = response.has_next;
@@ -51,23 +46,22 @@ Vue.mixin({
     searchUsers: function() {
       vm = this;
       vm.users = [];
-      // vm.usersPage = 1
-      // vm.params.page = 1
 
-      vm.fetchUsers(1)
+      vm.fetchUsers(1);
 
     },
     bottomVisible: function() {
       vm = this;
 
-      const scrollY = window.scrollY
-      const visible = document.documentElement.clientHeight
-      const pageHeight = document.documentElement.scrollHeight - 500
-      const bottomOfPage = visible + scrollY >= pageHeight
-      if( bottomOfPage || pageHeight < visible) {
+      const scrollY = window.scrollY;
+      const visible = document.documentElement.clientHeight;
+      const pageHeight = document.documentElement.scrollHeight - 500;
+      const bottomOfPage = visible + scrollY >= pageHeight;
+
+      if (bottomOfPage || pageHeight < visible) {
         if (vm.usersHasNext) {
           vm.fetchUsers();
-          vm.usersHasNext = false
+          vm.usersHasNext = false;
         }
       }
     }
@@ -94,34 +88,14 @@ if (document.getElementById('gc-users-directory')) {
       this.fetchUsers();
     },
     beforeMount() {
-      // window.addEventListener('scroll', this.onScroll);
-      
       window.addEventListener('scroll', () => {
-        this.bottom = this.bottomVisible()
-      }, false)
-      // this.onScroll()
-      // this.sendState();
+        this.bottom = this.bottomVisible();
+      }, false);
     },
-    beforeDestroy () {
+    beforeDestroy() {
       window.removeEventListener('scroll', () => {
-        this.bottom = this.bottomVisible()
+        this.bottom = this.bottomVisible();
       });
     }
   });
 }
-
-
-// const createQueryParams = params => 
-//       Object.keys(params)
-//             .map(k => `${k}=${encodeURI(params[k])}`)
-//             .join('&');
-
-
-// const data2 = {
-//   var1: 'value1',
-//   var2: 'value2'
-// };
-
-// const searchParams = new URLSearchParams(data);
-
-// searchParams.toString()
