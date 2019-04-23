d0c66d8f7ec0cf758df57bded3145e4604e558b8
Dan Lipert <danlipert@gmail.com>
2019-04-23 19:41:20 +0900

    fix migration

--- app/dashboard/migrations/0029_profile_fee_percentage.py
+++ app/dashboard/migrations/0029_profile_fee_percentage.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dashboard', '0026_auto_20190328_1314'),
+        ('dashboard', '0028_profile_actions_count'),
     ]
 
     operations = [
