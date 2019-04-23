e21266f297df17e45a2b1a196b81b743f55b91df
Dan Lipert <danlipert@gmail.com>
2019-04-23 20:00:56 +0900

    fix fee percentage display on new bounty

--- app/assets/v2/js/pages/new_bounty.js
+++ app/assets/v2/js/pages/new_bounty.js
@@ -687,7 +687,7 @@ const populateBountyTotal = () => {
     total = `${totalBounty} ${bountyToken}`;
   }
 
-  $('#fee-percentage').html(FEE_PERCENTAGE);
+  $('#fee-percentage').html(FEE_PERCENTAGE * 100);
   $('#fee-amount').html(bountyFee);
   $('#fee-token').html(bountyToken);
   $('#summary-total-amount').html(total);
