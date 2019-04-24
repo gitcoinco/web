20e5274bfcae7227ba00dadc187994e14336a663
Dan Lipert <danlipert@gmail.com>
2019-04-24 22:40:38 +0900

    fix migration conflict

--- app/dashboard/migrations/0030_profile_fee_percentage.py
+++ app/dashboard/migrations/0030_profile_fee_percentage.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dashboard', '0028_profile_actions_count'),
+        ('dashboard', '0029_grant_activity'),
     ]
 
     operations = [
