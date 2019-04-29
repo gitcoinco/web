d7ada3c46f6c352841fe50afff8c78ae7f3d14da
octavioamu <octavioamuchastegui@gmail.com>
2019-04-29 18:39:36 -0300

    fix styles

--- app/assets/v2/js/users.js
+++ app/assets/v2/js/users.js
@@ -100,7 +100,7 @@ Vue.mixin({
       let postInvite = fetchData(
         apiUrlInvite,
         'POST',
-        { 'url': bounty.github_url, 'usersId': [user], 'bountyId': bounty.id, 'msg': 'check this'},
+        { 'url': bounty.github_url, 'usersId': [user], 'bountyId': bounty.id},
         {'X-CSRFToken': csrftoken}
       );
 
