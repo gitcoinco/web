c8767071a68d20e19d4043dfa7db28afb56a9b5b
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-06-14 16:26:59 +0100

    Build UI for surgical email unsubscribe

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -2277,6 +2277,12 @@ def sync_web3(request):
 
     return JsonResponse(result, status=result['status'])
 
+def unsubscribe(request):
+    context = {
+        'title': _('Email unsubscription successful'),
+        'type': 'Product Update'
+    }
+    return TemplateResponse(request, 'email_unsubscribed.html', context)
 
 # LEGAL
 @xframe_options_exempt
