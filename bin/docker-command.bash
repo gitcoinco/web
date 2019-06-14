6f777c72a2905c351257a11979e2c138bb7bb230
acolytec3 <a@b.com>
2019-06-14 06:23:59 -0400

    Add initial kudos data

--- bin/docker-command.bash
+++ bin/docker-command.bash
@@ -58,12 +58,14 @@ if [ ! -f /provisioned ] || [ "$FORCE_PROVISION" = "on" ]; then
         python manage.py loaddata app/fixtures/users.json
         python manage.py loaddata app/fixtures/economy.json
         python manage.py loaddata app/fixtures/profiles.json
-        python manage.py loaddata app/fixtures/hackathon.json        
-        python manage.py loaddata app/fixtures/bounties.json
-        python manage.py loaddata app/fixtures/feedback.json
-        python manage.py loaddata app/fixtures/interest.json
-        python manage.py loaddata app/fixtures/bountyfulfillments.json
-        python manage.py loaddata app/fixtures/activity.json
+#        python manage.py loaddata app/fixtures/hackathon.json        
+#        python manage.py loaddata app/fixtures/bounties.json
+#        python manage.py loaddata app/fixtures/feedback.json
+#        python manage.py loaddata app/fixtures/interest.json
+#        python manage.py loaddata app/fixtures/bountyfulfillments.json
+#        python manage.py loaddata app/fixtures/activity.json
+        python manage.py loaddata app/fixtures/dashboard.json
+        python manage.py loaddata app/fixtures/kudos.json
         python manage.py loaddata app/fixtures/grants.json
         python manage.py loaddata app/fixtures/marketing.json
         
