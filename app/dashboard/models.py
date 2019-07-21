5bf35b14b0cd175cf3cdbd6c89a79b20a92342b2
Rafal <rafal.arte@gmail.com>
2019-07-21 22:37:10 +0200

    Fix

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -53,17 +53,16 @@ from git.utils import (
     _AUTH, HEADERS, TOKEN_URL, build_auth_dict, get_gh_issue_details, get_issue_comments, issue_number, org_name,
     repo_name,
 )
-from marketing.mails import featured_funded_bounty
+from marketing.mails import featured_funded_bounty, start_work_approved
 from marketing.models import LeaderboardRank
 from rest_framework import serializers
 from web3 import Web3
 
-from .signals import m2m_changed_interested
 from .notifications import (
-    maybe_market_to_github, maybe_market_to_slack, maybe_market_to_twitter,
-    maybe_market_to_user_slack,
+    maybe_market_to_github, maybe_market_to_slack, maybe_market_to_twitter, maybe_market_to_user_slack,
 )
-from marketing.mails import start_work_approved
+from .signals import m2m_changed_interested
+
 logger = logging.getLogger(__name__)
 
 
