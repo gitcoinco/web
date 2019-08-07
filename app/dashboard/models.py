afc43c01cf80acdaca6504ffeaf6a0dbcd65d6ed
octavioamu <octavioamuchastegui@gmail.com>
2019-08-07 13:42:25 -0300

    fix conflicts

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -2715,7 +2715,7 @@ class Profile(SuperModel):
 
         try:
             if bounties.exists():
-                eth_sum = sum([amount for amount in bounty.values_list("value_in_eth", flat=True)])
+                eth_sum = sum([amount for amount in bounties.values_list("value_true", flat=True)])
         except Exception:
             pass
 
