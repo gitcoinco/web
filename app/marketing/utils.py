27488df3b54285ac9babfc0dec2db1cc3778f07c
Dan Lipert <danlipert@gmail.com>
2019-08-08 06:13:26 -0700

    cast first arg to string for sup email (#4991)

--- app/marketing/utils.py
+++ app/marketing/utils.py
@@ -205,7 +205,7 @@ def get_or_save_email_subscriber(email, source, send_slack_invite=True, profile=
     # Prevent syncing for those who match the suppression list
     suppressions = EmailSupressionList.objects.all()
     for suppression in suppressions:
-        if re.match(suppression.email, email):
+        if re.match(str(suppression.email), email):
             return None
 
     # GDPR fallback just in case
