67b67b63fe924c931ae244589e65e61e880608a1
octavioamu <octavioamuchastegui@gmail.com>
2019-08-15 14:53:18 -0300

    fix eslint

--- app/assets/v2/js/base.js
+++ app/assets/v2/js/base.js
@@ -20,11 +20,12 @@ $(document).ready(function() {
   force_no_www();
 
   // Fix multiple modals at same time.
-  $(document).on('show.bs.modal', '.modal', function (event) {
-    var zIndex = 1040 + (10 * $('.modal:visible').length);
+  $(document).on('show.bs.modal', '.modal', function(event) {
+    let zIndex = 1040 + (10 * $('.modal:visible').length);
+
     $(this).css('z-index', zIndex);
     setTimeout(function() {
-        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+      $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
     }, 0);
   });
 
