210eb68891dad46a70998b2ad21e659d1a1d047c
Aditya Anand M C <aditya.anandmc@gmail.com>
2019-08-26 21:00:06 +0530

    Update mails.py

--- app/marketing/mails.py
+++ app/marketing/mails.py
@@ -434,7 +434,7 @@ def warn_account_out_of_eth(account, balance, denomination):
     cur_language = translation.get_language()
     try:
         setup_lang(to_email)
-        subject = account + str(_("is out of gas"))
+        subject = account + str(_(" is out of gas"))
         body_str = _("is down to ")
         body = f"{account } {body_str} {balance} {denomination}"
         if not should_suppress_notification_email(to_email, 'admin'):
