404d316c13b8738695207c6b187a0e5c7785d691
Dan Lipert <danlipert@gmail.com>
2019-08-29 19:49:10 +0900

    mgmt command to select 0 app bounties and fire email

---
+++ app/marketing/management/commands/no_applicants_email.py
@@ -0,0 +1,48 @@
+'''
+    Copyright (C) 2019 Gitcoin Core
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+'''
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db.models import Q
+from django.utils import timezone
+
+from dashboard.models import Bounty
+from marketing.mails import no_applicant_reminder
+
+
+class Command(BaseCommand):
+
+    help = 'sends feedback emails to bounties that were closed in last xx hours, but only if its the first time a bounty has been accepted by taht persona'
+
+    def handle(self, *args, **options):
+        if settings.DEBUG:
+            print("not active in non prod environments")
+            return
+
+        start_time_3_days = timezone.now() - timezone.timedelta(hours=24 * 3)
+        end_time_3_days = timezone.now() - timezone.timedelta(hours=24 * 4)
+
+        start_time_7_days = timezone.now() - timezone.timedelta(hours=24 * 7)
+        end_time_7_days = timezone.now() - timezone.timedelta(hours=24 * 8)
+        bounties = Bounty.objects.current().filter(
+            network='mainnet',
+            (Q(created_on__range=[end_time_3_days, start_time_3_days]) | Q(created_on__range=[end_time_7_days, start_time_7_days]))
+            idx_status='open',
+            )
+
+        for bounty in [b for b in bounties if b.no_of_applicants == 0]:
+            no_applicant_reminder(bounty.bounty_owner_email, bounty)
