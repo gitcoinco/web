bf45a955c092500007cd0fe70da9e63d53c20441
Andrew Redden <opensource@andrewredden.com>
2019-09-06 11:35:11 +0200

    added a filter to exclude users who have already been added to the clr round 3 group

--- app/dashboard/management/commands/setup_grants_clr_3.py
+++ app/dashboard/management/commands/setup_grants_clr_3.py
@@ -15,21 +15,25 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 '''
-
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User, Group, Permission
+from django.contrib.auth.models import Group, Permission, User
+from django.db.models import Q
 
 
 class Command(BaseCommand):
     help = "Setup CLR Round 3"
 
     def handle(self, *args, **options):
-        clr_group_r3 = Group.objects.get_or_create(name="Grants-CLR-Round-3")[0]
+        clr_r3_group_name = "Grants-CLR-Round-3"
+        clr_group_r3 = Group.objects.get_or_create(name=clr_r3_group_name)[0]
         add_clr_match = Permission.objects.get(codename="add_clrmatch")
         clr_group_r3.permissions.add(add_clr_match)
 
-        valid_users = User.objects.filter(profile__data__created_at__lte="2019-03-01").select_related(
-            'profile') | User.objects.filter(
-            profile__trust_profile=True)
+        print('Fetching Valid Grant CLR Round 3 Users')
+        valid_users = User.objects.filter(~Q(groups__name__in=[clr_r3_group_name]),
+                                          profile__data__created_at__lte="2019-03-01") | User.objects.filter(
+            ~Q(groups__name__in=[clr_r3_group_name]), profile__trust_profile=True)
         for user in valid_users:
             user.groups.add(clr_group_r3)
+
+        print('Grant CLR Round 3 Setup Finished')
