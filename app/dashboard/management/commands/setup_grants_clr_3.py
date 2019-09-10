abb583f0ec28e5f2c06834e264af3695dd33fcee
Andrew Redden <opensource@andrewredden.com>
2019-09-10 17:19:42 +0200

    sorted imports to appease jenkins

--- app/dashboard/management/commands/setup_grants_clr_3.py
+++ app/dashboard/management/commands/setup_grants_clr_3.py
@@ -15,8 +15,8 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 '''
-from django.core.management.base import BaseCommand
 from django.contrib.auth.models import Group, Permission, User
+from django.core.management.base import BaseCommand
 from django.db.models import Q
 
 
