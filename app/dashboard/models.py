8f3b4c77f6cb4e4084ac26c95c25e8e82b1c2aa2
Owocki <ksowocki@gmail.com>
2019-09-11 14:26:22 -0600

    correct value in eth

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -3012,7 +3012,7 @@ class Profile(SuperModel):
 
         try:
             if bounties.exists():
-                eth_sum = sum([amount for amount in bounties.values_list("value_in_eth", flat=True)])
+                eth_sum = sum([bounty.get_value_in_eth for bounty in bounties])
         except Exception:
             pass
 
