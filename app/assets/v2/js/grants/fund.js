38f392765ce670aa1b4c03344baaf4002e4a39b7
Dan Lipert <danlipert@gmail.com>
2019-09-21 02:44:56 +0900

    fix clr bug where phantom funding value is $1

--- app/assets/v2/js/grants/fund.js
+++ app/assets/v2/js/grants/fund.js
@@ -514,12 +514,12 @@ const predictPhantomCLRMatch = () => {
 
     var curve_per_grant = clr_prediction_curve_per_grant[grant_id].map(function(value,index) { return value[1]; });
 
-    if (0 < amount && amount < 1) {
+    if (0 <= amount && amount <= 1) {
       x_lower = 0;
       x_upper = 1;
       y_lower = curve_per_grant[0];
       y_upper = curve_per_grant[1];
-    } else if (1 < amount && amount < 10) {
+    } else if (1 < amount && amount <= 10) {
       x_lower = 1;
       x_upper = 10;
       y_lower = curve_per_grant[1];
