dcdfe9bc985fe367599f04b665ae102f8a429b59
Aditya Anand M C <aditya.anandmc@gmail.com>
2019-09-20 18:07:45 +0530

    isort fix

--- app/grants/urls.py
+++ app/grants/urls.py
@@ -20,8 +20,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 from django.urls import path, re_path
 
 from grants.views import (
-    grant_details, grant_fund, grant_new, grant_new_v0, grants, leaderboard, milestones, new_matching_partner,
-    profile, quickstart, subscription_cancel,
+    grant_details, grant_fund, grant_new, grant_new_v0, grants, leaderboard, milestones, new_matching_partner, profile,
+    quickstart, subscription_cancel,
 )
 
 app_name = 'grants'
