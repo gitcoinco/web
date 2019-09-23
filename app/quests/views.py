81ccad649ffc9278812dc1b0412c28cb791997fc
Owocki <ksowocki@gmail.com>
2019-09-22 19:27:30 -0600

    leaderboard

--- app/quests/views.py
+++ app/quests/views.py
@@ -1,17 +1,19 @@
 from django.shortcuts import render
 from django.template.response import TemplateResponse
 from django.http import Http404, JsonResponse
-from quests.models import Quest
+from quests.models import Quest, QuestAttempt
 from django.shortcuts import redirect
+from django.db.models import Count
 import re
 import json
 
 # Create your views here.
 def index(request):
     quests = [(ele.is_unlocked_for(request.user), ele.is_beaten(request.user), ele) for ele in Quest.objects.all()]
-    
+    leaderboard = QuestAttempt.objects.filter(success=True).order_by('profile').values_list('profile__handle').annotate(amount=Count('quest', distinct=True)).order_by('-amount')
     params = {
         'quests': quests,
+        'leaderboard': leaderboard,
         'title': 'Quests on Gitcoin',
         'card_desc': 'Use Gitcoin to learn about the Ethereum ecosystem and level up while you do it!',
     }
