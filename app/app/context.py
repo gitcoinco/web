d1686b11087663ce7038b60b4b02e32d0a33fa07
Owocki <ksowocki@gmail.com>
2019-10-07 14:12:52 +0900

    logs referrer and path for visit useractions

--- app/app/context.py
+++ app/app/context.py
@@ -60,7 +60,6 @@ def preprocess(request):
         record_visit = not profile.last_visit or profile.last_visit < (
             timezone.now() - timezone.timedelta(seconds=RECORD_VISIT_EVERY_N_SECONDS)
         )
-
         if record_visit:
             ip_address = get_ip(request)
             profile.last_visit = timezone.now()
@@ -69,7 +68,11 @@ def preprocess(request):
                 profile.save()
             except Exception as e:
                 logger.exception(e)
-            metadata = {'useragent': request.META['HTTP_USER_AGENT'], }
+            metadata = {
+                'useragent': request.META['HTTP_USER_AGENT'],
+                'referrer': request.META.get('HTTP_REFERER', None),
+                'path': request.META.get('PATH_INFO', None),
+            }
             UserAction.objects.create(
                 user=request.user,
                 profile=profile,
