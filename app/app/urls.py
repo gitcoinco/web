418f790e111ca71df549c9a416b3318e202f1fb9
Owocki <ksowocki@gmail.com>
2019-10-25 12:30:53 -0600

    fix for https://gitcoincore.slack.com/archives/CAXQ7PT60/p1572026398138300

--- app/app/urls.py
+++ app/app/urls.py
@@ -220,6 +220,7 @@ urlpatterns = [
     ),
 
     # Rating
+    path('modal/rating/<int:bounty_id>/<str:username>', dashboard.views.rating_modal, name='rating_modal2'),
     path('modal/rating/<int:bounty_id>/<str:username>/', dashboard.views.rating_modal, name='rating_modal'),
     path('modal/rating_capture/', dashboard.views.rating_capture, name='rating_capture'),
     url(r'^api/v0.1/unrated_bounties/', dashboard.views.unrated_bounties, name='unrated_bounties'),
