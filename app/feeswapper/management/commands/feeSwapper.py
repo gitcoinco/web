dba645f007cf009c2b439ffd9412b58cb71d2deb
Owocki <ksowocki@gmail.com>
2019-10-29 13:29:52 -0600

    fixes https://sentry.io/organizations/gitcoin/issues/1294526806/

--- app/feeswapper/management/commands/feeSwapper.py
+++ app/feeswapper/management/commands/feeSwapper.py
@@ -192,14 +192,14 @@ class Command(BaseCommand):
                         self.factoryAddress = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95'
 
                 # Set up factory contract
-                with open("app/feeswapper/management/commands/factoryABI.json","r") as factoryABIfile:
+                with open("feeswapper/management/commands/factoryABI.json","r") as factoryABIfile:
                         self.factoryABI = json.load(factoryABIfile)
                 self.factoryContract = self.web3.eth.contract(address = self.factoryAddress, abi = self.factoryABI)
 
                 # Get token and contract ABIs
-                with open("app/feeswapper/management/commands/tokenABI.json","r") as tokenABIfile:
+                with open("feeswapper/management/commands/tokenABI.json","r") as tokenABIfile:
                         self.tokenABI = json.load(tokenABIfile)
-                with open("app/feeswapper/management/commands/exchangeABI.json","r") as exchangeABIfile:
+                with open("feeswapper/management/commands/exchangeABI.json","r") as exchangeABIfile:
                         self.exchangeABI = json.load(exchangeABIfile)
                 
                 
