fa504954b80133586da4b511a523418c1fcde4e7
Owocki <ksowocki@gmail.com>
2019-10-31 15:25:09 -0600

    make fix + css fixes for new quests form

--- app/assets/v2/js/pages/quests.new.js
+++ app/assets/v2/js/pages/quests.new.js
@@ -16,9 +16,10 @@ $(document).ready(function() {
   });
   $(document).on('click', '.new_quest_background', function(e) {
     e.preventDefault();
-    $(".new_quest_background").removeClass('selected');
+    $('.new_quest_background').removeClass('selected');
     var clone = $(this).addClass('selected');
-    $("#background").val($(this).data('value'));
+
+    $('#background').val($(this).data('value'));
 
     dupe_me.after(clone);
   });
