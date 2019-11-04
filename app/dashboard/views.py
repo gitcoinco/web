7cca8f60abc2707f046e6f1b89289139f98ffd15
Owocki <ksowocki@gmail.com>
2019-11-04 10:29:57 -0700

    fix for https://github.com/gitcoinco/web/issues/5259#issuecomment-549055456

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -3671,7 +3671,7 @@ def funder_dashboard(request, bounty_type):
             Q(idx_status='open') | Q(override_status='open'),
             current_bounty=True,
             network=network,
-            bounty_owner_github_username=profile.handle,
+            bounty_owner_github_username__iexact=profile.handle,
             ).order_by('-interested__created', '-web3_created'))
         interests = list(Interest.objects.filter(
             bounty__pk__in=[b.pk for b in bounties],
@@ -3683,7 +3683,7 @@ def funder_dashboard(request, bounty_type):
             Q(idx_status='started') | Q(override_status='started'),
             current_bounty=True,
             network=network,
-            bounty_owner_github_username=profile.handle,
+            bounty_owner_github_username__iexact=profile.handle,
             ).order_by('-interested__created', '-web3_created'))
         interests = list(Interest.objects.filter(
             bounty__pk__in=[b.pk for b in bounties],
@@ -3696,7 +3696,7 @@ def funder_dashboard(request, bounty_type):
             current_bounty=True,
             network=network,
             fulfillments__accepted=False,
-            bounty_owner_github_username=profile.handle,
+            bounty_owner_github_username__iexact=profile.handle,
             )
         bounties.order_by('-fulfillments__created_on')
         return JsonResponse(serialize_funder_dashboard_submitted_rows(bounties), safe=False)
@@ -3706,7 +3706,7 @@ def funder_dashboard(request, bounty_type):
             Q(idx_status='expired') | Q(override_status='expired'),
             current_bounty=True,
             network=network,
-            bounty_owner_github_username=profile.handle,
+            bounty_owner_github_username__iexact=profile.handle,
             ).order_by('-expires_date')
 
         return JsonResponse([{'title': b.title,
