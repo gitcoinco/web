6dee6dd5489f6af30e62f5da387d45cb14d4645c
mulili <mulili.nzuki@gmail.com>
2019-11-06 16:42:12 +0300

    removed my tokens

--- scripts/create_stickers_emojis/config.js
+++ scripts/create_stickers_emojis/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	// for telegram
 	telegramBotToken: "",
 	// for discord
-	discordBotToken: "NjI4MjAyODUxMDg4MzM0ODcx.XbgbTQ.puzWUePZAgjl1g7lTL90D-L2NsY",
-	discordBotClientId: "628202851088334871",
-	discordServerId: "628121685568127006"
+	discordBotToken: "",
+	discordBotClientId: "",
+	discordServerId: ""
 };
