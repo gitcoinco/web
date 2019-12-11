5654ad661c96e954982a963f3b1fbc18a7028ff3
Kevin Owocki <ksowocki@gmail.com>
2019-12-11 05:05:00 -0700

    bufficorn avatar builder (#5621)

--- app/assets/v2/js/avatar3d.js
+++ app/assets/v2/js/avatar3d.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-  $('#skin_tones li:nth-child(6)').addClass('selected');
-  $('#hair_tones li:nth-child(3)').addClass('selected');
+  $('#skin_tones li:nth-child(1)').addClass('selected');
+  $('#hair_tones li:nth-child(1)').addClass('selected');
   document.td_ids = [];
   document.skin_tone = $('#skin_tones li.selected').data('tone');
   document.hair_tone = $('#hair_tones li.selected').data('tone');
