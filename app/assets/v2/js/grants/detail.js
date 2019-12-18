95b0fc46807b35c526dbca7192a41b87faa88cb4
Paul <41552663+molecula451@users.noreply.github.com>
2019-12-17 22:51:48 -0400

    Update detail.js

--- app/assets/v2/js/grants/detail.js
+++ app/assets/v2/js/grants/detail.js
@@ -1,7 +1,7 @@
 const editableFields = [
   '#form--input__title',
   '#form--input__reference-url',
-  '#contract_owner_address',
+  '#gran_contract_owner_address',
   '#grant-members',
   '#amount_goal'
 ];
