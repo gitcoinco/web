ea7bf30cca21de409d7793fc7c619a41a7eec8cb
Paul <41552663+molecula451@users.noreply.github.com>
2019-12-21 16:18:09 -0400

    Update detail.js

--- app/assets/v2/js/grants/detail.js
+++ app/assets/v2/js/grants/detail.js
@@ -15,7 +15,8 @@ $(document).ready(function() {
       $('#grant-admin').text(),
       $('#contract_owner_address').text(),
       '#cancel_grant',
-      'Looks like your grant has been created with ' + $('#contract_owner_address').text() + '. Switch to take action on your grant.'
+      'Looks like your grant has been created with ' + 
+      $('#contract_owner_address').text() + '. Switch to take action on your grant.'
     );
 
     if ($('#cancel_grant').attr('disabled')) {
