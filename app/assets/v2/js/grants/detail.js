bf53ca004dd677097d118e5948eda3e25878454d
Paul <41552663+molecula451@users.noreply.github.com>
2019-12-22 00:38:41 -0400

    Update detail.js

--- app/assets/v2/js/grants/detail.js
+++ app/assets/v2/js/grants/detail.js
@@ -13,10 +13,10 @@ $(document).ready(function() {
   setInterval (() => {
     notifyOwnerAddressMismatch(
       $('#grant-admin').text(),
-      $('#contract_owner_address').html(),
+      $('#contract_owner_address').text(),
       '#cancel_grant',
       'Looks like your grant has been created with ' + 
-      $('#grant_contract_owner_address').html() + '. Switch to take action on your grant.'
+      $('#grant_contract_owner_address').text() + '. Switch to take action on your grant.'
     );
 
     if ($('#cancel_grant').attr('disabled')) {
