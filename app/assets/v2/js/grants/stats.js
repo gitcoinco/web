acd510cdfb672299313f673b005027cac74aaa44
Owocki <ksowocki@gmail.com>
2019-12-26 09:58:32 -0700

    adds graphs showing the contribution amounts, by type, over time, for each gitcoin grant

---
+++ app/assets/v2/js/grants/stats.js
@@ -0,0 +1,48 @@
+google.charts.load('current', { packages: [ 'corechart', 'bar' ]});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+  var target = document.getElementById('grant_chart');
+
+  if (!target) {
+    return;
+  }
+  var data = google.visualization.arrayToDataTable(document.history);
+
+  var view = new google.visualization.DataView(data);
+  var width = parseInt(500);
+
+  var ticks = [];
+  var increment = 100;
+
+  if (document.max_graph > 1000) {
+    increment = 1000;
+  }
+  if (document.max_graph > 10000) {
+    increment = 10000;
+  }
+
+  for (var i = 0; (i * increment) < document.max_graph; i += 1) {
+    ticks.push(i * increment);
+  }
+
+  var options = {
+    legend: { position: 'none' },
+    bar: { groupWidth: '75%' },
+    colors: [ '#011f4b', '#03396c', '#005b96', '#b3cde0' ],
+    isStacked: true,
+    backgroundColor: 'transparent',
+    height: 400,
+    width: width,
+    vAxis: { title: 'USD', ticks: ticks, format: 'short', gridlines: { color: 'transparent' } }
+  };
+
+  var chart = new google.visualization.ColumnChart(target);
+
+  chart.draw(view, options);
+}
+
+$(window).resize(function() {
+  drawChart();
+
+});
