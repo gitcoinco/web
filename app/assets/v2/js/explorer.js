283f71e878e7920ebf2bdcacb3a812a2c93758cd
octavioamu <octavioamuchastegui@gmail.com>
2020-01-04 01:58:04 -0300

    fix logic and styles

--- app/assets/v2/js/explorer.js
+++ app/assets/v2/js/explorer.js
@@ -1,4 +1,5 @@
 let bounties = [];
+let featuredBounties = [];
 let bountiesPage = 1;
 let BountiesLimit = 10;
 let BountiesOffset = 0;
@@ -18,26 +19,26 @@ let paramsDefault = {
   permission_type: [],
   project_length: [],
   bounty_type: [],
-  network: ['mainnet']
-}
+  network: ['mainnet'],
+  keywords: []
+};
+
+Vue.filter('stringFixer', (str) => {
+  return str.replace(/_/i, ' ');
+});
 
 Vue.mixin({
   methods: {
-    fetchBounties: function(newPage) {
+    fetchBounties: function(newPage, featured) {
       let vm = this;
 
       vm.isLoading = true;
       vm.noResults = false;
-      // vm.params.network = 'rinkeby';
-      // vm.params.page = vm.bountiesPage;
       vm.params.limit = vm.BountiesLimit;
-      if (newPage || newPage === 0 ) {
-        // vm.bountiesPage = newPage;
+      if (newPage || newPage === 0) {
         vm.BountiesOffset = newPage;
       }
       vm.params.offset = vm.BountiesOffset;
-      console.log(vm.params.limit, vm.params.offset)
-
       if (vm.searchTerm) {
         vm.params.search = vm.searchTerm;
       } else {
@@ -45,35 +46,41 @@ Vue.mixin({
       }
 
       let searchParams = new URLSearchParams(vm.params);
+
       window.history.replaceState({}, '', `${location.pathname}?${searchParams}`);
 
-
       let apiUrlBounties = `/api/v0.1/bounties/slim/?${searchParams.toString()}`;
+
+      if (featured) {
+        apiUrlBounties = `/api/v0.1/bounties/slim/?${searchParams.toString()}&is_featured=True`;
+      }
 
       var getBounties = fetchData (apiUrlBounties, 'GET');
 
       $.when(getBounties).then(function(response) {
 
         response.forEach(function(item) {
-          vm.bounties.push(item);
+          if (featured) {
+            vm.featuredBounties.push(item);
+          } else {
+            vm.bounties.push(item);
+          }
         });
 
-        // vm.bountiesNumPages = response.num_pages;
+        if (featured) {
+          return vm.featuredBounties;
+        }
         vm.numBounties = response.length;
         if (vm.numBounties < vm.BountiesLimit) {
           vm.bountiesHasNext = false;
         } else {
           vm.bountiesHasNext = true;
         }
-        // vm.bountiesHasNext = response.has_next;
         if (vm.bountiesHasNext) {
           vm.BountiesOffset = vm.BountiesOffset + vm.BountiesLimit;
-        //   vm.bountiesPage = ++vm.bountiesPage;
 
         } else {
-        //   vm.bountiesPage = 1;
           vm.BountiesOffset = 0;
-
         }
 
         if (vm.bounties.length) {
@@ -91,17 +98,24 @@ Vue.mixin({
       const params = new URLSearchParams(url.search);
 
       for (let p of params) {
-        console.log(p, typeof vm.params[p[0]])
-        vm.params[p[0]] = typeof vm.params[p[0]] === 'object' ? p[1].split(',') : p[1];
+        if (typeof vm.params[p[0]] === 'object') {
+          if (p[1].length > 0) {
+            vm.params[p[0]] = p[1].split(',');
+          } else {
+            vm.$delete(vm.params[p[0]]);
+          }
+        } else {
+          vm.params[p[0]] = p[1];
+        }
       }
     },
     searchBounties: function() {
       let vm = this;
 
       vm.bounties = [];
+      vm.featuredBounties = [];
       vm.fetchBounties(0);
-      console.log( 'here')
-
+      vm.fetchBounties(0, true);
     },
     bottomVisible: function() {
       let vm = this;
@@ -120,33 +134,28 @@ Vue.mixin({
     },
     clearParams() {
       let vm = this;
-      vm.params = paramsDefault;
-    },
-    // check() {
-    //   let vm = this;
 
-    //   vm.params['experience_level'] =  vm.experienceSelected;
-    // },
+      vm.params = JSON.parse(JSON.stringify(paramsDefault));
+    },
     closeModal() {
       this.$refs['user-modal'].closeModal();
     },
+    removeFilter(prop, index, obj) {
+      let vm = this;
+
+      if (obj) {
+        vm.$delete(vm.params[prop], index);
+      } else {
+        vm.$set(vm.params, prop, '');
+      }
+    },
     popoverConfig(bounty) {
-      console.log(bounty)
-      // Both title and content specified as a function in this example
-      // and will be called the each time the popover is opened
+      moment.locale('en');
       return `
         <div>
           <div class="popover-bounty__content">
             <div class="d-flex justify-content-between mb-2">
-              <span class="status-${bounty.status}">
-                <i class="g-icon g-icon__dot-circle"></i>
-                ${bounty.status === 'open' ? 'Ready to work' :
-                  bounty.status === 'started' ? 'Work Started' :
-                  bounty.status === 'submitted' ? 'Work Submitted' :
-                  bounty.status === 'done' ? 'Work Done' :
-                  bounty.status === 'cancelled' ? 'Cancelled' :
-                  bounty.status === 'expired' ? 'Expired' : ''}
-              </span>
+              <span class="status-${bounty.status}"> <i class="g-icon g-icon__dot-circle"></i> ${bounty.status === 'open' ? 'Ready to work' : bounty.status === 'started' ? 'Work Started' : bounty.status === 'submitted' ? 'Work Submitted' : bounty.status === 'done' ? 'Work Done' : bounty.status === 'cancelled' ? 'Cancelled' : bounty.status === 'expired' ? 'Expired' : ''}</span>
               <span><img src="${bounty.avatar_url}" alt="${bounty.github_org_name}" class="rounded-circle ${bounty.github_org_name}" height="14"> ${bounty.github_org_name}</span>
             </div>
             <div class="font-subheader title text-truncate">${bounty.title}</div>
@@ -154,7 +163,7 @@ Vue.mixin({
               ${bounty.keywords.split(',').map((keyword) => `<span class="tag keyword">${keyword}</span>`).join(' ')}
             </div>
             <div class="mt-2">
-              ${bounty.expires_date ? `<i class="far fa-clock expired-icon"></i> ${moment.utc(bounty.expires_date).fromNow()}` : '' }
+              ${moment(bounty.expires_date).diff(moment(), 'years') < 1 ? `<i class="far fa-clock expired-icon"></i> ${moment(bounty.expires_date) < moment() ? 'Expired' : 'Expires'}  ${moment.utc(bounty.expires_date).fromNow()}` : 'Never expires' }
             </div>
           </div>
           ${ bounty.latest_activity ? `
@@ -164,21 +173,21 @@ Vue.mixin({
                   <span>${bounty.latest_activity.activity_type.replace(/_/i, ' ')} by <b>${bounty.latest_activity.profile.handle}</b></span>
                   <span>${moment.utc(bounty.latest_activity.created).fromNow()}</span>
                 </div>
-            </div>`
-          : ''}
+            </div>` : ''}
 
-        </div>`
+        </div>`;
 
     }
   }
 });
 
 if (document.getElementById('gc-explorer')) {
-  var app = new Vue({
+  var appExplorer = new Vue({
     delimiters: [ '[[', ']]' ],
     el: '#gc-explorer',
     data: {
       bounties,
+      featuredBounties,
       bountiesPage,
       BountiesLimit,
       BountiesOffset,
@@ -188,7 +197,7 @@ if (document.getElementById('gc-explorer')) {
       media_url,
       searchTerm: null,
       bottom: false,
-      experienceSelected:[],
+      experienceSelected: [],
       showExtraParams: false,
       params: {
         order_by: '-web3_created',
@@ -202,57 +211,29 @@ if (document.getElementById('gc-explorer')) {
         permission_type: [],
         project_length: [],
         bounty_type: [],
-        network: ['mainnet']
+        network: ['mainnet'],
+        keywords: []
       },
-      // experience_options: [
-      //   {
-      //     id: 1,
-      //     name: 'beginner',
-      //   },
-      //   {
-      //     id: 2,
-      //     name: 'intermediate',
-      //   },
-      //   {
-      //     id: 3,
-      //     name: 'advanced',
-      //   }
-      // ],
       showFilters: true,
       noResults: false,
       isLoading: true
     },
-    computed:{
-      // flatSelected () {
-      //   return this.params.experience_level.reduce((acc, cur) => [ ...acc, ...cur ], [])
-      // }
-      foos(){
-
-          // return this.params.experience_level = this.experienceSelected
-
-        // return this.experienceSelected.map(item => this.params.experience_level = item);
-        // this.check()
-      },
-
-
-
-    },
     mounted() {
       this.getUrlParams();
       this.fetchBounties();
+      this.fetchBounties(0, true);
       this.$watch('params', function(newVal, oldVal) {
         this.searchBounties();
       }, {
         deep: true
       });
     },
-    created() {
-      // this.fetchBounties();
-    },
     updated() {
-      $(function () {
-          $('[data-toggle="popover"]').popover()
-      })
+      $(function() {
+        $('[data-toggle="popover"]').popover();
+      }),
+      scrollSlider($('#featured-card-container'), 288);
+
     },
     beforeMount() {
       window.addEventListener('scroll', () => {
@@ -267,6 +248,29 @@ if (document.getElementById('gc-explorer')) {
   });
 }
 
-Vue.filter('stringFixer', (str) => {
-  return str.replace(/_/i, ' ');
-});
+function scrollSlider(element, cardSize) {
+  const arrowLeft = $('#arrowLeft');
+  const arrowRight = $('#arrowRight');
+
+  arrowLeft.on('click', function() {
+    element[0].scrollBy({left: -cardSize, behavior: 'smooth'});
+  });
+  arrowRight.on('click', function() {
+    element[0].scrollBy({left: cardSize, behavior: 'smooth'});
+  });
+
+  element.on('scroll mouseenter', function() {
+    if (this.clientWidth === (this.scrollWidth - this.scrollLeft)) {
+      arrowRight.hide();
+    } else {
+      arrowRight.show();
+    }
+
+    if (this.scrollLeft < 10) {
+      arrowLeft.hide();
+    } else {
+      arrowLeft.show();
+    }
+  });
+}
+
