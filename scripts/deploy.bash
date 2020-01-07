a645f4018652e75f29c848c21b09ee813abf3003
Andrew Redden <opensource@andrewredden.com>
2020-01-07 15:27:02 -0400

    remove reliance on the z check to ensure that if check passes correctly on all hosts

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -79,8 +79,7 @@ fi
 
 # let gunicorn know its ok to restart
 if ! [ "$JOBS_NODE" ]; then
-    if ! [ -z CELERY_NODE  ];
-    then
+    if ! [ "$CELERY_NODE"  ]; then
       echo "- gunicorn"
       for pid in $(pgrep -fl "gunicorn: worke" | awk '{print $1}'); do
       sudo kill -1 $pid
