e0207cc6460c580ee2f49d13bc3c29ff6f61e36f
Owocki <ksowocki@gmail.com>
2020-01-10 11:50:30 -0700

    make fix; and grant fix

--- app/assets/v2/js/pages/chat.js
+++ app/assets/v2/js/pages/chat.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
-  $("a.comingsoon").click(function(e){
-      alert("Coming soon!");
-      e.preventDefault();
-  })
+  $('a.comingsoon').click(function(e) {
+    alert('Coming soon!');
+    e.preventDefault();
+  });
 });
