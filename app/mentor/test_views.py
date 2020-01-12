66c8d503adc116525984eb79e6783e8807ef651f
Miguel Angel G <miguel@gordian.dev>
2020-01-12 06:41:57 -0600

    Add minimal views to create a session

---
+++ app/mentor/test_views.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""Test the Kudos Views.
+
+Copyright (C) 2020 Gitcoin Core
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+import logging
+from unittest import skip
+
+from django.test import Client, TestCase
+
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("web3").setLevel(logging.WARNING)
