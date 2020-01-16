64f2c31657de775a4820db2dd1724a794a75a9a2
Owocki <ksowocki@gmail.com>
2020-01-16 13:34:56 -0700

    button to copy/auto-schedule offers

--- app/townsquare/admin.py
+++ app/townsquare/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils import timezone
 
 from .models import Announcement, Comment, Flag, Like, Offer, OfferAction
 
@@ -17,6 +18,29 @@ class OfferActionAdmin(admin.ModelAdmin):
 class OfferAdmin(admin.ModelAdmin):
     list_display = ['created_on', 'key', 'valid_from', 'valid_to', '__str__']
     raw_id_fields = ['persona', 'created_by']
+
+    def response_change(self, request, obj):
+        if "_copy_offer" in request.POST:
+            obj.pk = None
+            days = 1 if obj.key == 'daily' else 7
+            if obj.key == 'monthly':
+                days = 30
+            obj.valid_from = timezone.now() - timezone.timedelta(days=days)
+            while True:
+                next_timestamp = obj.valid_from + timezone.timedelta(days=days)
+                other_offers = Offer.objects.filter(valid_from__gte=obj.valid_from, valid_from__lt=next_timestamp, key=obj.key)
+                if not other_offers.exists():
+                    break
+                obj.valid_from = next_timestamp
+            obj.valid_from = obj.valid_from + timezone.timedelta(days=days)
+            obj.valid_from = obj.valid_from - timezone.timedelta(hours=int(obj.valid_from.strftime('%H')))
+            obj.valid_from = obj.valid_from - timezone.timedelta(minutes=int(obj.valid_from.strftime('%M')))
+            obj.valid_from = obj.valid_from - timezone.timedelta(seconds=int(obj.valid_from.strftime('%S')))
+            obj.valid_to = obj.valid_from + timezone.timedelta(days=days)
+            self.message_user(request, f"Copied Object")
+
+            obj.save()
+        return super().response_change(request, obj)
 
 
 class AnnounceAdmin(admin.ModelAdmin):
