dc5c25cad2015b994c126235e1fbb644b4b8a9b3
Loic Coenen <loic.coenen@email.com>
2020-01-18 23:08:07 +0000

    Whole flow

--- app/assets/v2/js/pages/mentor_active_session.js
+++ app/assets/v2/js/pages/mentor_active_session.js
@@ -1,4 +1,56 @@
-const closer_multiple = (deposit, delta) => deposit - (deposit % delta);
+// Test DAI have 18 decimals
+const TESTDAI_DECIMAL = Math.pow(10, 18);
+// Waiting time between approval and attempting to call the contract (in ms)
+const CONFIRMATION_WAIT_TIME = 30000;
+// Delay before stream startTime (in ms)
+const STREAM_START_TIME_DELAY = 300000;
+
+// Find the closer multiple
+const closerMultiple = (deposit, delta) => deposit - (deposit % delta);
+// Show an element if a condition is filled, hide it otherwise
+const showIf = (condition, element) =>
+  conndition ? element.show() : element.hide();
+
+const startEarningRefresh = function(stream) {
+  const { startTime, endTime } = stream;
+  const refresh = setInterval(() => {
+    const now = Math.round(new Date().getTime() / 1000);
+    const diff = startTime - now;
+
+    $(".diff .min").text(Math.floor(diff / 60));
+    $(".diff .sec").text(diff % 60);
+
+    const total = endTime - startTime;
+    $(".total .min").text(Math.floor(total / 60));
+    $(".total .sec").text(total % 60);
+
+    const depositDai = stream.deposit / TESTDAI_DECIMAL;
+    $(".deposit-dai").text(depositDai.toFixed(2));
+
+    const streamedDai = ((diff / total) * deposit) / TESTDAI_DECIMAL;
+    $(".streamed-dai").text(streamedDai.toFixed(2));
+  });
+};
+
+const startStreamCountdown = function(stream) {
+  const { startTime, endTime } = stream;
+  const countDown = setInterval(() => {
+    const now = Math.round(new Date().getTime() / 1000);
+    const diff = startTime - now;
+    const diffMin = Math.floor(diff / 60);
+    const diffSec = y % 60;
+    $(".wait-stream .min").text(diffMin);
+    showIf(diffMin < 0, $(".wait-stream .if-min"));
+    $(".wait-stream .sec").text(diffSec);
+
+    if (now > startTime) {
+      $(".main").show();
+      $(".wait-stream").hide();
+      clearInterval(countDown);
+      startEarningRefresh({ startTime, endTime });
+    }
+  }, 1000);
+};
 
 $(document).ready(function() {
   // Connect the videoplayer
@@ -16,81 +68,121 @@ $(document).ready(function() {
 
   $(".main").hide();
 
-  ethereum.enable().then(address => {
-    // Check if the stream is ongoing
+  ethereum.enable().then(([address]) => {
+    // Pool to recover stream from Sablier API
 
+    console.log("address", address);
     const query = `
-					{
-						streams (where:{recipient: "${address}"}) {
-							id
-							deposit
-							sender
-							recipient
-						}
-					}
-				`;
+			{
+				streams (where: {recipient: "${room_address}"}) {
+					id
+					deposit
+					sender
+					recipient
+					startTime
+					stopTime
+				}
+			}
+		`;
 
-    setInterval(() => {
-      fetch("https://api.thegraph.com/subgraphs/name/sablierhq/sablier", {
-        method: "POST",
-        body: query
-      }).then(answer => {
-        if (answer.data.streams.length) {
-          // Check beginning and end
-          // Show the stream
-          $(".main").show();
-          // Hide the form and the waiting message
-          $(".out").hide();
+    console.log("query", query);
+
+    const pooling = setInterval(() => {
+      fetch(
+        "https://api.thegraph.com/subgraphs/name/sablierhq/sablier-rinkeby",
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ query })
         }
-      });
+      )
+        .then(answer => answer.json())
+        .then(answer => {
+          console.log("answer", answer);
+          // Find if there's a next stream
+          if (answer.data.streams.length) {
+            const now = Math.round(new Date().getTime() / 1000);
+            const streams = answer.data.streams.sort(
+              (a, b) => a.startTime < b.startTime
+            );
+            console.log("streams", streams);
+
+            const ongoingStream = streams.find(
+              stream => stream.startTime < now && now < stream.endTime
+            );
+						console.log('ongoingStream', ongoingStream);
+
+            if (ongoingStream) {
+							startEarningRefresh(ongoingStream);	
+              clearInterval(pooling);
+							$('.main').show()
+							$('.wait').hide()
+            }
+
+            const nextStream = streams.find(stream => stream.startTime > now);
+            console.log("nextStream", nextStream);
+
+            if (nextStream) {
+              startStreamCountdown(nextStream);
+              clearInterval(pooling);
+
+              $(".main").show();
+              $(".wait").hide();
+            }
+          }
+        });
     }, 10000);
 
     // Check if the user is the room owner
-    console.warn("room_address == address", room_address === address);
     if (room_address === address) {
       $(".wait").show();
     } else {
       $(".create-stream").show();
     }
 
+		// Load contracts
+
+		const sablier_contract = web3.eth
+			.contract(sablier_abi)
+			.at(sablier_address());
+		const testdai_contract = web3.eth
+			.contract(token_abi)
+			.at(testdai_address());
+
     // Create the stream
 
     $(".create-btn").click(() => {
-      const deposit_min = parseInt($("#deposit").val());
-			if(deposit_min === NaN) return;
-
-      const sablier_contract = web3.eth
-        .contract(sablier_abi)
-        .at(sablier_address());
-      const testdai_contract = web3.eth
-        .contract(token_abi)
-        .at(testdai_address());
+      const depositMin = parseInt($("#deposit").val());
+      if (depositMin === NaN) return;
+      // TODO: show an error message
 
       const mentor_address = room_address;
 
       // Compute a deposit_rounded (deposit should be a multiple of delta)
       const now = Math.round(new Date().getTime() / 1000);
-      const startTime = now + 3600;
-      const stopTime = now + 3600 * 2;
+      const startTime = now + STREAM_START_TIME_DELAY;
+      const stopTime = now + STREAM_START_TIME_DELAY + depositMin;
       const delta = stopTime - startTime;
       const deposit = deposit_min * rate_per_min;
-      const deposit_rounded = closer_multiple(deposit, delta);
+      const deposit_rounded = closerMultiple(deposit, delta);
 
       if (deposit_rounded % delta) throw "Not multiple";
-			if (now > startTime) throw 'startTime should be in the future';
-			if (startTime > stopTime) throw 'endTime should be after startTime';
+      if (now > startTime) throw "startTime should be in the future";
+      if (startTime > stopTime) throw "endTime should be after startTime";
 
       console.warn("mentor_address", mentor_address);
 
       console.warn("deposit", deposit);
       console.warn("deposit_rounded", deposit_rounded);
-			console.warn("deposit in dai", deposit / 1000000000000000000);
+      console.warn("deposit in dai", deposit / 1000000000000000000);
       console.warn("now", now);
       console.warn("startTime", startTime);
       console.warn("stopTime", stopTime);
 
       testdai_contract.approve(sablier_address(), deposit, () => {
         setTimeout(() => {
+          $(".wait-stream-register").show();
+          $(".create-stream").hide();
           console.warn(
             "creating the stream at ",
             Math.round(new Date().getTime() / 1000)
@@ -106,11 +198,9 @@ $(document).ready(function() {
               console.warn("DONE", Math.round(new Date().getTime() / 1000));
             }
           );
-        }, 30000);
+        }, CONFIRMATION_WAIT_TIME);
       });
     });
-
-    // Update data during streaming
 
     // Stop the stream
 
