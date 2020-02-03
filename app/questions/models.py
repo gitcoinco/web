dc6d1d3de9eb2fafe1af07bc1983c6133d08932d
Dan Lipert <danlipert@gmail.com>
2020-02-03 21:38:54 +0900

    forgot model changes

--- app/questions/models.py
+++ app/questions/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from rest_framework import serializers, viewsets
 import django_filters
 
+from dashboard.models import ProfileSerializer, TipSerializer
 from economy.models import SuperModel
 
 
@@ -13,6 +14,7 @@ class Question(SuperModel):
                               related_name='questions', blank=True, null=True)
     value_in_eth = models.DecimalField(default=1, decimal_places=2,
                                        max_digits=50)
+    tip = models.ForeignKey('dashboard.Tip', blank=True, null=True, on_delete=models.SET_NULL, related_name='question')
 
 
 class Answer(SuperModel):
@@ -23,18 +25,23 @@ class Answer(SuperModel):
     question = models.ForeignKey(Question, on_delete=models.CASCADE,
                                  related_name='answers')
     is_accepted = models.BooleanField(default=False)
+    tip = models.ForeignKey('dashboard.Tip', blank=True, null=True, on_delete=models.SET_NULL, related_name='answer')
 
 
 class AnswerSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(read_only=True)
+    tip = TipSerializer(read_only=True)
 
     class Meta:
         model = Answer
-        fields = ('text', 'owner', 'question', 'is_accepted', 'id')
+        fields = ('text', 'owner', 'question', 'is_accepted', 'id', 'tip',)
 
 
 class QuestionSerializer(serializers.ModelSerializer):
     answers = AnswerSerializer(many=True, read_only=True)
+    owner = ProfileSerializer(read_only=True)
+    tip = TipSerializer(read_only=True)
 
     class Meta:
         model = Question
-        fields = ('text', 'owner', 'value_in_eth', 'id', 'answers')
+        fields = ('text', 'owner', 'value_in_eth', 'id', 'answers', 'tip',)
