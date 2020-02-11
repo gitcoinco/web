5cee2b605c79b3e83509f058d5798e93dbe82c2a
Aditya Anand M C <aditya.anandmc@gmail.com>
2020-02-11 13:55:03 +0530

    Update activity.js

--- app/assets/v2/js/activity.js
+++ app/assets/v2/js/activity.js
@@ -99,13 +99,13 @@ $(document).ready(function() {
   // delete activity
   $(document).on('click', '.delete_activity', function(e) {
     e.preventDefault();
-      if (!document.contxt.github_handle) {
-        _alert('Please login first.', 'error');
-        return;
+    if (!document.contxt.github_handle) {
+      _alert('Please login first.', 'error');
+      return;
     }
 
-      if (!confirm('Are you sure you want to delete this?')) {
-        return;
+    if (!confirm('Are you sure you want to delete this?')) {
+      return;
     }
 
     // update UI
