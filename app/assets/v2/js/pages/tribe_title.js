165e00dcbf1f0c5f022057f12db0ddfe4d8aa75a
Dan Lipert <danlipert@gmail.com>
2020-02-13 00:14:30 +0900

    extra cleanup

--- app/assets/v2/js/pages/tribe_title.js
+++ app/assets/v2/js/pages/tribe_title.js
@@ -5,7 +5,6 @@ const tribeTitle = () => {
       let title = prompt('Enter a new title for your tribe member');
       const memberId = $(elem).data('tribetitle');
       const url = '/tribe/title/';
-      const template = '<span class="tribe_title text-center p-1 text-highlight-green">' + title + '</span>';
 
       const sendTitle = fetchData (
         url,
@@ -16,7 +15,6 @@ const tribeTitle = () => {
 
       $.when(sendTitle).then(function(response) {
         $(elem).closest('.card').find('.tribe_title').text(title);
-        $(elem).before(template);
         $(elem).attr('disabled', false);
       }).fail(function(error) {
         console.log(error);
