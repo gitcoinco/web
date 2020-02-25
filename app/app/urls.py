dbe94756f56edf34cd20eb4fc3506ef3431de9d0
1337haxxor <1337haxxor@gmail.com>
2020-02-24 21:40:35 -0700

    allow new articles to be written AND updates nav

--- app/app/urls.py
+++ app/app/urls.py
@@ -61,6 +61,9 @@ from kudos.router import router as kdrouter
 from .sitemaps import sitemaps
 
 urlpatterns = [
+
+    path('docs/', include('wiki.urls')),
+
     # oauth2 provider
     url('^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
     path('jsi18n/', JavaScriptCatalog.as_view(), name='javascript-catalog'),
@@ -658,7 +661,6 @@ urlpatterns = [
 
     # wiki
     path('docs/notifications/', include('django_nyt.urls')),
-    path('docs/', include('wiki.urls')),
 
 ]
 
@@ -679,11 +681,11 @@ if settings.DEBUG:
 
 urlpatterns += [
     re_path(
-        r'^([a-z|A-Z|0-9|\.](?:[a-z\d]|-(?=[a-z\d]))+)/([a-z|A-Z|0-9|\.]+)/?$',
+        r'^(?!docs)([a-z|A-Z|0-9|\.](?:[a-z\d]|-(?=[a-z\d]))+)/([a-z|A-Z|0-9|\.]+)/?$',
         dashboard.views.profile,
         name='profile_min'
     ),
-    re_path(r'^([a-z|A-Z|0-9|\.](?:[a-z\d]|-(?=[a-z\d]))+)/?$', dashboard.views.profile, name='profile_min'),
+    re_path(r'^(?!docs)([a-z|A-Z|0-9|\.](?:[a-z\d]|-(?=[a-z\d]))+)/?$', dashboard.views.profile, name='profile_min'),
 ]
 
 LOGIN_REDIRECT_URL = '/login'
