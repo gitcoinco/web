f97d672acda02dd2571042f65c81819c3c2e05d0
octavioamu <octavioamuchastegui@gmail.com>
2020-02-29 18:57:26 -0300

    pre cleanup

--- app/assets/v2/js/shared.js
+++ app/assets/v2/js/shared.js
@@ -406,10 +406,10 @@ var uninterested = function(bounty_pk, profileId, slash) {
 //   });
 // };
 
-var profileHtml = function(handle, name) {
-  return '<span><a href="/profile/' +
-    handle + '" target="_blank">' + (name ? name : handle) + '</span></a>';
-};
+// var profileHtml = function(handle, name) {
+//   return '<span><a href="/profile/' +
+//     handle + '" target="_blank">' + (name ? name : handle) + '</span></a>';
+// };
 
 // Update the list of bounty submitters.
 // var update_fulfiller_list = function(bounty_pk) {
@@ -455,43 +455,43 @@ function getParam(parameterName) {
   return result;
 }
 
-if ($.views) {
-  $.views.converters({
-    timedifference: timedifferenceCvrt,
-    activitytext: activitytextCvrt
-  });
+// if ($.views) {
+//   $.views.converters({
+//     timedifference: timedifferenceCvrt,
+//     activitytext: activitytextCvrt
+//   });
 
-}
+// }
 
-function timedifferenceCvrt(date) {
-  return timeDifference(new Date(), new Date(date), false, 60 * 60);
-}
+// function timedifferenceCvrt(date) {
+//   return timeDifference(new Date(), new Date(date), false, 60 * 60);
+// }
 
-const activity_names = {
-  new_bounty: gettext('New bounty'),
-  start_work: gettext('Work started'),
-  stop_work: gettext('Work stopped'),
-  work_submitted: gettext('Work submitted'),
-  work_done: gettext('Work done'),
-  worker_approved: gettext('Worker approved'),
-  worker_rejected: gettext('Worker rejected'),
-  worker_applied: gettext('Worker applied'),
-  increased_bounty: gettext('Increased funding'),
-  killed_bounty: gettext('Canceled bounty'),
-  new_crowdfund: gettext('New crowdfund contribution'),
-  new_tip: gettext('New tip'),
-  receive_tip: gettext('Tip received'),
-  bounty_abandonment_escalation_to_mods: gettext('Escalated for abandonment of bounty'),
-  bounty_abandonment_warning: gettext('Warned for abandonment of bounty'),
-  bounty_removed_slashed_by_staff: gettext('Dinged and removed from bounty by staff'),
-  bounty_removed_by_staff: gettext('Removed from bounty by staff'),
-  bounty_removed_by_funder: gettext('Removed from bounty by funder'),
-  bounty_changed: gettext('Bounty details changed')
-};
+// const activity_names = {
+//   new_bounty: gettext('New bounty'),
+//   start_work: gettext('Work started'),
+//   stop_work: gettext('Work stopped'),
+//   work_submitted: gettext('Work submitted'),
+//   work_done: gettext('Work done'),
+//   worker_approved: gettext('Worker approved'),
+//   worker_rejected: gettext('Worker rejected'),
+//   worker_applied: gettext('Worker applied'),
+//   increased_bounty: gettext('Increased funding'),
+//   killed_bounty: gettext('Canceled bounty'),
+//   new_crowdfund: gettext('New crowdfund contribution'),
+//   new_tip: gettext('New tip'),
+//   receive_tip: gettext('Tip received'),
+//   bounty_abandonment_escalation_to_mods: gettext('Escalated for abandonment of bounty'),
+//   bounty_abandonment_warning: gettext('Warned for abandonment of bounty'),
+//   bounty_removed_slashed_by_staff: gettext('Dinged and removed from bounty by staff'),
+//   bounty_removed_by_staff: gettext('Removed from bounty by staff'),
+//   bounty_removed_by_funder: gettext('Removed from bounty by funder'),
+//   bounty_changed: gettext('Bounty details changed')
+// };
 
-function activitytextCvrt(activity_type) {
-  return activity_names[activity_type];
-}
+// function activitytextCvrt(activity_type) {
+//   return activity_names[activity_type];
+// }
 
 function timeDifference(current, previous, remaining, now_threshold_seconds) {
 
