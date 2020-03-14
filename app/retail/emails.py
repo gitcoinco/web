c144db25014446600f269955944cf73c3431b387
iamonuwa <matrix4u2002@gmail.com>
2020-03-14 10:45:23 +0100

    Redesign Gitcoin weekly email template

--- app/retail/emails.py
+++ app/retail/emails.py
@@ -1046,14 +1046,10 @@ to have you! You can get a sneak preview of this in our <a href="https://gitcoin
 Interested in sneak peaks at what the product team is doing or being an early tester? <a href=“https://gitcoin.co/pixelantdesign”>Hit me up</a> on the <a href=“https://gitcoin.co/chat”>Gitcoin Chat</a>!
 </p>
 
-{kudos_friday}
-
 <h3>What else is new?</h3>
-    <ul>
-        <li>
-        <a href=“gitcoin.co/livestream”>Join us</a> for the Gitcoin Livestream today at 2 pm ET. This Livestream will be an ETH 2.0 stream hosted by our own Kevin Owocki!
-        </li>
-    </ul>
+<div>
+    <a href=“gitcoin.co/livestream”>Join us</a> for the Gitcoin Livestream today at 2 pm ET. This Livestream will be an ETH 2.0 stream hosted by our own Kevin Owocki!
+ </div>
 </p>
 <p>
 Back to (remote) work,
@@ -1158,11 +1154,14 @@ Back to (remote) work,
         'invert_footer': False,
         'hide_header': False,
         'highlights': highlights,
+        'kudos_friday': kudos_friday,
         'subscriber': get_or_save_email_subscriber(to_email, 'internal'),
         'kudos_highlights': kudos_highlights,
         'sponsor': sponsor,
 		'email_type': 'roundup',
         'email_style': email_style,
+        'hide_bottom_logo': True,
+        'hide_preferred_payout': True
     }
 
     response_html = premailer_transform(render_to_string("emails/bounty_roundup.html", params))
