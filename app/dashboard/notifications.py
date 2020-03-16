fdd4e099582a41d77e677d2af7187086023f2889
owocki <ksowocki@gmail.com>
2020-03-16 12:46:05 -0600

    fixes https://gitcoincore.slack.com/archives/CAXQ7PT60/p1584380570094600.. or at laest moves issue away from the user into the queues

--- app/dashboard/notifications.py
+++ app/dashboard/notifications.py
@@ -307,6 +307,11 @@ def maybe_market_to_user_slack_helper(bounty, event_name):
 
 
 def maybe_market_to_user_discord(bounty, event_name):
+    from dashboard.tasks import maybe_market_to_user_discord
+    maybe_market_to_user_discord.delay(bounty.pk, event_name)
+
+
+def maybe_market_to_user_discord_helper(bounty, event_name):
     """Send a Discord message to the user's discord channel for the specified Bounty.
 
     Args:
