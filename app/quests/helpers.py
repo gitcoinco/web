ab054168ea172e378cedb68ca6bc82b9f45e5c96
owocki <ksowocki@gmail.com>
2020-03-27 11:14:16 -0600

    allow redemptions of same kudos for diff quests https://github.com/gitcoinco/web/issues/6300#issuecomment-605117830

--- app/quests/helpers.py
+++ app/quests/helpers.py
@@ -125,6 +125,7 @@ def get_prize_url_if_redeemable(user, quest):
     btcs = BulkTransferCoupon.objects.filter(
         token=quest.kudos_reward,
         tag='quest',
+        comments_to_put_in_kudos_transfer=f"Congrats on beating the '{quest.title}' Gitcoin Quest",
         metadata__recipient=user.profile.pk,
         num_uses_remaining__gt=0,
         )
