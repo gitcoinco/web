a6283fe9eef17dd282757e9f0c9edeb5ba0ac0eb
@codingsh <codingsh@pm.me>
2020-03-30 18:37:16 -0300

    comment  get_symbol function

--- app/grants/clr.py
+++ app/grants/clr.py
@@ -605,28 +605,25 @@ def grants_transaction_validator(list_contributions):
 
     df = pd.read_csv(list_contributions, sep=" ")
     check_transaction = lambda txid: w3.eth.getTransaction(txid)
-    check_amount = lambda amount: int(amount[75:], 16) if len(amount) == 138 else print (f"{bcolors.FAIL}{bcolors.UNDERLINE}{index_transaction} txid: {txid[:10]} -> status: 0 False - amount was off by 0.001{bcolors.ENDC}")
+    check_amount = lambda amount: int(amount[75:], 16) if len(amount) == 138 else print (f"{bcolors.FAIL}{bcolors.UNDERLINE} {index_transaction} txid: {txid[:10]} -> status: 0 False - amount was off by 0.001{bcolors.ENDC}")
     check_token = lambda token_address: len(token_address) == 42
     check_contract = lambda token_address, abi : w3.eth.contract(token_address, abi=abi)
     check_event_transfer =  lambda contract_address, search, txid : w3.eth.filter({ "address": contract_address, "topics": [search, txid]})
     get_decimals = lambda contract : int(contract.functions.decimals().call())
-    get_symbol = lambda contract : contract.functions.symbol().call()
+    # To do waiting issue: https://github.com/ethereum/web3.py/issues/1618
+    # get_symbol = lambda contract: str(contract.functions.symbol().call())
 
-    for index_transaction, txid in enumerate(df.txid):
-        try:
-            transaction = check_transaction(txid)
-            amount =  check_amount(transaction.input)
-            token_address = check_token(transaction.to)
-            if token_address == True  and isinstance(amount, int):
-                contract = check_contract(transaction.to, erc20_abi)
-                contract_symbol = get_symbol(contract)
-                transfer_event = check_event_transfer(contract.address, SEARCH_METHOD, txid )
-                decimals = get_decimals(contract)
-                human_readable_value = Decimal(int(transaction.input[75:], 16)) / Decimal(10 ** decimals) if decimals else None
-                if (transfer_event):
-                    print (f"{bcolors.OKGREEN} {index_transaction} txid: {txid[:10]} amount: {human_readable_value} {contract_symbol} -> status: 1{bcolors.ENDC}")
-        except TransactionNotFound:
-            print (f"{bcolors.FAIL}{bcolors.UNDERLINE} {index_transaction} txid: {txid[:10]} -> status: 0 - tx failed{bcolors.ENDC}")
+    for column in df[['txid']]:
+        txid_list = df[column]
+        for index_transaction, txid in enumerate(txid_list):
+            try:
+                transaction = check_transaction(txid)
+                amount =  check_amount(transaction.input)
+                token_address = check_token(transaction.to)
+                if token_address == True  and isinstance(amount, int):
+                    transaction_status(transaction)
+            except TransactionNotFound:
+                print (f"{bcolors.FAIL}{bcolors.UNDERLINE} {index_transaction} txid: {txid[:10]} -> status: 0 - tx failed{bcolors.ENDC}")
 
 
     # Colors for Console.
@@ -641,4 +638,13 @@ def grants_transaction_validator(list_contributions):
         UNDERLINE = '\033[4m'
 
 
-
+def transaction_status(transaction):
+    """This function is core for check grants transaction list"""
+    contract = check_contract(transaction.to, erc20_abi)
+    transfer_event = check_event_transfer(contract.address, SEARCH_METHOD, txid )
+    decimals = get_decimals(contract)
+    contract_value = contract.decode_function_input(transaction.input)[1]['_value']
+    human_readable_value = Decimal(int(contract_value)) / Decimal(10 ** decimals) if decimals else None
+    if (transfer_event):
+        # contract_symbol = get_symbol(contract)
+        print (f"{bcolors.OKGREEN} {index_transaction} txid: {txid[:10]} amount: {human_readable_value}   -> status: 1{bcolors.ENDC}")
