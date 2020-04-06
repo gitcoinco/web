4dda333ae6c3b8ef5023c60a354b18d156fbe305
owocki <ksowocki@gmail.com>
2020-04-06 11:28:47 -0600

    ads a new avatar

--- app/avatar/views_3d.py
+++ app/avatar/views_3d.py
@@ -353,6 +353,16 @@ def get_avatar_attrs(theme, key):
             'tone_maps': [],
             'path': 'assets/v2/images/avatar3d/mage.svg',
         },
+        'powerrangers': {
+            'preview_viewbox': {
+                'background': '0 0 350 350',
+            },
+            'skin_tones': [],
+            'hair_tones': [],
+            'skin_tones': [],
+            'tone_maps': [],
+            'path': 'assets/v2/images/avatar3d/powerrangers.svg',
+        },
         'orc_gitcoin': {
             'preview_viewbox': {
                 'background': '0 0 350 350',
