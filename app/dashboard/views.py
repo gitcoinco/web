296f98f58686bdb7908a1060f23b72c754efa93d
owocki <ksowocki@gmail.com>
2020-04-07 07:33:14 -0600

    start date ascending per connor request

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -4088,9 +4088,9 @@ def get_hackathons(request):
     """Handle rendering all Hackathons."""
 
     events = {
-        'current': HackathonEvent.objects.current().filter(visible=True).order_by('-start_date'),
-        'upcoming': HackathonEvent.objects.upcoming().filter(visible=True).order_by('-start_date'),
-        'finished': HackathonEvent.objects.finished().filter(visible=True).order_by('-start_date'),
+        'current': HackathonEvent.objects.current().filter(visible=True).order_by('start_date'),
+        'upcoming': HackathonEvent.objects.upcoming().filter(visible=True).order_by('start_date'),
+        'finished': HackathonEvent.objects.finished().filter(visible=True).order_by('start_date'),
     }
     params = {
         'active': 'hackathons',
