f691ea90be4a67d599736c8b716bcc60b76a2d9b
owocki <ksowocki@gmail.com>
2020-04-08 11:35:30 -0600

    search results

--- app/search/views.py
+++ app/search/views.py
@@ -1,5 +1,7 @@
 import json
+import logging
 
+from django.conf import settings
 from django.db.models import Q
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -10,19 +12,24 @@ from retail.helpers import get_ip
 
 from .models import SearchResult, search
 
+logger = logging.getLogger(__name__)
+
 
 @ratelimit(key='ip', rate='30/m', method=ratelimit.UNSAFE, block=True)
 def search(request):
     keyword = request.GET.get('term', '')
-    
+
     # attempt elasticsearch first
+    mimetype = 'application/json'
+    return_results = []
     try:
-        all_result_sets = search(keyword)  
+        all_result_sets = search(keyword)
         return_results = [ele['_source'] for ele in all_result_sets['hits']['hits']]
-        mimetype = 'application/json'
-        return HttpResponse(json.dumps(return_results), mimetype)
     except Exception as e:
         print(e)
+    finally:
+        if not settings.DEBUG or len(return_results):
+            return HttpResponse(json.dumps(return_results), mimetype)
 
     all_result_sets = [SearchResult.objects.filter(title__icontains=keyword), SearchResult.objects.filter(description__icontains=keyword)]
     return_results = []
