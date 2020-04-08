53f17bd634880296781f794613b615c8dad92a91
Dan Lipert <danlipert@gmail.com>
2020-04-08 22:49:02 +0900

    fix eslint

--- app/assets/v2/js/grants/fund.js
+++ app/assets/v2/js/grants/fund.js
@@ -152,8 +152,9 @@ $(document).ready(function() {
   $('#js-token').change(function(e) {
     const val = $(this).val();
     const is_eth = val == '0x0000000000000000000000000000000000000000';
-    if(val == '0xdac17f958d2ee523a2206206994597c13d831ec7'){
-      _alert("WARNING: USDT is not well supported, it is recommended to use $USDC or $DAI instead. <a target=new href=https://twitter.com/owocki/status/1247546241862348801>More info here</a>", 'error', 2000);
+
+    if (val == '0xdac17f958d2ee523a2206206994597c13d831ec7') {
+      _alert('WARNING: USDT is not well supported, it is recommended to use $USDC or $DAI instead. <a target=new href=https://twitter.com/owocki/status/1247546241862348801>More info here</a>', 'error', 2000);
     }
 
     if (is_eth && $('#recurring_or_not').val() == 'recurring') {
@@ -362,6 +363,7 @@ $(document).ready(function() {
 
       if (data.contract_version == 1 || data.num_periods == 1) {
         const approve_buffer = 100000;
+
         realApproval = Number(((grant_amount + gitcoin_grant_amount) * data.num_periods * Math.pow(10, decimals)) + approve_buffer);
       } else if (data.contract_version == 0) {
         console.log('grant amount: ' + grant_amount);
@@ -423,6 +425,7 @@ $(document).ready(function() {
 
                 var first = Number(grant_amount * Math.pow(10, decimals)).toLocaleString('fullwide', {useGrouping: false});
                 var second = Number(gitcoin_grant_amount * Math.pow(10, decimals)).toLocaleString('fullwide', {useGrouping: false});
+
                 splitPayment(accounts[0], to_address, gitcoinDonationAddress, first, second);
               } else {
                 if (data.contract_version == 0 && gitcoin_grant_amount > 0) {
