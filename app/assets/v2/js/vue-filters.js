67d6a55e3a71edd74d335126ebf40f2a8deca589
Andrew Redden <opensource@andrewredden.com>
2020-04-13 15:01:46 -0300

    fix for the hackathon banner background and text overrides

--- app/assets/v2/js/vue-filters.js
+++ app/assets/v2/js/vue-filters.js
@@ -91,19 +91,10 @@ Vue.filter('stringReplace', function(activity_type) {
 });
 
 
-Vue.filter('truncate', function(text, length, clamp) {
-  text = text || '';
-  clamp = clamp || '...';
-  length = length || 30;
-
-  if (text.length <= length)
-    return text;
-
-  let tcText = text.slice(0, length - clamp.length);
-  let last = tcText.length - 1;
-
-  while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0])
-    last -= 1;
+Vue.filter('truncate', function(value, length = 30, clamp = '...') {
+  if (!value) {
+    return '';
+  }
 
   value = value.toString();
   if (value.length <= length) {
