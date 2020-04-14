791c67e219ecfa5cd9af712984a981b455154431
Andrew Redden <opensource@andrewredden.com>
2020-04-13 21:25:23 -0300

    updates for the project-panel, removing redundant css, adding a truncate filter to prevent long running paragraphs

--- app/assets/v2/js/vue-filters.js
+++ app/assets/v2/js/vue-filters.js
@@ -90,3 +90,25 @@ Vue.filter('stringReplace', function(activity_type) {
   return activity_names[activity_type];
 });
 
+Vue.filter('truncate', function(text, length, clamp) {
+  text = text || '';
+  clamp = clamp || '...';
+  length = length || 30;
+
+  if (text.length <= length)
+    return text;
+
+  var tcText = text.slice(0, length - clamp.length);
+  var last = tcText.length - 1;
+
+
+  while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0]) last -= 1;
+
+  // Fix for case when text dont have any `space`
+  last = last || length - clamp.length;
+
+  tcText = tcText.slice(0, last);
+
+  return tcText + clamp;
+});
+
