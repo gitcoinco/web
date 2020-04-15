d69a10bcd6575b6a3c92b0409b32fb0409e557f5
Aditya Anand M C <aditya.anandmc@gmail.com>
2020-04-15 18:20:26 +0530

    Update vue-filters.js

--- app/assets/v2/js/vue-filters.js
+++ app/assets/v2/js/vue-filters.js
@@ -99,9 +99,8 @@ Vue.filter('truncate', function(text, length, clamp) {
   if (text.length <= length)
     return text;
 
-  var tcText = text.slice(0, length - clamp.length);
-  var last = tcText.length - 1;
-
+  let tcText = text.slice(0, length - clamp.length);
+  let last = tcText.length - 1;
 
   while (last > 0 && tcText[last] !== ' ' && tcText[last] !== clamp[0])
     last -= 1;
