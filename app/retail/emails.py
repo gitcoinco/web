04c1af231583e5ccc0ba42529cbc237abe7f9252
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-04-19 23:21:28 +0530

    add Upcoming Grant

--- app/retail/emails.py
+++ app/retail/emails.py
@@ -517,11 +517,19 @@ appreciate you being a part of the community + let us know if you'd like some Gi
     return response_html, response_txt
 
 
-def render_new_bounty(to_email, bounties, old_bounties, offset=3, quest_of_the_day={}):
+def render_new_bounty(to_email, bounties, old_bounties, offset=3, quest_of_the_day={}, upcoming_grant={}):
     from townsquare.utils import is_email_townsquare_enabled, is_there_an_action_available
     email_style = (int(timezone.now().strftime("%-j")) + offset) % 24
     sub = get_or_save_email_subscriber(to_email, 'internal')
     email_style = 26
+    upcoming_events = [{
+        'event': upcoming_grant,
+        'title': upcoming_grant.title,
+        'image_url': upcoming_grant.logo.url,
+        'url': upcoming_grant.url,
+        'date': upcoming_grant.next_clr_calc_date.strftime("%Y-%d-%m")
+    },
+    ]
     params = {
         'old_bounties': old_bounties,
         'bounties': bounties,
@@ -532,6 +540,7 @@ def render_new_bounty(to_email, bounties, old_bounties, offset=3, quest_of_the_d
         'base_url': settings.BASE_URL,
         'show_action': True,
         'quest_of_the_day': quest_of_the_day,
+        'upcoming_events': upcoming_events,
         'show_action': is_email_townsquare_enabled(to_email) and is_there_an_action_available()
     }
 
@@ -1213,11 +1222,12 @@ def resend_new_tip(request):
 @staff_member_required
 def new_bounty(request):
     from dashboard.models import Bounty
-    from marketing.views import quest_of_the_day
+    from marketing.views import quest_of_the_day, upcoming_grant
     bounties = Bounty.objects.current().order_by('-web3_created')[0:3]
     old_bounties = Bounty.objects.current().order_by('-web3_created')[0:3]
     quest = quest_of_the_day()
-    response_html, _ = render_new_bounty(settings.CONTACT_EMAIL, bounties, old_bounties='', offset=int(request.GET.get('offset', 2)), quest_of_the_day=quest)
+    grant = upcoming_grant()
+    response_html, _ = render_new_bounty(settings.CONTACT_EMAIL, bounties, old_bounties='', offset=int(request.GET.get('offset', 2)), quest_of_the_day=quest, upcoming_grant=grant)
     return HttpResponse(response_html)
 
 
