9f59a6bffb6facdb71fd52cb50b770c13c44dae5
Andrew Redden <opensource@andrewredden.com>
2020-04-28 22:34:47 -0300

    enable add

--- app/chat/management/commands/setup_hackathon_project_chat.py
+++ app/chat/management/commands/setup_hackathon_project_chat.py
@@ -126,7 +126,7 @@ class Command(BaseCommand):
                     print(len(profiles_to_connect))
                     # for r in remove:
                     #     chat_driver.channels.remove_channel_member(project.chat_channel_id, r)
-                    # add_to_channel.delay({'id': project.chat_channel_id}, profiles_to_connect)
+                    add_to_channel.delay({'id': project.chat_channel_id}, profiles_to_connect)
                 except Exception as e:
                     logger.error(str(e))
                     continue
