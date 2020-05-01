6758427dacbb806502cafb48593342ff2d7fe0a6
Miguel Angel G <miguel@gordian.dev>
2020-05-01 14:19:10 -0500

    Add fix for follow/following

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -2818,8 +2818,8 @@ def profile(request, handle, tab=None):
     context['feedbacks_got'] = [fb.pk for fb in profile.feedbacks_got.all() if fb.visible_to(request.user)]
     context['all_feedbacks'] = context['feedbacks_got'] + context['feedbacks_sent']
     context['tags'] = [('#announce','bullhorn'), ('#mentor','terminal'), ('#jobs','code'), ('#help','laptop-code'), ('#other','briefcase'), ]
-    context['followers'] = TribeMember.objects.filter(org=request.user.profile)
-    context['following'] = TribeMember.objects.filter(profile=request.user.profile)
+    context['followers'] = TribeMember.objects.filter(org=profile)
+    context['following'] = TribeMember.objects.filter(profile=profile)
     context['foltab'] = request.GET.get('sub', 'followers')
 
     tab = get_profile_tab(request, profile, tab, context)
