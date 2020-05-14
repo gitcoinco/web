423d2857b893ae6594c7ec74a67c6b340bd20216
Ben DiFrancesco <ben@scopelift.co>
2020-05-14 17:25:33 -0400

    Add grant data to local storage button is pressed

--- app/assets/v2/js/grants/info.js
+++ app/assets/v2/js/grants/info.js
@@ -7,7 +7,8 @@ $(document).ready(function() {
         event.preventDefault();
 
         const formData = objectifySerialized($(this).serializeArray());
-        console.log("Form data", formData);
+        addToCart(formData);
+        // console.log("CART", loadCart());
     });
 });
 
@@ -22,4 +23,37 @@ function objectifySerialized(data) {
     }
 
     return objectData;
+}
+
+function cartContainsGrantWithSlug(grantSlug) {
+    const cart = loadCart();
+    const slugList = cart.map(grant => {
+        return grant.grant_slug;
+    });
+
+    return slugList.includes(grantSlug);
+}
+
+function addToCart(grantData) {
+    if (cartContainsGrantWithSlug(grantData.grant_slug)) {
+        return;
+    }
+
+    let cartList = loadCart()
+    cartList.push(grantData);
+    setCart(cartList);
+}
+
+function loadCart() {
+    let cartList = localStorage.getItem('grants_cart');
+
+    if (!cartList) {
+        return [];
+    }
+
+    return JSON.parse(cartList);
+}
+
+function setCart(list) {
+    localStorage.setItem('grants_cart', JSON.stringify(list));
 }
