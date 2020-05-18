f6464f0060685bd49a0ecdb4a7611178c8114fb6
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2020-05-18 06:25:48 +0100

    Add modal files

--- app/assets/v2/js/pages/profile_tokens.js
+++ app/assets/v2/js/pages/profile_tokens.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  $("#buy_ptoken").on("click", function () {
+    $("#buy_ptoken_modal").bootstrapModal("show");
+  });
+
   $(document, "#buy_ptoken_modal").on("hidden.bs.modal", function (e) {
     $("#buy_ptoken_modal").remove();
     $("#buy_ptoken_modal").bootstrapModal("dispose");
