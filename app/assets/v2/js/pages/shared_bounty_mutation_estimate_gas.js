1d159b16527ca1344fd51acf9c767070ae8f994d
Aditya Anand M C <aditya.anandmc@gmail.com>
2020-05-18 18:04:05 +0530

    remove commented out code

--- app/assets/v2/js/pages/shared_bounty_mutation_estimate_gas.js
+++ app/assets/v2/js/pages/shared_bounty_mutation_estimate_gas.js
@@ -36,39 +36,3 @@ var gas_amount = function(page_url) {
   }
   return gasLimitEstimate;
 };
-
-
-// jQuery(document).ready(function() {
-//   var gasLimitEstimate = gas_amount(document.location.href);
-//   var estimateGas = function(success_callback, failure_callback, final_callback) {
-//     // TODO: could do a web3 .estimateGas()
-//     success_callback(gasLimitEstimate, gasLimitEstimate, '');
-//     final_callback();
-//   };
-//   // updates recommended metamask settings
-//   var updateInlineGasEstimate = function() {
-//     var success_callback = function(gas, gasLimit, _) {
-//       jQuery('#gasLimit').val(parseInt(gas));
-//       update_metamask_conf_time_and_cost_estimate();
-//     };
-//     var failure_callback = function(errors) {
-//       jQuery('#gasLimit').val('Unknown');
-//       update_metamask_conf_time_and_cost_estimate();
-//     };
-//     var final_callback = function() {
-//       // …
-//     };
-
-//     estimateGas(success_callback, failure_callback, final_callback);
-//   };
-
-//   // only check for updating gas estimates IFF we're on a page where
-//   // update_metamask_conf_time_and_cost_estimate() is defined
-//   if (typeof update_metamask_conf_time_and_cost_estimate != 'undefined') {
-//     setTimeout(function() {
-//       updateInlineGasEstimate();
-//     }, 500);
-//     jQuery('input').change(updateInlineGasEstimate);
-//     jQuery('#gasPrice').keyup(update_metamask_conf_time_and_cost_estimate);
-//   }
-// });
