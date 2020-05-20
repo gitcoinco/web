6f71f6e910245ed4072d3b6ca05ff5b02c70ea1e
mds <17163988+mds1@users.noreply.github.com>
2020-05-20 11:13:06 -0700

    Add total and summary sections

--- app/assets/v2/js/cart.js
+++ app/assets/v2/js/cart.js
@@ -12,6 +12,78 @@ Vue.component('grants-cart', {
     };
   },
 
+  computed: {
+    donationTotals() {
+      const totals = {};
+
+      this.grantData.forEach(grant => {
+        const token = grant.grant_donation_currency;
+
+        if (!totals[token]) {
+          // First time seeing this token, set the field and initial value
+          totals[token] = grant.grant_donation_amount;
+        } else {
+          // We've seen this token, so just update the total
+          totals[token] += grant.grant_donation_amount;
+        }
+      });
+      return totals;
+    },
+
+    donationTotalsString() {
+      const totals = this.donationTotals;
+      let string = '';
+
+      if (!totals) {
+        return undefined;
+      }
+
+      Object.keys(totals).forEach(key => {
+        if (string === '') {
+          string += `${totals[key]} ${key}`;
+        } else {
+          string += `+ ${totals[key]} ${key}`;
+        }
+      });
+      return string;
+    },
+
+    donationsToGitcoin() {
+      const totals = {};
+
+      this.grantData.forEach(grant => {
+        const token = grant.grant_donation_currency;
+
+        if (!totals[token]) {
+          // First time seeing this token, set the field and initial value
+          totals[token] = grant.grant_donation_amount * 0.05;
+        } else {
+          // We've seen this token, so just update the total
+          totals[token] += (grant.grant_donation_amount * 0.05);
+        }
+      });
+      return totals;
+    },
+
+    donationsToGitcoinString() {
+      const totals = this.donationsToGitcoin;
+      let string = '';
+
+      if (!totals) {
+        return undefined;
+      }
+
+      Object.keys(totals).forEach(key => {
+        if (string === '') {
+          string += `${totals[key]} ${key}`;
+        } else {
+          string += `+ ${totals[key]} ${key}`;
+        }
+      });
+      return string;
+    }
+  },
+
   mounted() {
     // Read array of grants in cart from localStorage
     this.grantData = JSON.parse(window.localStorage.getItem('grants_cart'));
