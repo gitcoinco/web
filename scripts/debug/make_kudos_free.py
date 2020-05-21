083ac8a755319f7b99de00ffb3e81ca283ed6c9a
owocki <ksowocki@gmail.com>
2020-05-21 11:50:11 -0600

    make kudos free for certain users

--- scripts/debug/make_kudos_free.py
+++ scripts/debug/make_kudos_free.py
@@ -1,1 +1,12 @@
+from kudos.models import BulkTransferCoupon
+from dashboard.models import Profile
+
 handles = ['xtrullols35', 'sergisi', 'bcaspi', 'gerardguerrero', 'rafacucurull']
+for handle in handles:
+    profile = Profile.objects.get(handle__iexact=handle)
+    btcs = BulkTransferCoupon.objects.filter(metadata__recipient=profile.pk)
+    print(handle, btcs.count())
+    for btc in btcs:
+        btc.make_paid_for_first_minutes = 0
+        btc.save()
+        
