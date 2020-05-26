9e4d789ceef6def9bebaa3be38729ca6e39be676
owocki <ksowocki@gmail.com>
2020-05-26 14:10:16 -0600

    updates kudos script

--- scripts/debug/make_kudos_free.py
+++ scripts/debug/make_kudos_free.py
@@ -1,7 +1,7 @@
 from kudos.models import BulkTransferCoupon
 from dashboard.models import Profile
 
-handles = ['xtrullols35', 'sergisi', 'bcaspi', 'gerardguerrero', 'rafacucurull']
+handles = ['xtrullols35', 'sergisi', 'bcaspi', 'gerardguerrero', 'rafacucurull', 'davidcascos', 'pau1838', 'ivancg98']
 for handle in handles:
     profile = Profile.objects.get(handle__iexact=handle)
     btcs = BulkTransferCoupon.objects.filter(metadata__recipient=profile.pk)
