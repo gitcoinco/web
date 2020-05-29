15c0b74afae7f624cf23c18bce66b1b0bba26ad9
owocki <ksowocki@gmail.com>
2020-05-29 09:03:14 -0600

    revert breaking premailer fix

--- app/retail/emails.py
+++ app/retail/emails.py
@@ -77,11 +77,10 @@ NOTIFICATION_EMAILS = [
 
 ALL_EMAILS = MARKETING_EMAILS + TRANSACTIONAL_EMAILS + NOTIFICATION_EMAILS
 
-# per speed notes at https://pypi.org/project/premailer/
-premailer = Premailer(base_url=settings.BASE_URL)
 
 def premailer_transform(html):
     cssutils.log.setLevel(logging.CRITICAL)
+    p = premailer.Premailer(html, base_url=settings.BASE_URL)
     return premailer.transform(html)
 
 
