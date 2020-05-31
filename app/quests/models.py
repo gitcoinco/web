b817b095305512b556b2501710605e0237db9e07
owocki <ksowocki@gmail.com>
2020-05-30 20:19:39 -0600

    more video backgrounds for quests

--- app/quests/models.py
+++ app/quests/models.py
@@ -11,7 +11,7 @@ from django.utils.text import slugify
 from economy.models import SuperModel
 
 num_backgrounds = 34
-video_enabled_backgrounds = [8, 9, 10, 11, 13, 23, 34]
+video_enabled_backgrounds = [6, 8, 9, 10, 11, 13, 14, 22, 23, 34]
 
 
 class Quest(SuperModel):
