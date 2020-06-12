33875a9ac693df75d1c88c6380fd69178cc70166
mds <17163988+mds1@users.noreply.github.com>
2020-06-12 10:36:58 -0700

    Initial working implementation of showing CLR match in cart

--- app/assets/v2/js/cart.js
+++ app/assets/v2/js/cart.js
@@ -719,6 +719,73 @@ Vue.component('grants-cart', {
 
     onResize() {
       this.windowWidth = window.innerWidth;
+    },
+
+    lerp(x_lower, x_upper, y_lower, y_upper, x) {
+      return y_lower + (((y_upper - y_lower) * (x - x_lower)) / (x_upper - x_lower));
+    },
+
+    valueToDai(amount, tokenAddr) {
+      // TODO convert token amount of token address to equivalent amount in DAI
+      return amount
+    },
+
+    predictCLRMatch(grant) {
+      const rawAmount = Number(grant.grant_donation_amount);
+      let amount = this.valueToDai(rawAmount, grant.grant_token_address);
+      const clr_prediction_curve_2d = JSON.parse(grant.grant_clr_prediction_curve);
+      const clr_prediction_curve = clr_prediction_curve_2d.map(row => row[2])
+
+      if (amount > 10000) {
+        amount = 10000;
+      }
+
+      let predicted_clr = 0;
+
+      const contributions_axis = [ 0, 1, 10, 100, 1000, 10000 ];
+
+      let index = 0;
+      if (isNaN(amount)) {
+        predicted_clr = clr_prediction_curve[index];
+      } else if (contributions_axis.indexOf(amount) >= 0) {
+        index = contributions_axis.indexOf(amount);
+        predicted_clr = clr_prediction_curve[index];
+      } else {
+        let x_lower = 0;
+        let x_upper = 0;
+        let y_lower = 0;
+        let y_upper = 0;
+
+        if (0 < amount && amount < 1) {
+          x_lower = 0;
+          x_upper = 1;
+          y_lower = clr_prediction_curve[0];
+          y_upper = clr_prediction_curve[1];
+        } else if (1 < amount && amount < 10) {
+          x_lower = 1;
+          x_upper = 10;
+          y_lower = clr_prediction_curve[1];
+          y_upper = clr_prediction_curve[2];
+        } else if (10 < amount && amount < 100) {
+          x_lower = 10;
+          x_upper = 100;
+          y_lower = clr_prediction_curve[2];
+          y_upper = clr_prediction_curve[3];
+        } else if (100 < amount && amount < 1000) {
+          x_lower = 100;
+          x_upper = 1000;
+          y_lower = clr_prediction_curve[3];
+          y_upper = clr_prediction_curve[4];
+        } else {
+          x_lower = 1000;
+          x_upper = 10000;
+          y_lower = clr_prediction_curve[4];
+          y_upper = clr_prediction_curve[5];
+        }
+
+        predicted_clr = this.lerp(x_lower, x_upper, y_lower, y_upper, amount);
+      }
+      return predicted_clr;
     }
 
   },
@@ -728,6 +795,9 @@ Vue.component('grants-cart', {
     grantData: {
       handler() {
         CartData.setCart(this.grantData);
+        this.grantData.forEach((grant, index) => {
+          this.grantData[index].grant_donation_clr_match = this.predictCLRMatch(grant).toFixed(2);
+        })
       },
       deep: true
     },
@@ -756,6 +826,9 @@ Vue.component('grants-cart', {
     this.isLoading = true;
     // Read array of grants in cart from localStorage
     this.grantData = CartData.loadCart();
+    this.grantData.forEach((grant, index) => {
+      this.grantData[index].grant_donation_clr_match = this.predictCLRMatch(grant).toFixed(2);
+    })
     // Initialize array of empty comments
     this.comments = this.grantData.map(grant => undefined);
     // Wait until we can load token list
