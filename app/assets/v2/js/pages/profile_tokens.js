52ad09902a257512814a8a44dbecbd5ea87637b3
iamonuwa <matrix4u2002@gmail.com>
2020-06-18 09:38:20 +0100

    add create token

--- app/assets/v2/js/pages/profile_tokens.js
+++ app/assets/v2/js/pages/profile_tokens.js
@@ -5,18 +5,22 @@ $(document).on("click", "#submit_create_token", (event) => {
   let tokenPrice = $("#createPTokenPrice").val();
   let tokenSupply = $("#createPTokenSupply").val();
 
-  deployToken(tokenName, tokenSymbol, tokenPrice, tokenSupply);
+  createPToken(tokenName, tokenSymbol, tokenPrice, tokenSupply);
 });
 
-async function deployToken(tokenName, tokenSymbol, tokenPrice, tokenSupply) {
-  console.log("Deploying token");
+async function createPToken(tokenName, tokenSymbol, tokenPrice, tokenSupply) {
   [user] = await web3.eth.getAccounts();
   const factoryAddress = "0x7bE324A085389c82202BEb90D979d097C5b3f2E8";
-  const factory = await new web3.eth.Contract(ptoken_abi.abi, factoryAddress);
+  const DAI = "0x0fbe13fcf9c8b33a5c20fa93160ded33d4df702e"
+  const factory = await new web3.eth.Contract(
+    document.contxt.ptoken_factory_abi,
+    factoryAddress
+  );
 
-  await factory.methods
-    .createPToken(tokenName, tokenSymbol, tokenPrice, tokenSupply)
+  let result = await factory.methods
+    .createPToken(tokenName, tokenSymbol, tokenPrice, tokenSupply, DAI)
     .send({
       from: user,
     });
+
 }
