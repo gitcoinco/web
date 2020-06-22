5e3ac58264738d78b11f0bfb6ed21bc0fd835db3
@codingsh <codingsh@pm.me>
2020-06-22 10:39:02 -0300

    fixed: design features on hackthon box  #6870

--- app/townsquare/migrations/0020_auto_20200521_1020.py
+++ app/townsquare/migrations/0020_auto_20200521_1020.py
@@ -12,7 +12,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.AlterField(
-            model_name='PinnedPost',
+            model_name='pinnedpost',
             name='activity',
             field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='pin', to='dashboard.Activity'),
         ),
