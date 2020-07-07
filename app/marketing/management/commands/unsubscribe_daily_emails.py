d7678f5eaae5e184f1c9bab58c755aabf2c0e061
owocki <ksowocki@gmail.com>
2020-07-07 14:42:32 -0600

    unsubscribe daily email people if they haven't read in the last 30 days

---
+++ app/marketing/management/commands/unsubscribe_daily_emails.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""Define the GDPR reconsent command for EU users.
+
+Copyright (C) 2020 Gitcoin Core
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+import time
+import warnings
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+from django.utils import timezone
+from marketing.mails import gdpr_reconsent
+from marketing.models import EmailSubscriber, EmailEvent
+from marketing.utils import should_suppress_notification_email
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+
+class Command(BaseCommand):
+
+    help = 'unsubscribes people from their daily emails if they are not reading them, per https://gitcoincore.slack.com/archives/CB1N0L6F7/p1594141095159400'
+
+    def handle(self, *args, **options):
+        days = 90 if settings.DEBUG else 30
+        time_threshold = timezone.now() - timezone.timedelta(days=days)
+        eses = EmailSubscriber.objects.order_by('-created_on')
+        email_type = "new_bounty_daily"
+        for es in eses:
+            if not should_suppress_notification_email(es.email, 'new_bounty_notifications'):
+                base_email_events = EmailEvent.objects.filter(email=es.email, created_on__gt=time_threshold, event__icontains=email_type)
+                num_sends = base_email_events.filter(event='delivered').count()
+                num_opens = base_email_events.filter(event='open').count()
+                num_clicks = base_email_events.filter(event='click').count()
+                print(f"{es.email} {num_clicks} {num_opens} {num_clicks}")
+
+                do_unsubscribe = num_sends > 5 and num_opens < 1 and num_clicks < 1
+                if do_unsubscribe:
+                    unsubscribed_email_type = {}
+                    unsubscribed_email_type[email_type] = True
+                    es.build_email_preferences(unsubscribed_email_type)
+                    print("unsubscribed ")
+                    es.save()
+
