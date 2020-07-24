a2ae49c5e1cd88b4f16c1210ca347fd04b790f8c
Sebastian T F <sebastian.tharakan97@gmail.com>
2020-07-24 23:57:31 +0530

    management command for tribe_hackathon_prizes

--- app/marketing/mails.py
+++ app/marketing/mails.py
@@ -47,6 +47,20 @@ from retail.emails import (
     render_successful_contribution_email, render_support_cancellation_email, render_tax_report,
     render_thank_you_for_supporting_email, render_tip_email, render_unread_notification_email_weekly_roundup,
     render_wallpost, render_weekly_recap,
+    render_bounty_expire_warning, render_bounty_feedback, render_bounty_request, render_bounty_startwork_expire_warning,
+    render_bounty_unintersted, render_comment, render_faucet_rejected, render_faucet_request,
+    render_featured_funded_bounty, render_funder_payout_reminder, render_funder_stale, render_gdpr_reconsent,
+    render_gdpr_update, render_grant_cancellation_email, render_grant_recontribute, render_grant_txn_failed,
+    render_grant_update, render_kudos_email, render_match_distribution, render_match_email, render_mention,
+    render_new_bounty, render_new_bounty_acceptance, render_new_bounty_rejection, render_new_bounty_roundup,
+    render_new_grant_email, render_new_supporter_email, render_new_work_submission, render_no_applicant_reminder,
+    render_nth_day_email_campaign, render_quarterly_stats, render_remember_your_cart, render_request_amount_email,
+    render_reserved_issue, render_share_bounty, render_start_work_applicant_about_to_expire,
+    render_start_work_applicant_expired, render_start_work_approved, render_start_work_new_applicant,
+    render_start_work_rejected, render_subscription_terminated_email, render_successful_contribution_email,
+    render_support_cancellation_email, render_tax_report, render_thank_you_for_supporting_email, render_tip_email,
+    render_unread_notification_email_weekly_roundup, render_wallpost, render_weekly_recap, render_bounty_hypercharged,
+    render_tribe_hackathon_prizes, render_unread_notification_email_weekly_roundup, render_wallpost, render_weekly_recap,
 )
 from sendgrid.helpers.mail import Attachment, Content, Email, Mail, Personalization
 from sendgrid.helpers.stats import Category
@@ -1997,3 +2011,37 @@ def remember_your_cart(profile, cart_query, grants, hours):
             send_mail(from_email, to_email, subject, text, html, categories=['marketing', func_name()])
     finally:
         translation.activate(cur_language)
+
+def tribe_hackathon_prizes(hackathon):
+    sponsors = hackathon.sponsors.all()
+
+    for sponsor in sponsors:
+        prizes = hackathon.get_current_bounties.filter(bounty_owner_profile=sponsor.tribe)
+
+        tribe_members = sponsor.tribe.tribe_members
+
+        subject = f"{sponsor.name} is participating on {hackathon.name} on Gitcoin 🚀"
+
+        try:
+            html, text = render_tribe_hackathon_prizes(hackathon, sponsor, prizes)
+        except:
+            return
+
+        for tribe_member in tribe_members:
+            profile = tribe_member.profile
+
+            to_email = profile.email
+            from_email = settings.CONTACT_EMAIL
+            if not to_email:
+                if profile and profile.user:
+                    to_email = profile.user.email
+            if not to_email:
+                return
+
+            cur_language = translation.get_language()
+
+            try:
+                setup_lang(to_email)
+                send_mail(from_email, to_email, subject, text, html, categories=['marketing', func_name()])
+            finally:
+                translation.activate(cur_language)
