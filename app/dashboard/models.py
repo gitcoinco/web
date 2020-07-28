d8aeb8a5ac623f1b633a4dbf1a0c480c15879502
octavioamu <octavioamuchastegui@gmail.com>
2020-07-28 20:57:10 -0300

    fix sponsor dashboard

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -4908,7 +4908,7 @@ class HackathonProject(SuperModel):
         return {
             'pk': self.pk,
             'name': self.name,
-            'logo': self.logo.url,
+            'logo': self.logo.url if self.logo else '',
             'badge': self.badge,
             'profiles': profiles,
             'work_url': self.work_url,
