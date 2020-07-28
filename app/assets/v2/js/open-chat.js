175478d7ce8216de599bc8a0d8f0e52ea300ddef
@codingsh <codingsh@pm.me>
2020-07-28 13:39:39 -0300

    feat: add chat button on hackathon page  #7138 *- [x] chat button added to hackathon view right rail next to follow *- [x] add chat button on org townsquare left rail in the follow section
    screen test -> https://share.getcloudapp.com/Qwu0qdWw

--- app/assets/v2/js/open-chat.js
+++ app/assets/v2/js/open-chat.js
@@ -4,9 +4,26 @@ const openChat = () => {
 
     $(elem).on('click', function() {
       const user = $(elem).data('openchat');
-      const url = user ? `https://chat.gitcoin.co/gitcoin/messages/@${user}` : 'https://chat.gitcoin.co/';
+      const url = user ? `https://chat.gitcoin.co/gitcoin/messages/@${user}` :
+        'https://chat.gitcoin.co/';
 
-      chatWindow = window.open(url, 'Loading', 'top=0,left=0,width=400,height=600,status=no,toolbar=no,location=no,menubar=no,titlebar=no');
+      chatWindow = window.open(
+        url, 'Loading',
+        'top=0,left=0,width=400,height=600,status=no,toolbar=no,location=no,menubar=no,titlebar=no');
+    });
+  });
+  $('[data-sponser-openchat').each(function(index, elem) {
+    let chatWindow;
+
+    $(elem).on('click', function() {
+      const sponserChannel = $(elem).data('openchat');
+      const url = sponserChannel ?
+        `https://chat.gitcoin.co/gitcoin/messages/channels/company-${sponserChannel}` :
+        'https://chat.gitcoin.co/';
+
+      chatWindow = window.open(
+        url, 'Loading',
+        'top=0,left=0,width=400,height=600,status=no,toolbar=no,location=no,menubar=no,titlebar=no');
     });
   });
 };
