ec4adaeecd3632a38561b006330f6bedcc10c3bd
Andrew Redden <opensource@andrewredden.com>
2020-07-31 17:09:16 -0300

    WIP, Frank's Super Query added as a view, connected to a ViewSet and returned through the api

--- app/app/urls.py
+++ app/app/urls.py
@@ -694,7 +694,6 @@ urlpatterns = [
 
     # users
     url(r'^api/v0.1/user_bounties/', dashboard.views.get_user_bounties, name='get_user_bounties'),
-    url(r'^api/v0.1/users_fetch/', dashboard.views.users_fetch, name='users_fetch'),
 
     # wiki
     path('wiki/notifications/', include('django_nyt.urls')),
