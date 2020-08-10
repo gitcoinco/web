0e4915b27ad6f10817df42894c30e265ad11bc87
Anoof Shaikh <anoofs@gmail.com>
2020-08-11 01:39:29 +0530

    lint fixes

--- app/assets/v2/js/pages/kudos_bulk_receive.js
+++ app/assets/v2/js/pages/kudos_bulk_receive.js
@@ -61,14 +61,14 @@ $(document).ready(function() {
   });
 
   if ($('#free_countdown > span').length > 0) {
-    var timeDifference = new Date($("#free_countdown > span").attr("data-time")) - Date.now();
+    var timeDifference = new Date($('#free_countdown > span').attr('data-time')) - Date.now();
 
     setTimeout(function() {
       $('#free_countdown').remove();
       $('.btn-gc-purple').removeAttr('disabled');
       var gasLabel = $('label[for=pay_gas]')[0].innerText;
 
-      $('label[for=pay_gas]')[0].innerText = $('label[for=pay_gas]')[0].innerText.substring(0,gasLabel.length - 28) + '(Optional, but recommended)';
+      $('label[for=pay_gas]')[0].innerText = $('label[for=pay_gas]')[0].innerText.substring(0, gasLabel.length - 28) + '(Optional, but recommended)';
     }, timeDifference);
   }
 });
