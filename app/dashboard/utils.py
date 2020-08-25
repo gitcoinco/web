6bcbf732bd508babf830f5d36568d9332100c23f
Walid Mujahid وليد مجاهد <walid.mujahid.dev@gmail.com>
2020-08-25 09:12:30 -0400

    Fix hackathon list page loadtime (#7254)
    * remove unused fields
    
    * refactor: load context from perftools.models.JSONStore
    
    * fix isort
    
    Co-authored-by: Dan Lipert <danlipert@gmail.com>

--- app/dashboard/utils.py
+++ app/dashboard/utils.py
@@ -1091,16 +1091,35 @@ def get_token_recipient_senders(network, recipient_address, token_address):
     return [process_log(log) for log in logs]
 
 
-def get_hackathon_event(title, event, network):
-    event_bounties = Bounty.objects.filter(event=event, network=network)
+def get_hackathons_page_default_tabs():
+    from perftools.models import JSONStore
 
+    return JSONStore.objects.get(key='hackathons', view='hackathons').data[0]
+
+
+def get_hackathon_events():
+    from perftools.models import JSONStore
+
+    return JSONStore.objects.get(key='hackathons', view='hackathons').data[1]
+
+
+def set_hackathon_event(type, event):
     return {
-        'title': title,
-        'hackathon': event,
-        'value_in_usdt': sum(
-            prize_usdt.value_in_usdt_now
-            for prize_usdt
-            in event_bounties
-        ),
-        'registrants': HackathonRegistration.objects.filter(hackathon=event).count()
+        'type': type,
+        'name': event.name,
+        'slug': event.slug,
+        'background_color': event.background_color,
+        'logo': event.logo.name or event.logo_svg.name,
+        'start_date': event.start_date.isoformat(),
+        'end_date': event.end_date.isoformat(),
+        'summary': event.hackathon_summary,
+        'sponsor_profiles': [
+            {
+                'absolute_url': sponsor.absolute_url,
+                'avatar_url': sponsor.avatar_url,
+            }
+            for sponsor in event.sponsor_profiles.all()
+        ],
+        'display_showcase': event.display_showcase,
+        'show_results': event.show_results,
     }
