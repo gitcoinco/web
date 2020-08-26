7d880cd6781c0626b216f4fb85c293fcedb5f81d
Andrew Redden <opensource@andrewredden.com>
2020-08-26 14:45:45 -0300

    haystack settings configured to retrieve the proper env variable, removing stale ui components

--- app/app/settings.py
+++ app/app/settings.py
@@ -149,17 +149,6 @@ INSTALLED_APPS = [
     'haystack',
 ]
 
-
-HAYSTACK_CONNECTIONS = {
-    'default': {
-        'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
-        'URL': 'elasticsearch:9200/',
-        'INDEX_NAME': 'haystack',
-    },
-}
-# Update Search index in realtime (using models.db.signals)
-HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
-
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -835,6 +824,16 @@ TIP_PAYOUT_PRIVATE_KEY = env('TIP_PAYOUT_PRIVATE_KEY', default='0x00De4B13153673
 
 ELASTIC_SEARCH_URL = env('ELASTIC_SEARCH_URL', default='')
 ELASTIC_SEARCH_LB_URL = env('ELASTIC_SEARCH_LB_URL', default='')
+
+HAYSTACK_CONNECTIONS = {
+    'default': {
+        'ENGINE': 'haystack.backends.elasticsearch2_backend.Elasticsearch2SearchEngine',
+        'URL': f"{ELASTIC_SEARCH_URL}:9200",
+        'INDEX_NAME': 'haystack',
+    },
+}
+# Update Search index in realtime (using models.db.signals)
+HAYSTACK_SIGNAL_PROCESSOR = 'haystack.signals.RealtimeSignalProcessor'
 
 account_sid = env('TWILIO_ACCOUNT_SID', default='')
 auth_token = env('TWILIO_AUTH_TOKEN', default='')
