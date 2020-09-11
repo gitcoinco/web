1b6f84215f6ce55b284624ac1750deb2e1370c43
Ben DiFrancesco <ben@scopelift.co>
2020-09-10 23:29:31 -0400

    Increase the size of the BrightID QR Code

--- app/assets/v2/js/pages/profile-trust.js
+++ app/assets/v2/js/pages/profile-trust.js
@@ -11,15 +11,6 @@ $(document).ready(function() {
   });
 });
 
-Vue.component('grants-cart', {
-  delimiters: [ '[[', ']]' ],
-
-  data: function() {
-  },
-
-  template: `<h1>HULLO WORLD</h1>`,
-}
-
 let show_brightid_connect_modal = function(brightid_uuid) {
   const brightIdLink = `https://app.brightid.org/link-verification/http:%2f%2fnode.brightid.org/Gitcoin/${brightid_uuid}`;
   const brightIdAppLink = `brightid://link-verification/http:%2f%2fnode.brightid.org/Gitcoin/${brightid_uuid}`;
@@ -75,8 +66,8 @@ let show_brightid_connect_modal = function(brightid_uuid) {
     const element = document.getElementById('qrcode');
     const qrCodeData = {
       text: brightIdAppLink,
-      width: 100,
-      height: 100
+      width: 175,
+      height: 175
     };
 
     new QRCode(element, qrCodeData); // eslint-disable-line
@@ -143,16 +134,3 @@ let show_brightid_verify_modal = function(brightid_uuid) {
   $(content).appendTo('body');
   $('#verify_brightid_modal').bootstrapModal('show');
 };
-
-if (document.getElementById('gc-trust-bonus-tab')) {
-
-  const app = new Vue({
-    delimiters: [ '[[', ']]' ],
-    el: '#gc-trust-bonus-tab',
-    data: {
-    }
-  });
-
-  console.log('APP', app);
-}
-
