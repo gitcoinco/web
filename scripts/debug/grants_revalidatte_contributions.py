b5a0006dab062f55e3fb069b914bc125c4875ac3
owocki <ksowocki@gmail.com>
2020-09-18 10:37:24 -0600

    https://gitcoincore.slack.com/archives/CAXQ7PT60/p1600390329000200

--- scripts/debug/grants_revalidatte_contributions.py
+++ scripts/debug/grants_revalidatte_contributions.py
@@ -1,11 +1,17 @@
 from grants.models import *
 from django.utils import timezone
+
+search_str = 'Expecting value:' #alethio issue
+search_str = 'tuple index out of range' #unknown
+search_str = 'zkSync' #zksync issue
+num_to_process = 100
+
 start = timezone.now() - timezone.timedelta(hours=24)
 contributions = Contribution.objects.filter(created_on__gt=start).order_by('-created_on')
-contributions = contributions.filter(validator_comment__icontains='is_correct_recipient')
-contributions = contributions[:10]
+contributions = contributions.filter(validator_comment__icontains=search_str)
+contributions = contributions[:num_to_process]
 for contrib in contributions:
     print(contrib.pk)
     contrib.update_tx_status()
-
+    contrib.save()
 
