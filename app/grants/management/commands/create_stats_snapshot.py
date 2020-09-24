4df9a37b529fd87012d485fe994b9fdbd7885bf2
owocki <ksowocki@gmail.com>
2020-09-24 09:42:38 -0600

    grants stats fix + also u can now add ur grant to cart

--- app/grants/management/commands/create_stats_snapshot.py
+++ app/grants/management/commands/create_stats_snapshot.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand
 
-from grants.models import Grant, GrantStat
+from grants.models import Grant, GrantStat, CartActivity
+from django.utils import timezone
 
 
 class Command(BaseCommand):
@@ -15,7 +16,8 @@ class Command(BaseCommand):
             # snapshot
             data = {
                 'impressions': grant.get_view_count,
-                'in_cart': grant.cart_actions.filter(latest=True).count(),
+                'in_cart': CartActivity.objects.filter(metad
+   ...: ata__icontains=f'/grants/{grant.pk}/').filter(latest=True, created_on__gt=(timezone.now() - timezone.timedelta(days=14))).count(),
                 'contributions': grant.contribution_count
             }
             snapshot_type = 'total'
