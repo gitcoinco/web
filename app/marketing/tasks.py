b496d604a10ee074b6dac3f847f8995d53de9a7b
owocki <ksowocki@gmail.com>
2020-09-25 12:43:42 -0600

    task re-routing

--- app/marketing/tasks.py
+++ app/marketing/tasks.py
@@ -8,28 +8,43 @@ from marketing.mails import new_bounty_daily as new_bounty_daily_email
 from marketing.mails import weekly_roundup as weekly_roundup_email
 from marketing.models import EmailSubscriber
 
+import socket
+
 logger = get_task_logger(__name__)
 
 redis = RedisService().redis
 
 
-@app.shared_task(bind=True, max_retries=1, rate_limit='2/s')
+@app.shared_task(bind=True, rate_limit='2/s')
 def new_bounty_daily(self, email_subscriber_id, retry: bool = True) -> None:
     """
     :param self:
     :param pk:
     :return:
     """
+
+    # dont send roundup emails on this server, dispurse them back into the queue
+    if socket.gethostname() == 'ip-172-31-5-127':
+        self.retry(countdown=5)
+        return
+    # actually do the task
     es = EmailSubscriber.objects.get(pk=email_subscriber_id)
     new_bounty_daily_email(es)
 
-@app.shared_task(bind=True, max_retries=1, rate_limit='2/s')
+@app.shared_task(bind=True, rate_limit='2/s')
 def weekly_roundup(self, to_email, retry: bool = True) -> None:
     """
     :param self:
     :param pk:
     :return:
     """
+
+    # dont send roundup emails on this server, dispurse them back into the queue
+    if socket.gethostname() == 'ip-172-31-5-127':
+        self.retry(countdown=5)
+        return
+    
+    # actually do the task
     weekly_roundup_email([to_email])
 
 
