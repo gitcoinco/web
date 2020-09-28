f293a187ccadd36d8640589ac5e0914310b7266c
Andrew Redden <opensource@andrewredden.com>
2020-09-27 21:34:15 -0300

    UI fixes for filters, working on expanding proper filter support, some qu eries might have performance issues

--- app/dashboard/migrations/0150_userdirectory.py
+++ app/dashboard/migrations/0150_userdirectory.py
@@ -7,7 +7,6 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('dashboard', '0142_auto_20200818_0807'),
     ]
 
     operations = [
@@ -81,9 +80,6 @@ class Migration(migrations.Migration):
                 ('following_count', models.IntegerField()),
                 ('num_repeated_relationships', models.IntegerField()),
                 ('verification_status', models.CharField(max_length=255, null=True)),
-            ],
-            options={
-                'managed': False,
-            },
+            ]
         ),
     ]
