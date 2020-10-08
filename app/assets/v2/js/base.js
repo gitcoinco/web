583e5e2f96dba4a89d9ed7b7ca00e9c79dae2493
Miguel Angel G <miguel@gordian.dev>
2020-10-07 23:53:18 -0500

    remove missed code

--- app/assets/v2/js/base.js
+++ app/assets/v2/js/base.js
@@ -278,7 +278,6 @@ const closeButton = function(msg) {
 };
 
 const alertMessage = function(msg) {
-  return ''
   var html = `<strong>${typeof msg['title'] !== 'undefined' ? msg['title'] : ''}</strong>${msg['message']}`;
 
   return html;
