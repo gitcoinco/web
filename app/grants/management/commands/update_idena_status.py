bf2e7a22b5aa393db04c46ca385f8f6ef317b836
Arash Fatahzade <fatahzade@gmail.com>
2020-10-30 01:55:53 +0330

    Added update_idena_status command

---
+++ app/grants/management/commands/update_idena_status.py
@@ -0,0 +1,18 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+
+from dashboard.models import Profile
+
+
+class Command(BaseCommand):
+
+    help = 'Checks for connected Idena users and updates them in the database'
+
+    def handle(self, *args, **options):
+        connected_profiles = Profile.objects.filter(is_idena_connected=True)
+        total_count = Profile.objects.filter(is_idena_connected=True).count()
+
+        for index, profile in enumerate(connected_profiles):
+            print(f'{index + 1}/{total_count}: Updating {profile.handle}')
+            profile.update_idena_status()
+            profile.save()
