e951288ce875ef89e078bb3728c379ed96e4d6de
Zachary <1734015+nopslip@users.noreply.github.com>
2020-10-31 06:03:17 -0600

    cleaning up gtc-token-claim.js, dissallow non-authd users to claim tokens on demo2

--- app/assets/quadraticlands/js/gtc-token-claim.js
+++ app/assets/quadraticlands/js/gtc-token-claim.js
@@ -6,41 +6,179 @@ var signed_message_payload
 /// payback (sign msg)
 
 window.addEventListener('dataWalletReady', function(e) {
-    console.log('hereherhere!', selectedAccount)
+    // $('.wallet-address').html(selectedAccount) //class
     $('#wallet-address').html(selectedAccount)
  }, false);
 
-$( document ).ready(function() {
-    console.log( "ready!" );
+// $( document ).ready(function() {
+//     console.log( "ready!" );
    
 $(document).on('click', '#beginClaim', (event) => {
     console.log('-->#beginClaim successfully triggered')
-    console.log('selectedAccount', selectedAccount);
-    // var sendForm = fetchData('/quadraticlands/claim2', 'POST', {'address':selectedAccount}, {'X-CSRFToken': csrftoken})
-    
-    var sendForm = fetchData('/quadraticlands/claim2', 'POST', {'address':selectedAccount}, {'X-CSRFToken': csrftoken})
 
-    $.when(sendForm).then((response, status, statusCode) => {
+    // confirm we have a working web3 connection, if not, return error 
+    try {
+        const network = checkWeb3();
+    } catch (error) {
+        // @Richard, this is where we could trigger a user notification 
+        // that they are not connected to a wallet/web3 and thus the claim will not proceed. 
+        return console.log('Please confirm you have a wallet connected!!')
+    }
+
+    // ZW TODO - check that eth address is_valid before proceeding? - if not valid, return 
+
+    var getClaimData = fetchData('/quadraticlands/claim2', 'POST', {'address':selectedAccount}, {'X-CSRFToken': csrftoken})
+
+    $.when(getClaimData).then((response, status, statusCode) => {
         // for debugging, remove for prod 
-        // console.log('POST RETURNED!', statusCode, response, status)
+        console.log('POST RETURNED!', statusCode, response, status)
         
         // if we don't get a 200 from the Ethereum Message Signing Service then return error  
         if (statusCode.status != 200) {
-            /**  
-            * I need to confirm/deny _alert will trigger here, I don't think it's all setup correctly yet 
-            * _alert is currently set/driven by web3alert.js included directly in demo2.html 
-            * as needed, we can adjust and change that so that all our _alerts are imported on base.html 
-            **/
-            return _alert(response.msg, 'error');
+            console.log('getClaimData response: ', statusCode.status )
+            return //_alert(response.msg, 'error');
         }
        
         // setup & send our contract call 
         setupGTCTokenClaim(response)
 
+        // used for debugging if you want to confirm calling function after POST
+        // will be removed for prod 
+        // testClaim(response);
+
 
     });
 
 });
+
+async function setupGTCTokenClaim(emss_response) {
+    
+    const user_id = emss_response.user_id;
+    const user_amount = emss_response.user_amount;
+    const user_address_cleaned = web3.utils.toChecksumAddress(emss_response["user_address"].replace(/\"/g, '')); // make sure it's check summ'd address and strip quotes
+    const eth_signed_message_hash_hex = emss_response.eth_signed_message_hash_hex.replace(/\"/g, ''); // strip quotes 
+    const eth_signed_signature_hex = emss_response.eth_signed_signature_hex.replace(/\"/g, ''); // strip quotes 
+    
+    try {
+      // who will send the transaction? (active user wallet account)
+      [user] = await web3.eth.getAccounts();
+      // ZW TODO - move token contract addy to env or standard abi.js file, added new Rinkeby tokendist contract 10/29/2020
+      // also, we will want to update the contract ABI at some point. it's old but still works as the function name is the same :) 
+      const tokenDistributor = await new web3.eth.Contract(
+        token_distributor_abi,
+        '0xa4c8B8a59805F6B049b977296881CE76f538D7C4'
+      );
+          
+      const claimGTCtokens = () => {
+        // waitingState(true);
+               tokenDistributor.methods
+          .claimTokens(user_id, user_address_cleaned, user_amount, eth_signed_message_hash_hex, eth_signed_signature_hex)
+          
+          /** TODO - Evaluate optimal gas settings for token claims 
+          * strat: hard coded price is fallback, dynamic real time gas adjustment is optimal  - how much gas can we save? 
+          *  // Legacy Note- We hardcode gas limit otherwise web3's `estimateGas` is used and this will show the user
+          *  // that their transaction will fail because the approval tx has not yet been confirmed
+          */
+          .send({ from: user, gasLimit: '300000' })
+          .on('transactionHash', async function(transactionHash) {
+            // @Richard, this is where we can trigger to the PENDING screen - or whatever you want to do upon receiving the txid confirming the tx has been broadcast     
+            // _alert('Your claim has been broadcast to the network!.', 'success', 5000);
+            const successMsg = 'Congratulations, your token claim transaction was broadcast to the Ethereum network! ';
+            console.log('successMsg', successMsg, 'txid: ', transactionHash);
+
+            // this is not live yet, need to create DB models first before we can write txid to postgres db 
+            // pushTXID2DB(transactionHash); 
+            
+          })
+          // this was added for debug/testing - I'm not sure there is any value in having this and it will likely be removed. 
+          .on('receipt', receipt => {
+                console.log('receipt:', receipt);
+          })
+          // also added for debug/testing - I will keep it in for now in case we want to trigger an event on tx confirmation 
+          .on('confirmation', (confirmationNumber, receipt) => {
+                if (confirmationNumber = 1 ) {
+                  console.log('confirmations:', confirmationNumber, receipt);
+                }
+                // @Richard, do we want to react to confirmation? 
+          })
+          .on('error', (error) => { 
+            const errorMsg = 'Oops, something went wrong with token claim. Please check transaction for more info, try again, and/or contact support@gitcoin.co';
+            console.log('errorMsg',errorMsg);
+            // console.error(error)
+            // waitingState(false);
+            // handleError(error);
+          });
+        // indicateMetamaskPopup();
+        // make our claim
+         
+      };
+      //indicateMetamaskPopup();
+      claimGTCtokens();  
+           
+ 
+    } catch (error) {
+      console.log('error on setupTokenClaim: ', error)
+      // handleError(error);
+    }
+
+  }
+
+function pushTXID2DB(transactionHash) {
+    
+    // check csrf token settings for second api fetch 
+    var sendForm = fetchData('/quadraticlands/write-claim-TXID', 'POST', {'txid':transactionHash}, {'X-CSRFToken': csrftoken})
+    $.when(sendForm).then((response, status, statusCode) => {
+        // for debugging, remove for prod 
+        console.log('POST RETURNED!', statusCode, response, status)
+        
+        // if we don't get a 200 from the database api then we return the error 
+        if (statusCode.status != 200) {
+            console.log('statusCode on pushTXID2DB: ', statusCode)
+            return //_alert(response.msg, 'error');
+        }
+   });  
+
+}
+
+
+function checkNetwork() {
+    const supportedNetworks = [ 'rinkeby', 'mainnet' ];
+    
+    if (!supportedNetworks.includes(document.web3network)) {
+      //_alert('Unsupported network', 'error');
+      throw new Error;
+    }
+    return document.web3network;
+}
+  
+function checkWeb3() {
+    if (!web3) {
+      // _alert('Please connect a wallet', 'error');
+      throw new Error;
+    }
+    return checkNetwork();
+  }
+  
+function handleError(err) {
+    console.error(err);
+    let message = 'There was an error';
+    
+    if (err.message)
+      message = err.message;
+    else if (err.msg)
+      message = err.msg;
+    else if (err.responseJSON)
+      message = err.responseJSON.message;
+    else if (typeof err === 'string')
+      message = err;
+    
+    //_alert(message, 'error');
+    indicateMetamaskPopup(true);
+}
+  
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 async function testClaim(response) {
     try {
@@ -61,116 +199,6 @@ async function testClaim(response) {
     }
 
 }
-
-async function setupGTCTokenClaim(emss_response) {
-    
-    // if user messes with these, the contract will reject the claim
-    // for this reason, extensive validation is not necessary     
-    const user_id = emss_response.user_id;
-    const user_amount = emss_response.user_amount;
-    const user_address_cleaned = web3.utils.toChecksumAddress(emss_response["user_address"].replace(/\"/g, '')); // make sure it's check summ'd address and strip quotes
-    const eth_signed_message_hash_hex = emss_response.eth_signed_message_hash_hex;
-    const eth_signed_signature_hex = emss_response.eth_signed_signature_hex;
-    
-    try {
-      // confirm we have a working web3 connection, if not, bail   
-      const network = checkWeb3();
-       
-      // get the current active account/address from the user 
-      // not needed here unless we want to do a final check to confirm something with active address
-      // i dont think so tho...? 
-      [user] = await web3.eth.getAccounts();
-  
-      // TODO - move token contract addy to env or standard abi.js file
-      // ZW: added new Rinkeby tokendist contract 10/29/2020
-      // also, we will want to update the contract ABI at somepoint. it's old but still works as the function name is the same :) 
-      const tokenDistributor = await new web3.eth.Contract(
-        token_distributor_abi,
-        '0xa4c8B8a59805F6B049b977296881CE76f538D7C4'
-      );
-          
-      const claimGTCtokens = () => {
-        // waitingState(true);
-        tokenDistributor.methods
-          .claimTokens(user_id, user_address_cleaned, user_amount, eth_signed_message_hash_hex.replace(/\"/g, ''), eth_signed_signature_hex.replace(/\"/g, ''))
-        // We hardcode gas limit otherwise web3's `estimateGas` is used and this will show the user
-        // that their transaction will fail because the approval tx has not yet been confirmed
-          .send({ from: user, gasLimit: '300000' })
-          .on('transactionHash', async function(transactionHash) {
-            // push txid to DB 
-            // idk what these are for yet 
-            // set/send variable with TXID --> view 
-        
-            // indicateMetamaskPopup(true);
-            _alert('Your claim has been broadcast to the network!.', 'success', 5000);
-            const successMsg = 'Congratulations, your token purchase was successful!';
-            const errorMsg = 'Oops, something went wrong purchasing the token. Please try again or contact support@gitcoin.co';
-          })
-          .on('receipt', receipt => {
-                console.log('receipt:', receipt);
-          })
-          .on('confirmation', (confirmationNumber, receipt) => {
-                if (confirmationNumber >= 1) {
-                  console.log('confirmations:', confirmationNumber, receipt);
-                }
-          })
-          .on('error', (error, receipt) => {
-            // waitingState(false);
-            handleError(error);
-          });
-          
-      };
-       
-      // now we run the function.
-      indicateMetamaskPopup();
-      claimGTCtokens();
-  
-    } catch (error) {
-      handleError(error);
-    }
-  }
-  
-  function checkNetwork() {
-    const supportedNetworks = [ 'rinkeby', 'mainnet' ];
-    
-    if (!supportedNetworks.includes(document.web3network)) {
-      _alert('Unsupported network', 'error');
-      throw new Error('Please connect a wallet');
-    }
-    return document.web3network;
-  }
-  
-  function checkWeb3() {
-    if (!web3) {
-      _alert('Please connect a wallet', 'error');
-      throw new Error('Please connect a wallet');
-    }
-    return checkNetwork();
-  }
-  
-  function handleError(err) {
-    console.error(err);
-    let message = 'There was an error';
-    
-    if (err.message)
-      message = err.message;
-    else if (err.msg)
-      message = err.msg;
-    else if (err.responseJSON)
-      message = err.responseJSON.message;
-    else if (typeof err === 'string')
-      message = err;
-    
-    _alert(message, 'error');
-    indicateMetamaskPopup(true);
-  }
-  
-  function isNumeric(n) {
-    return !isNaN(parseFloat(n)) && isFinite(n);
-  }
-
-
-}); // document.on ready --> 
 
 
 
