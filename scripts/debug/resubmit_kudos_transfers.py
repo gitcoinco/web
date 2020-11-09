1c4a71e11758a688955c85187c835b311201aaf4
owocki <ksowocki@gmail.com>
2020-11-09 14:16:04 -0700

    a few small cleanup commits

--- scripts/debug/resubmit_kudos_transfers.py
+++ scripts/debug/resubmit_kudos_transfers.py
@@ -4,11 +4,13 @@ from django.conf import settings
 
 from dashboard.utils import get_web3
 from gas.utils import recommend_min_gas_price_to_confirm_in_time
+from dashboard.utils import has_tx_mined
 from kudos.utils import kudos_abi
 from web3 import Web3
 
 #setup
-w3 = get_web3('mainnet')
+network = 'mainnet'
+w3 = get_web3(network)
 kudos_contract_address = Web3.toChecksumAddress(settings.KUDOS_CONTRACT_MAINNET)
 kudos_owner_address = Web3.toChecksumAddress(settings.KUDOS_OWNER_ACCOUNT)
 nonce = w3.eth.getTransactionCount(kudos_owner_address)
@@ -17,8 +19,13 @@ nonce = w3.eth.getTransactionCount(kudos_owner_address)
 min_pk = 1684
 max_pk = 1740
 TIME_SLEEP = 1
+username = 'jhanlew'
+gas_clear_within_mins = 1
 
-for kt in KudosTransfer.objects.filter(pk__gte=min_pk, pk__lte=max_pk):
+kts = KudosTransfer.objects.filter(pk__gte=min_pk, pk__lte=max_pk)
+kts = KudosTransfer.objects.filter(username=username, tx_status='dropped')
+
+for kt in kts:
 
     token_id = kt.kudos_token_cloned_from.token_id
     address = kt.receive_address
@@ -28,19 +35,23 @@ for kt in KudosTransfer.objects.filter(pk__gte=min_pk, pk__lte=max_pk):
     tx = contract.functions.clone(address, token_id, 1).buildTransaction({
         'nonce': nonce,
         'gas': 500000,
-        'gasPrice': int(recommend_min_gas_price_to_confirm_in_time(2) * 10**9),
+        'gasPrice': int(recommend_min_gas_price_to_confirm_in_time(gas_clear_within_mins) * 10**9),
         'value': int(price_finney / 1000.0 * 10**18),
     })
 
     signed = w3.eth.account.signTransaction(tx, settings.KUDOS_PRIVATE_KEY)
-    time.sleep(TIME_SLEEP)
 
     try:
         txid = w3.eth.sendRawTransaction(signed.rawTransaction).hex()
         nonce += 1
         print(f'sent tx {nonce}')
         kt.txid = txid
+        kt.tx_status = 'pending'
         kt.save()
+
+        while not has_tx_mined(txid, network):
+            time.sleep(TIME_SLEEP)
+
 
     except Exception as e:
         print(e)
