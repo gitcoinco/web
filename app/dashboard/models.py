b7306a2b3bc84fee63ace858b50e1272dfcfbe2b
owocki <ksowocki@gmail.com>
2020-11-19 08:30:42 -0700

    a handful of YOLO xdai/kudos fixes, for quests

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -1640,7 +1640,7 @@ class SendCryptoAsset(SuperModel):
     @property
     def receive_tx_blockexplorer_link(self):
         if self.network == 'xdai':
-            return f"https://explorer.anyblock.tools/ethereum/poa/xdai/transaction/{self.receive_txid}"
+            return f"https://explorer.anyblock.tools/ethereum/poa/xdai/tx/{self.receive_txid}"
         if self.network == 'mainnet':
             return f"https://etherscan.io/tx/{self.receive_txid}"
         return f"https://{self.network}.etherscan.io/tx/{self.receive_txid}"
