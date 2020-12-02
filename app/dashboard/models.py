582a0bd163f3653d9432e78df68eb49b4fff2b0a
Ben DiFrancesco <ben@scopelift.co>
2020-12-01 23:04:11 -0500

    Trust Bonus Cleanup and Improvments (#7987)
    * Prevent past expiration date in redis for Idena validation ceremony time
    
    * Move Idena validation to an 'Active Beta' section of the Trust Bonus
    
    * Alphabetize Trust Bonus services w/ equal matcha mount
    
    * Remove Idena from consideration when showing the cart Trust Bonus banner
    
    * Remove Idena from Trust Bonus calculation; add Google and POAP

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -2963,8 +2963,10 @@ class Profile(SuperModel):
             tb *= 1.05
         if self.sms_verification:
             tb *= 1.05
-        if self.is_idena_verified:
-            tb *= 1.25
+        if self.is_google_verified:
+            tb *= 1.05
+        if self.is_poap_verified:
+            tb *= 1.05
         return tb
 
 
