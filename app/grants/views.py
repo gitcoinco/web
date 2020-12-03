a76d86064859ecb2a4e45b77d5cee7fdd65a7e61
octavioamu <octavioamuchastegui@gmail.com>
2020-12-03 17:12:03 -0300

    disable collections on the main grants page

--- app/grants/views.py
+++ app/grants/views.py
@@ -593,8 +593,9 @@ def get_grants(request):
             collections = paginator.get_page(page)
     else:
         _grants = build_grants_by_type(**filters)
-
-        collections = GrantCollection.objects.filter(grants__in=Subquery(_grants.values('id'))).distinct()[:3]
+        collections = []
+        # disabling collections on the main grant page to improve performance
+        # collections = GrantCollection.objects.filter(grants__in=Subquery(_grants.values('id'))).distinct()[:3]
 
         paginator = Paginator(_grants, limit)
         grants = paginator.get_page(page)
