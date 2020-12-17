20abed5cbb96d018945bafb2f1d48e970d7f0e31
Chibuotu Amadi <chibuotu@gmail.com>
2020-12-17 11:10:58 +0100

    fix: amount comparison for insufficient balance

--- app/assets/v2/js/lib/binance/utils.js
+++ app/assets/v2/js/lib/binance/utils.js
@@ -38,10 +38,10 @@ binance_utils.getAddressBalance = async address => {
 
   const result = await jsonRpcRequest('eth_getBalance', [address, 'latest']);
 
-  // convert hex balance to integer and account for decimal points
-  const bnbBalance = BigInt(result).toString(10) * 10 ** -18;
+  // convert hex balance to integer
+  const bnbBalance = BigInt(result).toString(10);
 
-  return Promise.resolve(bnbBalance.toFixed(4));
+  return Promise.resolve(bnbBalance);
 };
 
 /**
@@ -71,10 +71,10 @@ binance_utils.getAddressTokenBalance = async (address, tokenContractAddress) => 
   ]
   const result = await jsonRpcRequest('eth_call', params);
 
-  // convert hex balance to integer and account for decimal points
-  const tokenBalance = BigInt(result).toString(10) * 10 ** -18;
+  // convert hex balance to integer
+  const tokenBalance = BigInt(result).toString(10);
 
-  return Promise.resolve(tokenBalance.toFixed(4));
+  return Promise.resolve(tokenBalance);
 };
 
 
@@ -95,7 +95,7 @@ binance_utils.getExtensionConnectedAccounts = async () => {
 
 /**
  * Sign and transfer token to another address via extension and returns txn hash
- * @param {Number} amount
+ * @param {Number} amount : in wei
  * @param {String} to_address
  * @param {String} token_name
  * @param {String} from_address : optional, if not passed takes account first account from getExtensionConnectedAccounts
@@ -158,7 +158,7 @@ binance_utils.transferViaExtension = async (amount, to_address, from_address, to
 
       const account_balance = await binance_utils.getAddressTokenBalance(from_address);
 
-      if (Number(account_balance) < amount) {
+      if (Number(account_balance) < amount ) {
         reject(`transferViaExtension: insufficent balance in address ${from_address}`);
       }
 
@@ -169,7 +169,7 @@ binance_utils.transferViaExtension = async (amount, to_address, from_address, to
             ['transfer(address, uint256)']
           );
           const method_id = methodSignature.substr(0, 10);
-          const amount = BigInt(amount * 10 ** 18).toString(16).padStart(64, '0'); // convert to hex and pad with zeroes
+          const amount = amount.toString(16).padStart(64, '0'); // convert to hex and pad with zeroes
           const to_address = to_address.substr(2).padStart(64, '0'); // remove 0x and pad with zeroes
 
           const params = [
