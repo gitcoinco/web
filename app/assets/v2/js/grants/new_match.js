gitcoinco/web

commit 020a0dbb17d40a9fe9291d671df8140cde4e192e

fies the busted grant matching partner form so that it doenset send spammy email, and instead uses teh gitcoin grant for funding the match pool to fund teh match pool

app/assets/v2/js/grants/new_match.js
@@ -41,7 +41,8 @@ Vue.mixin({
         .send({from: selectedAccount},
           (error, tx_id) => {
             if (error) {
-              _alert({ message: gettext('Unable to pay the match pledge amount. Please try again.') }, 'error');
+              _alert('Transaction Failed. To fund the matching pool please visit this Grant.', 'error');
+              document.location.href = 'https://gitcoin.co/grants/12/gitcoin-grants-official-matching-pool-fund';
               console.error(`error: unable to pay pledge due to : ${error}`);
               return;
             }
@@ -110,17 +111,8 @@ Vue.mixin({
         'comment': form.comment
       };
 
-      if (form.stage == 'ready') {
-        if (!provider) {
-          onConnect();
-          return false;
-        }
-
-        vm.transfer_web3(params);
-      } else {
-        vm.submitted = true;
-        vm.createMatchingPledge(params);
-      }
+    vm.submitted = true;
+    vm.createMatchingPledge(params);
 
     },
     async createMatchingPledge(data) {
@@ -141,8 +133,9 @@ Vue.mixin({
         response = await fetchData(url, 'POST', data, headers);
 
         if (response.status == 200) {
-          _alert('Match Pledge Request Created.');
+          _alert('Match Pledge Request Recorded.  To fund the matching pool please visit this Grant.');
           vm.clearForm();
+          document.location.href = 'https://gitcoin.co/grants/12/gitcoin-grants-official-matching-pool-fund';
         } else {
           vm.submitted = false;
           _alert('Unable to create matching pledge. Please try again', 'error');
@@ -181,7 +174,7 @@ if (document.getElementById('gc-new-match')) {
   ];
   const stage_options = [
     {'key': 'ready', 'val': 'I am ready to transfer DAI'},
-    {'key': 'details', 'val': 'Send me more details'}
+    {'key': 'details', 'val': 'Not ready to transfer DAI'}
   ];
 
   appFormBounty = new Vue({

app/grants/views.py
@@ -2683,7 +2683,8 @@ def create_matching_pledge_v1(request):
     )
 
     match_pledge.save()
-    new_grant_match_pledge(match_pledge)
+    # dont' send spammy email
+    # new_grant_match_pledge(match_pledge)
 
     response = {
         'status': 200,

commit 2bf72fcc7254bce941773fb471668da951625e54

fix eslint errors caused by 020a0dbb17d40a9fe9291d671df8140cde4e192e

app/assets/v2/js/grants/new_match.js
@@ -111,8 +111,8 @@ Vue.mixin({
         'comment': form.comment
       };
 
-    vm.submitted = true;
-    vm.createMatchingPledge(params);
+      vm.submitted = true;
+      vm.createMatchingPledge(params);
 
     },
     async createMatchingPledge(data) {