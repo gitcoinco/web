5674aa786e99625e39e2c119853b898dd80530fc
Kevin Owocki <owocki@Kevins-MacBook-Pro.local>
2021-03-04 09:56:24 -0700

    updates email per joe request

--- app/marketing/mails.py
+++ app/marketing/mails.py
@@ -724,7 +724,7 @@ def new_faucet_request(fr):
 
 
 def new_grant_admin(grant):
-    to_emails = [settings.PERSONAL_CONTACT_EMAIL, 'scott.moore@consensys.net']
+    to_emails = ['support@gitcoin.co', 'joe@gitcoin.co']
     from_email = settings.SERVER_EMAIL
     cur_language = translation.get_language()
     for to_email in to_emails:
