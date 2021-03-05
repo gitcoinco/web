c9b9ec483d545a0267fe9ec6182062ff88665b73
owocki <ksowocki@gmail.com>
2021-03-05 13:45:14 -0700

    adds the ability to access grants API data via API KEYs

--- app/grants/models.py
+++ app/grants/models.py
@@ -246,6 +246,30 @@ class GrantCLR(SuperModel):
         )
 
 
+class GrantAPIKey(SuperModel):
+    """Define the structure of a GrantAPIKey."""
+
+    key = models.CharField(
+        max_length=255,
+        blank=True,
+        db_index=True,
+        help_text="the api key"
+    )
+    secret = models.CharField(
+        max_length=255,
+        blank=True,
+        db_index=True,
+        help_text="the api secret"
+    )
+    profile = models.ForeignKey(
+        'dashboard.Profile',
+        related_name='grant_apikey',
+        on_delete=models.CASCADE,
+        help_text=_('The GrantAPI key\'s profile.'),
+        null=True,
+    )
+
+
 class Grant(SuperModel):
     """Define the structure of a Grant."""
 
