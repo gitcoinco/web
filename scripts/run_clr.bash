231d40199bbb92b0ad2597acec0b8b0edb497bfa
owocki <ksowocki@gmail.com>
2021-03-14 11:17:20 -0600

    proper log path

--- scripts/run_clr.bash
+++ scripts/run_clr.bash
@@ -2,5 +2,11 @@ CURRENT_ROUNDS=$(bash scripts/run_management_command.bash get_active_clrs mainne
 for i in $(echo "$CURRENT_ROUNDS"); do
     echo $i
     LOGPATH="/var/log/gitcoin/estimate_clr_$i.log"
-    bash scripts/run_management_command_if_not_already_running.bash estimate_clr mainnet $i >> LOGPATH &
+    bash scripts/run_management_command_if_not_already_running.bash estimate_clr mainnet $i >> $LOGPATH 2>&1 &
+
+    # TODO: move to a screen perheps, per https://superuser.com/questions/454907/how-to-execute-a-command-in-screen-and-detach
+    # a la
+    # screen -S sleepy -dm sleep 60
+
 done
+
