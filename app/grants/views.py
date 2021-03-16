6dde489b1b0d01c153371c9032025c338aead86c
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-03-16 18:20:07 +0530

    allow admin to opt out of CLR

--- app/grants/views.py
+++ app/grants/views.py
@@ -1593,6 +1593,9 @@ def grant_edit(request, grant_id):
         if not description_rich:
             description_rich = description
 
+        is_clr_eligible = json.loads(request.POST.get('is_clr_eligible', 'true'))
+        grant.is_clr_eligible = description_rich = is_clr_eligible
+
         eth_payout_address = request.POST.get('eth_payout_address', '0x0')
         zcash_payout_address = request.POST.get('zcash_payout_address', '0x0')
         celo_payout_address = request.POST.get('celo_payout_address', '0x0')
