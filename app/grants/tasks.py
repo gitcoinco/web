a56716d90becd544e3769e518927ea3aea6735b9
owocki <ksowocki@gmail.com>
2021-03-22 11:50:33 -0600

    Leverages the fact that for 0 donations ( in the [0,1,10,100,1000] paradigm) we only need to run clr calculations once. Because of this, we can update the existing match estimates in about 1/1000th of the time it takes to do a whole calculation.
    This also updates the scheduler to run the slim calcs in parallel with the full calcs

--- app/grants/tasks.py
+++ app/grants/tasks.py
@@ -331,12 +331,13 @@ def recalc_clr(self, grant_id, retry: bool = True) -> None:
             from_date=timezone.now(),
             clr_round=clr_round,
             network=network,
-            only_grant_pk=obj.pk
+            only_grant_pk=obj.pk,
+            what='slim'
         )
 
 
 @app.shared_task(bind=True, max_retries=1)
-def process_predict_clr(self, save_to_db, from_date, clr_round, network) -> None:
+def process_predict_clr(self, save_to_db, from_date, clr_round, network, what) -> None:
     from grants.clr import predict_clr
 
     print(f"CALCULATING CLR estimates for ROUND: {clr_round.round_num} {clr_round.sub_round_slug}")
@@ -345,7 +346,8 @@ def process_predict_clr(self, save_to_db, from_date, clr_round, network) -> None
         save_to_db,
         from_date,
         clr_round,
-        network
+        network,
+        what=what,
     )
 
     print(f"finished CLR estimates for {clr_round.round_num} {clr_round.sub_round_slug}")
