82e525c355116022148890a55b054a27af68f5c2
Graham Dixon <graham@gdixon.co.uk>
2021-03-24 08:00:43 +0000

    Updates POAPs abi, from_blocks and adds xDai verification

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -6463,27 +6463,36 @@ def verify_user_poap(request, handle):
             'msg': 'Invalid signature',
         })
 
-    # commented out because network = get_default_network() results in dashboard.utils.UnsupportedNetworkException: rinkeby
-    # network = get_default_network()
-    network = "mainnet"
+    # POAP verification is only valid if the ethereum address has held the badge > 15 days
     fifteen_days_ago = datetime.now()-timedelta(days=15)
+    fitteen_days_ago_ts = fifteen_days_ago.timestamp()
 
-    timestamp = get_poap_earliest_owned_token_timestamp(network, eth_address)
-    if timestamp is None or timestamp > fifteen_days_ago.timestamp():
-        # We couldn't find any POAP badge for this ethereum address
+    timestamp = None
+
+    for network in ['mainnet', 'xdai']:
+        timestamp = get_poap_earliest_owned_token_timestamp(network, True, eth_address)
+        if timestamp or timestamp == 0 and timestamp <= fitteen_days_ago_ts:
+            break
+
+    if timestamp is None or timestamp > fitteen_days_ago_ts:
         return JsonResponse({
             'ok': False,
             'msg': 'No qualifying POAP badges (ERC721 NFTs held for at least 15 days) found for this account.',
+        })
+    elif timestamp == 0:
+        return JsonResponse({
+            'ok': False,
+            'msg': 'An error occured, please try again later.',
         })
 
     profile = profile_helper(handle, True)
     profile.is_poap_verified = True
     profile.save()
+    # Success response
     return JsonResponse({
-                'ok': True,
-                'msg': 'Found a POAP badge that has been sitting in this wallet more than 15 days'
-            }
-    )
+        'ok': True,
+        'msg': 'Found a POAP badge that has been sitting in this wallet more than 15 days'
+    })
 
 
 @csrf_protect
