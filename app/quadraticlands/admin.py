47ed15b1fb575bfd6dc5e9eb07632938b6d9cdb8
Zachary <1734015+nopslip@users.noreply.github.com>
2021-03-24 14:31:45 -0600

    fix to initial_dist admin

--- app/quadraticlands/admin.py
+++ app/quadraticlands/admin.py
@@ -2,12 +2,11 @@ from django.contrib import admin
 
 from quadraticlands.models import InitialTokenDistribution, MissionStatus, QuadLandsFAQ
 
+class InitialTokenDistributionAdmin(admin.ModelAdmin):
+    '''Needed to add raw_id_fields''' 
+    raw_id_fields = ['profile']
 
-class InitialtokenDistribution(admin.ModelAdmin):
-    '''Setup initial dist admin'''
-    raw_id_fields = ['profile'] 
-
-admin.site.register(InitialTokenDistribution)
+admin.site.register(InitialTokenDistribution, InitialTokenDistributionAdmin)
 admin.site.register(MissionStatus)
 admin.site.register(QuadLandsFAQ)
 
