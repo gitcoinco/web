bb3cdf79de943c58079451d207a036b80ef8fab3
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-04-22 08:18:59 +0530

    remove datatrace usage in celery (#8806)

--- app/taskapp/celery.py
+++ app/taskapp/celery.py
@@ -5,11 +5,9 @@ from django.conf import settings
 
 from celery import Celery
 from celery.signals import setup_logging
-from ddtrace import patch
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
-patch(celery=True)
 app = Celery('app')
 
 
