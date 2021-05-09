bc509eba99a1489a0a43ecb2a73f060147947647
Chibuotu Amadi <chibuotu@gmail.com>
2021-05-09 13:27:29 +0100

    tx sync
    - update headers
    - temp use of aggron for mainnet

--- app/dashboard/sync/nervos.py
+++ app/dashboard/sync/nervos.py
@@ -3,30 +3,41 @@ from django.utils import timezone
 import requests
 from dashboard.sync.helpers import record_payout_activity
 
+HEADERS = {
+    'Content-Type': 'application/vnd.api+json',
+    'Accept': 'application/vnd.api+json'
+}
+
 
 def get_nervos_txn_status(txnid, network='mainnet'):
     if not txnid:
         return None
 
-    if network == 'mainnet':
+    if network != 'mainnet': # TODO: network == 'mainnet'
         base_url = 'https://api.explorer.nervos.org/api/v1'
     else:
         base_url = 'https://api.explorer.nervos.org/testnet/api/v1'
     
     explorer_url = f'{base_url}/transactions/{txnid}'
-    tip_block_number = f'{base_url}/statistics/tip_block_number'
+    tip_block_number_url = f'{base_url}/statistics/tip_block_number'
 
-    tx_response = requests.get(explorer_url)
+    tx_response = requests.get(
+        explorer_url, headers=HEADERS).json()['data']['attributes']
 
     tx_data = tx_response.json()['data']['attributes']
 
     if tx_response.status_code == 200:
         tip_block_number = requests.get(
-            tip_block_number_url).json()['data']['attributes']['tip_block_number']
-        confirmations = tip_block_number - tx_data['block_number']
+            tip_block_number_url, headers=HEADERS
+        ).json()['data']['attributes']['tip_block_number']
+        confirmations = tip_block_number - int(tx_data['block_number'])
         output_status = tx_data['display_outputs'][0]['status']
 
-        if confirmations > 0 and tx_data['tx_status'] == 'committed' and output_status == 'live':
+        if (
+            confirmations > 0 and
+            tx_data['tx_status'] == 'committed' and
+            output_status == 'live'
+        ):
             return True
         else:
             return False
