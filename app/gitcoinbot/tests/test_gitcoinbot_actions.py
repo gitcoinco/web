55e73007359f77f1e27103c7bb9fe24502a2ed9b
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-05-12 11:58:45 +0530

    chore: update 2020 -> 2021

--- app/gitcoinbot/tests/test_gitcoinbot_actions.py
+++ app/gitcoinbot/tests/test_gitcoinbot_actions.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """Define Gitcoin Bot related tests.
 
-Copyright (C) 2020 Gitcoin Core
+Copyright (C) 2021 Gitcoin Core
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
