b91e0bae0976c175f66bcb993f6501e27cd262fa
scco <schumanncombo@gmail.com>
2021-05-20 20:08:59 +0200

    ui slim down for /mission GTC badge on mission 3 + slimdown on custom tweet button
    -removed jscript for reading data-atributes on buttons to create a twitter button - as we just use it in one place anyway and to probably get rid of franks bug report
    - removed display the amount within the badge of a mission as it is to big for super long numbers and not important for now.

--- app/assets/v2/js/quadraticlands/mission/receive/custom_tweet.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const tweet_button = document.getElementById('tweet_button');
-  const twitter_url = 'https://twitter.com/intent/tweet';
-
-  tweet_button.href = twitter_url + '?text=' + tweet_button.dataset.tweet_text + '&url=' + tweet_button.dataset.tweet_url + '&hashtags=' + tweet_button.dataset.tweet_hashtags;
-  tweet_button.target = '_blank';
-});
