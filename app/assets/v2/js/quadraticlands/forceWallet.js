0143ef82d7c3c4fbdeef3a198a215c8c8779384b
Graham Dixon <graham@gitcoin.co>
2021-05-27 21:19:02 +0100

    Forces wallet connection using wallet.js methods (#8958)

--- app/assets/v2/js/quadraticlands/forceWallet.js
+++ app/assets/v2/js/quadraticlands/forceWallet.js
@@ -1,11 +1,9 @@
 // force web3Modal to pop up when no Account - even when user logout / looses connection
 // this is here to prevent design "no wallet log in bla bla on all that pages that needs a wallet"
-document.addEventListener('DOMContentLoaded', function() {
-  setInterval(function() {
-    if (!selectedAccount) {
-      if (!web3Modal || !web3Modal.show) {
-        onConnect();
-      }
-    }
-  }, 1000);
+document.addEventListener('DOMContentLoaded', async function() {
+  if (!web3Modal) {
+    needWalletConnection();
+  } else if (!provider) {
+    await onConnect();
+  }
 });
