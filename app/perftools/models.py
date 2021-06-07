9009fd54a6acd6ad913dd528d384dbf12bb9286e
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-06-07 20:00:51 +0530

    chore: remove hardcoded data

--- app/perftools/models.py
+++ app/perftools/models.py
@@ -3,8 +3,6 @@ from django.db import models
 
 from economy.models import SuperModel
 
-# Create your models here.
-
 
 class JSONStore(SuperModel):
     """Define the JSONStore data model."""
@@ -14,7 +12,20 @@ class JSONStore(SuperModel):
     data = JSONField(blank=True, default=dict)
 
     def __str__(self):
-        """Define the string representation of GasProfile."""
+        """Define the string representation of JSONStore."""
         if not self:
             return "none"
         return f" {self.view} / {self.key} "
+
+
+class StaticJsonEnv(SuperModel):
+    """Define the StaticJsonEnv data model. Meant only for static env"""
+
+    key = models.CharField(max_length=30, default='', blank=True, db_index=True)
+    data = JSONField(blank=True, default=dict)
+
+    def __str__(self):
+        """Define the string representation of StaticJsonEnv."""
+        if not self:
+            return "none"
+        return f" {self.key}"
