070146143183936202c1102670f0a6e8a4ffd8ee
scco <schumanncombo@gmail.com>
2021-06-07 18:31:17 +0200

    quadratic diplomacy interface
    - room list ( index page ) v1 done
    - room detail - todo

--- app/assets/v2/js/quadraticlands/mission/diplomacy/index.js
+++ app/assets/v2/js/quadraticlands/mission/diplomacy/index.js
@@ -1,6 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  console.debug("mission diplomacy loaded");
+  console.debug("DIPLOMACY ROOMLIST");
+
+  // create hyperlinks to rooms
+  rooms = document.querySelectorAll('[data-roomlink]');
+  rooms.forEach(room => {
+    room.addEventListener('click', () => {
+        window.location.href = room.dataset.roomlink;
+    });
+  });
+
+
+  // show submit button on user input of create room input field
+  const create_room = document.getElementById('create_room');
+  const create_room_button = document.getElementById('create_room_button');
+
+  create_room.addEventListener('input', () => {
+    if(create_room.value =="" ) { create_room_button.classList.add('disabled'); }
+    else{ create_room_button.classList.remove('disabled'); }
+  });  
+
+
+
 
   // random floor polygones coloring / lights of town
   polygones = document.querySelectorAll('svg #hero polygon, svg #hero path');
