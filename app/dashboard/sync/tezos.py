f5503c21bf2b5dd4861c94a9a47f18d4a8d24352
Chibuotu Amadi <chibuotu@gmail.com>
2021-06-09 16:43:27 +0100

    finish sync

--- app/dashboard/sync/tezos.py
+++ app/dashboard/sync/tezos.py
@@ -3,69 +3,77 @@ from django.utils import timezone
 import requests
 from dashboard.sync.helpers import record_payout_activity, txn_already_used
 
-BASE_URL = 'https://siastats.info:3500/navigator-api'
+BASE_URL = 'https://api.tzkt.io/v1'
 
 
-def find_txn_on_sia_explorer(fulfillment):
+def find_txn_on_tezos_explorer(fulfillment):
     token_name = fulfillment.token_name
-
     funderAddress = fulfillment.funder_address
+    payeeAddress = fulfillment.fulfiller_address
     amount = fulfillment.payout_amount
+    txnid = fulfillment.payout_txid
 
-    if token_name != 'SC':
+    if token_name != 'XTZ':
         return None
 
-    url = f'{BASE_URL}/hash/{funderAddress}'
+    url = f'{BASE_URL}/accounts/{funderAddress}'
 
     response = requests.get(url).json()
 
     if response:
-        last100_txns = response[1]['last100Transactions']
-        if response[0]['Type'] == 'address' and last100_txns:
-            for txn in last100_txns:
-                if (
-                    txn['TxType'] == 'ScTx'
-                    and txn['ScChange'] < 0
-                    and abs(txn['ScChange']) / 10 ** 24 == amount / 10 ** 24
-                    and not txn_already_used(txn['MasterHash'], token_name)
-                ):
-                    return txn
+        for txn in response['operations']:
+            if (
+                txn['type'] == 'transaction'
+                and txn['hash'].strip() == txnid
+                and txn['sender']['address'] == funderAddress
+                and txn['target']['address'] == payeeAddress
+                and txn['amount'] == float(amount) * 10 ** 6
+                and not txn_already_used(txn['hash'], token_name)
+            ):
+                return txn
     return None
 
 
-def get_sia_txn_status(txnid, network='mainnet'):
+def get_tezos_txn_status(fulfillment):
+    txnid = fulfillment.payout_tx_id
+    token_name = fulfillment.token_name
+    funderAddress = fulfillment.funder_address
+    amount = fulfillment.payout_amount
+    payeeAddress = fulfillment.fulfiller_address
+
+    if token_name != 'XTZ':
+        return None
+
     if not txnid:
         return None
     
-    tx_url = f'{BASE_URL}/hash/{txnid}'
-    stats_url = f'{BASE_URL}/status'
+    tx_url = f'{BASE_URL}/operations/{txnid}'
 
     tx_response = requests.get(tx_url).json()
 
     if tx_response:
-        last_block = requests.get(stats_url).json()[0]['lastblock'] # or consensusblock ?
-
-        confirmations = last_block - tx_response[1]['Height']
-
         if (
-            tx_response[0]['Type'] == 'ScTx'
-            and tx_response[0]['MasterHash'].strip() == txnid
-            and confirmations > 0
+            tx_response['type'] == 'transaction'
+            and tx_response['hash'].strip() == txnid
+            and tx_response['sender']['address'] == funderAddress
+            and tx_response['target']['address'] == payeeAddress
+            and tx_response['amount'] == float(amount) * 10 ** 6
+            and tx_response['status'] == 'applied'
         ):
             return True
         else:
             return False
 
 
-def sync_sia_payout(fulfillment):
+def sync_tezos_payout(fulfillment):
     if not fulfillment.payout_tx_id or fulfillment.payout_tx_id == "0x0":
-        txn = find_txn_on_sia_explorer(fulfillment)
+        txn = find_txn_on_tezos_explorer(fulfillment)
         if txn:
-            fulfillment.payout_tx_id = txn['MasterHash']
+            fulfillment.payout_tx_id = txn['hash']
             fulfillment.save()
             
     if fulfillment.payout_tx_id and fulfillment.payout_tx_id != "0x0":
-        txn_status = get_sia_txn_status(fulfillment.payout_tx_id)
+        txn_status = get_tezos_txn_status(fulfillment)
         if txn_status:
             fulfillment.payout_status = 'done'
             fulfillment.accepted_on = timezone.now()
