8a4e6580a8eec78da2c05e976d1b3021295163dd
Tiago Nascimento <tiago.nascimento@spruceid.com>
2021-06-09 12:11:43 -0300

    Add passport-vc test file

---
+++ app/passport/tests/test_passport_vc.py
@@ -0,0 +1,51 @@
+import json
+from urllib.parse import urlencode
+
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from dashboard.models import Profile
+from didkit import verifyCredential
+from rest_framework import status
+
+CURRENT_USERNAME = "passport"
+CURRENT_PASSWORD = "passport"
+
+class PassportVCTest(TestCase):
+    """Define tests for passport verifiable credential."""
+    def setUp(self):
+        self.current_user = User.objects.create(
+                password=CURRENT_PASSWORD,
+                username=CURRENT_USERNAME)
+
+        Profile.objects.create(
+                user=self.current_user,
+                data={},
+                hide_profile=False,
+                handle=CURRENT_USERNAME)
+
+    def test_issue_and_verify_credential(self):
+        """ Tests that the issued passport credential verifies """
+        self.client.force_login(self.current_user)
+
+        data = {
+            "network":"mainnet",
+            "coinbase":"0x0000000000000000000000000000000000000000"}
+
+        response = self.client.get(reverse("passport_vc"),
+                data,
+                HTTP_USER_AGENT="none")
+
+        self.assertTrue(status.is_success(response.status_code))
+
+        vc = response.json()['vc']
+        options = {"proofPurpose": "assertionMethod"}
+        verifyStr = verifyCredential(
+                json.dumps(vc),
+                json.dumps(options))
+
+        verify = json.loads(verifyStr)
+        self.assertTrue(not verify["errors"])
+
+        pass
