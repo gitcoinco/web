8f4673e8a8eecbfc1514a89b1c20bac6c1d905d8
Chibuotu Amadi <chibuotu@gmail.com>
2021-06-10 23:24:47 +0100

    fix import + undefined variable (#9037)

--- app/grants/views.py
+++ app/grants/views.py
@@ -385,7 +385,7 @@ def grants(request):
 
 def get_collections(user, keyword, sort='-shuffle_rank', collection_id=None, following=None,
                     idle_grants=None, only_contributions=None, featured=False):
-    three_months_ago = timezone.now() - datetime.timedelta(days=90)
+    three_months_ago = timezone.now() - timezone.timedelta(days=90)
 
     _collections = GrantCollection.objects.filter(hidden=False)
 
@@ -641,7 +641,7 @@ def build_grants_by_type(
 
     sort_by_clr_pledge_matching_amount = None
     profile = request.user.profile if request.user.is_authenticated else None
-    three_months_ago = timezone.now() - datetime.timedelta(days=90)
+    three_months_ago = timezone.now() - timezone.timedelta(days=90)
     _grants = Grant.objects.filter(network=network, hidden=False)
 
     if clr_round:
@@ -1136,6 +1136,8 @@ def grants_by_grant_clr(request, clr_round):
     for _type in grant_types:
         if _type.get("keyword") == grant_type:
             grant_label = _type.get("label")
+
+    _, next_round_start, _ = get_clr_rounds_metadata()
 
     params = {
         'active': 'grants_landing',
