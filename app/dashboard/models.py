d12e3f42a72a1a81dac64a06968936cf877bc608
Chibuotu Amadi <chibuotu@gmail.com>
2021-06-22 19:42:40 +0100

    Revert "tezos crosschain | bounties/hackathons (#9025)"
    This reverts commit 59f7ef709e5115296238890c3d221bc752a0c80b.

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -299,7 +299,6 @@ class Bounty(SuperModel):
         ('nervos_ext', 'Nervos Ext'),
         ('algorand_ext', 'Algorand Ext'),
         ('sia_ext', 'Sia Ext'),
-        ('tezos_ext', 'Tezos Ext'),
         ('fiat', 'Fiat'),
         ('manual', 'Manual')
     )
@@ -1421,7 +1420,6 @@ class BountyFulfillment(SuperModel):
         ('nervos_ext', 'nervos_ext'),
         ('algorand_ext', 'algorand_ext'),
         ('sia_ext', 'sia_ext'),
-        ('tezos_ext', 'tezos_ext'),
         ('manual', 'manual')
     ]
 
@@ -1441,7 +1439,6 @@ class BountyFulfillment(SuperModel):
         ('NERVOS', 'NERVOS'),
         ('ALGORAND', 'ALGORAND'),
         ('SIA', 'SIA'),
-        ('TEZOS', 'TEZOS'),
         ('OTHERS', 'OTHERS')
     ]
 
