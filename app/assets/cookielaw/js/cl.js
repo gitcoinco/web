d6de805292003c8d430b9d7f8c8ecf6278874822
Graham Dixon <graham@gitcoin.co>
2021-06-27 13:50:41 +0100

    GITC-82: fixes "ReferenceError: Cookielaw is not defined" (#9211)

--- app/assets/cookielaw/js/cl.js
+++ app/assets/cookielaw/js/cl.js
@@ -1,27 +1,36 @@
-var Cookielaw = {
+document.addEventListener('DOMContentLoaded', () => {
 
-    createCookie: function (name, value, days) {
-        var date = new Date(),
-            expires = '';
-        if (days) {
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toGMTString();
-        } else {
-            expires = "";
-        }
-        document.cookie = name + "=" + value + expires + "; path=/";
-    },
+    const Cookielaw = {
 
-    createCookielawCookie: function () {
-        this.createCookie('cookielaw_accepted', '1', 10 * 365);
-
-        if (typeof (window.jQuery) === 'function') {
-            if(jQuery(".sumome-react-wysiwyg-popup-container").length < 1){
-                jQuery('#CookielawBanner').slideUp();
+        createCookie: function (name, value, days) {
+            var date = new Date(),
+                expires = '';
+            if (days) {
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toGMTString();
+            } else {
+                expires = "";
             }
-        } else {
-            document.getElementById('CookielawBanner').style.display = 'none';
-        }
-    }
+            document.cookie = name + "=" + value + expires + "; path=/";
+        },
 
-};
+        createCookielawCookie: function () {
+            this.createCookie('cookielaw_accepted', '1', 10 * 365);
+
+            if (typeof (window.jQuery) === 'function') {
+                if(jQuery(".sumome-react-wysiwyg-popup-container").length < 1){
+                    jQuery('#CookielawBanner').slideUp();
+                }
+            } else {
+                document.getElementById('CookielawBanner').style.display = 'none';
+            }
+        }
+    };
+
+    // check for banner content
+    const acceptCookieLaw = document.getElementById('CookielawBannerAccept');
+
+    if (acceptCookieLaw) {
+      acceptCookieLaw.addEventListener('click', () => Cookielaw.createCookielawCookie());
+    }
+});
