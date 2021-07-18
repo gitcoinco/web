569dbc2e0ad879345e039b5538f8bf60f7d95875
Chibuotu Amadi <chibuotu@gmail.com>
2021-07-18 17:25:23 +0100

    extension wip

--- app/assets/v2/js/pages/bounty_detail/casper_extension.js
+++ app/assets/v2/js/pages/bounty_detail/casper_extension.js
@@ -2,10 +2,56 @@ const payWithCasperExtension = async(fulfillment_id, to_address, vm, modal) => {
 
   const amount = vm.fulfillment_context.amount;
   const token_name = vm.bounty.token_name;
+
+  const { CasperClient, DeployUtil, PublicKey, Signer } = casper;
+  const casperClient = new CasperClient('http://3.142.224.108:7777/rpc');
+
+  const isConnected = await Signer.isConnected();
+
+  if (!isConnected) {
+    try {
+      Signer.sendConnectionRequest();
+    } catch (e) {
+      _alert({ message: gettext('Please download or enable CasperLabs Signer extension.') }, 'danger');
+    }
+  }
+
+  const selectedAddress = await Signer.getActivePublicKey();
+
+  const paymentAmount = 10000;
+  const id = fulfillment_id;
+  const ttl = 1800000;
+  const fromPublicKey = PublicKey.fromHex(selectedAddress);
+  const toPublicKey = PublicKey.fromHex(to_address);
   
-  let selectedAddress;
-    
-  console.log(casper);
+  let deployParams = new DeployUtil.DeployParams(fromPublicKey, 'casper', ttl);
+
+  const session = DeployUtil.ExecutableDeployItem.newTransfer(
+    amount * 10 ** vm.decimals,
+    toPublicKey,
+    fromPublicKey,
+    id
+  );
+  
+  const payment = DeployUtil.standardPayment(paymentAmount);
+  const deploy = DeployUtil.makeDeploy(deployParams, session, payment);
+
+  console.log(deploy);
+
+  const signedDeploy = await Signer.sign(deploy, fromPublicKey, toPublicKey);
+
+  console.log(signedDeploy);
+
+  try {
+    const txHash = await casperClient.putDeploy(signedDeploy);
+
+    console.log(txHash);
+    // callback(null, selectedAddress, txHash);
+  } catch (e) {
+    modal.closeModal();
+    _alert({ message: `${e.title} - ${e.description}` }, 'danger');
+    console.log(e);
+  }
     
   function callback(error, from_address, txn) {
     if (error) {
