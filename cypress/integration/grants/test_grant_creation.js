4d188200113ddc6d67f76a15ab4cadd17c375c62
Kevin Solorio <kevin.solorio@hey.com>
2021-08-04 12:27:18 -0500

    update grant test
    local navigation to the grant creation page works. it uses the root
    credentials from the README to impersonate an account avoiding the
    GitHub authentication. OAuth requests will likely need to be mocked out
    in order to test directly.

--- cypress/integration/grants/test_grant_creation.js
+++ cypress/integration/grants/test_grant_creation.js
@@ -1,14 +1,19 @@
 describe('Creating a new grant', () => {
   it('can navigate to the new grant screen', () => {
-    cy.visit('http://localhost:8000');
+    cy.visit('http://localhost:8000/_administrationlogin');
+    cy.get('[name=username]').type('root');
+    cy.get('[name=password]').type('gitcoinco');
+    cy.contains('Log in').click();
+
+    cy.contains('Impersonate Users').click();
+
+    cy.contains('test3').click();
 
     cy.get('#dropdownProducts').trigger('mouseenter');
-    cy.get('[data-submenu=grants]').click();
+    cy.get('[data-submenu=products]').find('[data-submenu=grants]').click();
     cy.url().should('eq', 'http://localhost:8000/grants/');
 
-    cy.visit('http://localhost:8000/grants/');
-
-    cy.contains('Create a Grant').click();
+    cy.get('#grants-showcase').contains('Create a Grant').click();
     cy.url().should('eq', 'http://localhost:8000/grants/new');
   });
 });
