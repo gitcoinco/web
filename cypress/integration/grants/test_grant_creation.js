cc7691a4fc39c30f57208c0ed8f4e9adc3537b3c
Kevin Solorio <kevin.solorio@hey.com>
2021-08-06 11:52:17 -0500

    add whitespace to create visual separation of test steps

--- cypress/integration/grants/test_grant_creation.js
+++ cypress/integration/grants/test_grant_creation.js
@@ -35,10 +35,12 @@ describe('Creating a new grant', () => {
         cy.get('.quill-editor').type('We’re on a mission to build an internet that is open source, collaborative, and economically empowering.');
         cy.get('input[name=reference_url]').type('https://gitcoin.co');
         cy.get('input[name=twitter_handle_1]').type('@gitcoin');
+
         cy.contains('ETH').click();
         cy.get('input[name=eth_payout_address]').type('0xd08Fe0c97c80491C6ee696Ee8151bc6E57d1Bf1d');
         cy.get('input[placeholder="Yes/No"]').click();
         cy.contains('No, this project has not raised external funding.').click();
+
         cy.get('input[placeholder="Pick a category"]').click();
         cy.contains('Community').click();
         cy.get('input[placeholder="Select categories"]').click();
@@ -46,7 +48,7 @@ describe('Creating a new grant', () => {
 
         cy.contains('Create Grant').click();
       });
-      
+
       cy.url().should('contain', 'gitcoin-fund');
     });
   });
