3baf2c9a62b59b4e0ec928c5809f759194242363
Jer-Sch <schuurm@ns.codes>
2021-08-09 11:35:58 -0500

    resolve circular dependency

--- app/grants/models/__init__.py
+++ app/grants/models/__init__.py
@@ -23,11 +23,10 @@ from .clr_match import CLRMatch
 from .contribution import Contribution
 from .donation import Donation
 from .flag import Flag
-from .grant import Grant
+from .grant import Grant, GrantCLR
 from .grant_api_key import GrantAPIKey
 from .grant_branding_routing_policy import GrantBrandingRoutingPolicy
 from .grant_category import GrantCategory
-from .grant_clr import GrantCLR
 from .grant_clr_calculation import GrantCLRCalculation
 from .grant_collection import GrantCollection
 from .grant_stat import GrantStat
