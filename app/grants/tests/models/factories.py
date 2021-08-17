b4548f7eb439ea55a01d8682adccb0962a4e0cee
Jer-Sch <schuurm@ns.codes>
2021-08-17 11:23:15 -0500

    add tests for CartActivity, add factory for CartActivity and Profile

---
+++ app/grants/tests/models/factories.py
@@ -0,0 +1,23 @@
+import factory
+import pytest
+from dashboard.models import Profile
+from grants.models import *
+
+
+class ProfileFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Profile
+    
+    handle = factory.Sequence(lambda n: "Contributor_%03d" % n)
+    data = {}
+
+    
+@pytest.mark.django_db
+class CartActivityFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = CartActivity
+
+    profile = factory.SubFactory(ProfileFactory)
+    metadata = {}
+    bulk = True
+   
