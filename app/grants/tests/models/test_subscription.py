341f03aabb17da3464636d2a8d0012bc95a1c898
Jer-Sch <schuurm@ns.codes>
2021-08-30 15:58:33 -0500

    add test case for 'error' attribute

--- app/grants/tests/models/test_subscription.py
+++ app/grants/tests/models/test_subscription.py
@@ -22,3 +22,12 @@ class TestSubscription:
 
         assert hasattr(subscription, 'active')
         assert subscription.active == True
+
+    def test_subscription_has_error_attribute(self):
+        """Terror 'error' attribute and default value."""
+
+        subscription = SubscriptionFactory()
+
+        assert hasattr(subscription, 'error')
+        assert subscription.error == False
+    
