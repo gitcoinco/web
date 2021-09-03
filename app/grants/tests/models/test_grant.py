a851967cb67b9fe84df3542f888efe4960bc3f4d
Jer-Sch <schuurm@ns.codes>
2021-09-03 07:30:17 -0500

    add initial test case

---
+++ app/grants/tests/models/test_grant.py
@@ -0,0 +1,17 @@
+import pytest
+from grants.models.grant import Grant
+
+from .factories.grant_factory import GrantFactory
+
+
+@pytest.mark.django_db
+class TestGrant:
+    """Test Grant model."""
+
+    def test_creation(self):
+        """Test instance of Grant returned by factory is valid."""
+
+        grant = GrantFactory()
+
+        assert isinstance(grant, Grant)
+
