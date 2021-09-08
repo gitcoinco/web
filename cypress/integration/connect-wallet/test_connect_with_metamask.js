43fa673209fe3636a62ada55693263b4c95810b1
Kevin Solorio <kevin.solorio@hey.com>
2021-09-08 15:57:31 -0500

    no longer need the scrolling command

--- cypress/integration/connect-wallet/test_connect_with_metamask.js
+++ cypress/integration/connect-wallet/test_connect_with_metamask.js
@@ -14,8 +14,6 @@ describe('connect wallet: metamask', () => {
 
     cy.get('#navbarDropdownWallet').as('wallet').click();
     cy.contains('Connect Wallet').click();
-
-    cy.get('.web3modal-modal-card').scrollIntoView();
     cy.contains('MetaMask').click();
 
     cy.acceptMetamaskAccess();
