268a65e0e19fa46890031eaa8a5a0b78c7dfc3a6
Jer-Sch <schuurm@ns.codes>
2021-09-08 16:56:01 -0500

    add initial test

---
+++ app/grants/tests/models/factories/grant_clr_factory.py
@@ -0,0 +1,18 @@
+
+from datetime import datetime, timedelta
+
+import factory
+import pytest
+from grants.models.grant import GrantCLR
+
+
+@pytest.mark.django_db
+class GrantCLRFactory(factory.django.DjangoModelFactory):
+    """Create mock GrantCLR for testing."""
+
+    class Meta:
+        model = GrantCLR
+
+    round_num = 2
+    start_date = datetime.now()
+    end_date = start_date + timedelta(weeks=2)
