c3164fe92909c612d7cfe9c5aac31f303151ced9
Kevin Solorio <103829+ksolo@users.noreply.github.com>
2021-09-09 16:50:19 -0500

    Cypress testing/add connection metamask wallet test (#9429)
    * updating dependencies to use custom fork of cypress-metamask
    
    * add script and options for running cypress tests
    
    * redirect webserver logs to /dev/null
    
    * update docker-compose file: use default accounts for ganache
    
    * test running connect site with metamask on ci
    
    * attempt at passing in env from config
    
    * add ganache-cli to dev dependencies
    
    * updated script call
    
    * moving where NETWORK_NAME is defined
    
    * build please
    
    * moving network_name again
    
    * remove network_name
    
    * add superuser creation to script
    
    * take two at creating a superuser
    
    * third times a charm - create superuser
    
    * try loading user data from fixtures
    
    * scroll metamask button into view
    
    * add cypress settings to gain access to screenshots/videos
    
    * no longer need the scrolling command

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+import 'cypress-metamask';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
@@ -24,3 +25,4 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   // failing the test
   return false;
 });
+
