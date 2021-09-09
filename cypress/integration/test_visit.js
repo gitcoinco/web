c642d0a83dca84c331b6d9b6568efa7235b14f92
Kevin Solorio <103829+ksolo@users.noreply.github.com>
2021-09-09 15:36:27 -0500

    Travis running cypress (#9411)
    * updating dependencies to use custom fork of cypress-metamask
    
    * add script and options for running cypress tests
    
    * add wait-on package for scripting cypress runs
    
    * travis docs indicate that bionic will start xvfb by adding it as a service
    
    * add migration step
    
    * another attempt at including cypress tests
    
    * redirect webserver logs to /dev/null
    
    * bad idea to move stdout to dev/null
    
    * add NETWORK_NAME to travis env file
    
    * update runserver command to go to background
    
    * updating existing cypress tests to run on ci
    
    * update docker-compose file: use default accounts for ganache
    
    * add set -x to get more visibility into cypress command args
    
    * update call to cypress run to see if travis will recognize flags
    
    * try running cypress directly from node_modules
    
    * remove set -x now that arguments are making their way to cypress

--- cypress/integration/test_visit.js
+++ cypress/integration/test_visit.js
@@ -16,26 +16,12 @@ describe('Visit Tests', function() {
   it('Visits the Kudos Marketplace', function() {
     cy.visit('http://localhost:8000/kudos/marketplace');
   });
-  /*
-  it('Visits the Kudos Sender', function() {
-    cy.visit('http://localhost:8000/kudos/send');
-    cy.url().should('contain', 'github.com');
-  });
-  */
   it('Visits the Labs Page', function() {
     cy.visit('http://localhost:8000/explorer');
-  });
-  it('Visits the About Page', function() {
-    cy.visit('http://localhost:8000/about');
   });
   it('Visits the Mission Page', function() {
     cy.visit('http://localhost:8000/mission');
   });
-  /*
-  it('Visits the Results Page', function() {
-    cy.visit('http://localhost:8000/results');
-  });
-  */
   it('Visits the Activity Page', function() {
     cy.visit('http://localhost:8000/activity');
   });
