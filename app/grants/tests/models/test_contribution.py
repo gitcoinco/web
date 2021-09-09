466ac0262bbd2c96ece3ba8c78ee28d23a06bfce
Jer-Sch <schuurm@ns.codes>
2021-09-08 21:47:57 -0500

    define all the test cases

--- app/grants/tests/models/test_contribution.py
+++ app/grants/tests/models/test_contribution.py
@@ -15,4 +15,62 @@ class TestContribution:
 
         assert isinstance(contribution, Contribution)
 
-    
+    def test_contribution_has_success_attribute(self):
+        
+        pass
+
+    def test_contribution_has_tx_cleard_attribute(self):
+        
+        pass
+
+    def test_contribution_has_tx_override_attribute(self):
+
+        pass
+
+    def test_contribution_has_tx_id_attribute(self):
+
+        pass
+
+    def test_contribution_has_split_tx_id_attribute(self):
+
+        pass
+
+    def test_contribution_has_split_tx_confirmed_attribute(self):
+
+        pass
+
+    def test_contribution_has_associated_subscription(self):
+
+        pass
+
+    def test_contribution_has_normalized_data(self):
+
+        pass
+
+    def test_contribution_has_match_attribute(self):
+
+        pass
+
+    def test_contribution_has_originated_address(self):
+
+        pass
+
+    def test_contribution_has_validator_passed_attribute(self):
+
+        pass
+
+    def test_contribution_has_validator_comment_attribute(self):
+
+        pass
+
+    def test_contribution_has_profile_for_clr_attribute(self):
+
+        pass
+
+    def test_contribution_has_checkout_type(self):
+
+        pass
+
+    def test_contribution_has_anonymous_attribute(self):
+
+        pass
