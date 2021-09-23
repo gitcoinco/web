91989ee2d2c5c3c0cacb0ade2b3a4a11bfc7f707
Kevin Solorio <kevin.solorio@hey.com>
2021-09-23 13:39:06 -0700

    contributing grant tests are green

--- cypress/integration/grants/test_contributing_to_grant.js
+++ cypress/integration/grants/test_contributing_to_grant.js
@@ -30,16 +30,15 @@ describe('contributing to grant', () => {
       cy.changeMetamaskNetwork('localhost');
       cy.acceptMetamaskAccess();
 
-      cy.get('.grant-checkout').contains('Add to Cart').scrollIntoView().click();
-      cy.wait(1000); // slow the test down to allow the grant to show up in the cart.
+      cy.get('.grant-checkout').contains('Add to Cart').click();
+      cy.wait(1000); // slow the test down to allow cart data to load
 
       cy.get('#gc-cart').click();
       cy.contains('Checkout').click();
 
       cy.get('#vs3__combobox').click().type('ETH{enter}');
-
-      cy.contains('0%').click();
-      cy.contains('Standard Checkout').scrollIntoView().click();
+      cy.get('#gitcoin-grant-input-amount').type('{backspace}');
+      cy.get('#js-fundGrants-button').scrollIntoView().click();
 
       cy.confirmMetamaskTransaction();
 
@@ -47,60 +46,52 @@ describe('contributing to grant', () => {
     });
   });
 
-  // it('contributes eth to multiple grants', () => {
-  //   cy.createGrantSubmission().then((response) => {
-  //     const grantUrl = response.body.url;
-  //
-  //     cy.wrap(grantUrl).as('grant1');
-  //
-  //     cy.approveGrant(grantUrl);
-  //   });
-  //
-  //   cy.createGrantSubmission().then((response) => {
-  //     const grantUrl = response.body.url;
-  //
-  //     cy.wrap(grantUrl).as('grant2');
-  //
-  //     cy.approveGrant(grantUrl);
-  //   });
-  //
-  //   cy.impersonateUser();
-  //
-  //   cy.get('#CookielawBannerAccept').click();
-  //
-  //   cy.get('@grant1').then((grantUrl) => {
-  //     cy.visit(grantUrl);
-  //     cy.get('.grant-checkout').contains('Add to Cart').scrollIntoView().click();
-  //
-  //     const pk = grantUrl.match(/\/grants\/(\d*)\//)[1];
-  //
-  //     cy.get(`#select2-side-cart-currency-${pk}-container`).click();
-  //     cy.get('input[role=searchbox]').type('ETH{enter}');
-  //   });
-  //
-  //   cy.get('@grant2').then((grantUrl) => {
-  //     cy.visit(grantUrl);
-  //     cy.get('.grant-checkout').contains('Add to Cart').scrollIntoView().click();
-  //
-  //     const pk = grantUrl.match(/\/grants\/(\d*)\//)[1];
-  //
-  //     cy.get(`#select2-side-cart-currency-${pk}-container`).click();
-  //     cy.get('input[role=searchbox]').type('ETH{enter}');
-  //   });
-  //
-  //   cy.visit('grants/cart?');
-  //
-  //
-  //   cy.contains('MetaMask').click();
-  //
-  //   cy.changeMetamaskNetwork('localhost');
-  //   cy.acceptMetamaskAccess();
-  //
-  //   cy.get('#gitcoin-grant-input-amount').type('{backspace}0');
-  //   cy.contains('Standard Checkout').click();
-  //
-  //   cy.confirmMetamaskTransaction();
-  //
-  //   cy.get('body').should('contain.text', 'Thank you for contributing to open source!');
-  // });
+  it('contributes eth to multiple grants', () => {
+    cy.createGrantSubmission().then((response) => {
+      const grantUrl = response.body.url;
+
+      cy.wrap(grantUrl).as('grant1');
+
+      cy.approveGrant(grantUrl);
+    });
+
+    cy.createGrantSubmission().then((response) => {
+      const grantUrl = response.body.url;
+
+      cy.wrap(grantUrl).as('grant2');
+
+      cy.approveGrant(grantUrl);
+    });
+
+    cy.impersonateUser();
+
+    cy.get('#CookielawBannerAccept').click();
+
+    cy.get('@grant1').then((grantUrl) => {
+      cy.visit(grantUrl);
+      cy.get('.grant-checkout').contains('Add to Cart').click();
+    });
+
+    cy.get('@grant2').then((grantUrl) => {
+      cy.visit(grantUrl);
+      cy.get('.grant-checkout').contains('Add to Cart').click();
+    });
+
+    cy.visit('grants/cart?');
+
+
+    cy.contains('MetaMask').click();
+
+    cy.changeMetamaskNetwork('localhost');
+    cy.acceptMetamaskAccess();
+
+    cy.get('#vs3__combobox').click().type('ETH{enter}');
+    cy.get('#vs4__combobox').click().type('ETH{enter}');
+    cy.get('#gitcoin-grant-input-amount').type('{backspace}');
+    cy.get('#js-fundGrants-button').scrollIntoView().click();
+
+    cy.confirmMetamaskTransaction();
+
+    cy.get('body').should('contain.text', 'Thank you for contributing to open source!');
+  });
 });
