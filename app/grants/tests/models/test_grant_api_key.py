7c17e7b46a0fc0ba8fad416434497e59b3107028
Jeremy Schuurmans <schuurm@ns.codes>
2021-09-29 08:17:13 -0500

    10/Test GrantAPIKey model (#9452)
    * Extract grants models into individual files (#9341)
    
    * create new directory for models, copy over Contribution model
    
    * extract grants models to individual files
    
    * rename relocated_models directory, remove original models directory, add imports, resolve circular dependencies
    
    * extract CLRMatch into separate file
    
    * extract Flag into separate file
    
    * extract MatchPledge to separate file
    
    * extract Donation and PhantomFunding
    
    * extract GrantStat into separate file
    
    * refactor
    
    * extract GrantBrandingRoutingPolicy to separate file
    
    * update migration
    
    * add missing import to MatchPledge, remove imports from __init__.py
    
    * add missing import
    
    * decouple GrantCLRCalculation and move to own file
    
    * extract GrantType to own file
    
    * extract GrantCLR to own file
    
    * add missing import
    
    * refactor, add missing imports
    
    * remove whitespace
    
    * resolve circular dependency
    
    * run 'make fix'
    
    * import changes from #9314
    
    * add try/except to migration file instead of editing migration directly
    
    * refactor
    
    * add pytest-factoryboy
    
    * create directories and test files
    
    * add initial test case
    
    * fix typo
    
    * add test for key
    
    * add test for secret attribute
    
    * add test for profile
    
    * refactor
    
    * refactor

---
+++ app/grants/tests/models/test_grant_api_key.py
@@ -0,0 +1,39 @@
+import pytest
+from dashboard.models import Profile
+from grants.models.grant_api_key import GrantAPIKey
+
+from .factories.grant_api_key_factory import GrantAPIKeyFactory
+
+
+@pytest.mark.django_db
+class TestGrantAPIKey:
+    """Test GrantAPIKey model."""
+
+    def test_creation(self):
+        """Test GrantAPIKey returned by factory is valid."""
+
+        grant_api_key = GrantAPIKeyFactory()
+
+        assert isinstance(grant_api_key, GrantAPIKey)
+
+    def test_grant_api_key_has_a_key(self):
+        """Test 'key' attribute is present."""
+
+        grant_api_key = GrantAPIKeyFactory()
+
+        assert hasattr(grant_api_key, 'key')
+
+    def test_grant_api_key_has_a_secret(self):
+        """Test secret attribute is present."""
+
+        grant_api_key = GrantAPIKeyFactory()
+
+        assert hasattr(grant_api_key, 'secret')
+
+    def test_grant_api_key_has_associated_profile(self):
+        """Test profile attribute is present and is an instance of Profile."""
+
+        grant_api_key = GrantAPIKeyFactory()
+
+        assert hasattr(grant_api_key, 'profile')
+        assert isinstance(grant_api_key.profile, Profile)
