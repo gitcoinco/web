596f7c088cc417613ab08304b366cee74f011728
Jeremy Schuurmans <schuurm@ns.codes>
2021-09-29 08:08:46 -0500

    7/Test Flag model (#9398)
    * Extract grants models into individual files (#9341)
    
    * create new directory for models, copy over Contribution model
    
    * extract grants models to individual files
    
    * rename relocated_models directory, remove original models directory, add imports, resolve circular dependencies
    
    * extract CLRMatch into separate file
    
    * extract Flag into separate file
    
    * extract MatchPledge to separate file
    
    * extract Donation and PhantomFunding
    
    * extract GrantStat into separate file
    
    * refactor
    
    * extract GrantBrandingRoutingPolicy to separate file
    
    * update migration
    
    * add missing import to MatchPledge, remove imports from __init__.py
    
    * add missing import
    
    * decouple GrantCLRCalculation and move to own file
    
    * extract GrantType to own file
    
    * extract GrantCLR to own file
    
    * add missing import
    
    * refactor, add missing imports
    
    * remove whitespace
    
    * resolve circular dependency
    
    * run 'make fix'
    
    * import changes from #9314
    
    * add try/except to migration file instead of editing migration directly
    
    * refactor
    
    * add initial test case
    
    * add test case for associated Grant
    
    * add test case for associated Profile
    
    * add test case for comments
    
    * add test case for processed attribute
    
    * add test case for comments_admin
    
    * add test case for tweet
    
    * add test case for post_flag() method
    
    * refactor test for post_flag() method
    
    * refactor
    
    * add missing checkout_type (present in migration but not in model)

---
+++ app/grants/tests/models/test_flag.py
@@ -0,0 +1,84 @@
+from unittest.mock import patch
+
+import pytest
+from dashboard.models import Activity, Profile
+from grants.models.flag import Flag
+from grants.models.grant import Grant
+from townsquare.models import Comment
+
+from .factories.flag_factory import FlagFactory
+from .factories.profile_factory import ProfileFactory
+
+
+@pytest.mark.django_db
+class TestFlag:
+    """Test Flag model."""
+
+    def test_creation(self):
+        """Test instance of Flag returned by factory is valid."""
+
+        flag = FlagFactory()
+
+        assert isinstance(flag, Flag)
+
+    def test_flag_has_associated_grant(self):
+        """Test 'grant' attribute is present and is an instance of Grant."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'grant')
+        assert isinstance(flag.grant, Grant)
+
+    def test_flag_has_associated_profile(self):
+        """Test 'profile' attribute is present and is an instance of Profile."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'profile')
+        assert isinstance(flag.profile, Profile)
+
+    def test_flag_has_comments(self):
+        """Test 'comments' attribute is present."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'comments')
+        assert flag.comments == ''
+
+    def test_flag_has_processed_attribute(self):
+        """Test 'processed' attribute is present and defaults to False."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'processed')
+        assert flag.processed == False
+
+    def test_flag_has_comments_admin(self):
+        """Test 'comments_admin' attribute is present and defaults to empty string."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'comments_admin')
+        assert flag.comments_admin == ''
+
+    def test_flag_has_tweet_attribute(self):
+        """Test 'tweet' attribute is present."""
+
+        flag = FlagFactory()
+
+        assert hasattr(flag, 'tweet')
+
+    def test_post_flag_method_calls_collaborators_with_appropriate_attributes(self):
+        """Test post_flag() method calls filter() on Profile.objects, create() on Activity.objects, and create() on Comment.objects."""
+        
+        flag = FlagFactory()
+
+        with patch.object(Profile.objects, 'filter') as filter:
+            with patch.object(Activity.objects, 'create') as activity:
+                with patch.object(Comment.objects, 'create') as comment:
+
+                    flag.post_flag()
+
+        filter.assert_called_with(handle='gitcoinbot')
+        activity.assert_called_with(profile=filter().first(), activity_type='flagged_grant', grant=flag.grant)
+        comment.assert_called_with(profile=filter().first(), activity=activity(), comment=f"Comment from anonymous user: {flag.comments}")
