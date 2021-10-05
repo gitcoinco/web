64bc27442b16443908348fcaa2ded9fd080c8f25
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-10-05 10:50:14 +0530

    fix unpack many values

--- app/dashboard/views.py
+++ app/dashboard/views.py
@@ -2853,7 +2853,7 @@ def get_profile_tab(request, profile, tab, prev_context):
             brightid['upcoming_calls'] = []
 
         # QF round info
-        _, round_start_date, round_end_date, show_round_banner = get_clr_rounds_metadata()
+        _, round_start_date, round_end_date, show_round_banner, _, _, _, _ = get_clr_rounds_metadata()
         # place clr dates (as unix ts)
         context['round_start_date'] = calendar.timegm(round_start_date.utctimetuple())
         context['round_end_date'] = calendar.timegm(round_end_date.utctimetuple())
