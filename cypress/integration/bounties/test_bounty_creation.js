aa6d0d5ace8f9283c6d70e2f63f0c73829c9dba8
Kevin Solorio <kevin.solorio@hey.com>
2021-10-24 13:38:08 -0500

    update comments on tests and add force true back to click

--- cypress/integration/bounties/test_bounty_creation.js
+++ cypress/integration/bounties/test_bounty_creation.js
@@ -27,8 +27,17 @@ describe('Creating a new bounty', () => {
 
     cy.contains('I agree').click();
 
+    // unfortunately some of the events do not seem bound in time for this
+    // to run without any errors. adding in this manual wait is not advised
+    // and is used here to slow the tests down to allow the modal close click
+    // events to bind properly
+    // in addition to the wait, we are using the force options on the click,
+    // if there is an alternative means for clicking on buttons, please use those
+    // instead of jumping to force. force may click on items hidden by other elements
+    // and is not how a user would interact with the application. we should make every
+    // attempt to use the UI as a real user would.
     cy.wait(1000);
-    cy.get('button.btn-primary[data-dismiss]').scrollIntoView().click();
+    cy.get('button.btn-primary[data-dismiss]').scrollIntoView().click({force: true});
 
     cy.contains('ETH').click();
     cy.contains('MetaMask').click();
