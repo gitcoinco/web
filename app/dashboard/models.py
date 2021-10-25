868c9dbef6d5326926e5ed082b70f2b8e2cf4d22
Aditya Anand M C <aditya.anandmc@gmail.com>
2021-10-25 13:47:08 +0530

    create multicolumn index on ActivityIndex

--- app/dashboard/models.py
+++ app/dashboard/models.py
@@ -2321,6 +2321,10 @@ class ActivityIndex(SuperModel):
         blank=True
     )
 
+    class Meta:
+        indexes = [
+            models.Index(fields=['id', 'key']),
+        ]
 
 class Activity(SuperModel):
     """Represent Start work/Stop work event.
