00c2178540863fca2c0f1922d96ca1c870711af0
owocki <ksowocki@gmail.com>
2021-10-27 08:27:15 -0600

    increases the size of the s3cmd multipart chunk size so that we dont go over the part limit on full backups

--- scripts/backup.bash
+++ scripts/backup.bash
@@ -28,15 +28,16 @@ export HOST=$(cat app/app/.env | grep "DATABASE_URL" | grep -v REPLICA | awk -F 
 IS_PROD=$(cat app/app/.env | grep ENV | grep prod | wc -l)
 if [ "$IS_PROD" -eq "1" ]; then
     # schema
-    $PG_DUMP gitcoin -U gitcoin -h $HOST --schema-only | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/create-$BACKUPSTR-$(hostname).sql
+    $PG_DUMP gitcoin -U gitcoin -h $HOST --schema-only | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/create-$BACKUPSTR-$(hostname).sql --multipart-chunk-size-mb=500
 
     # lite backup
-    $PG_DUMP gitcoin -U gitcoin -h $HOST --data-only --exclude-table=marketing_emailevent --exclude-table=dashboard_objectview --exclude-table=marketing_stat --exclude-table=gas_gasprofile --exclude-table=marketing_githubevent --exclude-table=gas_gasguzzler --exclude-table=marketing_slackpresence | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/litedata-$BACKUPSTR-$(hostname).sql
+    $PG_DUMP gitcoin -U gitcoin -h $HOST --data-only --exclude-table=marketing_emailevent --exclude-table=dashboard_objectview --exclude-table=marketing_stat --exclude-table=gas_gasprofile --exclude-table=marketing_githubevent --exclude-table=gas_gasguzzler --exclude-table=marketing_slackpresence | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/litedata-$BACKUPSTR-$(hostname).sql --multipart-chunk-size-mb=500
+
     # minimal backup
-    $PG_DUMP gitcoin -U gitcoin -h $HOST --data-only --exclude-table=marketing_emailevent --exclude-table=dashboard_objectview --exclude-table=marketing_stat --exclude-table=gas_gasprofile --exclude-table=marketing_githubevent --exclude-table=gas_gasguzzler --exclude-table=marketing_slackpresence --exclude-table=dashboard_activity --exclude-table=marketing_leaderboardrank --exclude-table=dashboard_activity --exclude-table=perftools_jsonstore --exclude-table=pg_toast --exclude-table=dashboard_searchhistory --exclude-table=townsquare_matchranking --exclude-table=dashboard_useraction --exclude-table=grants_cartactivity --exclude-table=dashboard_profilestathistory --exclude-table=marketing_githubevent --exclude-table=django_session --exclude-table=search_searchresult | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/minimal-$BACKUPSTR-$(hostname).sql
+    $PG_DUMP gitcoin -U gitcoin -h $HOST --data-only --exclude-table=marketing_emailevent --exclude-table=dashboard_objectview --exclude-table=marketing_stat --exclude-table=gas_gasprofile --exclude-table=marketing_githubevent --exclude-table=gas_gasguzzler --exclude-table=marketing_slackpresence --exclude-table=dashboard_activity --exclude-table=marketing_leaderboardrank --exclude-table=dashboard_activity --exclude-table=perftools_jsonstore --exclude-table=pg_toast --exclude-table=dashboard_searchhistory --exclude-table=townsquare_matchranking --exclude-table=dashboard_useraction --exclude-table=grants_cartactivity --exclude-table=dashboard_profilestathistory --exclude-table=marketing_githubevent --exclude-table=django_session --exclude-table=search_searchresult | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/minimal-$BACKUPSTR-$(hostname).sql --multipart-chunk-size-mb=500
 
     # full backup
-    $PG_DUMP gitcoin -U gitcoin -h $HOST | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/full-$BACKUPSTR-$(hostname).sql
+    $PG_DUMP gitcoin -U gitcoin -h $HOST | s3cmd put - s3://gitcoinbackups/$YEAR/$MONTH/$DAY/full-$BACKUPSTR-$(hostname).sql --multipart-chunk-size-mb=500
 
 
 else
