ed1e1c7f16ed38cf4b74e7bce1bc9ac3b5cbc7d9
Jer-Sch <schuurm@ns.codes>
2021-10-29 09:57:29 -0500

    refactor

--- cypress/integration/grants/test_contributing_to_grant.js
+++ cypress/integration/grants/test_contributing_to_grant.js
@@ -85,4 +85,26 @@ describe('contributing to grant', () => {
 
     cy.get('body').should('contain.text', 'Thank you for contributing to open source!');
   });
+
+  it('defaults donation amount to 25 DAI in the cart', () => {
+    cy.createGrantSubmission().then((response) => {
+      const grantUrl = response.body.url;
+
+      cy.approveGrant(grantUrl);
+      cy.impersonateUser();
+
+      cy.visit(grantUrl);
+
+      cy.get('.grant-checkout').contains('Add to Cart').click();
+      cy.wait(1000); // slow the test down to allow cart data to load
+
+      cy.get('#gc-cart').click();
+      cy.contains('Checkout').click();
+
+      cy.contains('MetaMask').click();
+      cy.acceptMetamaskAccess();
+
+      cy.get('[placeholder="Amount"]').should('have.value', '25'); // assert donation input field has default value of 25 DAI
+    });
+  });
 });
