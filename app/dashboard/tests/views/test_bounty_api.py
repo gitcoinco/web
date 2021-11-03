461f31df6ac52845c2269ecc0ec0c890ef22018d
Jer-Sch <schuurm@ns.codes>
2021-11-03 09:20:02 -0500

    add tests for successful bounty api request, and successful bounty payout

---
+++ app/dashboard/tests/views/test_bounty_api.py
@@ -0,0 +1,16 @@
+import pytest
+from dashboard.tests.factories.bounty_factory import BountyFactory
+from rest_framework.test import APIClient
+
+
+class TestBountyAPI:
+    def test_retrieves_activities(self, django_user_model):
+        github_url = {'github_url': 'https://github.com/gitcoinco/web/issues/1'}
+        BountyFactory(**github_url)
+        user = django_user_model.objects.create(username="gitcoin", password="password123")
+        client = APIClient()
+
+        client.force_login(user)
+        response = client.get('/actions/api/v0.1/bounty/', github_url, format='json')
+
+        assert response.status_code == 200
