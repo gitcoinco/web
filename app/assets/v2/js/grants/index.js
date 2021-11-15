9c9eba42cb13e23399ab2c5551671902470d4d3a
Jer-Sch <schuurm@ns.codes>
2021-11-15 17:32:07 -0600

    refactor options to mimic optgroups, add more styling

--- app/assets/v2/js/grants/index.js
+++ app/assets/v2/js/grants/index.js
@@ -126,7 +126,24 @@ if (document.getElementById('grants-showcase')) {
       editingCollection: false,
       createCollectionRedirect: false,
       activeTimeout: null,
-      scrollTriggered: false
+      selectOptions: [
+        {group: 'Discover', label: null},
+        {label: 'Weighted Shuffle', value: 'weighted_shuffle'},
+        {label: 'Trending', value: '-metadata__upcoming'},
+        {label: 'Undiscovered Gems', value: '-metadata__gem'},
+        {label: 'Recently Updated', value: '-last_update'},
+        {label: 'Newest', value: '-created_on'},
+        {label: 'Oldest', value: 'created_on'},
+        {label: 'A to Z', value: 'title'},
+        {label: 'Z to A', value: '-title'},
+        {group: 'Current Round', label: null},
+        {label: 'Most Relevant', value: ''},
+        {label: 'Highest Amount Raised', value: '-amount_received_in_round'},
+        {label: 'Highest Contributor Count', value: '-positive_round_contributor_count'},
+        {group: 'All-Time', label: null},
+        {label: 'Highest Amount Raised', value: '-amount_received'},
+        {label: 'Highest Contributor Count', value: '-contributor_count'},
+      ],
     },
     methods: {
       toggleStyle: function(style) {
