44968404cc5f621210289132f8a0f2dd46c4f8e7
Jer-Sch <schuurm@ns.codes>
2021-11-17 17:02:35 -0600

    refactor

--- cypress/integration/grants/test_grant_explorer.js
+++ cypress/integration/grants/test_grant_explorer.js
@@ -21,21 +21,25 @@ describe('Grants Explorer page', () => {
   
       cy.get('.vselect-clean').click();
 
-      cy.get('.vs__dropdown-menu').should('contain', 'Discover');
-      cy.get('.vs__dropdown-menu').should('contain', 'Weighted Shuffle');
-      cy.get('.vs__dropdown-menu').should('contain', 'Trending');
-      cy.get('.vs__dropdown-menu').should('contain', 'Undiscovered Gems');
-      cy.get('.vs__dropdown-menu').should('contain', 'Recently Updated');
-      cy.get('.vs__dropdown-menu').should('contain', 'Newest');
-      cy.get('.vs__dropdown-menu').should('contain', 'Oldest');
-      cy.get('.vs__dropdown-menu').should('contain', 'A to Z');
-      cy.get('.vs__dropdown-menu').should('contain', 'Z to A');
-      cy.get('.vs__dropdown-menu').should('contain', 'Current Round');
-      cy.get('.vs__dropdown-menu').should('contain', 'Most Relevant');
-      cy.get('.vs__dropdown-menu').should('contain', 'All-Time');
-      cy.get('.vs__dropdown-menu').should('contain', 'Highest Amount Raised');
-      cy.get('.vs__dropdown-menu').should('contain', 'Highest Contributor Count');
-
+      cy.get('.vs__dropdown-menu')
+        .should('contain', 'Discover')
+        .should('contain', 'Weighted Shuffle')
+        .should('contain', 'Trending')
+        .should('contain', 'Undiscovered Gems')
+        .should('contain', 'Recently Updated')
+        .should('contain', 'Newest')
+        .should('contain', 'Oldest')
+        .should('contain', 'A to Z')
+        .should('contain', 'Z to A')
+        .should('contain', 'Current Round')
+        .should('contain', 'Most Relevant')
+        .should('contain', 'All-Time')
+        .should('contain', 'Highest Amount Raised')
+        .should('contain', 'Highest Contributor Count')
+        .should('not.contain', 'Misc')
+        .should('not.contain', 'ADMIN: Risk Score')
+        .should('not.contain', 'ADMIN: Sybil Score');
+        
       cy.contains('Discover').parent().should('have.class', 'vs__dropdown-option--disabled');
       cy.contains('Current Round').parent().should('have.class', 'vs__dropdown-option--disabled');
       cy.contains('All-Time').parent().should('have.class', 'vs__dropdown-option--disabled');
