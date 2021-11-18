2463080dd16faf3c047c930f16dfb8f5a70b3d0f
Krishnendu Sengupta <k.sengupta.29.11@gmail.com>
2021-11-18 20:45:52 +0530

    bsc info from https://chainid.network/chains.json

--- app/assets/v2/js/data-chains.js
+++ app/assets/v2/js/data-chains.js
@@ -1296,24 +1296,33 @@ var dataChains =
       "infoURL": "https://matic.network/"
     },
     {
-      "name": "Binance Smart Chain",
+      "name": "Binance Smart Chain Mainnet",
       "chainId": 56,
-      "shortName": "bsc",
+      "shortName": "bnb",
       "chain": "BSC",
       "network": "mainnet",
       "networkId": 56,
-      "nativeCurrency": {
-        "name": "Binance Coin",
-        "symbol": "BNB",
-        "decimals": 8
-      },
       "rpc": [
-        "https://bsc-dataseed.binance.org/"
+        "https://bsc-dataseed1.binance.org",
+        "https://bsc-dataseed2.binance.org",
+        "https://bsc-dataseed3.binance.org",
+        "https://bsc-dataseed4.binance.org",
+        "https://bsc-dataseed1.defibit.io",
+        "https://bsc-dataseed2.defibit.io",
+        "https://bsc-dataseed3.defibit.io",
+        "https://bsc-dataseed4.defibit.io",
+        "https://bsc-dataseed1.ninicoin.io",
+        "https://bsc-dataseed2.ninicoin.io",
+        "https://bsc-dataseed3.ninicoin.io",
+        "https://bsc-dataseed4.ninicoin.io",
+        "wss://bsc-ws-node.nariox.org"
       ],
-      "faucets": [
-
-      ],
-      "infoURL": "https://www.binance.org/en/smartChain"
+      "nativeCurrency": {
+        "name": "Binance Chain Native Token",
+        "symbol": "BNB",
+        "decimals": 18
+      },
+      "infoURL": "https://www.binance.org",
     }
   ];
 
