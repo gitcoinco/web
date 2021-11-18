1675f87c7ac7483211a4e9e3c40763d74dfece41
Jer-Sch <schuurm@ns.codes>
2021-11-17 23:24:19 -0600

    add a test for sorting functionality

--- cypress/integration/grants/test_grant_explorer.js
+++ cypress/integration/grants/test_grant_explorer.js
@@ -73,6 +73,91 @@ describe('Grants Explorer page', () => {
         .should('contain', 'ADMIN: Risk Score')
         .should('contain', 'ADMIN: Sybil Score');
     });
+
+    it('sends the proper sort request to the API', () => {
+      cy.impersonateStaffUser();
+
+      cy.visit('grants/explorer');
+  
+      cy.get('.vselect-clean').click();
+
+      // Options in Discover category
+      cy.get('.vs__dropdown-menu').contains('Weighted Shuffle').click();
+      cy.url().should('contain', 'sort_option=weighted_shuffle');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Trending').click();
+      cy.url().should('contain', 'sort_option=-metadata__upcoming');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Undiscovered Gems').click();
+      cy.url().should('contain', 'sort_option=-metadata__gem');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Recently Updated').click();
+      cy.url().should('contain', 'sort_option=-last_update');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Newest').click();
+      cy.url().should('contain', 'sort_option=-created_on');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Oldest').click();
+      cy.url().should('contain', 'sort_option=created_on');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('A to Z').click();
+      cy.url().should('contain', 'sort_option=title');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Z to A').click();
+      cy.url().should('contain', 'sort_option=-title');
+
+      // Options in Current Round category
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Most Relevant').click();
+      cy.url().should('contain', 'sort_option='); // The value of this option is purposely set to empty string (see app/assets/v2/js/grants/index.js)
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Highest Amount Raised').click();
+      cy.url().should('contain', 'sort_option=-amount_received_in_round');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('Highest Contributor Count').click();
+      cy.url().should('contain', 'sort_option=-positive_round_contributor_count');
+
+      // Options in All-Time category
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').find('#vs3__option-14').contains('Highest Amount Raised').click(); // Need to be more specific here because the same options exist above
+      cy.url().should('contain', 'sort_option=-amount_received');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').find('#vs3__option-15').contains('Highest Contributor Count').click();
+      cy.url().should('contain', 'sort_option=-contributor_count');
+
+      //Admin options
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('ADMIN: Risk Score').click();
+      cy.url().should('contain', 'sort_option=-weighted_risk_score');
+
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu').contains('ADMIN: Sybil Score').click();
+      cy.url().should('contain', 'sort_option=-sybil_score');
+    });
   });
 });
   
