a0a6756afc92a6c66d31be560fbe44a0f127ba4c
Jer-Sch <schuurm@ns.codes>
2021-11-17 21:57:35 -0600

    refactor specs

--- cypress/integration/grants/test_grant_explorer.js
+++ cypress/integration/grants/test_grant_explorer.js
@@ -33,17 +33,35 @@ describe('Grants Explorer page', () => {
         .should('contain', 'Most Relevant')
         .should('contain', 'All-Time')
         .should('contain', 'Highest Amount Raised')
-        .should('contain', 'Highest Contributor Count')
-        .should('not.contain', 'Misc')
-        .should('not.contain', 'ADMIN: Risk Score')
-        .should('not.contain', 'ADMIN: Sybil Score');
-        
+        .should('contain', 'Highest Contributor Count');
+    });
+
+    it('divides the sort options into category names with disabled labels', () => {
+      cy.impersonateUser();
+
+      cy.visit('grants/explorer');
+  
+      cy.get('.vselect-clean').click();
+
       cy.contains('Discover').parent().should('have.class', 'vs__dropdown-option--disabled');
       cy.contains('Current Round').parent().should('have.class', 'vs__dropdown-option--disabled');
       cy.contains('All-Time').parent().should('have.class', 'vs__dropdown-option--disabled');
     });
 
-    it('displays admin options in addition to the normal options when user is staff', () => {
+    it('does not display admin options when user is not staff', () => {
+      cy.impersonateUser();
+
+      cy.visit('grants/explorer');
+  
+      cy.get('.vselect-clean').click();
+
+      cy.get('.vs__dropdown-menu')
+        .should('not.contain', 'Misc')
+        .should('not.contain', 'ADMIN: Risk Score')
+        .should('not.contain', 'ADMIN: Sybil Score');
+    });
+
+    it('displays admin options when user is staff', () => {
       cy.impersonateStaffUser();
 
       cy.visit('grants/explorer');
@@ -51,27 +69,9 @@ describe('Grants Explorer page', () => {
       cy.get('.vselect-clean').click();
 
       cy.get('.vs__dropdown-menu')
-        .should('contain', 'Discover')
-        .should('contain', 'Weighted Shuffle')
-        .should('contain', 'Trending')
-        .should('contain', 'Undiscovered Gems')
-        .should('contain', 'Recently Updated')
-        .should('contain', 'Newest')
-        .should('contain', 'Oldest')
-        .should('contain', 'A to Z')
-        .should('contain', 'Z to A')
-        .should('contain', 'Current Round')
-        .should('contain', 'Most Relevant')
-        .should('contain', 'All-Time')
-        .should('contain', 'Highest Amount Raised')
-        .should('contain', 'Highest Contributor Count')
         .should('contain', 'Misc')
         .should('contain', 'ADMIN: Risk Score')
         .should('contain', 'ADMIN: Sybil Score');
-        
-      cy.contains('Discover').parent().should('have.class', 'vs__dropdown-option--disabled');
-      cy.contains('Current Round').parent().should('have.class', 'vs__dropdown-option--disabled');
-      cy.contains('All-Time').parent().should('have.class', 'vs__dropdown-option--disabled');
     });
   });
 });
