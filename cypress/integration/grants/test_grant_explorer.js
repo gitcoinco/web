9cd1a49c61d8c291478847cd4c7ef0dd5e6f866e
Jer-Sch <schuurm@ns.codes>
2021-11-23 16:58:51 -0600

    remove 'Most Relevant' option, update tests

--- cypress/integration/grants/test_grant_explorer.js
+++ cypress/integration/grants/test_grant_explorer.js
@@ -30,12 +30,11 @@ describe('Grants Explorer page', () => {
         .should('contain', 'A to Z')
         .should('contain', 'Z to A')
         .should('contain', 'Current Round')
-        .should('contain', 'Most Relevant')
         .should('contain', 'Highest Amount Raised')
         .should('contain', 'Highest Contributor Count')
         .should('contain', 'All-Time');
-      cy.get('.vs__dropdown-menu').find('#vs3__option-14').should('contain', 'Highest Amount Raised'); // need to be more specific to test two elements with same name
-      cy.get('.vs__dropdown-menu').find('#vs3__option-15').should('contain', 'Highest Contributor Count');
+      cy.get('.vs__dropdown-menu').find('#vs3__option-13').should('contain', 'Highest Amount Raised'); // need to be more specific to test two elements with same name
+      cy.get('.vs__dropdown-menu').find('#vs3__option-14').should('contain', 'Highest Contributor Count');
     });
 
     it('divides the sort options into category names with disabled labels', () => {
@@ -125,11 +124,6 @@ describe('Grants Explorer page', () => {
       // Options in Current Round category
       cy.get('.vselect-clean').click();
 
-      cy.get('.vs__dropdown-menu').contains('Most Relevant').click();
-      cy.url().should('contain', 'sort_option='); // The value of this option is purposely set to empty string (see app/assets/v2/js/grants/index.js)
-
-      cy.get('.vselect-clean').click();
-
       cy.get('.vs__dropdown-menu').contains('Highest Amount Raised').click();
       cy.url().should('contain', 'sort_option=-amount_received_in_round');
 
@@ -141,12 +135,12 @@ describe('Grants Explorer page', () => {
       // Options in All-Time category
       cy.get('.vselect-clean').click();
 
-      cy.get('.vs__dropdown-menu').find('#vs3__option-14').contains('Highest Amount Raised').click(); // Need to be more specific here because the same options exist above
+      cy.get('.vs__dropdown-menu').find('#vs3__option-13').contains('Highest Amount Raised').click(); // Need to be more specific here because the same options exist above
       cy.url().should('contain', 'sort_option=-amount_received');
 
       cy.get('.vselect-clean').click();
 
-      cy.get('.vs__dropdown-menu').find('#vs3__option-15').contains('Highest Contributor Count').click();
+      cy.get('.vs__dropdown-menu').find('#vs3__option-14').contains('Highest Contributor Count').click();
       cy.url().should('contain', 'sort_option=-contributor_count');
 
       // Admin options
