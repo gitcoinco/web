d60e9e3ac2309af7c4d38b317c64f94bc19de755
Jeremy Schuurmans <schuurm@ns.codes>
2021-11-24 00:44:25 -0600

    remove pageIsFetched check (#9720)

--- app/assets/v2/js/grants/index.js
+++ app/assets/v2/js/grants/index.js
@@ -467,7 +467,7 @@ if (document.getElementById('grants-showcase')) {
         if (bottomOfPage || pageHeight < visible) {
           if (vm.params.tab === 'collections' && vm.collectionsPage) {
             await vm.fetchCollections(true);
-          } else if (vm.grantsHasNext && !vm.pageIsFetched(vm.params.page + 1)) {
+          } else if (vm.grantsHasNext) {
             await vm.fetchGrants(vm.params.page, true, true);
           }
         }
@@ -567,12 +567,6 @@ if (document.getElementById('grants-showcase')) {
         this.$set(this, 'tabSelected', this.params.tab);
         // load the correct tab
         this.loadTab(true);
-      },
-      pageIsFetched: function(page) {
-        let vm = this;
-
-        return vm.fetchedPages.includes(page);
-
       },
       showFilter: function() {
         let vm = this;
