9f34b9173a71d48996b3485b302fcad125f5e4c9
Jer-Sch <schuurm@ns.codes>
2022-01-06 16:58:07 -0600

    add test cases for 'active', 'grant_type', and 'title' attributes

--- app/grants/tests/models/test_grant.py
+++ app/grants/tests/models/test_grant.py
@@ -1,5 +1,6 @@
 import pytest
 from grants.models.grant import Grant
+from grants.models.grant_type import GrantType
 from grants.tests.factories import GrantFactory
 
 
@@ -18,3 +19,20 @@ class TestGrant:
         grant = GrantFactory()
 
         assert hasattr(grant, 'vector_column')
+
+    def test_grant_has_active_attribute(self):
+        grant = GrantFactory()
+
+        assert hasattr(grant, 'active')
+        assert grant.active == True
+
+    def test_grant_has_associated_grant_type(self):
+        grant = GrantFactory()
+
+        assert hasattr(grant, 'grant_type')
+        assert isinstance(grant.grant_type, GrantType)
+
+    def test_grant_has_a_title(self):
+        grant = GrantFactory()
+
+        assert hasattr(grant, 'title')
