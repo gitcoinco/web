3a30c743dba87eff4bbc0ff031ff989c71274d89
Jer-Sch <schuurm@ns.codes>
2022-01-06 23:28:27 -0600

    add test for slug and docstrings

--- app/grants/tests/models/test_grant.py
+++ app/grants/tests/models/test_grant.py
@@ -16,23 +16,38 @@ class TestGrant:
         assert isinstance(grant, Grant)
 
     def test_grant_has_vector_column(self):
+        """Test Grant has vector_column attribute."""
+
         grant = GrantFactory()
 
         assert hasattr(grant, 'vector_column')
 
     def test_grant_has_active_attribute(self):
+        """Test Grant has active attribute and that it defaults to true."""
+
         grant = GrantFactory()
 
         assert hasattr(grant, 'active')
         assert grant.active == True
 
     def test_grant_has_associated_grant_type(self):
+        """Test Grant has associated GrantType."""
+
         grant = GrantFactory()
 
         assert hasattr(grant, 'grant_type')
         assert isinstance(grant.grant_type, GrantType)
 
     def test_grant_has_a_title(self):
+        """Test Grant has a title attribute."""
+
         grant = GrantFactory()
 
         assert hasattr(grant, 'title')
+
+    def test_grant_has_a_slug(self):
+        """Test Grant has a slug attribute."""
+
+        grant = GrantFactory()
+
+        assert hasattr(grant, 'slug')
