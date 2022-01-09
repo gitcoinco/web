bf1bb548f87895f1ffbf3a4b694249f1d87ca8c9
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-01-09 20:10:10 +0530

    fix conflicts

--- app/grants/urls.py
+++ app/grants/urls.py
@@ -28,10 +28,10 @@ from grants.views import (
     get_clr_sybil_input, get_collection, get_collections_list, get_ethereum_cart_data, get_grant_payload,
     get_grant_tags, get_grants, get_interrupted_contributions, get_replaced_tx, get_trust_bonus, grant_activity,
     grant_details, grant_details_api, grant_details_contributions, grant_details_contributors, grant_edit, grant_fund,
-    grant_new, grants, grants_addr_as_json, grants_bulk_add, grants_by_grant_type, grants_cart_view, grants_info,
-    grants_landing, grants_type_redirect, hall_of_fame, ingest_contributions, ingest_contributions_view, invoice,
-    leaderboard, manage_ethereum_cart_data, matching_funds, new_matching_partner, profile, quickstart,
-    remove_grant_from_collection, save_collection, toggle_grant_favorite, upload_sybil_csv, verify_grant,
+    grants, grants_addr_as_json, grants_bulk_add, grants_by_grant_type, grants_cart_view, grants_info, grants_landing,
+    grants_type_redirect, hall_of_fame, ingest_contributions, ingest_contributions_view, invoice, leaderboard,
+    manage_ethereum_cart_data, matching_funds, new_matching_partner, profile, quickstart, remove_grant_from_collection,
+    save_collection, toggle_grant_favorite, upload_sybil_csv, verify_grant,
 )
 
 app_name = 'grants/'
