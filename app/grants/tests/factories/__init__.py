d3f5270a93dfce61f67af3cdc1e14fd3fbf146d9
Kevin Solorio <kevin.solorio@hey.com>
2022-01-13 18:26:45 -0600

    add tests for post route of clr_match

--- app/grants/tests/factories/__init__.py
+++ app/grants/tests/factories/__init__.py
@@ -10,6 +10,7 @@ from .grant_clr_calculation_factory import GrantCLRCalculationFactory
 from .grant_clr_factory import GrantCLRFactory
 from .grant_collection_factory import GrantCollectionFactory
 from .grant_factory import GrantFactory
+from .grant_payout_factory import GrantPayoutFactory
 from .grant_stat_factory import GrantStatFactory
 from .grant_type_factory import GrantTypeFactory
 from .match_pledge_factory import MatchPledgeFactory
--- app/grants/tests/views/test_clr_matches_view_function.py
+++ app/grants/tests/views/test_clr_matches_view_function.py
@@ -4,8 +4,11 @@ import pytest
 from rest_framework.test import APIClient
 
 from dashboard.tests.factories import ProfileFactory
-
-from grants.tests.factories import GrantFactory, CLRMatchFactory
+from grants.tests.factories import (
+    GrantFactory,
+    CLRMatchFactory,
+    GrantPayoutFactory
+)
 
 
 @pytest.fixture
@@ -20,10 +23,11 @@ def profile(user):
 
 @pytest.fixture
 def grant(profile):
-    grant = GrantFactory.create(admin_profile=profile)
-    CLRMatchFactory(grant=grant)
+    grnt = GrantFactory.create(admin_profile=profile)
+    grant_payout = GrantPayoutFactory()
+    CLRMatchFactory(grant=grnt, grant_payout=grant_payout)
 
-    return grant
+    return grnt
 
 
 @pytest.mark.django_db
@@ -65,3 +69,71 @@ class TestCLRMatchesGetRequestView:
         assert response.status_code == 200
         assert len(content) == 1
         assert sorted(content[0].keys()) == sorted(expected_keys)
+
+
+@pytest.mark.django_db
+class TestCLRMatchesPostRequestView:
+    def test_requires_login(self):
+        client = APIClient()
+        response = client.post('/grants/v1/api/clr-matches/')
+
+        assert response.status_code == 302
+
+    def test_requires_profile(self, user):
+        client = APIClient()
+        client.force_login(user=user)
+
+        response = client.post('/grants/v1/api/clr-matches/')
+        content = json.loads(response.content)
+
+        assert response.status_code == 404
+        assert content.get('message') == 'Profile not found!'
+
+    def test_requires_pk_field(self, profile):
+        client = APIClient()
+        client.force_login(user=profile.user)
+
+        response = client.post('/grants/v1/api/clr-matches/', {}, format='json')
+        content = json.loads(response.content)
+
+        assert response.status_code == 400
+        assert content.get('message') == 'pk field is required!'
+
+    def test_requires_claim_tx_field(self, profile):
+        client = APIClient()
+        client.force_login(user=profile.user)
+
+        response = client.post('/grants/v1/api/clr-matches/', {'pk': 1}, format='json')
+        content = json.loads(response.content)
+
+        assert response.status_code == 400
+        assert content.get('message') == 'claim_tx field is required!'
+
+    def test_404_when_clr_match_not_found(self, profile):
+        client = APIClient()
+        client.force_login(user=profile.user)
+        request_data = {'pk': 1, 'claim_tx': 'abd123'}
+
+        response = client.post('/grants/v1/api/clr-matches/', request_data, format='json')
+        content = json.loads(response.content)
+
+        assert response.status_code == 404
+        assert content.get('message') == 'CLR Match not found!'
+
+    def test_updates_clr_match(self, grant):
+        match = grant.clr_matches.first()
+        existing_claim_tx = match.claim_tx
+        request_data = {
+            'pk': match.pk,
+            'claim_tx': 'abc123'
+        }
+
+        client = APIClient()
+        client.force_login(user=grant.admin_profile.user)
+
+        response = client.post('/grants/v1/api/clr-matches/', request_data, format='json')
+        match.refresh_from_db()
+
+        assert response.status_code == 200
+        assert match.claim_tx != existing_claim_tx
+        assert match.claim_tx == 'abc123'
