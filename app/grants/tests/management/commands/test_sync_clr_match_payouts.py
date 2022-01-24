b454d61e53bc1e1d82b2ab0f6df12a9cbd010d14
Kevin Solorio <kevin.solorio@hey.com>
2022-01-24 13:37:03 -0800

    refactor the grant_payout creation to fixture

--- app/grants/tests/management/commands/test_sync_clr_match_payouts.py
+++ app/grants/tests/management/commands/test_sync_clr_match_payouts.py
@@ -7,6 +7,14 @@ from django.core.management import call_command, CommandError
 from grants.tests.factories import GrantFactory, GrantPayoutFactory, CLRMatchFactory
 
 
+@pytest.fixture
+def grant_payout():
+    return GrantPayoutFactory(
+        contract_address='0x0EbD2E2130b73107d0C45fF2E16c93E7e2e10e3a',
+        network='mainnet'
+    )
+
+
 @pytest.mark.django_db
 class TestSyncCLRMatchPayouts:
     def test_error_message_informs_to_pass_in_network(self):
@@ -27,12 +35,9 @@ class TestSyncCLRMatchPayouts:
 
         assert error == "Error: the following arguments are required: --contract-address/-c"
 
-    def test_reports_number_of_clr_matches_missing_claim_tx(self):
-        grant = GrantFactory(admin_address='0xB81C935D01e734b3D8bb233F5c4E1D72DBC30f6c')
-        grant_payout = GrantPayoutFactory(
-            contract_address='0x0EbD2E2130b73107d0C45fF2E16c93E7e2e10e3a',
-            network='mainnet'
-        )
+    def test_reports_number_of_clr_matches_missing_claim_tx(self, grant_payout):
+        grant = GrantFactory(admin_address='0x230Fc981F7CaE90cFC4ed4c18F7C178B239e5F9F')
+
         CLRMatchFactory(grant=grant, grant_payout=grant_payout)
 
         out = StringIO()
@@ -46,12 +51,8 @@ class TestSyncCLRMatchPayouts:
         result = out.getvalue()
         assert "Number of unclaimed CLR Matches: 1" in result
 
-    def test_reports_each_item_being_updated(self):
+    def test_reports_each_item_being_updated(self, grant_payout):
         grant = GrantFactory(admin_address='0x230Fc981F7CaE90cFC4ed4c18F7C178B239e5F9F')
-        grant_payout = GrantPayoutFactory(
-            contract_address='0x0EbD2E2130b73107d0C45fF2E16c93E7e2e10e3a',
-            network='mainnet'
-        )
         match = CLRMatchFactory(grant=grant, grant_payout=grant_payout)
 
         out = StringIO()
@@ -65,12 +66,8 @@ class TestSyncCLRMatchPayouts:
         result = out.getvalue()
         assert f"Updating CLR Match - {match.pk}" in result
 
-    def test_reports_total_updates(self):
+    def test_reports_total_updates(self, grant_payout):
         grant = GrantFactory(admin_address='0x230Fc981F7CaE90cFC4ed4c18F7C178B239e5F9F')
-        grant_payout = GrantPayoutFactory(
-            contract_address='0x0EbD2E2130b73107d0C45fF2E16c93E7e2e10e3a',
-            network='mainnet'
-        )
         CLRMatchFactory(grant=grant, grant_payout=grant_payout)
 
         out = StringIO()
@@ -84,12 +81,7 @@ class TestSyncCLRMatchPayouts:
         result = out.getvalue()
         assert f"Total CLR Matches updated 1" in result
 
-    def test_skips_updating_clr_matches_with_existing_claim_tx(self):
-        grant_payout = GrantPayoutFactory(
-            contract_address='0x0EbD2E2130b73107d0C45fF2E16c93E7e2e10e3a',
-            network='mainnet'
-        )
-
+    def test_skips_updating_clr_matches_with_existing_claim_tx(self, grant_payout):
         grant = GrantFactory(admin_address='0x230Fc981F7CaE90cFC4ed4c18F7C178B239e5F9F')
         CLRMatchFactory(grant=grant, grant_payout=grant_payout)
 
