b2a4b40d63f3a11fcb47a7bf625d8050ace2371c
Chibuotu Amadi <chibuotu@gmail.com>
2022-01-27 16:38:40 +0100

    update zksync cart

--- app/assets/v2/js/cart-ethereum-zksync.js
+++ app/assets/v2/js/cart-ethereum-zksync.js
@@ -168,6 +168,7 @@ Vue.component('grantsCartEthereumZksync', {
 
     // Reset zkSync modal status after a checkout failure
     resetZkSyncModal() {
+      appCart.$refs.cart.showConfirmationModal = false;
       this.zksync.checkoutStatus = 'not-started';
     },
 
@@ -227,7 +228,11 @@ Vue.component('grantsCartEthereumZksync', {
         }
 
         // Save off cart data
+        appCart.$refs.cart.activeCheckout = 'zksync';
         this.zksync.checkoutStatus = 'pending';
+
+        appCart.$refs.cart.showConfirmationModal = true;
+        this.zksync.showModal = false;
 
         // Send user to zkSync to complete checkout
         const txHashes = await this.zksync.checkoutManager.zkSyncBatchCheckout(
