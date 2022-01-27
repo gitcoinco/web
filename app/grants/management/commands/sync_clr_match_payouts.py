1411d46817e6f1c8ead26dc082f94d4389ad421e
Kevin Solorio <kevin.solorio@hey.com>
2022-01-27 09:59:11 -0800

    debugging

--- app/grants/management/commands/sync_clr_match_payouts.py
+++ app/grants/management/commands/sync_clr_match_payouts.py
@@ -52,6 +52,9 @@ class Command(BaseCommand):
 
         event_logs = MatchesContract(address=contract_address, network=network).get_payout_claimed_entries()
 
+        self.stdout.write(f'event_logs: {event_logs}')
+        self.stdout.write(f'grouped_matches: {grouped_matches}')
+
         updates_completed = 0
         for event in event_logs:
            matches = grouped_matches.get(event['recipient'], [])
