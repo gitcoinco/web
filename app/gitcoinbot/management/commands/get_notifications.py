4b166226bf66df97d69b300ebffb40b50bc275a8
Graham Dixon <graham@gitcoin.co>
2022-01-27 12:24:12 +0000

    fix: `get_notifications()` fixes (#10032)
    * fix: ensures url is defined
    
    * fix: ensures non-numeric comments are ignored and fixes post_issue_comment_reaction

--- app/gitcoinbot/management/commands/get_notifications.py
+++ app/gitcoinbot/management/commands/get_notifications.py
@@ -40,17 +40,18 @@ class Command(BaseCommand):
             for notification in notifications:
                 if hasattr(notification, '_subject') and notification.reason == 'mention':
                     try:
-                        url = notification.subject.url
+                        url = notification.subject.url if notification.subject.url else ''
                         url = url.replace('/repos', '')
                         url = url.replace('//api.github', '//github')
                         latest_comment_url = notification.subject.latest_comment_url
-                        if latest_comment_url is None:
+                        if not latest_comment_url:
                             print("no latest comment url")
                             continue
                         _org_name = org_name(url)
                         _repo_name = repo_name(url)
                         _issue_number = issue_number(url)
-                        if not latest_comment_url:
+
+                        if not _issue_number.isnumeric():
                             continue
                         _comment_id = latest_comment_url.split('/')[-1]
                         comment = get_issue_comments(_org_name, _repo_name, _issue_number, _comment_id)
@@ -65,7 +66,7 @@ class Command(BaseCommand):
                             is_from_gitcoinbot = settings.GITHUB_API_USER in comment_from
                             if num_reactions == 0 and not is_from_gitcoinbot:
                                 print("unprocessed")
-                                post_issue_comment_reaction(_org_name, _repo_name, _comment_id, 'heart')
+                                post_issue_comment_reaction(_org_name, _repo_name, _issue_number, _comment_id, 'heart')
                     except RateLimitExceededException as e:
                         logging.debug(e)
                         print(e)
