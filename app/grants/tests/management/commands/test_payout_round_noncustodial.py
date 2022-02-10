5b7cd8298aa86c9876181cea7bc20c9455d899ff
Tim Schultz <timschultz@MacBook-Pro.local>
2022-02-10 14:53:07 -0700

    starting to layout test and factories

--- app/grants/tests/management/commands/test_payout_round_noncustodial.py
+++ app/grants/tests/management/commands/test_payout_round_noncustodial.py
@@ -1,7 +1,17 @@
 import pytest
-from django.core.management import call_command
+from django.core.management import CommandError, call_command
+from io import StringIO
+from unittest import mock
 
 from dashboard.tests.factories import ProfileFactory
 from grants.tests.factories import ContributionFactory, CLRMatchFactory, GrantFactory, SubscriptionFactory
 
-# from grants.management.commands.payout_round_noncustodial import 
+def test_error_message_informs_to_pass_in_network(self):
+        error = ''
+        try:
+            call_command('payout_round_noncustodial', '--contract-address=abc123')
+        except CommandError as e:
+            import pdb; pdb.set_trace()
+            error = str(e)
+
+        assert error == "Error: the following arguments are required: --network/-n"
