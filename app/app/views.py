d5fb3b3fb0e83e08afee1f3ca456c59ee532e476
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-02-17 10:51:31 +0530

    chore: address feedback comments

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect
 
 def redirect_view(request):
-    response = redirect('http://decentralized.quadraticlands.com/')
-    return response
+    return redirect('http://decentralized.quadraticlands.com/')
