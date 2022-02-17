057d8c15a2d99a5c54fa58174d2b8b8ec79591ff
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-02-17 14:54:51 +0000

    Adding pulumi infrastructure spec

---
+++ infra/infra-review/env_vars.sample.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+export POC_DB_NAME=
+export POC_DB_PASSWORD=
+export POC_DB_USER=
+export POC_PUBLIC_KEY_GE=
+export POC_PUBLIC_KEY_GR=
+
+export POC_GITHUB_API_USER=
+export POC_GITHUB_API_TOKEN=
+export POC_GITHUB_CLIENT_ID=
+export POC_GITHUB_CLIENT_SECRET=
+export POC_GITHUB_APP_NAME=
