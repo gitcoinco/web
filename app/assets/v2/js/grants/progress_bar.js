c2ef3be0197d4ea5d512d6ada489676790a361af
Tim Schultz <timschultz@Computer.local>
2022-02-24 13:30:39 -0700

    added responsive styles to hr

--- app/assets/v2/js/grants/progress_bar.js
+++ app/assets/v2/js/grants/progress_bar.js
@@ -2,5 +2,13 @@ Vue.component('progress-bar', {
   props: {
     steps: { type: Array, 'default': () => [] }
   },
-  delimiters: [ '[[', ']]' ]
+  delimiters: [ '[[', ']]' ],
+  computed: {
+    lineWidth() {
+      if (this.steps.length >= 4) {
+        return 80;
+      }
+      return this.steps.length * 20 + 10;
+    }
+  }
 });
