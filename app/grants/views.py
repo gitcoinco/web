106aa4a5cf837a10af134f2df9399a1a733ada99
Tim Schultz <schultztimothy@users.noreply.github.com>
2022-03-07 20:01:05 -0700

    updated twitter unfurl img and half banner shown on grants landing (#10268)
    Co-authored-by: Tim Schultz <timschultz@Computer.local>

--- app/grants/views.py
+++ app/grants/views.py
@@ -968,7 +968,7 @@ def grants_landing(request):
             'title': 'Grants',
             'EMAIL_ACCOUNT_VALIDATION': EMAIL_ACCOUNT_VALIDATION,
             'card_desc': f'{live_now}',
-            'avatar_url': request.build_absolute_uri(static('v2/images/twitter_cards/grants13.png')),
+            'avatar_url': request.build_absolute_uri(static('v2/images/twitter_cards/grants13-1.png')),
             'card_type': 'summary_large_image',
             'avatar_height': 675,
             'avatar_width': 1200,
@@ -2346,7 +2346,7 @@ def hall_of_fame(request):
     params = {
         'active': 'hall_of_fame',
         'title': _('Hall of Fame'),
-        'avatar_url': request.build_absolute_uri(static('v2/images/twitter_cards/grants13.png')),
+        'avatar_url': request.build_absolute_uri(static('v2/images/twitter_cards/grants13-1.png')),
 
         'total_donations': hall_of_fame.total_donations,
         'top_individual_donors_url': hall_of_fame.top_individual_donors.url,
