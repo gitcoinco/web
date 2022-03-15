c58c470b478b7299ed087634523df66fbdf69e0f
nutrina <nutrina9@gmail.com>
2022-03-15 18:19:52 +0200

    Validating payout address when submitting bounty work. (#10339)
    * Validating payout address when submitting bounty work.
    
       - have moved the validation logic into a standalone file, so it can be reused
       - add validation logic to the bbounty work submission process
    
    * Linter fixes
    
    * Dynamic placeholder for payout address, depending on selected chain

--- app/assets/v2/js/pages/fulfill_bounty/index.js
+++ app/assets/v2/js/pages/fulfill_bounty/index.js
@@ -17,7 +17,49 @@ if (web3_type === 'web3_modal') {
   }, false);
 }
 
+var getWalletAddressPlaceholder = function(chainId) {
+
+  switch (chainId) {
+    case '1': // ETH
+    case '56': // Binance
+    case '61': // ETC
+      return '0x...';
+    case '1995': // NERVOS ...
+      return 'ckb...';
+    case '50797': // Tezos
+      return 'tz1...';
+    case '0': // Bitcoin
+      return 'bc1...';
+    case '270895': // Casper
+      return '01...';
+    case '1000':// Harmony
+      return 'one...';
+    case '58':// Polkadot
+      return '1...';
+    case '59':// Kusama
+      return 'C...';
+    case '102':// Zilliqa
+      return 'zil...';
+    case '600': // Filecoin
+      return 'f...';
+    case '42220': // Celo
+      return '0x...';
+    case '30': // RSK
+      return '0x...';
+    case '50': // Xinfin
+      return 'xdc...';
+    case '1001': // Algorand
+      return 'GRUNDTKRXHX...';
+    case '1935': // Sia
+      return '';
+  }
+
+  return '';
+};
+
+
 window.onload = function() {
+  $('#payoutAddress').attr('placeholder', getWalletAddressPlaceholder(bountyChainId));
 
   $('.rating input:radio').attr('checked', false);
 
@@ -35,7 +77,21 @@ window.onload = function() {
     $('input[name=issueURL]').val(getParam('source'));
   }
 
+  jQuery.validator.addMethod('cryptoAddress', function(value, element, params) {
+    let chainId = params[0];
+    let ret = validateWalletAddress(chainId, value);
+
+    return ret;
+  }, jQuery.validator.format('Please enter a valid {1} wallet address.'));
+
+
   $('#submitBounty').validate({
+    rules: {
+      payoutAddress: {
+        required: true,
+        cryptoAddress: [ bountyChainId, tokenName ]
+      }
+    },
     submitHandler: function(form) {
       loading_button($('.js-submit'));
       let data = {};
