de73b91bf6a0998753291cd353b5bd50f619600e
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-03-24 16:54:16 +0200

    Adjusting bounty creation test

--- cypress/integration/bounties/test_bounty_creation.js
+++ cypress/integration/bounties/test_bounty_creation.js
@@ -35,26 +35,72 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     // attempt to use the UI as a real user would.
     cy.wait(1000);
 
-    cy.contains('ETH').click();
-
-    cy.get('#issueURL').type('https://github.com/gitcoinco/web/issues/1');
-
-    cy.contains('Front End').click();
-    cy.contains('Traditional').click();
-
-    cy.contains('Approval Required').click();
-
+    // Screen 1
+    cy.contains('Feature').click();
+    cy.get('#bounty_tags').find('.vs__search').click();
+    cy.contains('Python').click();
     cy.get('#experience_level').find('.vs__search').click();
     cy.contains('Beginner').click();
     cy.get('#project_length').find('.vs__search').click();
     cy.contains('Hours').click();
-    cy.get('#bounty_type').find('.vs__search').click();
-    cy.contains('Bug').click();
 
-    cy.get('#terms').check();
-    cy.get('#termsPrivacy').check();
+    cy.contains('Next').click();
 
-    cy.get('Button').contains('Fund Issue').click();
+    // Screen 2
+    cy.contains('Import from GitHub').click();
+    cy.get('#new-bounty-issue-url').type('https://github.com/gitcoinco/web/issues/1');
+    cy.get('#new-bounty-acceptace-criteria').type('Custom bounty acceptance criteria');
+    cy.get('#new-bounty-resources').type('Custom bounty resource');
+    cy.get('#new-bounty-organisation-url').type('https://github.com/gitcoinco/');
+
+    cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-type').find('.vs__search').click().type('Discord{enter}');
+    cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-value').clear().type('#myhandle289346');
+
+    // Add another row
+    cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-btn-add-contact').click();
+    cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-type').find('.vs__search').click().type('Telegram{enter}');
+    cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-value').clear().type('telegram-user');
+
+    // Add another row
+    cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-btn-add-contact').click();
+    cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-type').find('.vs__search').click().type('Mail{enter}');
+    cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-value').clear().type('my.demo.name@email.com');
+
+
+    cy.contains('Next').click();
+
+    // Screen 3
+
+    cy.contains('Next').click();
+    
+    // Screen 4
+
+    cy.contains('Next').click();
+    
+    // Screen 5
+
+    cy.contains('Confirm').click();
+
+    // cy.contains('ETH').click();
+
+    // cy.get('#issueURL').type('https://github.com/gitcoinco/web/issues/1');
+
+    // cy.contains('Front End').click();
+    // cy.contains('Traditional').click();
+
+    // cy.contains('Approval Required').click();
+
+    // cy.get('#experience_level').find('.vs__search').click();
+    // cy.contains('Beginner').click();
+    // cy.get('#project_length').find('.vs__search').click();
+    // cy.contains('Hours').click();
+    // cy.get('#bounty_type').find('.vs__search').click();
+    // cy.contains('Bug').click();
+
+    // cy.get('#terms').check();
+    // cy.get('#termsPrivacy').check();
+
+    // cy.get('Button').contains('Fund Issue').click();
 
   });
 });
