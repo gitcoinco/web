556df1c19e50d9f742939c70c2cd57be427ae828
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-04-05 08:28:55 +0530

    Erc20 support tests (#10448)
    * resolving modules
    
    * remove migration comment
    
    Co-authored-by: Tim Schultz <timschultz@Computer.local>

---
+++ app/economy/tests/factories/__init__.py
@@ -0,0 +1,1 @@
+from .token_factory import TokenFactory
--- app/economy/tests/factories/token_factory.py
+++ app/economy/tests/factories/token_factory.py
@@ -3,6 +3,6 @@ import pytest
 from economy.models import Token
 
 @pytest.mark.django_db
-class Token(factory.django.DjangoModelFactory):
+class TokenFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Token
