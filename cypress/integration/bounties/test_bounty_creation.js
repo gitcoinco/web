ec8f06cc19a75a25e09d83d6b001668401149462
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-04-13 15:58:45 +0300

    Testing input validation in bounty creation flow, steps 1 & 2)

--- cypress/integration/bounties/test_bounty_creation.js
+++ cypress/integration/bounties/test_bounty_creation.js
@@ -21,7 +21,7 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.url().should('contain', 'bounty/new');
   });
 
-  it('can create a new bounty', () => {
+  it.skip('can create a new bounty', () => {
     cy.visit('bounty/new');
 
     // unfortunately some of the events do not seem bound in time for this
@@ -39,7 +39,7 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.contains('Feature').click();
     cy.get('#bounty_tags').find('.vs__search').click();
 
-    let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+    let tags = ['Python', 'Lua', 'Web Assembly'];
 
     tags.forEach(tag => {
       if (tag === 'Python') {
@@ -99,14 +99,14 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.get('#usd_amount').clear().type('123.34');
 
     cy.contains('Next').click();
-    
+
     // Screen 4
 
     cy.contains('Standard').click();
     cy.contains('Approval Required').click();
 
     cy.contains('Next').click();
-    
+
     // Screen 5
 
     tags.forEach(tag => {
@@ -125,7 +125,7 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.get('#experience_level').contains('Beginner');
     // cy.contains('Permission').parent().contains('Approval');
 
-    
+
     // Save and navigate to the details screen
     cy.contains('Confirm').click();
 
@@ -145,5 +145,140 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.get('#experience_level').contains('Beginner');
     // cy.contains('Permission').parent().contains('Approval');
   });
+
+
+  describe('Verify bounty field validation', () => {
+    it.skip('Should validated the mandatory fields on screen 1 ', () => {
+      cy.visit('bounty/new');
+
+      // unfortunately some of the events do not seem bound in time for this
+      // to run without any errors. adding in this manual wait is not advised
+      // and is used here to slow the tests down to allow the modal close click
+      // events to bind properly
+      // in addition to the wait, we are using the force options on the click,
+      // if there is an alternative means for clicking on buttons, please use those
+      // instead of jumping to force. force may click on items hidden by other elements
+      // and is not how a user would interact with the application. we should make every
+      // attempt to use the UI as a real user would.
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Next').click();
+
+      // Screen 3
+      cy.contains('Next').click();
+
+      // Screen 4
+      cy.contains('Next').click();
+
+      // Screen 5
+      // Clicking 'Confirm' should trigger the validation
+      cy.contains('Confirm').click();
+
+      // Back to screen 1
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+
+      cy.contains('Select at least one category').should('be.visible');
+      cy.contains('Please select the bounty type').should('be.visible');
+      cy.contains('Please select the experience level').should('be.visible');
+      cy.contains('Please select the project length').should('be.visible');
+
+    });
+
+    it.skip('Should validated that bounty information source was selected on screen 2 ', () => {
+      cy.visit('bounty/new');
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Next').click();
+
+      // Screen 3
+      cy.contains('Next').click();
+
+      // Screen 4
+      cy.contains('Next').click();
+
+      // Screen 5
+      // Clicking 'Confirm' should trigger the validation
+      cy.contains('Confirm').click();
+
+      // Back to screen 2
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+
+      cy.contains('Select the bounty information source').should('be.visible');
+    });
+
+    it('Should validated that bounty github details are loaded on screen 2 ', () => {
+      cy.visit('bounty/new');
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Import from GitHub').click();
+      cy.contains('Next').click();
+
+      // Screen 3
+      cy.contains('Next').click();
+
+      // Screen 4
+      cy.contains('Next').click();
+
+      // Screen 5
+      // Clicking 'Confirm' should trigger the validation
+      cy.contains('Confirm').click();
+
+      // Back to screen 2
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+
+      cy.contains('Please input a GitHub issue').should('be.visible');
+    });
+
+
+    it('Should validated that bounty title and description for custom bounties mandatory on screen 2 ', () => {
+      cy.visit('bounty/new');
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Create Custom Bounty').click();
+      cy.contains('Next').click();
+
+      // Screen 3
+      cy.contains('Next').click();
+
+      // Screen 4
+      cy.contains('Next').click();
+
+      // Screen 5
+      // Clicking 'Confirm' should trigger the validation
+      cy.contains('Confirm').click();
+
+      // Back to screen 2
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+      cy.contains('Back').click();
+
+      cy.contains('Please input bounty title').should('be.visible');
+      cy.contains('Please input bounty description').should('be.visible');
+    });
+  });
+
 });
 
