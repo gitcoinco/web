1893e128a2f8131a478fcb7998248800728e96c3
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-04-14 12:41:04 +0300

    Testing that validation works in new bounty creation flow

--- cypress/integration/bounties/test_bounty_creation.js
+++ cypress/integration/bounties/test_bounty_creation.js
@@ -14,14 +14,14 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.logout();
   });
 
-  it.skip('can navigate to the create bounty screen', () => {
+  it('can navigate to the create bounty screen', () => {
     cy.get('#dropdownProfile').trigger('mouseenter');
     cy.get('.gc-profile-submenu').contains('Create a Bounty').click();
 
     cy.url().should('contain', 'bounty/new');
   });
 
-  it.skip('can create a new bounty', () => {
+  it('can create a new bounty', () => {
     cy.visit('bounty/new');
 
     // unfortunately some of the events do not seem bound in time for this
@@ -39,7 +39,7 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
     cy.contains('Feature').click();
     cy.get('#bounty_tags').find('.vs__search').click();
 
-    let tags = ['Python', 'Lua', 'Web Assembly'];
+    let tags = [ 'Python', 'Lua', 'Web Assembly' ];
 
     tags.forEach(tag => {
       if (tag === 'Python') {
@@ -148,41 +148,16 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
 
 
   describe('Verify bounty field validation', () => {
-    it.skip('Should validated the mandatory fields on screen 1 ', () => {
-      cy.visit('bounty/new');
+    /*
+ * Screen 1
+ */
 
-      // unfortunately some of the events do not seem bound in time for this
-      // to run without any errors. adding in this manual wait is not advised
-      // and is used here to slow the tests down to allow the modal close click
-      // events to bind properly
-      // in addition to the wait, we are using the force options on the click,
-      // if there is an alternative means for clicking on buttons, please use those
-      // instead of jumping to force. force may click on items hidden by other elements
-      // and is not how a user would interact with the application. we should make every
-      // attempt to use the UI as a real user would.
+    it('Should validated the mandatory fields on screen 1 ', () => {
+      cy.visit('bounty/new');
       cy.wait(1000);
 
       // Screen 1
       cy.contains('Next').click();
-
-      // Screen 2
-      cy.contains('Next').click();
-
-      // Screen 3
-      cy.contains('Next').click();
-
-      // Screen 4
-      cy.contains('Next').click();
-
-      // Screen 5
-      // Clicking 'Confirm' should trigger the validation
-      cy.contains('Confirm').click();
-
-      // Back to screen 1
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-      cy.contains('Back').click();
 
       cy.contains('Select at least one category').should('be.visible');
       cy.contains('Please select the bounty type').should('be.visible');
@@ -191,93 +166,254 @@ describe('Creating a new bounty', { tags: ['bounties'] }, () => {
 
     });
 
-    it.skip('Should validated that bounty information source was selected on screen 2 ', () => {
+    /*
+ * Screen 2 - Initial
+ */
+
+    it('Should validated that bounty information source was selected on screen 2 ', () => {
       cy.visit('bounty/new');
       cy.wait(1000);
 
       // Screen 1
+      cy.contains('Feature').click();
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+
+      tags.forEach(tag => {
+        if (tag === 'Python') {
+          cy.contains(tag).click();
+        } else {
+          cy.get('#bounty_tags').find('.vs__search').type(tag + '{enter}');
+        }
+      });
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      cy.get('#experience_level').find('.vs__search').click();
+      cy.contains('Beginner').click();
+      cy.get('#project_length').find('.vs__search').click();
+      cy.contains('Hours').click();
+
       cy.contains('Next').click();
+
 
       // Screen 2
       cy.contains('Next').click();
 
-      // Screen 3
-      cy.contains('Next').click();
-
-      // Screen 4
-      cy.contains('Next').click();
-
-      // Screen 5
-      // Clicking 'Confirm' should trigger the validation
-      cy.contains('Confirm').click();
-
-      // Back to screen 2
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-
       cy.contains('Select the bounty information source').should('be.visible');
     });
 
+    /*
+     * Screen 2 - GitHub Issue
+     */
     it('Should validated that bounty github details are loaded on screen 2 ', () => {
       cy.visit('bounty/new');
       cy.wait(1000);
 
       // Screen 1
+      cy.contains('Feature').click();
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+
+      tags.forEach(tag => {
+        if (tag === 'Python') {
+          cy.contains(tag).click();
+        } else {
+          cy.get('#bounty_tags').find('.vs__search').type(tag + '{enter}');
+        }
+      });
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      cy.get('#experience_level').find('.vs__search').click();
+      cy.contains('Beginner').click();
+      cy.get('#project_length').find('.vs__search').click();
+      cy.contains('Hours').click();
+
       cy.contains('Next').click();
 
       // Screen 2
       cy.contains('Import from GitHub').click();
       cy.contains('Next').click();
 
-      // Screen 3
-      cy.contains('Next').click();
-
-      // Screen 4
-      cy.contains('Next').click();
-
-      // Screen 5
-      // Clicking 'Confirm' should trigger the validation
-      cy.contains('Confirm').click();
-
-      // Back to screen 2
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-
       cy.contains('Please input a GitHub issue').should('be.visible');
+
     });
 
-
+    /*
+     * Screen 2 - Custom Issue
+     */
     it('Should validated that bounty title and description for custom bounties mandatory on screen 2 ', () => {
       cy.visit('bounty/new');
       cy.wait(1000);
 
       // Screen 1
+      cy.contains('Feature').click();
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+
+      tags.forEach(tag => {
+        if (tag === 'Python') {
+          cy.contains(tag).click();
+        } else {
+          cy.get('#bounty_tags').find('.vs__search').type(tag + '{enter}');
+        }
+      });
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      cy.get('#experience_level').find('.vs__search').click();
+      cy.contains('Beginner').click();
+      cy.get('#project_length').find('.vs__search').click();
+      cy.contains('Hours').click();
+
       cy.contains('Next').click();
 
       // Screen 2
       cy.contains('Create Custom Bounty').click();
       cy.contains('Next').click();
 
+      cy.contains('Please input bounty title').should('be.visible');
+      cy.contains('Please input bounty description').should('be.visible');
+    });
+
+
+    /*
+     * Screen 3
+     */
+    it('Should validated screen 3 ', () => {
+      cy.visit('bounty/new');
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Feature').click();
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+
+      tags.forEach(tag => {
+        if (tag === 'Python') {
+          cy.contains(tag).click();
+        } else {
+          cy.get('#bounty_tags').find('.vs__search').type(tag + '{enter}');
+        }
+      });
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      cy.get('#experience_level').find('.vs__search').click();
+      cy.contains('Beginner').click();
+      cy.get('#project_length').find('.vs__search').click();
+      cy.contains('Hours').click();
+
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Create Custom Bounty').click();
+
+      cy.get('#new-bounty-custom-title').type('My Custom Bounty');
+      cy.get('#new-bounty-custom-editor').type('My custom bounty long long long description');
+
+      cy.get('#new-bounty-acceptace-criteria').type('Custom bounty acceptance criteria');
+      cy.get('#new-bounty-resources').type('Custom bounty resource');
+      cy.get('#new-bounty-organisation-url').type('https://github.com/gitcoinco/');
+
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-type').find('.vs__search').click().type('Discord{enter}');
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-value').clear().type('#myhandle289346');
+
+      // Add another row
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-btn-add-contact').click();
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-type').find('.vs__search').click().type('Telegram{enter}');
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-value').clear().type('telegram-user');
+
+      // Add another row
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-btn-add-contact').click();
+      cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-type').find('.vs__search').click().type('Mail{enter}');
+      cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-value').clear().type('my.demo.name@email.com');
+      cy.contains('Next').click();
+
+
       // Screen 3
+      cy.contains('Next').click();
+      cy.contains('Please select a chain').should('be.visible');
+    });
+
+    /*
+    * Screen 4
+    */
+    it('Should validated screen 4 ', () => {
+      cy.visit('bounty/new');
+      cy.wait(1000);
+
+      // Screen 1
+      cy.contains('Feature').click();
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      let tags = [ 'Python', 'Lua', 'Web Assembly' ];
+
+      tags.forEach(tag => {
+        if (tag === 'Python') {
+          cy.contains(tag).click();
+        } else {
+          cy.get('#bounty_tags').find('.vs__search').type(tag + '{enter}');
+        }
+      });
+      cy.get('#bounty_tags').find('.vs__search').click();
+
+      cy.get('#experience_level').find('.vs__search').click();
+      cy.contains('Beginner').click();
+      cy.get('#project_length').find('.vs__search').click();
+      cy.contains('Hours').click();
+
+      cy.contains('Next').click();
+
+      // Screen 2
+      cy.contains('Create Custom Bounty').click();
+
+      cy.get('#new-bounty-custom-title').type('My Custom Bounty');
+      cy.get('#new-bounty-custom-editor').type('My custom bounty long long long description');
+
+      cy.get('#new-bounty-acceptace-criteria').type('Custom bounty acceptance criteria');
+      cy.get('#new-bounty-resources').type('Custom bounty resource');
+      cy.get('#new-bounty-organisation-url').type('https://github.com/gitcoinco/');
+
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-type').find('.vs__search').click().type('Discord{enter}');
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-contact-value').clear().type('#myhandle289346');
+
+      // Add another row
+      cy.get('.new-bounty-contact-details-form-0').find('.new-bounty-btn-add-contact').click();
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-type').find('.vs__search').click().type('Telegram{enter}');
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-contact-value').clear().type('telegram-user');
+
+      // Add another row
+      cy.get('.new-bounty-contact-details-form-1').find('.new-bounty-btn-add-contact').click();
+      cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-type').find('.vs__search').click().type('Mail{enter}');
+      cy.get('.new-bounty-contact-details-form-2').find('.new-bounty-contact-value').clear().type('my.demo.name@email.com');
+      cy.contains('Next').click();
+
+      // Screen 3
+      cy.get('#usd_amount').should('be.disabled');
+      cy.get('#amount').should('be.disabled');
+      cy.get('#new_bounty_peg_to_usd').should('be.disabled');
+      cy.get('#bounty_owner').should('be.enabled');
+
+      cy.get('#payout_chain').find('.vs__search').click().type('BTC{enter}');
+
+      // cy.get('#payout_token').find('.vs__search').click().type('ETH{enter}');
+
+      cy.get('#usd_amount').should('be.enabled');
+      cy.get('#usd_amount').should('be.enabled');
+      cy.get('#new_bounty_peg_to_usd').should('be.enabled');
+
+      cy.get('#usd_amount').clear().type('123.34');
+
       cy.contains('Next').click();
 
       // Screen 4
       cy.contains('Next').click();
-
-      // Screen 5
-      // Clicking 'Confirm' should trigger the validation
-      cy.contains('Confirm').click();
-
-      // Back to screen 2
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-      cy.contains('Back').click();
-
-      cy.contains('Please input bounty title').should('be.visible');
-      cy.contains('Please input bounty description').should('be.visible');
+      cy.contains('Select the project type').should('be.visible');
+      cy.contains('Select the permission type').should('be.visible');
     });
+
   });
 
 });
