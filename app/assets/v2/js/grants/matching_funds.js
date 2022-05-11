e803f47c253a62971192971e6a879d982fa197bd
Aditya Anand M C <aditya.anandmc@gmail.com>
2022-05-11 10:34:28 +0530

    chore: show only actionable claims in claim section

--- app/assets/v2/js/grants/matching_funds.js
+++ app/assets/v2/js/grants/matching_funds.js
@@ -251,6 +251,11 @@ Vue.mixin({
         a => a.claim_tx === null &&
         a.grant_payout).length;
     },
+    filterPendingClaims(matches) {
+      return this.filterMatchingPayout(matches).filter(
+        match => !match.claim_tx || match.claim_tx == ''
+      )
+    },
     filterMatchingPayout(matches) {
       return matches.filter(match => match.grant_payout);
     }
