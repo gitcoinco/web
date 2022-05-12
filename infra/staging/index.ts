d0ea757e3abad76e133f81fa45f588df7eec7a14
Nick Kammerdiener <nicolas@kammerdiener.us>
2022-05-12 10:31:40 -0400

    adding env

--- infra/staging/index.ts
+++ infra/staging/index.ts
@@ -647,6 +647,10 @@ let environment = [
         value: "https://dpc6bywmosi9y.cloudfront.net/" // Need to swap to read from resource, but just making sure this works
     },
     {
+        name: "AWS_S3_CUSTOM_DOMAIN",
+        value: "https://dpc6bywmosi9y.cloudfront.net/" // Need to swap to read from resource, but just making sure this works
+    },
+    {
         name: "STATIC_URL",
         value: "static/"
     },
