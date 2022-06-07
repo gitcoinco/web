6aa307c9f5afe0c715a152f4d7fa9fc350afd34c
gdixon <graham@gdixon.co.uk>
2022-06-07 20:07:04 +0100

    fix: adds docker-compose to docker install script

--- bin/docker-install.bash
+++ bin/docker-install.bash
@@ -17,3 +17,7 @@ sudo apt-get install -y docker-ce
 
 # add current user to dockers usergroup
 sudo usermod -aG docker ${USER}
+
+# download and expose docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
