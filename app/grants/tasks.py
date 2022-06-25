af3aae0d74481560d2096b24e8912d9479e19879
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-06-25 13:37:58 +0000

    Hack while trying to get tasks processed quicker and we empty the default queue

--- app/grants/tasks.py
+++ app/grants/tasks.py
@@ -45,7 +45,8 @@ def update_grant_metadata(self, grant_id, retry: bool = True) -> None:
     # this will prevent tasks on grants that have been issued from an app server from being immediately
     # rewritten by the celery server.  not elegant, but it works.  perhaps in the future,
     # a delay could be introduced in the call of the task, not the task itself.
-    time.sleep(3)
+    # Gerald 25.06.2022: commenting the line below in order to speed up the processing - this is only intended as  temporary measure
+    # time.sleep(3)
 
     # setup
     print(lineno(), round(time.time(), 2))
