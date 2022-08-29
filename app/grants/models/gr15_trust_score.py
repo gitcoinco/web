55a11f65e03bfdedc9d2907aef7ff0b89dbf14a2
Gerald Iakobinyi-Pich <nutrina9@gmail.com>
2022-08-29 11:16:05 +0000

    Adding additional fields to GR15TrustScore model: apu, score, timestamps

--- app/grants/models/gr15_trust_score.py
+++ app/grants/models/gr15_trust_score.py
@@ -8,10 +8,26 @@ from economy.models import SuperModel
 class GR15TrustScore(SuperModel):
     """Stores the trust bonus calculations for GR15"""
 
-    user = models.ForeignKey(
-        User,
-        help_text=_("User"),
+    profile = models.ForeignKey(
+        "dashboard.Profile",
+        related_name="gr15_trustbonus",
         on_delete=models.CASCADE,
-        db_index=True,
+        null=False,
+        unique=True,
+        help_text=_("The owner of the trust bonus"),
     )
-    score = models.DecimalField(null=False, blank=False, max_digits=7, decimal_places=5)
+
+    apu_score = models.DecimalField(
+        "APU Score",
+        decimal_places=18,
+        max_digits=64,
+    )
+
+    trust_bonus = models.DecimalField(
+        "Trust bonus",
+        decimal_places=2,
+        max_digits=5,
+    )
+
+    last_apu_calculation = models.DateTimeField("Last APU calculation")
+    last_trust_bonus_update = models.DateTimeField("Last trust bonus update")
