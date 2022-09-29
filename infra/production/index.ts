4d7a314d407fba52173c109c675ff0981a0a8cfa
Nick Kammerdiener <nicolas@kammerdiener.us>
2022-09-29 10:51:01 -0400

    adding redirect

--- infra/production/index.ts
+++ infra/production/index.ts
@@ -362,6 +362,27 @@ const staticBucket = new aws.lb.ListenerRule("static", {
     ],
 });
 
+const blog = new aws.lb.ListenerRule("blog", {
+    listenerArn: httpsListener.listener.arn,
+    priority: 100,
+    actions: [{
+        type: "redirect",
+        redirect: {
+            host: "go.gitcoin.co",
+            port: "443",
+            protocol: "HTTPS",
+            statusCode: "HTTP_301",
+        },
+    }],
+    conditions: [
+        {
+            pathPattern: {
+                values: ["/blog/*"],
+            },
+        },
+    ],
+});
+
 // Create a DNS record for the load balancer
 const www = new aws.route53.Record("www", {
     zoneId: route53Zone,
