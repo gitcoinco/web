268f11fb4c69b2261ee579ad00ffbf838453a76c
Tim Schultz <schultztimothy@users.noreply.github.com>
2022-10-27 10:25:50 -0600

    Revert "Revert "switch infura key with alchemy (#10941)" (#10942)" (#10943)
    This reverts commit 0b020ccb2d78c883747f37d8185171f33d51e31f.

--- app/dashboard/utils.py
+++ app/dashboard/utils.py
@@ -318,7 +318,7 @@ def get_web3(network, sockets=False, chain='std'):
         
         # Infura is throwing 403 errors on polygon due to current plan. Using Polygon RPC instead
         if network == 'polygon-mainnet':
-            provider = HTTPProvider(f'https://polygon-mainnet.g.alchemy.com/v2/{settings.INFURA_V3_PROJECT_ID}')
+            provider = HTTPProvider(f'https://polygon-mainnet.g.alchemy.com/v2/{settings.ALCHEMY_KEY}')
 
         w3 = Web3(provider)
         if network == 'rinkeby':
